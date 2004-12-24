56655b2d8f58b3fd25568c5b30e9fd576f6f5785
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2004-12-24 22:53:57 +0000

    Code/Python/buildAxonRelease.sh - Fixed to build releases nicely.     (Merges a few things in before running setup.py sdist) Code/Python/Axon/setup.py - Version numbers, etc changes Website/KamaeliaStatus.html - minor updates
    Michael

--- Code/Python/buildAxonRelease.sh
+++ Code/Python/buildAxonRelease.sh
@@ -1,12 +1,13 @@
-#!/usr/bin/python
+#!/bin/sh
+
 
 cp ../../AUTHORS Axon/
 cp ../../COPYING Axon/
 
 (
-cd Axon
-cvs update -d .
-rm -rf dist
-python setup.py sdist
-cp dist/* ../Releases
+   cd Axon
+   cvs update -d .
+   rm -rf dist
+   python setup.py sdist
+   cp dist/* ../Releases
 )
