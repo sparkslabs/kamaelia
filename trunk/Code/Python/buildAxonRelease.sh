ab54610276f6d3b931005567b8d6cda23d137c17
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2005-01-05 11:26:15 +0000

    Axon 1.0.1 changes.
    Version in setup.py bumped, changelog added, MANIFEST.in updated,
    buildAxonRelease.sh script updated to handle pulling in multiple parts of
    the tree.
    
    Michael.

--- trunk/Code/Python/buildAxonRelease.sh
+++ trunk/Code/Python/buildAxonRelease.sh
@@ -4,6 +4,12 @@
 cp ../../AUTHORS Axon/
 cp ../../COPYING Axon/
 
+rm -rf Axon/Docs
+mkdir Axon/Docs
+cp -R Kamaelia/Docs/Axon/ Axon/Docs/
+cp Kamaelia/Docs/Axon.html Axon/Docs/Axon.html
+cp Kamaelia/Docs/cat.gif Axon/Docs/cat.gif
+
 (
    cd Axon
    cvs update -d .
