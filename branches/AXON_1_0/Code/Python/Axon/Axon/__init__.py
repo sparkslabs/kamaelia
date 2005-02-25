e34518f90d60b0526bc50e580543bfebf1c9a2fc
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2005-02-25 21:51:43 +0000

    Debugging subsystem re-activated for actual use. Still needs a bit of work. (Ideally we should *merge* the debugging selected in the debug.conf file with the defaults, rather than have them replace the defaults)
    Michael.

--- branches/AXON_1_0/Code/Python/Axon/Axon/__init__.py
+++ branches/AXON_1_0/Code/Python/Axon/Axon/__init__.py
@@ -22,7 +22,7 @@
 
 
 """
-import Component, Ipc, Linkage, Microprocess, Postman, Scheduler, debug, util, AdaptiveCommsComponent, AxonExceptions, CoordinatingAssistantTracker
+import Component, Ipc, Linkage, Microprocess, Postman, Scheduler, debug, util, AdaptiveCommsComponent, AxonExceptions, CoordinatingAssistantTracker, debugConfigFile
 from Axon import AxonObject, AxonType
 
 Microprocess.microprocess.setSchedulerClass(Scheduler.scheduler)
