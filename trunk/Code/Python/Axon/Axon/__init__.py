8e3016099619b99afdfb952372c7915c6817c13b
jtl21 <jtl21@2682807a-6752-0410-967a-793f22d1dfe9>
2005-04-13 10:51:40 +0000

    Undoing previous commit which was unnecessary.  My testing error led to me thinking I needed to include Splitter here but import Axon.Splitter works even without it.

--- trunk/Code/Python/Axon/Axon/__init__.py
+++ trunk/Code/Python/Axon/Axon/__init__.py
@@ -22,7 +22,7 @@
 
 
 """
-import Component, Ipc, Linkage, Microprocess, Postman, Scheduler, debug, util, AdaptiveCommsComponent, AxonExceptions, CoordinatingAssistantTracker, Splitter
+import Component, Ipc, Linkage, Microprocess, Postman, Scheduler, debug, util, AdaptiveCommsComponent, AxonExceptions, CoordinatingAssistantTracker
 from Axon import AxonObject, AxonType
 
 Microprocess.microprocess.setSchedulerClass(Scheduler.scheduler)
