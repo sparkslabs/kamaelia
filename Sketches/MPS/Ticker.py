e45bd6ba445c729cda2d3c924a63f65089e27936
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2005-06-05 19:14:12 +0000

    First step is to shift into a simple single class.
    Michael.

--- Sketches/MPS/Ticker.py
+++ Sketches/MPS/Ticker.py
@@ -40,72 +40,83 @@ def word_source():
    for x in text.split():
       yield x
 
-#
-# Bunch of initial configs.
-#
-screen_width = 800
-screen_height = 600
-text_height = 39
-line_spacing = text_height/7
-background_colour = (48,48,128)
-ticker_background_colour = (128,48,128)
-text_colour = (232, 232, 48)
-ticker_outline_colour = (128,232,128)
-ticker_outline_width = 1
-render_area = pygame.Rect((50,201,700,300))
 
-pygame.init()
+class Ticker:
 
-display = pygame.display.set_mode((screen_width, screen_height))
-my_font = pygame.font.Font(None, text_height)
+   def __init__(self, **argd):
 
-initial_postition = (render_area.left,render_area.top)
-position = [ render_area.left, render_area.top ]
+      #
+      # Bunch of initial configs.
+      #
+      self.screen_width = argd.get("screen_width",800)
+      self.screen_height = argd.get("screen_height",600)
+      self.text_height = argd.get("text_height",39)
+      self.line_spacing = argd.get("line_spacing", self.text_height/7)
+      self.background_colour = argd.get("background_colour", (48,48,128))
+      self.ticker_background_colour = argd.get("ticker_background_colour", (128,48,128))
+      self.text_colour = argd.get("text_colour", (232, 232, 48))
+      self.ticker_outline_colour = argd.get("ticker_outline_colour", (128,232,128))
+      self.ticker_outline_width = argd.get("ticker_outline_width", 1)
+      self.render_area = pygame.Rect((argd.get("render_left",50),
+                                      argd.get("render_top",200),
+                                      argd.get("render_right",700),
+                                      argd.get("render_bottom",300)))
+   def main(self):
+      pygame.init()
 
-display.fill(background_colour)
-pygame.draw.rect(display, 
-                 ticker_outline_colour, 
-                 ( render_area.left-ticker_outline_width,
-                   render_area.top-ticker_outline_width,
-                   render_area.width+ticker_outline_width,
-                   render_area.height+ticker_outline_width),
-                 ticker_outline_width)
+      display = pygame.display.set_mode((self.screen_width, self.screen_height))
+      my_font = pygame.font.Font(None, self.text_height)
 
-pygame.draw.rect(display, 
-                ticker_background_colour, 
-                (render_area.left, render_area.top, 
-                    render_area.width-1,render_area.height-1),
-                0)
+      initial_postition = (self.render_area.left,self.render_area.top)
+      position = [ self.render_area.left, self.render_area.top ]
 
-pygame.display.update()
-maxheight = 0
-import sys
-for word in word_source():
-   word = " " + word
-   wordsize = my_font.size(word)
-   word_render= my_font.render(word, 1, text_colour)
+      display.fill(self.background_colour)
+      pygame.draw.rect(display, 
+                       self.ticker_outline_colour, 
+                       ( self.render_area.left-self.ticker_outline_width,
+                         self.render_area.top-self.ticker_outline_width,
+                         self.render_area.width+self.ticker_outline_width,
+                         self.render_area.height+self.ticker_outline_width),
+                       self.ticker_outline_width)
 
-   if position[0]+wordsize[0] > render_area.right:
-      position[0] = initial_postition[0]
-      if position[1] + (maxheight + line_spacing)*2 > render_area.bottom:
-         display.blit(display, 
-                      (render_area.left, render_area.top),
-                      (render_area.left, render_area.top+text_height+line_spacing,
-                         render_area.width-1, position[1]-render_area.top ))
-         pygame.draw.rect(display, 
-                         ticker_background_colour, 
-                         (render_area.left, position[1], 
-                            render_area.width-1,render_area.top+render_area.height-1-(position[1])),
-                         0)
+      pygame.draw.rect(display, 
+                      self.ticker_background_colour, 
+                      (self.render_area.left, self.render_area.top, 
+                          self.render_area.width-1,self.render_area.height-1),
+                      0)
+
+      pygame.display.update()
+      maxheight = 0
+      import sys
+      for word in word_source():
+         word = " " + word
+         wordsize = my_font.size(word)
+         word_render= my_font.render(word, 1, self.text_colour)
+
+         if position[0]+wordsize[0] > self.render_area.right:
+            position[0] = initial_postition[0]
+            if position[1] + (maxheight + self.line_spacing)*2 > self.render_area.bottom:
+               display.blit(display, 
+                            (self.render_area.left, self.render_area.top),
+                            (self.render_area.left, self.render_area.top+self.text_height+self.line_spacing,
+                               self.render_area.width-1, position[1]-self.render_area.top ))
+               pygame.draw.rect(display, 
+                               self.ticker_background_colour, 
+                               (self.render_area.left, position[1], 
+                                  self.render_area.width-1,self.render_area.top+self.render_area.height-1-(position[1])),
+                               0)
+               pygame.display.update()
+            else:
+               position[1] += maxheight + self.line_spacing
+
+         display.blit(word_render, position)
          pygame.display.update()
-      else:
-         position[1] += maxheight + line_spacing
+         waitkey()
+         position[0] += wordsize[0]
+         if wordsize[1] > maxheight:
+            maxheight = wordsize[1]
 
-   display.blit(word_render, position)
-   pygame.display.update()
-   waitkey()
-   position[0] += wordsize[0]
-   if wordsize[1] > maxheight:
-      maxheight = wordsize[1]
+      waitkey(0)
 
-waitkey(0)
+t = Ticker()
+t.main()
