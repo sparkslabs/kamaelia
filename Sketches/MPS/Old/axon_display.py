48c9022db336dcdc21c643e762ff245d0ac1a413
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2005-06-11 11:20:31 +0000

    Sync of some ancient local changes:    * Sketches/axon_display.py - probably going to be deleted since Matt's      code is much nicer :-)    * Code/CPP/Scratch/README - A small readme explaining what the contents      of the CPP version is.
    Michael.

--- Sketches/MPS/Old/axon_display.py
+++ Sketches/MPS/Old/axon_display.py
@@ -42,7 +42,9 @@ import pygame
 from pygame.locals import *
 import random
 import math
-   
+import time
+
+donthogresource = True   
 def slices(num):
    v = 360/float(num)
    x=0
@@ -95,9 +97,30 @@ class app:
                                    return
 
                    pygame.display.flip()
+                   if donthogresource:
+                      time.sleep(0.1)
 
    def box(self,size=(10,10),location=(100,100),colour=(127,127,127)):
          pygame.draw.rect(self.screen, colour, (location,size))
+
+   def arrow(self,start, end,size=14):
+      (sx,sy),(ex,ey) = start,end
+      # The following calculations dealing with calculating the
+      # position/size of the arrow heads
+      lx = ex-sx
+      ly = ey-sy
+      lh = math.sqrt(lx**2 + ly**2)
+      factor = lh/size
+      mx,my = ex-(lx/factor),ey-(ly/factor)
+      factor2 = lh/size
+      nnlx = ly/factor2
+      nnly = lx/factor2
+      mmx1,mmy1 = mx+(nnlx/2),my-(nnly/2)
+      mmx2,mmy2 = mx-(nnlx/2),my+(nnly/2)
+
+      pygame.draw.line(self.screen, (224,10,10), start, end,2)
+      pygame.draw.polygon(self.screen, (224,10,10), [
+                          (ex,ey), (mmx1,mmy1), (mmx2,mmy2)])
 
    def init(self):
            background = self.Background()
@@ -125,12 +148,6 @@ class myapp3(app):
 
    def init(self):
       background = self.Background()
-
-   def arrow(self,start, end):
-      for d in [-3,-2,-1,0,1,2,3]:
-         x = start[0]+d
-         y = start[1]+d
-         pygame.draw.line(self.screen, (224,10,10), (x,y), end)
 
    def draw_graph(self):
       for edge in self.digraph:
@@ -175,9 +192,7 @@ class myapp3(app):
           self.draw_parts()
           yield 1
 
-if __name__ == '__main__':
-
-   def myapp3_test():
+   def smoketest():
       producer = ["producer", "inbox", "outbox", "signal","control"]
       transformer1 = ["DCT", "inbox", "outbox", "signal","control"]
       transformer2 = ["HUFF", "inbox", "outbox", "signal","control"]
@@ -196,5 +211,121 @@ if __name__ == '__main__':
       ]
       x = myapp3([producer,transformer1,transformer2,transformer3,consumer], digraph1)
       x._main()
+   smoketest = staticmethod(smoketest)
 
-   myapp3_test()
+class myapp4(app):
+   def __init__(self, component):
+      app.__init__(self)
+#      x = myapp4(component)
+#      #  [ name, boxes, subcomponents, digraph]
+
+#      wrapperinfo, parts, digraph = component
+      self.component = component
+      self.parts = component[2]
+      self.digraph = component[3]
+      self.nodes = []
+
+   def init(self):
+      background = self.Background()
+
+   def draw_graph(self):
+      for edge in self.digraph:
+         if not edge[0] == edge[1]:
+            try:
+               self.arrow(self.node_pos[edge[0]], self.node_pos[edge[1]])
+            except KeyError,k:
+               print "DEPRECATION: Key Error", k
+
+   def draw_nodes(self):
+      for node in self.node_pos:
+         stripped_name = node[node.find(".")+1:]
+         self.text(stripped_name,location=self.node_pos[node],size=20)
+
+   def draw_parts(self):
+      brightblue = (10,10,240)
+      for part in self.part_info:
+         location,radius = self.part_info[part]
+         self.text(part,location=location,size=28)
+         pygame.draw.circle(self.screen,brightblue,location, radius,2)
+
+   def makePart(self, component, radius,centre):
+      print "COMPONENT", component
+      print "\n"
+      (partname, boxes, subparts, digraph) = component
+      self.part_info[partname] = (centre,radius)
+      numpoints = len(boxes)
+      for j in xrange(numpoints):
+         box = boxes[j]
+         box_name = "%s.%s" % (partname,box)
+         self.nodes.append(box_name )
+         angle = (int(j * (360.0/numpoints)) +20) %360
+         self.node_pos[box_name] = radial_to_cartesian(angle,radius,centre)
+      if subparts is not None:
+         num_parts = len(subparts)
+         for i in xrange(num_parts):
+            part = subparts[i]
+            angle = int(i * (360.0/num_parts))
+            part_centre = radial_to_cartesian(angle,int(radius/1.7))
+            print "SUB", radius/3.7, part_centre, part
+            self.makePart(part, int(radius/3.7),part_centre)
+
+   def calculate_node_positions(self,radius,centre):
+      self.node_pos = {}
+      self.part_info = {}
+      self.makePart(self.component, radius,centre)
+
+   def main(self):
+      self.calculate_node_positions(radius=220, centre=(500,250))
+      while 1:
+          self.draw_graph()
+          self.draw_nodes()
+          self.draw_parts()
+          yield 1
+
+   def smoketest():
+      digraph1 = [
+         ("system1._outbox","producer.inbox"),  
+         ("producer.outbox","DCT.inbox"),
+         ("producer.signal","DCT.control"),
+         ("DCT.outbox","HUFF.inbox"),
+         ("DCT.signal","HUFF.control"),
+         ("HUFF.outbox","RLE.inbox"),  
+         ("HUFF.signal","RLE.control"),   
+         ("RLE.outbox","consumer.inbox"),
+         ("RLE.signal","consumer.control"),
+         ("consumer.signal","system1._control"),
+         ("consumer.outbox","system1._inbox"),
+      ]
+
+      sp = ["sp",["inbox", "outbox", "signal","control"], None,None]
+      sc = ["sc",["inbox", "outbox", "signal","control"], None,None]
+      digraph2 = [
+         ("sp.outbox","sc.inbox"),
+         ("sp.signal","sc.control"),
+         ("sp.signal","producer.control"),
+      ]
+
+      # name, boxes, subcomponents, digraph
+      producer = ["producer",["inbox", "outbox", "signal","control"], [sp,sc],digraph2]
+      transformer1 = ["DCT",["inbox", "outbox", "signal","control"], None,None]
+      transformer2 = ["HUFF",["inbox", "outbox", "signal","control"], None,None]
+      transformer3 = ["RLE",["inbox", "outbox", "signal","control"], None,None]
+      consumer = ["consumer",["inbox", "outbox", "signal","control"],None,None]
+
+
+      # The following list consists of the following structure:
+      #  [ name, boxes, subcomponents, digraph]
+      component1 =  ["system1", 
+                     ["inbox", "outbox", "_inbox", "_outbox", "_control"],
+                     [ producer, transformer1, transformer2, transformer3, consumer],
+                     digraph1,
+                    ]
+
+      x = myapp4(component1)
+      x._main()
+   smoketest = staticmethod(smoketest)
+
+if __name__ == '__main__':
+
+   #myapp3.smoketest()
+   myapp4.smoketest()
