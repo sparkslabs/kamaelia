10038975942dd534ddcf832658c98f103fc5dd03
matth_rd <matth_rd@2682807a-6752-0410-967a-793f22d1dfe9>
2005-06-21 15:34:32 +0000

    A 'chooser' component for stepping forwards and backwards through a list of items. controlled by sending messages:   "SAME", "NEXT", "PREV", "FIRST", "LAST"

---
+++ trunk/Code/Python/Kamaelia/Kamaelia/Util/Chooser.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python
+#
+# (C) 2005 British Broadcasting Corporation and Kamaelia Contributors(1)
+#     All Rights Reserved.
+#
+# You may only modify and redistribute this under the terms of any of the
+# following licenses(2): Mozilla Public License, V1.1, GNU General
+# Public License, V2.0, GNU Lesser General Public License, V2.1
+#
+# (1) Kamaelia Contributors are listed in the AUTHORS file and at
+#     http://kamaelia.sourceforge.net/AUTHORS - please extend this file,
+#     not this notice.
+# (2) Reproduced in the COPYING file, and at:
+#     http://kamaelia.sourceforge.net/COPYING
+# Under section 3.5 of the MPL, we are using this text since we deem the MPL
+# notice inappropriate for this file. As per MPL/GPL/LGPL removal of this
+# notice is prohibited.
+#
+# Please contact us via: kamaelia-list-owner@lists.sourceforge.net
+# to discuss alternative licensing.
+# -------------------------------------------------------------------------
+#
+
+import Axon
+from Axon.Ipc import producerFinished
+
+class Chooser(Axon.Component.component):
+   """Chooses items out of a set, as directed by commands sent to its inbox"""
+   
+   Inboxes = { "inbox"   : "receive commands",
+               "control" : ""
+             }
+   Outboxes = { "outbox" : "emits chosen items",
+                "signal" : ""
+              }
+   
+   def __init__(self, items = []):
+      """Initialisation.
+         items = set of items that can be iterated over
+      """
+      super(Chooser,self).__init__()
+      
+      self.items = [item for item in items]
+      self.useditems = []
+
+      
+   def main(self):
+      try:
+         self.send( self.getCurrentChoice(), "outbox")
+      except IndexError:
+         pass
+         
+      while 1:
+         yield 1
+         
+         while self.dataReady("inbox"):
+            send = True
+            msg = self.recv("inbox")
+            
+            if msg == "SAME":
+               pass
+            elif msg == "NEXT":
+               self.gotoNext()
+            elif msg == "PREV":
+               self.gotoPrev()
+            elif msg == "FIRST":
+               self.gotoFirst()
+            elif msg == "LAST":
+               self.gotoLast()
+            else:
+               send = False
+               
+            if send:
+               try:
+                  self.send( self.getCurrentChoice(), "outbox")
+               except IndexError:
+                  pass
+               
+   
+   def getCurrentChoice(self):
+      """Return the current choice to the outbox"""
+      return self.items[0]
+            
+   def gotoNext(self):
+      """Advance the choice forwards one"""
+      if len(self.items) > 1:
+         self.useditems.append(self.items[0])
+         del(self.items[0])
+
+   def gotoPrev(self):
+      """Backstep the choice backwards one"""
+      try:
+         self.items.insert(0, self.useditems[-1])
+         del(self.useditems[-1])
+      except IndexError:
+         pass
+   
+   def gotoLast(self):
+      """Goto the last item in the set"""
+      self.useditems.extend(self.items[:-1])
+      self.items = [self.items[-1]]
+            
+   def gotoFirst(self):
+      """Goto the first item in the set"""
+      self.useditems.extend(self.items)
+      self.items = self.useditems
+      self.useditems = []
+      
