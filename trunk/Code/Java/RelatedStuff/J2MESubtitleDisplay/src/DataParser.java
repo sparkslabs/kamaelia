8a89a023c3b87aed0c0defb63d8bf1f5a92de0f8
jtl21 <jtl21@2682807a-6752-0410-967a-793f22d1dfe9>
2005-06-22 14:46:49 +0000

    Added Javadoc and comments.  Also cleaned up some unused variables and functions by removing them or commenting them out.

--- trunk/Code/Java/RelatedStuff/J2MESubtitleDisplay/src/DataParser.java
+++ trunk/Code/Java/RelatedStuff/J2MESubtitleDisplay/src/DataParser.java
@@ -5,34 +5,52 @@
  * Window - Preferences - Java - Code Generation - Code and Comments
  */
 /**
- * @author josephl
- *
- * To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Generation - Code and Comments
+ * @author Joseph Lord
+ * 
+ * Receives byte arrays and turns them into Strings by naively assumming that
+ * it will only receive single byte characters in native character set.  These
+ * happen to be correct assumptions for the subtitle data stream so it works
+ * here. 
  */
 public class DataParser
 {
     //private final int bufsize = 2048;
 //    StringBuffer strbuf = new StringBuffer();
-    int bufpos = 0;
-    int scanpos = 0;
-    int nextfree =0;
+//    int bufpos = 0;
+//    int scanpos = 0;
+//    int nextfree =0;
     
-    String delimit;
+//    String delimit;
     
     TextParser sink;
+    /**
+     * 
+     * @param out Set the destination of parsed text.  Could be generalised if TextParser implemented an interface so this could be more generic.
+     */
     DataParser(TextParser out)//, String delimter)
     {
         sink = out;
  //       delimit = delimter;
     }
     
+    /**
+     * The main method that receives and passes on the data.
+     * 
+     * @param data The raw binary data.
+     * @return Always true.
+     */
     public boolean handleData(byte [] data)
     {
         return handleData(data, data.length);
     }
-    /* (non-Javadoc) 
-     * @see DataReceiver#handleData(byte[])
+    /**
+     * Receives a byte sequence and reads from start converting to a String to
+     * pass to the sink obect.  It drops anything that won't convert as a
+     * single byte character or that is a '/r' or a '/n'.
+     *  
+     * @param data The binary data to process.
+     * @param len The length of the data to process, can be less than the full byte array.
+     * @return Always true.
      */
     public boolean handleData(byte[] data,int len)
     {
--- trunk/Code/Java/RelatedStuff/J2MESubtitleDisplay/src/TextDisplayCanvas.java
+++ trunk/Code/Java/RelatedStuff/J2MESubtitleDisplay/src/TextDisplayCanvas.java
@@ -13,28 +13,45 @@ import javax.microedition.lcdui.Image;
  */
 
 /**
- * @author josephl
+ * @author Joseph Lord
  *
- * To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Generation - Code and Comments
+ * The main display class for the SubtitleMidlet.  It is a scrolling ticker for
+ * multicolour text filling most of the screen. 
  */
 public class TextDisplayCanvas extends Canvas
 {
+    
+    /**
+     * @author Joseph Lord
+     *
+     * Contains a sequence of Phrase objects that can be drawn on a
+     * single line.  It represents a single line of text and where to draw it,
+     * and what colour it should be.
+     * 
+     */
     private class Line
     {
         private Vector phrases = new Vector(3);
         private int rmargin;
         private int initialpos;
         /**
-         * @param rmargin
-         * @param initialpos
+         * @param rmargin The right hand margin position.
+         * @param initialpos The left hand margin position.
          */
         Line(int rmargin, int initialpos)
         {
             this.rmargin = rmargin;
             this.initialpos = initialpos;
         }
-        String addWord(String s, int colour, boolean addspace)
+        /**
+         * Adds an additional word to the line if there is room.
+         * 
+         * @param word A string to add without line breaks if possible 
+         * @param colour The colour of the added word.
+         * @param addspace Whether the word should be followed by a space if not at the end of a line.
+         * @return null if the word was successfully added otherwise what couldn't be added.  This will usually be the whole word except where that is too long to fit on a single line where it will fit in as much as possible and return the remainder.
+         */
+        String addWord(String word, int colour, boolean addspace)
         {
             
             if(!phrases.isEmpty())
@@ -42,13 +59,13 @@ public class TextDisplayCanvas extends Canvas
                 Phrase cur = ((Phrase)phrases.lastElement());
                 if(cur.getColour() == colour) 
                 {//New word is same colour as last
-                    return cur.addWord(s, addspace);
+                    return cur.addWord(word, addspace);
                 }
                 else
                 {
                     cur = new Phrase(cur.getEndpos(),rmargin,colour,false);
-                    String result = cur.addWord(s, addspace);
-                    if(!s.equals(result))
+                    String result = cur.addWord(word, addspace);
+                    if(!word.equals(result))
                     {
                         phrases.addElement(cur);
                     }
@@ -58,8 +75,14 @@ public class TextDisplayCanvas extends Canvas
             Phrase initial = new Phrase(initialpos,rmargin,colour,addspace);
             phrases.addElement(initial);
 //            try{Manager.playTone(127, 500, 100);}catch (MediaException e) {}
-            return initial.addWord(s, addspace);
+            return initial.addWord(word, addspace);
         }
+        /**
+         * Instructs the Line to draw itself onto the given graphics object.
+         *  
+         * @param g The Graphics object to draw onto.
+         * @param ypos The vertical position at which to draw the string.
+         */
         void draw(Graphics g, int ypos)
         {
             for(int i = 0; i < phrases.size(); ++i)
@@ -69,6 +92,12 @@ public class TextDisplayCanvas extends Canvas
         }
         
     }
+    /**
+     * 
+     * @author josephl
+     *
+     * Contains a string of text and the colour it should be displayed in.
+     */
     private class Phrase
     {
         private int offset;
@@ -78,6 +107,13 @@ public class TextDisplayCanvas extends Canvas
         int spacewidth;
         private int colour;
         private boolean linestart;
+        /**
+         * 
+         * @param pos X axis position that the phrase should be drawn at.
+         * @param margin Position of right margin in pixels.
+         * @param colour The colour of the phrase text.
+         * @param startofline If this is true it idicates that the phrase is at the start of the line therefore an addword can be split if it won't fit on a line.
+         */
         protected Phrase(int pos, int margin, int colour, boolean startofline)
         {
             offset = pos;
@@ -88,15 +124,23 @@ public class TextDisplayCanvas extends Canvas
             this.colour = colour;
             linestart = startofline;
         }
-        protected String addWord(String s, boolean addspace)
+        /**
+         * 
+         * Adds a word to the phrase if there is sufficient room before the right margin.
+         * 
+         * @param word The word to add.
+         * @param addspace If there should be whitespace after the word this should be true.
+         * @return Whatever wasn't added.  null if succesful otherwise the whole word unless it was also the start of the line when the initial string will be broken.
+         */
+        protected String addWord(String word, boolean addspace)
         {
             
-            int wordwidth = font.stringWidth(s);
+            int wordwidth = font.stringWidth(word);
             if(wordwidth + endpos <= maxX)
             {
                 linestart = false;
                 endpos += wordwidth;
-                phrase.append(s);
+                phrase.append(word);
                 if(addspace)
                 {
                    endpos += spacewidth;
@@ -106,7 +150,7 @@ public class TextDisplayCanvas extends Canvas
             }
             else if(endpos == offset && linestart) //The word is more than a line put as much as possible in.
             {
-                char[] ca = s.toCharArray();
+                char[] ca = word.toCharArray();
                 int i = 0;
                 while(font.charsWidth(ca, 0, i) < maxX)
                 {
@@ -117,9 +161,15 @@ public class TextDisplayCanvas extends Canvas
             }
             else
             {
-                return s;
+                return word;
             }
         }
+        /**
+         * Draws the Phrase onto the given graphics object. 
+         * 
+         * @param g The Graphics object to draw to.
+         * @param ypos The height in pixels to draw the Phrase at.
+         */
         protected void draw(Graphics g, int ypos)
         {
             g.setColor(colour);
@@ -127,6 +177,8 @@ public class TextDisplayCanvas extends Canvas
         }
         
         /**
+         * Gets the Phrase's colour.
+         * 
          * @return Returns the colour.
          */
         int getColour()
@@ -134,6 +186,8 @@ public class TextDisplayCanvas extends Canvas
             return colour;
         }
         /**
+         * Gets the X position of the end of the phrase.
+         * 
          * @return Returns the endpos.
          */
         int getEndpos()
@@ -141,62 +195,130 @@ public class TextDisplayCanvas extends Canvas
             return endpos;
         }
     }
+    /**
+     * The graphics context associated with the offScreenImage.
+     */
     private Graphics graphic;
+    /**
+     * Image to draw to which is then copied to screen at paint().
+     */
     private Image offScreenImage;
+    /**
+     * The height of the canvas. avoids getHeight() calls for performance.
+     */
     private int height;
+    /**
+     * The width of the canvas. - avoids getWidth() calls for performance
+     */
     private int width;
-    private int linelength;
-    static Font font = Font.getFont(Font.FACE_MONOSPACE, Font.STYLE_PLAIN, Font.SIZE_LARGE);
-    private int margin;
+    /**
+     * Pixels between left and right margins.
+     */
+    private int linelength; // 
+    /**
+     * The font to be used by all drawn text.
+     */
+    final static Font font = Font.getFont(Font.FACE_MONOSPACE, Font.STYLE_PLAIN, Font.SIZE_LARGE);  // The font that will be used.
+    /**
+     * The width for the left and right margins.
+     */
+    private int margin; 
+    /**
+     * The actual Line objects containing Phrases.
+     */
     private Line[] lines;
-    private Line currentline;
-    private int nocurrentline;
+    /**
+     * The currently active line, it will also be in the lines array.
+     */
+    private Line currentline; // 
+    /**
+     * The array index of the currentline.
+     */
+    private int currentLineNumber;
+    /**
+     * An array to contain the y-positions to draw the lines at.
+     */
     private int[] linepositions;
     //private int colour = 0;
-    /**
-     * 
-     */
+    
     public TextDisplayCanvas()
     {
         super();
+        // If you want to use the full screen set this here.  Otherwise it will
+        // leave a title and menu bar.  If you change this while running it
+        // won't change the text or the way it is displayed at all.
         //setFullScreenMode(true);
+        
+        // These are why you must do setFullScreenMode either first or not at all.
         height = getHeight();
         width = getWidth();
+        
         offScreenImage = Image.createImage(width, height);
         graphic = offScreenImage.getGraphics();
-//        font = Font.getFont(Font.FACE_MONOSPACE, Font.STYLE_PLAIN, Font.SIZE_LARGE);
         graphic.setFont(font);
+        // (width * 9) / 10 is correct length to have 90% of line used and 5% margins both sides.
         linelength = (width * 9)/10;
         margin = width / 20;
         int fontheight = font.getHeight();
-        lines = new Line[(height - 8)/fontheight];
-        nocurrentline = -1;
+        // I've hardcoded te vertical margin to be at least 8 pixels total
+        // between top and an bottom.
+        int vMarginMinimumTotal = 8;
+        
+        // See how many lines of the font we will fit in and create the array
+        // of lines.
+        lines = new Line[(height - vMarginMinimumTotal)/fontheight];
+        // Set the currentLineNumber to -1 as line number is incremented when
+        // we add a new line and at that point it will be correct.
+        currentLineNumber = -1;
         currentline = addEmptyLine();
+
+        // Set up an array of line positions so that we don't have to calculate them repeatedly.
         linepositions = new int[lines.length];
         for(int i = 0; i < linepositions.length; ++i)
         {
-            linepositions[i] = ((height - 8)%fontheight)/2 + i * fontheight;
+            linepositions[i] = ((height - vMarginMinimumTotal)%fontheight)/2 + i * fontheight;
         }
+        
+        // Set title of canvas, may confuse things if fullScreenMode is set.
         setTitle("BBC News24");
+        
+        // Draw to offscreen Image.
         draw();
-        repaint();
+        
 //        currentline.addWord(""+height+" "+fontheight, 0xFF0000, false);
     }
+    
+    /**
+     * Adds a new Line to the screen at the current bottom of the array.  If
+     * the array is full then the top Line will be discarded and all other
+     * Lines will move up one.  
+     * @return A reference to the new Line which is also the currentline
+     */
     private Line addEmptyLine()
     {
-        if(++nocurrentline >= lines.length)
+        if(currentLineNumber + 1 >= lines.length)
         {
             scroll();
         }
-        return lines[nocurrentline] = currentline =  new Line(width - margin, margin);
+        ++currentLineNumber;
+        return lines[currentLineNumber] = currentline =  new Line(width - margin, margin);
     }
+    
+    /**
+     * This is really a helper function for addEmptyLine().  It moves every
+     * line of text up one line discarding the top one and setting to null the
+     * currentline as it as scrolled up a place (probably to be replaced as
+     * current by a new line).
+     *
+     */
     private void scroll()
     {
         for(int i = 1; i < lines.length; ++i)
         {
             lines[i-1] = lines[i];
         }
-        lines[--nocurrentline] = null;
+        lines[currentLineNumber] = null;
+        currentline = null;
     }
 
     /* (non-Javadoc)
@@ -207,6 +329,14 @@ public class TextDisplayCanvas extends Canvas
         g.drawImage(offScreenImage, 0, 0, Graphics.TOP|Graphics.LEFT );
     }
     
+    /**
+     * This is the main interface to this class and how words are added to be
+     * displayed.  It also updates the display.
+     * 
+     * @param word Typically a word but any String you don't want split between lines.
+     * @param colour The colour for this word.
+     * @param addSpace If true a space or a newline will be added after the word.
+     */
     public void addWord(String word, int colour,boolean addSpace)
     {
         String overflow = currentline.addWord(word, colour, addSpace);
@@ -216,35 +346,36 @@ public class TextDisplayCanvas extends Canvas
             overflow = currentline.addWord(overflow, colour, addSpace);
         }
         draw();
-        repaint();
     }
+    /**
+     * Clears the offscreen image by filling with black, draws all the lines at
+     * the correct positions and then repaints to onscreen. 
+     */
     private void draw()
     {
         graphic.setColor(0x0);
         graphic.fillRect(0, 0, width, height);
-        for(int i = 0; i <= nocurrentline; ++i)
+        for(int i = 0; i <= currentLineNumber; ++i)
         {
             ((Line)lines[i]).draw(graphic, linepositions[i]);
         }
         repaint();
     }
     /**
-     * 
+     * Blanks all the lines. 
      */
     public void clear()
     {
-        // TODO Auto-generated method stub
-        for(int i = 0; i < nocurrentline; ++i)
+        for(int i = 0; i < currentLineNumber; ++i)
         {
             lines[i] = null;
         }
-        nocurrentline = -1;
+        currentLineNumber = -1;
         currentline = addEmptyLine();
         draw();
-        repaint();
     }
     /**
-     * 
+     * Creates a new line and sets that as current.  For paragraph breaks etc.
      */
     public void newline()
     {
