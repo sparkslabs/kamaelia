3256eb27715f15eea48ad6cd57d5811b958760c5
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2005-09-26 01:36:10 +0000

    Changed a whole bunch of obfuscated function calls into proper function calls since they just create systems and are not really using the right "ISA" viewpoint.
    Michael.

--- trunk/Sketches/MH/filereading/ReadMultiFileAdapter.py
+++ trunk/Sketches/MH/filereading/ReadMultiFileAdapter.py
@@ -30,60 +30,6 @@ from Kamaelia.Util.PipelineComponent import pipeline
 from Kamaelia.Util.Graphline import Graphline
 
 
-class JoinChooserToCarousel(Graphline):
-    """Combines a Chooser with a Carousel
-
-       This component encapsulates and connects together a Chooser and a Carousel component.
-       
-       The chooser must have an inbox that accepts 'next' style commands, and an outbox for outputting
-       the next file information.
-       
-       The carousel must have a 'next' inbox for receiving next file info, and a 'requestNext'
-       outbox for outputting 'next' style messages.
-    """
-    
-    def __init__(self, chooser, carousel):
-        """Initialisation.
-           chooser = A Chooser component, or any with similar behaviour and interfaces.
-           carousel = A Carousel component, or any with similar behaviour and interfaces.
-        """
-        linkages = { ("CHOOSER", "outbox")        : ("CAROUSEL", "next"),
-                     ("CHOOSER", "signal")        : ("CAROUSEL", "control"),
-                     ("self", "inbox")            : ("CAROUSEL", "inbox"),
-                     ("self", "control")          : ("CHOOSER", "control"),
-                     ("CAROUSEL", "requestNext") : ("CHOOSER", "inbox"),
-                     ("CAROUSEL", "outbox")      : ("self", "outbox"),
-                     ("CAROUSEL", "signal")      : ("self", "signal")
-                   }
-        
-        super(JoinChooserToCarousel, self).__init__( CHOOSER = chooser,
-                                                     CAROUSEL = carousel,
-                                                     linkages = linkages
-                                                   )
-                                             
-
-    
-class RateControlledReadFileAdapter(Graphline):
-    """ReadFileAdapter combined with a RateControl component
-    """
-    
-    def __init__(self, filename, readmode = "bytes", **rateargs):
-        """Returns a component encapsulating a RateControl and ReadFileAdapter components.
-            filename   = filename
-            readmode   = "bytes" or "lines"
-            **rateargs = named arguments to be passed to RateControl
-        """
-        super(RateControlledReadFileAdapter, self).__init__(
-                     RC  = RateControl(**rateargs),
-                     RFA = ReadFileAdapter(filename, readmode),
-                     linkages = { ("RC",  "outbox")  : ("RFA", "inbox"),
-                                  ("RFA", "outbox")  : ("---", "outbox"),
-                                  ("RFA", "signal")  : ("RC",  "control"),
-                                  ("RC",  "signal")  : ("---", "signal"),
-                                  ("---", "control") : ("RFA", "control")
-                                }
-                   )
-
 class ReadFileAdapter_Carousel(Carousel):
     """A Carousel for file reading (with no rate control).
        Takes string filenames.
@@ -95,8 +41,6 @@ class ReadFileAdapter_Carousel(Carousel):
             return ReadFileAdapter(filename=filename, readmode=readmode)
 
         super(ReadFileAdapter_Carousel, self).__init__( RFAfactory )
-
-
         
 class RateControlledReadFileAdapter_Carousel(Carousel):
     """A Carousel for file reading (with rate control specified per file).
@@ -110,31 +54,68 @@ class RateControlledReadFileAdapter_Carousel(Carousel):
 
         super(RateControlledReadFileAdapter_Carousel, self).__init__( RCRFAfactory )
 
+def JoinChooserToCarousel(chooser, carousel):
+    """Combines a Chooser with a Carousel
+           chooser = A Chooser component, or any with similar behaviour and interfaces.
+           carousel = A Carousel component, or any with similar behaviour and interfaces.
+       This component encapsulates and connects together a Chooser and a Carousel component.
+       
+       The chooser must have an inbox that accepts 'next' style commands, and an outbox for outputting
+       the next file information.
+       
+       The carousel must have a 'next' inbox for receiving next file info, and a 'requestNext'
+       outbox for outputting 'next' style messages.
+    """
 
+    return Graphline(CHOOSER = chooser,
+                     CAROUSEL = carousel,
+                     linkages = { 
+                         ("CHOOSER", "outbox")        : ("CAROUSEL", "next"),
+                         ("CHOOSER", "signal")        : ("CAROUSEL", "control"),
+                         ("self", "inbox")            : ("CAROUSEL", "inbox"),
+                         ("self", "control")          : ("CHOOSER", "control"),
+                         ("CAROUSEL", "requestNext") : ("CHOOSER", "inbox"),
+                         ("CAROUSEL", "outbox")      : ("self", "outbox"),
+                         ("CAROUSEL", "signal")      : ("self", "signal")
+                     }
+    
+    )
 
-class FixedRate_ReadFileAdapter_Carousel(Graphline):
+def RateControlledReadFileAdapter(filename, readmode = "bytes", **rateargs):
+    """ReadFileAdapter combined with a RateControl component
+       Returns a component encapsulating a RateControl and ReadFileAdapter components.
+            filename   = filename
+            readmode   = "bytes" or "lines"
+            **rateargs = named arguments to be passed to RateControl
+        """
+    return Graphline(RC  = RateControl(**rateargs),
+                     RFA = ReadFileAdapter(filename, readmode),
+                     linkages = { ("RC",  "outbox")  : ("RFA", "inbox"),
+                                  ("RFA", "outbox")  : ("self", "outbox"),
+                                  ("RFA", "signal")  : ("RC",  "control"),
+                                  ("RC",  "signal")  : ("self", "signal"),
+                                  ("self", "control") : ("RFA", "control")
+                                }
+    )
+
+def FixedRate_ReadFileAdapter_Carousel(readmode = "bytes", **rateargs):
     """A file reading carousel, that reads at a fixed rate.
-
        Takes filenames on its inbox
     """
-    def __init__(self, readmode = "bytes", **rateargs):
-        """Initialisation
-        """
-        linkages = { ("self", "inbox")      : ("CAR", "next"),
-                    ("self", "control")    : ("RC", "control"),
-                    ("RC", "outbox")       : ("CAR", "inbox"),
-                    ("RC", "signal")       : ("CAR", "control"),
-                    ("CAR", "outbox")      : ("self", "outbox"),
-                    ("CAR", "signal")      : ("self", "signal"),
-                    ("CAR", "requestNext") : ("self", "requestNext"),
-                    ("self", "next")       : ("CAR", "next")
-                }
+    return Graphline(RC       = RateControl(**rateargs),
+                     CAR      = ReadFileAdapter_Carousel(readmode),
+                     linkages = { 
+                         ("self", "inbox")      : ("CAR", "next"),
+                         ("self", "control")    : ("RC", "control"),
+                         ("RC", "outbox")       : ("CAR", "inbox"),
+                         ("RC", "signal")       : ("CAR", "control"),
+                         ("CAR", "outbox")      : ("self", "outbox"),
+                         ("CAR", "signal")      : ("self", "signal"),
+                         ("CAR", "requestNext") : ("self", "requestNext"),
+                         ("self", "next")       : ("CAR", "next")
+                     }
     
-        super(FixedRate_ReadFileAdapter_Carousel, self).__init__(
-                        RC       = RateControl(**rateargs),
-                        CAR      = ReadFileAdapter_Carousel(readmode),
-                        linkages =linkages
-                        )
+    )
 
 
 if __name__ == "__main__":
