ad71b61e53a181045475de961ca7651025e751e1
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2005-10-11 14:12:09 +0000

    1.1.2 released, 0.3.0 released, bump revisions here, allowing building of next.
    Michael.

--- Code/Python/Axon/setup.py
+++ Code/Python/Axon/setup.py
@@ -23,7 +23,7 @@
 from distutils.core import setup
 
 setup(name = "Axon",
-      version = "1.1.2",
+      version = "1.1.3",
       description = "Axon - Asynchronous Isolated Generator Component System",
       author = "Michael",
       author_email = "ms_@users.sourceforge.net",
--- Code/Python/Kamaelia/setup.py
+++ Code/Python/Kamaelia/setup.py
@@ -23,7 +23,7 @@
 from distutils.core import setup
 
 setup(name = "Kamaelia",
-      version = "0.3.0",
+      version = "0.3.1",
       description = "Kamaelia - Server Development Kit",
       author = "Michael",
       author_email = "ms_@users.sourceforge.net",
