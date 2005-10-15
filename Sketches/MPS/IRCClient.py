c97f093826dd671caf4656c801ab249fe0ce0c24
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2005-10-15 23:10:52 +0000

    In preparation for some potential fun silliness >:-)
    Michael.

--- Sketches/MPS/IRCClient.py
+++ Sketches/MPS/IRCClient.py
@@ -25,8 +25,7 @@ import Axon as _Axon
 from Kamaelia.Internet.TCPClient import TCPClient
 
 class channel(object):
-   # Note: This used to be used with a socket, it's now using a component
-   # instead, with no change.
+   # Sock here is currently a component, and default inbox
    def __init__(self, sock, channel):
       self.sock = sock
       self.channel = channel
@@ -38,6 +37,8 @@ class channel(object):
       self.sock.send("PART %s\r\n" % self.channel)
 
 class IRC_Client(_Axon.Component.component):
+   Inboxes = ["inbox", "control", "talk"]
+   Outboxes = ["outbox", "signal", "heard" ]
    def __init__(self, nick="kamaeliabot",
                       nickinfo="Kamaelia IRC",
                       defaultChannel="#kamtest"):
@@ -61,12 +62,23 @@ class IRC_Client(_Axon.Component.component):
       "Handling here is pretty naff really :-)"
       self.login(self.nick, self.nickinfo)
       self.channels[self.defaultChannel] = self.join(self.defaultChannel)
-
+      seen_VERSION = False
       while 1:
          data=""
-         if self.dataReady():
+         if self.dataReady("talk"):
             data = self.recv()
             self.channels[self.defaultChannel].say("Woo: " + str(data[:80]))
+         elif self.dataReady("inbox"):
+            data = self.recv()
+            if "PRIVMSG" in data:
+                if data[0] == ":":
+                    data = data[1:]
+                if ("VERSION" in data) and not seen_VERSION:
+                    seen_Version = True
+                else:
+                    data = data[data.find(":")+1:]
+                    self.send(data, "heard")
+
          if data.find(self.nick) != -1:
             if data.find("LEAVE") != -1:
                break
@@ -75,6 +87,12 @@ class IRC_Client(_Axon.Component.component):
       self.channels[self.defaultChannel] .leave()
 
 class SimpleIRCClient(_Axon.Component.component):
+   Inboxes = {
+       "inbox" : "Stuff that's being said on the channel",
+       "control" : "Shutdown control/info",
+       "talk" : "Something to say on the channel",
+   }
+   Outboxes = ["outbox", "signal", "heard" ] 
    def __init__(self, host="127.0.0.1", 
                       port="6667", 
                       nick="kamaeliabot",
@@ -93,11 +111,16 @@ class SimpleIRCClient(_Axon.Component.component):
       import random
       port=6667
 
-      client = TCPClient("127.0.0.1",port)
+      host = "127.0.0.1"
+      host = "irc.freenode.net"
+      
+      client = TCPClient(host,port)
       clientProtocol = self.IRC_Handler(self.nick, self.nickinfo, self.defaultChannel)
 
       self.link((client,"outbox"), (clientProtocol,"inbox"))
       self.link((clientProtocol,"outbox"), (client,"inbox"))
+      self.link((clientProtocol, "heard"), (self, "heard"), passthrough=2)
+      self.link((clientProtocol, "talk"), (self, "talk"), passthrough=1)
 
       self.addChildren(clientProtocol, client)
       yield _Axon.Ipc.newComponent(*(self.children))
@@ -107,9 +130,19 @@ class SimpleIRCClient(_Axon.Component.component):
 
 if __name__ == '__main__':
    from Axon.Scheduler import scheduler
-   t = SimpleIRCClient().activate()
-   t.activate()
-   scheduler.run.runThreads(slowmo=0)
+   from Kamaelia.Util.Console import ConsoleReader
+   from Kamaelia.UI.Pygame.Ticker import Ticker
+   from Kamaelia.Util.Graphline import Graphline
+
+   Graphline(
+       SOURCE = ConsoleReader(),
+       CONNECTION = SimpleIRCClient(),
+       DISPLAY = Ticker(render_right = 800,render_bottom = 600),
+       linkages = {
+           ("SOURCE","outbox"):("CONNECTION","talk"),
+           ("CONNECTION","heard"):("DISPLAY","inbox"),
+       }
+   ).run()
 
 """
    if data.find ( 'PING' ) != -1: # Handle network PINGs
