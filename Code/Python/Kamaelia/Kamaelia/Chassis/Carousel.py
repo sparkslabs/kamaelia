c805c7ada010a99acc529000b45f63c5d4a6cd72
matth_rd <matth_rd@2682807a-6752-0410-967a-793f22d1dfe9>
2005-10-31 15:10:55 +0000

    Reworked documentation

--- Code/Python/Kamaelia/Kamaelia/Chassis/Carousel.py
+++ Code/Python/Kamaelia/Kamaelia/Chassis/Carousel.py
@@ -1,4 +1,103 @@
 #!/usr/bin/env python
+#
+# (C) 2004 British Broadcasting Corporation and Kamaelia Contributors(1)
+#     All Rights Reserved.
+#
+# You may only modify and redistribute this under the terms of any of the
+# following licenses(2): Mozilla Public License, V1.1, GNU General
+# Public License, V2.0, GNU Lesser General Public License, V2.1
+#
+# (1) Kamaelia Contributors are listed in the AUTHORS file and at
+#     http://kamaelia.sourceforge.net/AUTHORS - please extend this file,
+#     not this notice.
+# (2) Reproduced in the COPYING file, and at:
+#     http://kamaelia.sourceforge.net/COPYING
+# Under section 3.5 of the MPL, we are using this text since we deem the MPL
+# notice inappropriate for this file. As per MPL/GPL/LGPL removal of this
+# notice is prohibited.
+#
+# Please contact us via: kamaelia-list-owner@lists.sourceforge.net
+# to discuss alternative licensing.
+# -------------------------------------------------------------------------
+"""
+Component Carousel Chassis.
+
+This chassis component is for making a carousel of components. It gets its name
+from a broadcast carousel - where a programme (or set of programmes) is
+broadcast one after another, often on a loop. Alternatively, think of public
+information screens which display a looping carousel of slides of information.
+
+If this makes no sense, suppose you want to read data from a sequence of files -
+one after another. Provide a carousel with a filereader component and a source
+of filenames, and it will make a new filereader for each file in turn,
+outputting their data one after another. The carousel automatically asks the
+filename source for a new item when its current child signals that it has
+finished.
+
+You gain reusability from things that are not directly reusable and normally
+come to a halt.
+
+
+
+EXAMPLE USAGE : Reading from a sequence of files
+
+1. Write a factory function that takes a single argument and returns a new
+component for the carousel:
+
+    def makeFileReader(filename):
+        return ReadFileAdapter(filename = filename, ...other args... )
+
+2. Make the carousel giving it the factory function:
+
+    carousel = Carousel( componentFactory = makeFileReader )
+
+3. Make a source of instructions for the carousel: (in this case, a source of
+filenames)
+
+    filenames = Chooser( ["file1","file2","file3", ... ] )
+
+4. Wire the source and carousel together:
+
+    filecarousel = JoinChooserToCarousel( chooser = filenames, carousel = carousel )
+
+5. Activate:
+
+    filecarousel.activate()
+
+
+
+HOW DOES IT WORK?
+
+The carousel chassis creates and encapsulates (as a child) the component you
+want it to, and lets it get on with it.
+
+Anything sent to the carousel's "inbox" inbox is passed onto the child
+component. Anything the child sends out appears at the carousel's "outbox" and
+"signal" outboxes.
+
+If the child sends an Axon.Ipc.shutdownMicroprocess or Axon.Ipc.producerFinished
+message then the carousel gets rid of that component and sends a "NEXT" message
+to its "requestNext" outbox.
+
+Another component, such as a Chooser, should respond to this message by sending
+the new set of arguments (for the factory function) to the carousel's "next"
+inbox. The carousel then uses your factory function to create a new child
+component. And so the cycle repeats.
+
+If the argument source needs to receive a "NEXT" message before sending its
+first set of arguments, then set the argument make1stRequest=True when creating
+the carousel.
+
+You can send new orders to the "next" inbox at any time. The carousel will
+immediately unwire that child (and create the new one) and ask the old child to
+shut down by sending an Axon.Ipc.shutdownMicroprocess message to its "control"
+inbox.
+
+The carousel will shutdown in response to an Axon.Ipc.shutdownMicroprocess or
+Axon.Ipc.producerFinished message. It will also terminate any child component
+in the same way as described above.
+
+"""
 
 import Axon
 from Axon.Component import component
@@ -6,35 +105,37 @@ from Axon.Ipc import producerFinished, shutdownMicroprocess, newComponent
 
 
 class Carousel(component):
-    """Encapsulates a changing carousel of components. The current component is wired
-       such that it's inbox and outbox are exported  as "inbox" and "outbox".
-       When the component emits producerFinished on its 'signal' output, a 'next' message is
-       sent from 'requestNext'.
+    """
+    Carousel(...) -> new Carousel chassis component
 
-       Whenever arguments are received on 'next' eg. (but not necessarily) in response to
-       a 'next' request, the components are destroyed and replaced with new ones, created
-       with the arguments specified.
-
-       After initialisation, Carousel will wait for the first  set of instructions, unless you
-       set the make1stRequest argument, in which case it will issue a 'next' request immediately
-
-       A factory method that takes a single argument should be supplied to create the components
-       for the carousel.
-       If you want to support more complex argument forms, you'll need to put a wrapper in.
+    Creates a carousel component that makes child components one at a time
+    and lets them do their stuff. Replacing them when they terminate or if
+    requested to do so.
     """
 
-    Inboxes = { "inbox" : "child's inbox",
-                "next" : "single argument for factory method new child component",
-                "control" : "",
-                "_control" : "for child to signal 'producerFinished' or 'shutdownMicroprocess' to Carousel"
+    Inboxes = { "inbox"    : "child's inbox",
+                "next"     : "requests to replace child",
+                "control"  : "",
+                "_control" : "internal use: to receive 'producerFinished' or 'shutdownMicroprocess' from child"
               }
-    Outboxes = { "outbox" : "child's outbox",
-                 "signal" : "",
-                 "_signal" : "for signalling 'shutdownMicroprocess' to child",
+    Outboxes = { "outbox"      : "child's outbox",
+                 "signal"      : "",
+                 "_signal"     : "internal use: for sending 'shutdownMicroprocess' to child",
                  "requestNext" : "for requesting new child component"
                }
 
     def __init__(self, componentFactory, make1stRequest=False):
+        """
+        __init__(componentFactory)
+        __init__(componentFactory,make1stRequest=True)
+
+        componentFactory(argument) -> new component
+            factory function for creating a new child according to specified arguments
+
+        make1stRequest = True
+            Carousel will, immediately after activation, send a "NEXT" message to its
+            "requestNext" outbox. Otherwise the carousel just waits. 
+        """
         super(Carousel, self).__init__()
         
         self.factory = componentFactory
@@ -44,6 +145,7 @@ class Carousel(component):
 
         
     def main(self):
+        """Main loop"""
         if self.make1stRequest:
             self.requestNext()
         
@@ -55,15 +157,19 @@ class Carousel(component):
             yield self.handleNewChild()
 
         self.unplugChildren()
-#        print"Carousel done"
+
             
     def requestNext(self):
-#        print "REQUESTING NEXT"
+        """Sends 'next' out the 'requestNext' outbox"""
         self.send( "NEXT", "requestNext" )
     
 
 
     def handleFinishedChild(self):
+        """
+        Unplugs the child if a shutdownMicroprocess or producerFinished message is
+        received from it. Also sends a "NEXT" request if one has not already been sent.
+        """
         if self.dataReady("_control"):
             msg = self.recv("_control")
 
@@ -76,6 +182,13 @@ class Carousel(component):
     
 
     def handleNewChild(self):
+        """
+        If data received on "next" inbox, removes any existing child and creates and wires
+        in a new one.
+
+        Received data is passed as an argument to the factory function (supplied at
+        initialisation) that creates the new child.
+        """
         if self.dataReady("next"):
             arg = self.recv("next")
 
@@ -105,6 +218,9 @@ class Carousel(component):
 
 
     def unplugChildren(self):
+        """
+        Sends 'shutdownMicroprocess' to children and unwires and disowns them.
+        """
         for child in self.childComponents():
             self.send( shutdownMicroprocess(self), "_signal" )
             self.postoffice.deregisterlinkage(thecomponent=child)
@@ -112,6 +228,9 @@ class Carousel(component):
 
 
     def shutdown(self):
+        """
+        Returns True if a shutdownMicroprocess or producerFinished message was received.
+        """
         if self.dataReady("control"):
             msg = self.recv("control")
             if isinstance(msg, shutdownMicroprocess) or isinstance(msg, producerFinished):
