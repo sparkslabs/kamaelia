a699882c46752b9c5e54e9858ba7282c7642f5cb
matth_rd <matth_rd@2682807a-6752-0410-967a-793f22d1dfe9>
2005-11-04 16:50:22 +0000

    Reworked documentation.
    Matt

--- trunk/Code/Python/Kamaelia/Kamaelia/Internet/Simulate/__init__.py
+++ trunk/Code/Python/Kamaelia/Kamaelia/Internet/Simulate/__init__.py
@@ -19,3 +19,10 @@
 # Please contact us via: kamaelia-list-owner@lists.sourceforge.net
 # to discuss alternative licensing.
 # -------------------------------------------------------------------------
+"""\
+===================
+Internet Simulation
+===================
+
+Components to simulate properties of a network connection, such as lossyness or
+re-ordering of data.
