0b3a9be7fe37fc8b5fb502a42f954c723615ea8c
matth_rd <matth_rd@2682807a-6752-0410-967a-793f22d1dfe9>
2005-11-07 16:13:09 +0000

    Reworked documentation.
    Matt

--- trunk/Code/Python/Kamaelia/Kamaelia/Internet/ConnectedSocketAdapter.py
+++ trunk/Code/Python/Kamaelia/Kamaelia/Internet/ConnectedSocketAdapter.py
@@ -19,44 +19,66 @@
 # Please contact us via: kamaelia-list-owner@lists.sourceforge.net
 # to discuss alternative licensing.
 # -------------------------------------------------------------------------
-#
-# A connected socket adapter (CSA) component acts as a wrapper
-# between a network server socket which has a connected client,
-# and the component framework. It deals with new data arriving
-# on the socket, and packetises it into messages available in its
-# outbox, and taking messages from it's inbox "DataReady", and
-# sending the data verbatim out the network socket.
-#
-# A component does not directly create a CSA, but rather it gets
-# passed to a component from a Primary Listener Socket component,
-# when a new connection is recieved on the server socket.
-# A CSA presents its user with the following external connectors:
-#    * inboxes=DataReady, DataSend, Initialise, control
-#       * DataReady - this is a private connector, which tells the
-#         component to read from the socket. It is used by the PLS
-#         that created this component to tell the CSA that there is
-#         data ready for reading from the socket.
-#       * DataSend - a user of this component would expect to connect
-#         a linkage to this inbox - since this is the inbox whereby recieved
-#         data is sent to the client as soon as possible.
-#       * Initialise - not actually used for anything, candidate for removal.
-#       * control - standard control inbox, the socket will close connection
-#         when there is nothing left in its DataSend and DataReady boxes if it is
-#         sent a Axon.Ipc.producerFinished ipc object.
-#    * outboxes=outbox, FactoryFeedback, signal
-#       * outbox - Any data recieved from the socket will be made available
-#         on this outbox.
-#       * FactoryFeedback - In order to signal to the Primary Listener that the socket
-#         has died, this outbox is made available for signalling purposes.
-#       * signal - This performs a similar function, but is intended for use by
-#         the client of the CSA - ie what ever is actually expecting data from the
-#         CSA. You should check the signal for a message of the form:
-#         {'shutdownCSA': CSA} periodically to check whether the client
-#         has disappeared or not!
-#
-#    Other than the external interface, this component has no user servicable
-#    parts.
-#
+"""\
+==========================
+Talking to network sockets
+==========================
+
+A Connected Socket Adapter (CSA) component talks to a network server socket.
+Data is sent to and received from the socket via this component's inboxes and
+outboxes. A CSA is effectively a wrapper for a socket.
+
+Most components should not need to create CSAs themselves. Instead, use
+components such as TCPClient to make an outgoing connection, or TCPServer or
+SimpleServer to be a server that responds to incoming connections.
+
+
+
+Example Usage
+-------------
+See source code for TCPClient to see how Connected Socket Adapters can be used.
+
+
+See also
+--------
+- TCPClient     -- for making a connection to a server
+- TCPServer     -- 
+- SimpleServer  -- a prefab chassis for building a server
+
+
+How does it work?
+-----------------
+A CSA is usually created either by a component such as TCPClient that wants to
+establish a connection to a server; or by a primary listener socket - a
+component acting as a server - listening for incoming connections from clients.
+
+The socket should be set up and passed to the constructor to make the CSA.
+
+Incoming data, read by the CSA, is sent out of its "outbox" outbox as strings
+containing the received binary data. Send data by sending it, as strings, to
+the "DataSend" outbox.
+
+The CSA expects to be wired to a component that will notify it when new data
+has arrived at its socket (by sending an Axon.Ipc.status message to its
+"DataReady" inbox. This is to allow the CSA to sleep rather than busy-wait or
+blocking when waiting for new data to arrive. Typically this is the Selector
+component.
+
+This component will terminate (and close its socket) if it receives a
+producerFinished message on its "control" inbox.
+
+When this component terminates, it sends a socketShutdown(socket) message out of
+its "FactoryFeedback" outbox and a shutdownCSA((selfCSA,self.socket)) message
+out of its "signal" outbox.
+
+The message sent to "FactoryFeedback" is to notify the original creator that
+the socket is now closed and that this component should be unwired.
+
+The message sent to the "signal" outbox serves to notify any other component
+involved - such as the one feeding notifications to the "DataReady" inbox (eg.
+the Selector component).
+"""
+
 
 import socket, time
 import errno
@@ -105,10 +127,28 @@ def _saferecv(sock, size=1024):
    return data
 
 class ConnectedSocketAdapter(component):
-   Inboxes=["DataReady", "DataSend", "Initialise", "control"]
-   Outboxes=["outbox", "FactoryFeedback","signal"]
+   """\
+   ConnectedSocketAdapter(socket) -> new CSA component wrapping specified socket
+
+   Component for communicating with a socket. Send to its "DataSend" inbox to
+   send data, and receive data from its "outbox" outbox.
+
+   "DataReady" inbox must be wired to something that will notify it when new
+   data has arrived at the socket.
+   """
+       
+   Inboxes  = { "DataReady"  : "Notify this CSA that there is incoming data ready on the socket",
+                "DataSend"   : "Data for this CSA to send through the socket (Axon.Ipc.status message)",
+                "Initialise" : "NOT USED",
+                "control"    : "Shutdown on producerFinished message (incoming & outgoing data is flushed first)",
+              }
+   Outboxes = { "outbox"          : "Data received from the socket",
+                "FactoryFeedback" : "Signals socketShutdown (this socket has closed)",
+                "signal"          : "Signals shutdownCSA (this CSA is shutting down)",
+              }
 
    def __init__(self, listensocket):
+      """x.__init__(...) initializes x; see x.__class__.__doc__ for signature"""
       super(ConnectedSocketAdapter, self).__init__()
       self.time = time.time()
       self.socket = listensocket
@@ -116,6 +156,7 @@ class ConnectedSocketAdapter(component):
       self.file = None
 
    def handleDataReady(self):
+      """Handle situation when has been notified that there is data waiting to be read from the socket."""
       if self.dataReady("DataReady"):
          data = self.recv("DataReady")
          if (isinstance(data, status)):
@@ -131,6 +172,7 @@ class ConnectedSocketAdapter(component):
                self.send(socketdata, "outbox")
 
    def handleDataSend(self):
+       """Check for and send data to the socket"""
        try:
           if self.dataReady("DataSend"):
              data = self.recv("DataSend")
@@ -154,6 +196,7 @@ class ConnectedSocketAdapter(component):
           traceback.print_exc()
 
    def handleControl(self):
+      """Check for producerFinished message and shutdown in response"""
       #if self is Axon.Foo:
       #      print "CSA: CONTROL", self.dataReady("control"),
       #      print "CSA: DATASEND", self.dataReady("DataSend"),
@@ -166,6 +209,7 @@ class ConnectedSocketAdapter(component):
                raise connectionServerShutdown()
 
    def mainBody(self):
+      """Main loop."""
       self.pause()
       try:
          self.handleDataReady()
