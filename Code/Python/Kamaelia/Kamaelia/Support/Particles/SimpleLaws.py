76a53aef26500829521c116742f49c0fad522894
matth_rd <matth_rd@2682807a-6752-0410-967a-793f22d1dfe9>
2005-11-10 15:00:25 +0000

    Reworked documentation.
    Matt

--- Code/Python/Kamaelia/Kamaelia/Support/Particles/SimpleLaws.py
+++ Code/Python/Kamaelia/Kamaelia/Support/Particles/SimpleLaws.py
@@ -19,11 +19,94 @@
 # Please contact us via: kamaelia-list-owner@lists.sourceforge.net
 # to discuss alternative licensing.
 # -------------------------------------------------------------------------
+"""\
+============================
+Simple Particle Physics Laws
+============================
 
-# physics code for forces between particles
-#
-# unbonded force acts between all non bonded particles
-# bonded force acts between bonded particles
+A class implementing laws for interactions between particles in discrete time
+simulations. Can be used with the physics ParticleSystem class.
+Laws are based on the inverse square law.
+
+Different laws are applied for 'bonded' and 'unbonded' particles. Unbonded
+particles repel. Repulsion and attraction forces balance for bonded particles at
+a specified "bond length".
+
+There are a range of parameters that can be set at initialisation. All have
+sensible defaults.
+
+
+
+Example usage
+-------------
+
+Laws for particles that bond at a distance of 200 units::
+
+    >>> laws = SimpleLaws(bondLength=200)
+    >>> laws.bonded("","", 200, 200**2)
+    0.0
+    >>> laws.bonded("","", 210, 210**2)
+    2.0
+    >>> laws.bonded("","", 195, 195**2)
+    1.0
+
+Laws for particles that decelerate *fast*::
+
+    >>> laws = SimpleLaws(damp=0.5)
+    >>> velocity = [10.0, 5.0]
+    >>> laws.dampening("", velocity)
+    [5.0, 2.5]
+
+Laws for particles that don't repel much but bond extra strongly::
+
+    >>> laws = SimpleLaws(repulsionStrength = 1.0, maxBondForce = 40.0)
+    >>> laws.unbonded("","", 50, 50**2)
+    -4.0
+    >>> laws.bonded("","", 50, 50**2)
+    -20.0
+
+
+
+The physics model used
+----------------------
+    
+Instances of this class provide methods for calculating the force that acts
+between a pair of particles when bonded or unbonded. It can also calculate any
+reduction in velocity due to 'friction'.
+
+Repulsion forces are calculated using the inverse square law
+(1 / distance squared).
+
+Bonding attraction and repulsion forces are calculated using Hook's law for
+springs. However a cut-off is applied so the force is never greater than when
+the bond is stretched to twice its resting length.
+
+There are also other cut-offs (described below) to help prevent a simulation
+becoming unstable, and to help speed up simulation.
+
+
+
+How does it work?
+-----------------
+
+You specify arguments to control the strengths of bonding and repulsion
+(unbonded) forces and the distances they act over.
+
+You can also specify friction and cutoffs for maximum and minimum velocities.
+These latter arguments are 'fudge factors' that you can use to help stop a
+system spiralling out of control. Note that in a discrete time simulation,
+if particle velocities/accelerations are too great the simulation can become
+unstable and particles will fly everywhere!
+
+You may also specify a 'maximum interaction radius' - the distance at which,
+for unbonded particles, a simulator need not bother calculating the forces
+acting (because they are too small to worry about). This is to allow simulators
+to run faster by reducing the number of calculations performed per cycle.
+
+For unspecified arguments, their defaults are scaled appropriately for
+the bondLength you specify, such that behaviour will appear unchanged, just
+at a different scaling.
+"""
 
 from SpatialIndexer import SpatialIndexer
 
@@ -32,12 +115,26 @@ from operator import add as _add
 from operator import mul as _mul
 
 class SimpleLaws(object):
-    """Implements a simple set of physics laws for the particle system.
-       repulsion force : force proportional to 1/distance_squared 
-       bonds : force proportional to extension (Hook's law)
-       
-       All force strengths etc. are set up to vaguely sensible values on the basis of
-       the specified bond length
+    """\
+    SimpleLaws([bondlength][,maxRepelRadius][,repulsionStrength][,maxBondForce]
+               [,damp][,dampcutoff][,maxVelocity]) -> new SimpleLaws object
+
+    Computes forces between particle at specified separation distances.
+    Different forces are applied depending on whether they are bonded or unbonded.
+    The same forces are applied irrespective of particle type.
+
+    Keyword arguments:
+    - bondLength         -- Length of stable bonds between particles (default=100)
+    - maxRepelRadius     -- Maximum distance repulsion force acts at (default=200)
+    - repulsionStrength  -- Strength of repulsion of unbonded particles at bondLength separation (default=3.2)
+    - maxBondForce       -- Max force applied by bond at 0 or 2*bondLength separation (default=20.0)
+    - damp               -- amount of friction 0.0 = none, 1.0 = no movement possible (default=0.8)
+    - dampcutoff         -- velocity set to zero if below this value (default=0.4)
+    - maxVelocity        -- maximum allowed velocity for particles (default=32)
+
+    For unspecified arguments, their defaults are scaled appropriately for
+    the bondLength you specify, such that behaviour will appear unchanged, just
+    at a different scaling.
     """
     def __init__(self, bondLength        = 100,
                        maxRepelRadius    = None,
@@ -47,22 +144,7 @@ class SimpleLaws(object):
                        dampcutoff        = None,
                        maxVelocity       = None
                  ):
-        """bondlength argument is the 'master control'
-              maxInteractRadius is the maximum distance repulsions are considered over
-              repulsionStrength = strength of repulsion (when at bondLength distance)
-              maxBondForce      = force at distance 0 or 2*bondLength (ie. bond spring constant)
-              damp              = dampening (drag) 0.0 = none, 1.0 = no movement at all!
-              dampcutoff        = minimum velocity below which friction stops movement all together
-              maxVelocity       = maximum velocity limit
-              
-           maxVelocity is a fiddle factor to help you stop the system spiralling out of control
-           
-           damp and dampcutoff are effectively wind resistance and friction
-           
-           bondLengthsInteractRadius gives the physics system a way to reduce the amount of
-           
-           computation needed because particles are greater distances are deemed to not interact
-        """
+        """x.__init__(...) initializes x; see x.__class__.__doc__ for signature"""
         self.bondLength = bondLength
 
         scale = 100.0 / self.bondLength
@@ -84,10 +166,19 @@ class SimpleLaws(object):
         self.maxRepulsionForce = self.repulsionForceAtBondLength * self.bondLength**2
         
     def particleMaxInteractRadius(self, ptype):
+        """Returns the maximum distance interactions will occur. ptype is ignored."""
         return self.maxInteractRadius
         
     def unbonded(self, ptype1, ptype2, dist, distSquared):
-        """1/distance_squared unbonded repulsion force"""
+        """\
+        unbonded(ptype1,ptype2,dist,distSquared) -> amount of force between unbonded particles
+
+        Returns the force between two unbonded particles (types are ignored)
+        Positive values are attraction, negative values are repulsion.
+
+        dist and distSquared should both be specified since you've probably
+        already calculated them. (This is an efficiency optimisation)
+        """
         if distSquared < 1.0:
             return -self.maxRepulsionForce
         else:
@@ -95,7 +186,15 @@ class SimpleLaws(object):
 
     
     def bonded(self, ptype1, ptype2, dist, distSquared):
-        """proportional to extension bond force"""
+        """\
+        bonded(ptype1,ptype2,dist,distSquared) -> amount of force between bonded particles
+
+        Returns the force between two bonded particles (types are ignored)
+        Positive values are attraction, negative values are repulsion.
+
+        dist and distSquared should both be specified since you've probably
+        already calculated them. (This is an efficiency optimisation)
+        """
         # note, its import that this retains its sign, so the direction of the force is determined
         f = self.bondForce * (dist - self.bondLength)
         if f < -self.maxBondForce:
@@ -107,7 +206,13 @@ class SimpleLaws(object):
     
     
     def dampening(self, ptype, velocity):
-        """velocity dampening and minimal velocity (friction-like) cutoff"""
+        """\
+        dampening(ptype, velocity) -> damped velocity vector
+
+        Returned the dampened (reduced) velocity vector. (ptype is ignored)
+
+        velocity is a tuple/list of the vector components comprising the velocity.
+        """
         vmag = reduce(lambda a,b:abs(a)+abs(b), velocity)
         if vmag < self.dampcutoff:
             return [0] * len(velocity) #[0 for a in velocity]
@@ -117,5 +222,4 @@ class SimpleLaws(object):
                 damp = self.maxVelocity / vmag
                 
             return map(_mul, velocity, [damp] * len(velocity) )
-#            return [ damp * v for v in velocity ]
 
