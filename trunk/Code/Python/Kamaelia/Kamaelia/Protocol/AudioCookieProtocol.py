c6030b33b16f7fc5142eca90e7075bc1303af51a
matth_rd <matth_rd@2682807a-6752-0410-967a-793f22d1dfe9>
2005-11-14 10:33:48 +0000

    Reworked documentation, and a bit of code cleanup.
    Matt

--- trunk/Code/Python/Kamaelia/Kamaelia/Protocol/AudioCookieProtocol.py
+++ trunk/Code/Python/Kamaelia/Kamaelia/Protocol/AudioCookieProtocol.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.3
+#!/usr/bin/env python
 #
 # (C) 2004 British Broadcasting Corporation and Kamaelia Contributors(1)
 #     All Rights Reserved.
@@ -20,42 +20,91 @@
 # to discuss alternative licensing.
 # -------------------------------------------------------------------------
 """
-Simple Audio based fortune cookie server
+==============================================
+Simple "Audio fortune cookie" Protocol Handler
+==============================================
 
-To listen to the sounds, on a linux box do this:
+A simple protocol handler that simply sends the contents of a randomly chosen
+audio file to the client.
 
-netcat <server ip> 1501 | aplay -
 
-API Needs:
 
-1) We don't/can't recieve notification from the ReadFileAdaptor that the
-   file has been closed.
-2) We can't actually tell the CSA to close itself down either...
+Example Usage
+-------------
+::
+    >>> SimpleServer(protocol=AudioCookieProtocol, port=1500).run()
 
-EXTERNAL CONNECTORS
-      * inboxes : inboxes=["datain","inbox"]
-      * outboxes=["outbox"]
+On Linux client:
+    > netcat <server ip> 1500 | aplay -
+    
 
-Copies everything from Datain & inbox to outbox.
 
-Actually gets it's data from the command "afortune.pl" - which dumps out the
-bytes of a random wav wav via a ReadFile Adaptor, reading from it at a constant
-bit rate.
+How does it work?
+-----------------
+
+AudioCookieProtocol creates a ReadFileAdapter and configures it to read the
+standard output result of running the afortune.pl script, at a fixed rate of
+95.2kbit/s.
+
+afortune.pl randomly selects a file and returns its contents.
+
+The ReadFileAdapter's "outbox" outbox is directly wired to pass through to the
+"outbox" outbox of AudioCookieProtocol.
+
+This component does not terminate.
+
+No EOF/termination indication is given once the end of the file is reached.
+
 """
+
+# XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+#
+# XXX VOMIT
+#
+# - Relies on afortune.pl, and finding that will depend on what the current
+#   working directory is
+# - Component doesn't terminate
+# - "signal" outbox of the ReadFileAdapter not linked to anything
+#
+# Would a simpler solution be:
+#
+# def AudioCookieProtocol():
+#     return ReadFileAdapter(command="./afortune.pl", ...)
+#
+# XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 import sys
 
-from Axon.Component import component, scheduler, linkage, newComponent
+from Axon.Component import component
+from Axon.Ipc import newComponent
 from Kamaelia.ReadFileAdaptor import ReadFileAdaptor
-from Kamaelia.SimpleServerComponent import SimpleServer
 
 class AudioCookieProtocol(component):
-   def __init__(self,filename="Ulysses", debug=0):
+   """\
+   AudioCookieProtocol([debug]) -> new AudioCookieProtocol component.
+
+   A protocol that spits out raw audio data from a randomly selected audio file.
+
+   Keyword arguments:
+   - debug  -- Debugging output control (default=0)
+   """
+   Inboxes  = { "inbox"   : "NOT USED",
+                "control" : "NOT USED",
+              }
+   Outboxes = { "outbox"  : "Raw audio data",
+                "signal"  : "producerFinished() at end of data"
+              }
+   
+   def __init__(self, debug=0):
+      """x.__init__(...) initializes x; see x.__class__.__doc__ for signature"""
       super(AudioCookieProtocol, self).__init__()
-      self.filename=filename
       self.debug = debug
 
    def initialiseComponent(self):
+      """\
+      Initialises component. Sets up a ReadFileAdapter to read in the contents
+      of an audio file at 95.2kbit/s and wires it to fire the contents out
+      """
       myDataSource = ReadFileAdaptor(command="./afortune.pl",
                               readmode="bitrate",
                               bitrate=95200, chunkrate=25)
@@ -68,10 +117,13 @@ class AudioCookieProtocol(component):
       return newComponent(  myDataSource  )
 
    def mainBody(self):
-      """All the interesting work has been done by linking the file reader's output
-      to our output"""
+      """Main body - sits and waits, as ReadFileAdapter is getting on with the work for us"""
       return 1
 
+
+   
 if __name__ == '__main__':
-   SimpleServer(protocol=AudioCookieProtocol, port=1500).activate()
-   scheduler.run.runThreads(slowmo=0)
+   from Kamaelia.Chassis.ConnectedServer import SimpleServer
+
+   SimpleServer(protocol=AudioCookieProtocol, port=1500).run()
+
