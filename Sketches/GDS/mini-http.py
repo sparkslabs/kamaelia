c6b8b74111dc5e4384d8001531a5491476c37873
garethds <garethds@2682807a-6752-0410-967a-793f22d1dfe9>
2005-11-25 16:35:38 +0000

    Much n00b mucking about and tripping up. Some productivity.

--- Sketches/GDS/mini-http.py
+++ Sketches/GDS/mini-http.py
@@ -2,6 +2,8 @@
 import os, string
 from Axon.Component import component, scheduler, linkage
 from Kamaelia.Util.PipelineComponent import pipeline
+from Axon.Ipc import producerFinished, shutdownMicroprocess
+from Kamaelia.KamaeliaIPC import socketShutdown
 
 class LocalFileServer(component):
 	"""
@@ -59,17 +61,80 @@ class gds(component):
 		super(gds, self).__init__()
 			
 	def main(self):
+		#yield 1
+		self.send("/home/gareths/.bashrc","outbox")
 		yield 1
-		self.send("~/.bashrc","outbox")
-		yield 1
-		self.send(socketShutdown(), "signal")
+		self.send( producerFinished(self), "signal" )
+		yield 2
+
+
+class HelloComponent(component):
+  def __init__(self):
+    super(HelloComponent, self).__init__()
+
+  def main(self):
+    for x in range(0,10):
+      self.send( "hello world!", "outbox")
+      yield 1
+    self.send( "bye","outbox")
+    self.send( producerFinished(self), "signal" )
+    yield 2
+
+class Split(component):
+  Inboxes = {"inbox":"", "control":""}
+  Outboxes = {"outbox":"", "signal":"", "out2":""}
+  
+  def __init__(self):
+    super(Split, self).__init__()
+  
+  
+  def main(self):
+    while 1:
+      yield 1
+      if self.dataReady("inbox"):
+        data = self.recv("inbox")
+        self.send(data, "outbox")
+        self.send(data, "out2")
+
+from Axon.Scheduler import scheduler
 
 if __name__=="__main__":
 	
 	print "First bit\n"
-	m = 
+	m = gds().main()
+	try:
+		while 1:
+			m.next()
+	except StopIteration, e:
+		pass
+	
+	print "orig"
+#	s=Split()
+#	pipeline( HelloComponent(),
+#		s,
+#		Printer()
+#		).activate()
+#
+#	p=Printer().activate()
+#	s.link( (s,"out2"), (p, "inbox") )
+#	
+#	scheduler.run.runThreads()
+	
+	print "pop"
+	
+#	g = gds()
+#	p = Printer()
+#	g.link((g, "outbox"), (p, "inbox"))
+#	p.activate()
+#	g.activate()
+#	
+#	scheduler.run.runThreads()
+	
+	print "Muh\n"
 	
 	pipeline(gds(),
 		LocalFileServer(),
 		Printer()
 		).activate()
+		
+	scheduler.run.runThreads()
