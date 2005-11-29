99e6a75022757c27e18bc897813d69710499190d
garethds <garethds@2682807a-6752-0410-967a-793f22d1dfe9>
2005-11-29 11:36:48 +0000

    Discovered the unwisdom of "-"s in python filenames ;)

--- trunk/Sketches/GDS/mini_http.py
+++ trunk/Sketches/GDS/mini_http.py
@@ -1,4 +1,5 @@
- #!/usr/bin/env/python
+ #!/usr/bin/python
+
 import os, string
 from Axon.Component import component, scheduler, linkage
 from Kamaelia.Util.PipelineComponent import pipeline
@@ -8,7 +9,7 @@ from Kamaelia.KamaeliaIPC import socketShutdown
 class LocalFileServer(component):
 	"""
 	Listens to the inbox for paths. When it hears a path, cats the text of that file to the outbox.
-	On recieving a shutdown control signal, passes it on and then shuts down.
+	On recieving a shutdown or producerFinished control signal, passes it on and then shuts down.
 	"""
 	
 	Inboxes=["inbox","control"]
@@ -21,6 +22,9 @@ class LocalFileServer(component):
 		if self.dataReady("control"):
 			msg = self.recv("control")
 			if isinstance(msg, socketShutdown):
+				self.send(msg, "signal")
+				return 0
+			if isinstance(msg, producerFinished):
 				self.send(msg, "signal")
 				return 0
 		
@@ -53,85 +57,21 @@ class Printer(component):
 				if isinstance(msg, producerFinished):
 					done = True
 					self.send(msg, "signal")
-#    print "BYEBYE"
-
 
 class gds(component):
 	def __init__(self):
 		super(gds, self).__init__()
 			
 	def main(self):
-		#yield 1
 		self.send("/home/gareths/.bashrc","outbox")
 		yield 1
 		self.send( producerFinished(self), "signal" )
 		yield 2
 
-
-class HelloComponent(component):
-  def __init__(self):
-    super(HelloComponent, self).__init__()
-
-  def main(self):
-    for x in range(0,10):
-      self.send( "hello world!", "outbox")
-      yield 1
-    self.send( "bye","outbox")
-    self.send( producerFinished(self), "signal" )
-    yield 2
-
-class Split(component):
-  Inboxes = {"inbox":"", "control":""}
-  Outboxes = {"outbox":"", "signal":"", "out2":""}
-  
-  def __init__(self):
-    super(Split, self).__init__()
-  
-  
-  def main(self):
-    while 1:
-      yield 1
-      if self.dataReady("inbox"):
-        data = self.recv("inbox")
-        self.send(data, "outbox")
-        self.send(data, "out2")
-
 from Axon.Scheduler import scheduler
 
 if __name__=="__main__":
-	
-	print "First bit\n"
-	m = gds().main()
-	try:
-		while 1:
-			m.next()
-	except StopIteration, e:
-		pass
-	
-	print "orig"
-#	s=Split()
-#	pipeline( HelloComponent(),
-#		s,
-#		Printer()
-#		).activate()
-#
-#	p=Printer().activate()
-#	s.link( (s,"out2"), (p, "inbox") )
-#	
-#	scheduler.run.runThreads()
-	
-	print "pop"
-	
-#	g = gds()
-#	p = Printer()
-#	g.link((g, "outbox"), (p, "inbox"))
-#	p.activate()
-#	g.activate()
-#	
-#	scheduler.run.runThreads()
-	
-	print "Muh\n"
-	
+
 	pipeline(gds(),
 		LocalFileServer(),
 		Printer()
