7060e3f1b1e8bda4c96cdc4b0c84ae344ac81c76
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2006-03-21 11:26:09 +0000

    Add the most basic smoke test. We make a check that the resulting object is a minimal component at least.

--- Sketches/MPS/test/test_Selector.py
+++ Sketches/MPS/test/test_Selector.py
@@ -5,5 +5,11 @@ import unittest
 import sys; sys.path.append("../")
 from Selector import Selector
 
+class SmokeTests_Selector(unittest.TestCase):
+    def test_SmokeTest(self):
+        """__init__ - Called with no arguments succeeds"""
+        S = Selector()
+        self.assert_(isinstance(S, Axon.Component.component))
+
 if __name__=="__main__":
     unittest.main()
