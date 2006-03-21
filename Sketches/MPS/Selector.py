38a97d82a278f86fc459cc0c7aedcc078d49f2ac
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2006-03-21 11:39:25 +0000

    SmokeTests_Selector.test_shutdownMessageCausesShutdown

--- Sketches/MPS/Selector.py
+++ Sketches/MPS/Selector.py
@@ -2,9 +2,14 @@
 #
 
 import Axon
+from Axon.Ipc import shutdown
 
 class Selector(Axon.AdaptiveCommsComponent.AdaptiveCommsComponent): # SmokeTests_Selector.test_SmokeTest
     def main(self):
         while 1: # SmokeTests_Selector.test_RunsForever
             self.pause()
+            if self.dataReady("control"):
+                message = self.recv("control")
+                if isinstance(message,shutdown):
+                   return
             yield 1
