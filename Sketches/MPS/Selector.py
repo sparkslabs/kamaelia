24f11d0f7896e8806997dce23a422c52fa2d116c
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2006-03-21 12:28:13 +0000

    Readables_Selector.test_SelectIsMockable, Readables_Selector.test_SendingAReadableMessageResultsInItBeingSelectedAgainst

--- Sketches/MPS/Selector.py
+++ Sketches/MPS/Selector.py
@@ -3,13 +3,24 @@
 
 import Axon
 from Axon.Ipc import shutdown
+import select
 
 class Selector(Axon.AdaptiveCommsComponent.AdaptiveCommsComponent): # SmokeTests_Selector.test_SmokeTest
+    Inboxes = {
+         "control" : "Recieving a Axon.Ipc.shutdown() message here causes shutdown",
+         "inbox" : "Not used at present",
+         "notify" : "Used to be notified about things to select"
+    }
     def main(self):
         while 1: # SmokeTests_Selector.test_RunsForever
-            self.pause()
             if self.dataReady("control"):
                 message = self.recv("control")
                 if isinstance(message,shutdown):
                    return
+            if self.dataReady("notify"):
+                message = self.recv("notify")
+                reader = message.object
+                select.select([reader], [],[],0)
+            self.pause()
             yield 1
+
