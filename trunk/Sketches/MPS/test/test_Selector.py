45c703cce85957e6ff7cc3c0a401e57775ab9ee9
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2006-03-21 12:28:13 +0000

    Readables_Selector.test_SelectIsMockable, Readables_Selector.test_SendingAReadableMessageResultsInItBeingSelectedAgainst

--- trunk/Sketches/MPS/test/test_Selector.py
+++ trunk/Sketches/MPS/test/test_Selector.py
@@ -3,10 +3,12 @@
 import unittest
 
 import sys; sys.path.append("../")
+import Selector as SELECTORMODULE
 from Selector import Selector
 
 import Axon
 from Axon.Ipc import shutdown
+from Kamaelia.KamaeliaIPC import newReader
 
 class SmokeTests_Selector(unittest.TestCase):
     def test_SmokeTest(self):
@@ -48,6 +50,37 @@ class SmokeTests_Selector(unittest.TestCase):
         if not componentExit:
             self.fail("When sent a shutdown message, the component should shutdown")
 
+class MockSelect:
+   """This is needed because we need to test that select is being used correctly"""
+   def __init__(self):
+       self.log = [] 
+   # We're using this simply as a namespace.
+   def select(self,*args):
+       self.log.append(("select", args))
+
+class Readables_Selector(unittest.TestCase):
+    def test_SelectIsMockable(self):
+        "main - The module uses select, and that is externally mockable"
+        try:
+            SELECTORMODULE.select
+        except AttributeError:
+            self.fail("Should import the select module")
+
+    def test_SendingAReadableMessageResultsInItBeingSelectedAgainst(self):
+        "main - If we send a newReader message to the notify inbox, it results in the selectable reader being selected on in the readers set"
+        MOCKSELECTORMODULE = MockSelect()
+        SELECTORMODULE.select = MOCKSELECTORMODULE
+        S = Selector()
+        S.activate()
+        S._deliver(newReader(S,"LOOKINGFORTHIS"),"notify")
+        for i in xrange(100):
+            S.next()
+        func, args = MOCKSELECTORMODULE.log[0]
+        self.assertEqual("select", func, "select was called in the main loop")
+        self.assertEqual(["LOOKINGFORTHIS"], args[0], "The selectable was added to the list of readables")
+        self.assertEqual([], args[1], "Writable set should be empty")
+        self.assertEqual([], args[2], "Exception set should be empty")
+        self.assertEqual(0, args[3], "The select should be non-blocking")
 
 if __name__=="__main__":
     unittest.main()
