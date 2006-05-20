9defbfa8b11cbae88aa94cd6112e5c04b91eeb00
matth_rd <matth_rd@2682807a-6752-0410-967a-793f22d1dfe9>
2006-05-20 16:12:56 +0000

    Added explicit type declarations and renamed 'cbytes' to 'cpacket' (makes more clear its relationship to 'packet')
    Matt

--- branches/private_MH_axon_flowcontrolinversion/Code/Python/Bindings/python-dvb3/dvb3/soft_dmx.pyx
+++ branches/private_MH_axon_flowcontrolinversion/Code/Python/Bindings/python-dvb3/dvb3/soft_dmx.pyx
@@ -47,8 +47,12 @@ cdef class SoftDemux:
 #        """
         cdef object extracted
         cdef int remaining
-        cdef unsigned char *cbytes
         cdef int amount
+        cdef unsigned char *cpacket
+        cdef object packet
+        cdef int pid
+        cdef int scrambled
+        cdef int error
         
         extracted = []
 
@@ -58,16 +62,16 @@ cdef class SoftDemux:
                 
                 # allocate python string we're going to put the packet into
                 packet = PyString_FromStringAndSize(NULL, 188)
-                cbytes = <unsigned char*>PyString_AsString(packet)
+                cpacket = <unsigned char*>PyString_AsString(packet)
         
                 # copy the 188 bytes of packet out of the buffer and into the new string
                 remaining = 188
                 while remaining > 0:
                     amount = min(self.cfrag_remaining, remaining)
-                    memcpy(cbytes, self.cfrag, amount)
+                    memcpy(cpacket, self.cfrag, amount)
                     
                     remaining = remaining - amount
-                    cbytes = cbytes + amount
+                    cpacket = cpacket + amount
                     self.cfrag = self.cfrag + amount
                     self.cfrag_remaining = self.cfrag_remaining - amount
                     self.length = self.length - amount
@@ -80,9 +84,9 @@ cdef class SoftDemux:
                             self.cfrag_remaining = len(self.frag_buffer[0])
         
                 # go back to beginning of packet and extract pid and flags
-                pid       = ((cbytes[1-188] << 8) + cbytes[2-188]) & 0x1fff
-                error     = cbytes[1-188] & 0x80
-                scrambled = cbytes[3-188] & 0xc0
+                pid       = ((cpacket[1-188] << 8) + cpacket[2-188]) & 0x1fff
+                error     = cpacket[1-188] & 0x80
+                scrambled = cpacket[3-188] & 0xc0
                 
                 extracted.append( (pid,error,scrambled,packet) )
                 
