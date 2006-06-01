371974fb772d26c5e8604a1ed780601188a15732
celestialryan <celestialryan@2682807a-6752-0410-967a-793f22d1dfe9>
2006-06-01 11:07:19 +0000

    Added sleep duration parameter.

--- trunk/Sketches/RJL/Util/Lagger.py
+++ trunk/Sketches/RJL/Util/Lagger.py
@@ -2,7 +2,11 @@ from Axon.Component import component
 from time import sleep
 
 class Lagger(component):
-	def main(self):
-		while 1:
-			yield 1
-			sleep(0.1)
+   def __init__(self, sleeptime = 0.01):
+      super(Lagger, self).__init__()
+      self.sleeptime = sleeptime
+   
+   def main(self):
+      while 1:
+         yield 1
+         sleep(self.sleeptime)
