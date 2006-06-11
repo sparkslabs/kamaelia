0b9f07a22c1a0520c96f511131255e0f91440315
celestialryan <celestialryan@2682807a-6752-0410-967a-793f22d1dfe9>
2006-06-11 21:49:42 +0000

    Bugfixes.

--- Sketches/RJL/HTTP/HTTPServer.py
+++ Sketches/RJL/HTTP/HTTPServer.py
@@ -36,203 +36,11 @@ from Kamaelia.SimpleServerComponent import SimpleServer
 from Axon.Ipc import producerFinished, errorInformation
 import string, time, website
 from Lagger import Lagger
+from HTTPParser import HTTPParser
 
 def currentTimeHTTP():
     curtime = time.gmtime()
     return time.strftime("Date: %a, %d %b %Y %H:%M:%S GMT", curtime)
-
-def removeTrailingCr(line):
-    if len(line) == 0:
-        return line
-    elif line[-1] == "\r":
-        return line[0:-1]
-    else:
-        return line
-
-def splitUri(requestobject):
-    splituri = string.split(requestobject["raw-uri"], "://")
-    if len(splituri) > 1:
-        requestobject["uri-protocol"] = splituri[0]
-        requestobject["raw-uri"] = requestobject["raw-uri"][len(splituri[0] + "://"):]
-    
-    splituri = string.split(requestobject["raw-uri"], "/")
-    if splituri[0] != "":
-        requestobject["uri-server"] = splituri[0]
-        requestobject["raw-uri"] = requestobject["raw-uri"][len(splituri[0]):]
-    else:
-        requestobject["uri-server"] = ""
-        
-    splituri = string.split(requestobject["uri-server"], ":")
-    if len(splituri) == 2:
-        requestobject["uri-port"] = splituri[1]
-        requestobject["uri-server"] = splituri[0]
-    
-    splituri = string.split(requestobject["uri-server"], "@")
-    if len(splituri) == 2:
-        requestobject["uri-username"] = splituri[0]
-        requestobject["uri-server"] = requestobject["uri-server"][len(splituri[0] + "@"):]
-        splituri = string.split(requestobject["uri-username"], ":")
-        if len(splituri) == 2:
-            requestobject["uri-username"] = splituri[0]
-            requestobject["uri-password"] = splituri[1]
-    
-class HTTPParser(component):
-    Inboxes =  { "inbox"         : "Raw HTTP requests",
-                 "control"       : "UNUSED" }
-    Outboxes = { "outbox"        : "HTTP request object",
-                 "signal"        : "UNUSED" }
-    
-    def __init__(self):
-        super(HTTPParser, self).__init__()
-        readbuffer = ""
-        self.requeststate = 1 # awaiting request line
-        self.lines = []
-        self.readbuffer = ""
-
-    def dataFetch(self):
-        if self.dataReady("inbox"):
-            self.readbuffer += self.recv("inbox")
-            return 1
-        else:
-            return 0
-            
-    def shouldShutdown(self):
-        while self.dataReady("control"):
-            temp = self.recv("control")
-            if isinstance(temp, shutdownMicroprocess) or isinstance(temp, producerFinished):
-                return True
-        
-        return False
-        
-    def nextLine(self):
-        lineendpos = string.find(self.readbuffer, "\n")
-        if lineendpos == -1:
-            return None
-        else:
-            line = removeTrailingCr(self.readbuffer[:lineendpos])
-            self.readbuffer = self.readbuffer[lineendpos + 1:] #the remainder after the \n
-            #print "Received line: " + line
-            return line
-        
-    def main(self):
-
-        while 1:
-            requestobject = { "bad": False, "headers": {}, "raw-uri": "", "version": "0.9", "method":"", "protocol":"" }
-            
-            #state 1 - awaiting initial line
-            currentline = None
-            while currentline == None:
-                if self.shouldShutdown(): return
-                while self.dataFetch():
-                    pass
-                currentline = self.nextLine()
-                if currentline == None:
-                    self.pause()
-                    yield 1
-                
-            #print "Initial line found"
-            splitline = string.split(currentline, " ")
-            
-            if len(splitline) < 2:
-                requestobject["bad"] = True
-                # bad request
-            else:
-                if len(splitline) < 3:
-                    # must be HTTP/0.9
-                    requestobject["method"] = splitline[0]
-                    requestobject["raw-uri"] = splitline[1]
-                    requestobject["protocol"] = "HTTP"
-                    requestobject["version"] = "0.9"
-                else: #deal with normal HTTP including badly formed URIs
-                    requestobject["method"] = splitline[0]
-
-                    #next line supports all working clients but also 
-                    #some broken clients that don't encode spaces properly!
-                    requestobject["raw-uri"] = string.join(splitline[1:-1], "%20") 
-                    protvers = string.split(splitline[-1], "/")
-                    if len(protvers) != 2:
-                        requestobject["bad"] = True
-                        requestobject["version"] = "0.9"
-                    else:
-                        requestobject["protocol"] = protvers[0]
-                        requestobject["version"] = protvers[1]
-                    
-                    splitUri(requestobject)
-                    
-                #foo://toor:letmein@server.bigcompany.com:80/bla?this&that=other could be handled better
-
-                if requestobject["method"] == "PUT" or requestobject["method"] == "POST":
-                    bodiedrequest = True
-                else:
-                    bodiedrequest = False
-
-                if requestobject["version"] != "HTTP/0.9":
-                    #state 2 - as this is a valid request, we now accept headers	
-                    previousheader = ""
-                    endofheaders = False
-                    while not endofheaders:
-                        if self.shouldShutdown(): return						
-                        while self.dataFetch():
-                            pass
-                            
-                        currentline = self.nextLine()
-                        while currentline != None:
-                            if currentline == "":
-                                #print "End of headers found"
-                                endofheaders = True
-                                break
-                            else:
-                                if currentline[0] == " " or currentline[0] == "\t": #continued header
-                                    requestobject["headers"][previousheader] += " " + string.lstrip(currentline)
-                                else:
-                                    splitheader = string.split(currentline, ":")
-                                    #print "Found header: " + splitheader[0]
-                                    requestobject["headers"][string.lower(splitheader[0])] = string.lstrip(currentline[len(splitheader[0]) + 1:])
-                            currentline = self.nextLine()
-                            #should parse headers header
-                        if not endofheaders:
-                            self.pause()
-                            yield 1
-                if requestobject["headers"].has_key("host"):
-                    requestobject["uri-server"] = requestobject["headers"]["host"]
-                
-                if bodiedrequest:
-                    #state 3 - the headers are complete - awaiting the message
-                    if not requestobject["headers"].has_key("content-length"):
-                        #this is not strictly required - it breaks compatible with chunked encoding
-                        #but will do for now
-                        requestobject["bad"] = 411 #length required
-                    else:
-                        if string.lower(requestobject["headers"].get("expect", "")) == "100-continue":
-                            #we're supposed to say continue, but this is a pain
-                            #and everything still works if we don't
-                            pass
-                            
-                        bodylength = int(requestobject["headers"]["content-length"])
-                         
-                        while len(self.readbuffer) < bodylength:
-                            if self.shouldShutdown(): return						
-                            while self.dataFetch():
-                                pass
-                            if len(self.readbuffer) < bodylength:
-                                self.pause()
-                                yield 1
-                        requestobject["body"] = self.readbuffer[:bodylength]
-                        self.readbuffer = self.readbuffer[bodylength:]
-
-                #state 4 - request complete, send it on
-            #print "Request sent on."
-            #print requestobject
-            
-            if requestobject["version"] == "1.1":
-                requestobject["headers"]["connection"] = requestobject["headers"].get("connection", "keep-alive")
-            else:
-                requestobject["headers"]["connection"] = requestobject["headers"].get("connection", "close")
-                    
-            self.send(requestobject, "outbox")
-            if string.lower(requestobject["headers"].get("connection", "")) == "close":
-                self.send(producerFinished(), "signal") #this functionality is semi-complete
-                return
 
 class HTTPServer(component):
     """\
@@ -280,44 +88,45 @@ class HTTPServer(component):
         self.mimehandler.activate()
         #self.httphandler.filereader.activate()
 
-    def mainBody(self):
-        while self.dataReady("inbox"):
-            temp = self.recv("inbox")
-            self.send(temp, "mime-inbox")
+    def main(self):
+        while 1:
+            yield 1
+            while self.dataReady("inbox"):
+                temp = self.recv("inbox")
+                self.send(temp, "mime-inbox")
 
-        while self.dataReady("control"):
-            temp = self.recv("control")
-            if isinstance(temp, shutdownMicroprocess) or isinstance(temp, producerFinished):
-                self.send(temp, "mime-control")
-                self.send(temp, "http-control")
-                return 0
-        
-        while self.dataReady("http-outbox"):
-            temp = self.recv("http-outbox")
-            self.send(temp, "outbox")
+            while self.dataReady("control"):
+                temp = self.recv("control")
+                if isinstance(temp, shutdownMicroprocess) or isinstance(temp, producerFinished):
+                    self.send(temp, "mime-control")
+                    self.send(temp, "http-control")
+                    return
+            
+            while self.dataReady("http-outbox"):
+                temp = self.recv("http-outbox")
+                self.send(temp, "outbox")
 
-        while self.dataReady("mime-outbox"):
-            temp = self.recv("mime-outbox")
-            self.send(temp, "http-inbox")
+            while self.dataReady("mime-outbox"):
+                temp = self.recv("mime-outbox")
+                self.send(temp, "http-inbox")
 
-        while self.dataReady("mime-signal"):
-            temp = self.recv("mime-signal")
-            if isinstance(temp, producerFinished):
-                pass
-                #we don't need to care yet - wait 'til the request handler finishes
-        
-        while self.dataReady("http-signal"):
-            temp = self.recv("http-signal")
-            if isinstance(temp, producerFinished):
-                sig = producerFinished(self)
-                self.send(sig, "mime-control")                
-                self.send(sig, "signal")
-                return 0
-                #close the connection
-                
-        self.pause()
-        return 1
-
+            while self.dataReady("mime-signal"):
+                temp = self.recv("mime-signal")
+                if isinstance(temp, producerFinished):
+                    pass
+                    #we don't need to care yet - wait 'til the request handler finishes
+            
+            while self.dataReady("http-signal"):
+                temp = self.recv("http-signal")
+                if isinstance(temp, producerFinished):
+                    sig = producerFinished(self)
+                    self.send(sig, "mime-control")                
+                    self.send(sig, "signal")
+                    return
+                    #close the connection
+                    
+            self.pause()
+            
     def closeDownComponent(self):
         for child in self.childComponents():
             self.removeChild(child)
@@ -364,62 +173,60 @@ class HTTPRequestHandler(component):
             header += "Content-length: " + str(len(resource["data"])) + "\n\n"
         return header
 
-    def mainBody(self):
-        while self.dataReady("inbox"):
-            request = self.recv("inbox")
-            print "Request for " + request["raw-uri"]
-            if request["bad"] == "411":
-                pagedata = website.getErrorPage(411, "Um - content-length plz!")
-                self.send(self.formHeaderResponse(pagedata, request["version"]) + pagedata["data"], "outbox")
-            elif request["bad"]:
-                pagedata = website.getErrorPage(400, "Your request sucked!")
-                self.send(self.formHeaderResponse(pagedata, "1.0") + pagedata["data"], "outbox")
-
-            elif request["protocol"] != "HTTP":
-                pagedata = website.getErrorPage(400, "Non-HTTP")
-                self.send(self.formHeaderResponse(pagedata, request["version"]) + pagedata["data"], "outbox")
-            else:
-                if request["version"] > "1.0" and not request["headers"].has_key("host"):
-                    pagedata = website.getErrorPage(400, "could not find host header")
+    def main(self):
+        while 1:
+            yield 1
+            while self.dataReady("inbox"):
+                request = self.recv("inbox")
+                print "Request for " + request["raw-uri"]
+                if request["bad"] == "411":
+                    pagedata = website.getErrorPage(411, "Um - content-length plz!")
                     self.send(self.formHeaderResponse(pagedata, request["version"]) + pagedata["data"], "outbox")
-                elif request["method"] == "GET" or request["method"] == "POST":
-                    pagedata = self.fetchResource(request)
-                    if isinstance(pagedata["data"], unicode):
-                         pagedata["data"] = self.convertUnicodeToByteStream(pagedata["data"])
-                         pagedata["charset"] = "utf-8"
-                         
+                elif request["bad"]:
+                    pagedata = website.getErrorPage(400, "Your request sucked!")
+                    self.send(self.formHeaderResponse(pagedata, "1.0") + pagedata["data"], "outbox")
+
+                elif request["protocol"] != "HTTP":
+                    pagedata = website.getErrorPage(400, "Non-HTTP")
                     self.send(self.formHeaderResponse(pagedata, request["version"]) + pagedata["data"], "outbox")
                 else:
-                    pagedata = website.getErrorPage(501,"The request method is not implemented")
-                    self.send(self.formHeaderResponse(pagedata, request["version"]) + pagedata["data"], "outbox")
-                    print "Sent 501 not implemented response"                    
-                
-                if request["version"] == "1.1":
-                    connection = request["headers"].get("connection", "keep-alive")
-                else:
-                    connection = request["headers"].get("connection", "close")
-                if connection.lower() == "close":
-                    self.send(producerFinished(), "signal") #this functionality is semi-complete
-                    return 0
+                    if request["version"] > "1.0" and not request["headers"].has_key("host"):
+                        pagedata = website.getErrorPage(400, "could not find host header")
+                        self.send(self.formHeaderResponse(pagedata, request["version"]) + pagedata["data"], "outbox")
+                    elif request["method"] == "GET" or request["method"] == "POST":
+                        pagedata = self.fetchResource(request)
+                        if isinstance(pagedata["data"], unicode):
+                             pagedata["data"] = self.convertUnicodeToByteStream(pagedata["data"])
+                             pagedata["charset"] = "utf-8"
+                             
+                        self.send(self.formHeaderResponse(pagedata, request["version"]) + pagedata["data"], "outbox")
+                    else:
+                        pagedata = website.getErrorPage(501,"The request method is not implemented")
+                        self.send(self.formHeaderResponse(pagedata, request["version"]) + pagedata["data"], "outbox")
+                        print "Sent 501 not implemented response"                    
+                    
+                    if request["version"] == "1.1":
+                        connection = request["headers"].get("connection", "keep-alive")
+                    else:
+                        connection = request["headers"].get("connection", "close")
+                    if connection.lower() == "close":
+                        self.send(producerFinished(), "signal") #this functionality is semi-complete
+                        return
 
-        while self.dataReady("control"):
-            temp = self.recv("control")
-            if isinstance(temp, shutdownMicroprocess) or isinstance(temp, producerFinished):
-                return 0
+            while self.dataReady("control"):
+                temp = self.recv("control")
+                if isinstance(temp, shutdownMicroprocess) or isinstance(temp, producerFinished):
+                    return
 
-        self.pause()
-        #time.sleep(0.01)
-            
-        return 1
-
+            self.pause()
 
 if __name__ == '__main__':
     from Axon.Component import scheduler
     import socket
     SimpleServer(protocol=HTTPServer, port=8082, socketOptions=(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  ).activate()
-    #pipeline(
-    #    Introspector(),
-    #    TCPClient("127.0.0.1", 1500),
-    #).activate()
+    pipeline(
+        Introspector(),
+        TCPClient("127.0.0.1", 1500),
+    ).activate()
     #Lagger().activate()
     scheduler.run.runThreads(slowmo=0)
