5a150db2a001dbfc1c3d7afb08db74f129b05de1
anagha_m <anagha_m@2682807a-6752-0410-967a-793f22d1dfe9>
2006-06-20 18:59:35 +0000

    *** empty log message ***

--- trunk/Sketches/AM/aes/aesmod.pyx
+++ trunk/Sketches/AM/aes/aesmod.pyx
@@ -5,7 +5,7 @@ cdef extern from "aesint.h":
   int aes_enc(char* infile, char* outfile, char* enckey)
 
 cdef extern from "keygen.h":
-  void get_key(char* random_key)
+  void get_key(char* random_key, int keylen)
 
 def enc_aes(Pinfile, Poutfile, Penckey):
   cdef char* infile
@@ -27,9 +27,10 @@ def dec_aes(Pinfile, Poutfile, Pdeckey):
   ret = decrypt_aes(infile, outfile, deckey)
   return ret
 
-def generate_key(Prandom_key):
+def generate_key(Prandom_key, int Pkeylen):
   cdef char*random_key
+  cdef int keylen
   random_key = Prandom_key
-  get_key(random_key)
-
+  keylen = Pkeylen
+  get_key(random_key, keylen)
 
