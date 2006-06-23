8976994e7b5a241b625d7794450981235eb3ff20
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2006-06-23 09:47:33 +0000

    Code changed over to use the repository version of detupler - ie Kamaelia.Util.Detuple.SimpleDetupler
    Michael.

--- trunk/Code/Python/Kamaelia/Examples/Multicast/BasicSystems/MulticastStreamingClient.py
+++ trunk/Code/Python/Kamaelia/Examples/Multicast/BasicSystems/MulticastStreamingClient.py
@@ -5,17 +5,7 @@
 #
 
 import Axon
-
-class detuple(Axon.Component.component):
-   def __init__(self, index):
-      super(detuple,self).__init__()
-      self.index = index
-   def main(self):
-      while 1:
-         if self.dataReady("inbox"):
-            tuple=self.recv("inbox")
-            self.send(tuple[self.index], "outbox")
-         yield 1
+from Kamaelia.Util.Detuple import SimpleDetupler
 
 def tests():
    from Axon.Scheduler import scheduler
@@ -27,7 +17,7 @@ def tests():
    class testComponent(Axon.Component.component):
       def main(self):
         receiver = Multicast_transceiver("0.0.0.0", 1600, "224.168.2.9", 0)
-        detupler = detuple(1)
+        detupler = SimpleDetupler(1)
         decoder = VorbisDecode()
         player = AOAudioPlaybackAdaptor()
 
--- trunk/Code/Python/Kamaelia/Examples/Multicast/BasicSystems/MulticastStreamingSystem.py
+++ trunk/Code/Python/Kamaelia/Examples/Multicast/BasicSystems/MulticastStreamingSystem.py
@@ -4,24 +4,13 @@
 # components.
 #
 
-from Axon.Component import component
 from Kamaelia.ReadFileAdaptor import ReadFileAdaptor
 from Kamaelia.vorbisDecodeComponent import VorbisDecode, AOAudioPlaybackAdaptor
 from Kamaelia.Internet.Multicast_transceiver import Multicast_transceiver
 from Kamaelia.Util.PipelineComponent import pipeline
+from Kamaelia.Util.Detuple import SimpleDetupler
 
 file_to_stream = "/usr/share/wesnoth/music/wesnoth-1.ogg"
-
-class detuple(component):
-   def __init__(self, index):
-      super(detuple, self).__init__()
-      self.index = index
-   def main(self):
-      while 1:
-         if self.dataReady("inbox"):
-            tuple=self.recv("inbox")
-            self.send(tuple[self.index], "outbox")
-         yield 1
 
 # Server
 pipeline(
@@ -32,7 +21,7 @@ pipeline(
 # Client
 pipeline(
     Multicast_transceiver("0.0.0.0", 1600, "224.168.2.9", 0),
-    detuple(1),
+    SimpleDetupler(1),
     VorbisDecode(),
     AOAudioPlaybackAdaptor(),
 ).run()
