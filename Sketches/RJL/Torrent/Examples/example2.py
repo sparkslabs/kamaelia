dce8edb89345b2063391b96d42529735eac69877
celestialryan <celestialryan@2682807a-6752-0410-967a-793f22d1dfe9>
2006-06-27 15:21:43 +0000

    Now uses TorrentPatron instead of TorrentClient

--- Sketches/RJL/Torrent/Examples/example2.py
+++ Sketches/RJL/Torrent/Examples/example2.py
@@ -1,20 +1,34 @@
 from Kamaelia.Util.PipelineComponent import pipeline
 from Kamaelia.Util.Console import ConsoleReader, ConsoleEchoer
+from Axon.Scheduler import scheduler
 
-import sys ; sys.path.append("/home/ryan/kamaelia/Sketches/RJL/")
+import sys ; sys.path.append("../")
 
 from TriggeredFileReader import TriggeredFileReader
 from DataSource import DataSource
 from HTTPClient import HTTPClient
-from btkam import TorrentClient, BasicTorrentExplainer
+from btkam import BasicTorrentExplainer
+from btkamservice import TorrentPatron
+
 if __name__ == '__main__':
     
     # download a linux distro
-    pipeline(
+    X = pipeline(
+        DataSource( ["http://www.legaltorrents.com/bit/trusted-computing.torrent"] ),
+        HTTPClient(),
+        TorrentPatron(),
+        BasicTorrentExplainer(),
+        ConsoleEchoer(),    
+    )
+
+    Y = pipeline(
         DataSource( ["http://www.legaltorrents.com/bit/trusted-computing.torrent",
                      "http://www.legaltorrents.com/bit/freeculture.zip.torrent"] ),
         HTTPClient(),
-        TorrentClient(),
+        TorrentPatron(),
         BasicTorrentExplainer(),
         ConsoleEchoer(),    
-    ).run()   
+    )
+    X.activate()
+    Y.activate()
+    scheduler.run.runThreads(slowmo=0)
