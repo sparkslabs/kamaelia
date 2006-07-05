f933502e29bc849adff87339c4ab95af07b8daf7
celestialryan <celestialryan@2682807a-6752-0410-967a-793f22d1dfe9>
2006-07-05 21:40:09 +0000

    Commented out a few components to help track down a 100% CPU bug.

--- Sketches/RJL/Torrent/Examples/example2.py
+++ Sketches/RJL/Torrent/Examples/example2.py
@@ -7,33 +7,44 @@ from Axon.Scheduler import scheduler
 
 import sys
 sys.path.append("../../")
+sys.path.append("../../Util")
+sys.path.append("../../HTTP")
+sys.path.append("../../Torrent")
 
-from Util.DataSource import DataSource
+from DataSource import DataSource
+from OnDemandIntrospector import OnDemandIntrospector
 
-from HTTP.HTTPClient import SimpleHTTPClient
-
-from Torrent.TorrentClient import BasicTorrentExplainer
-from Torrent.TorrentPatron import TorrentPatron
-
+from HTTPClient import SimpleHTTPClient
+from Kamaelia.Util.Introspector import Introspector
+from TorrentClient import BasicTorrentExplainer
+from TorrentPatron import TorrentPatron
+from Kamaelia.Internet.TCPClient import TCPClient
 if __name__ == '__main__':
     
     # download a linux distro
     X = pipeline(
         DataSource( ["http://www.legaltorrents.com/bit/trusted-computing.torrent"] ),
         SimpleHTTPClient(),
-        TorrentPatron(),
-        BasicTorrentExplainer(),
-        ConsoleEchoer(),    
+        #TorrentPatron(),
+        #BasicTorrentExplainer(),
+        #ConsoleEchoer(),    
     )
 
     Y = pipeline(
         DataSource( ["http://www.legaltorrents.com/bit/trusted-computing.torrent",
                      "http://www.legaltorrents.com/bit/freeculture.zip.torrent"] ),
         SimpleHTTPClient(),
-        TorrentPatron(),
-        BasicTorrentExplainer(),
-        ConsoleEchoer(),    
+        #TorrentPatron(),
+        #BasicTorrentExplainer(),
+        #ConsoleEchoer(),    
+    )
+    Z = pipeline(
+        ConsoleReader(),
+        OnDemandIntrospector(), # Introspector(),
+        ConsoleEchoer()
+        #TCPClient("127.0.0.1", 1500),
     )
     X.activate()
     Y.activate()
+    Z.activate()
     scheduler.run.runThreads(slowmo=0)
