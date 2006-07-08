dac7e65747e023e0c7e68c75c942030d0349da33
anagha_m <anagha_m@2682807a-6752-0410-967a-793f22d1dfe9>
2006-07-08 23:16:35 +0000

    added interface to work with 8 bytes

--- Sketches/AM/xxtea/python/xxtea.pyx
+++ Sketches/AM/xxtea/python/xxtea.pyx
@@ -4,6 +4,8 @@
 cdef extern from "xxtea.h":
   int xxtea_decrypt(char* in_file, char* out_file, char* key)
   int xxtea_encrypt(char* in_file, char* out_file, char* key)
+  long btea_8bytes( unsigned char* v, int n , unsigned char* k )
+  
   cdef enum defines:
      XXTEA_SUCCESS = 0
      IN_FILE_ERROR = 1
@@ -12,7 +14,9 @@ cdef extern from "xxtea.h":
      OUT_FILE_ERROR = 4
      READ_ERROR = 5
      FILE_SIZE_ERROR = 6
-
+cdef extern from "Python.h":
+   object PyString_FromStringAndSize(char*,int)
+   
 cdef extern from "keygen.h":
   void get_key(char* random_key, unsigned key_len)
 
@@ -26,3 +30,10 @@ def enc_xxtea(char* infile, char* outfile, char* key):
 def dec_xxtea(char* infile, char* outfile, char* key):
    ret = xxtea_decrypt(infile, outfile, key)
    return ret
+# This might be the only function useful to kamaelia.
+# 8 bytes of a passed parameter are processed. The rest are just ignored.
+
+def xxbtea(char* data, int no , char* key):
+    btea_8bytes(<unsigned char*>data, no,<unsigned char*>key)
+    return PyString_FromStringAndSize(data,8)
+
