aa5cb85804ffd2b83ad7d0330017bfb708696769
celestialryan <celestialryan@2682807a-6752-0410-967a-793f22d1dfe9>
2006-07-17 08:52:50 +0000

    Useful for very simple components.

---
+++ trunk/Sketches/RJL/Util/PureTransformer.py
@@ -0,0 +1,24 @@
+from Axon.Component import component
+
+class PureTransformer(component):
+    def __init__(self, function=None):
+        super(PureTransformerComponent, self).__init__()
+        if function:
+            self.processMessage = function
+        
+    def processMessage(self, msg):
+        pass
+        
+    def main(self):
+        while 1:
+            yield 1
+            while self.dataReady("inbox"):
+                returnval = self.processMessage(self.recv("inbox"))
+                if returnval != None:
+                    self.send(returnval, "outbox")
+            while self.dataReady("control"):
+                msg = self.recv("control")
+                if isinstance(msg, producerFinished) or isinstnace(msg, shutdown):
+                    self.send(producerFinished(self), "signal")
+                    return
+            self.pause()
