f62b6d2de36d171358d7d988370d7ec7ba615345
celestialryan <celestialryan@2682807a-6752-0410-967a-793f22d1dfe9>
2006-07-18 19:07:12 +0000

    Documentation.

--- trunk/Sketches/RJL/Torrent/TorrentTkGUI.py
+++ trunk/Sketches/RJL/Torrent/TorrentTkGUI.py
@@ -1,15 +1,34 @@
+
+"""\
+=================
+TorrentWindow - a basic GUI for BitTorrent
+=================
+
+This component supports downloading from multiple torrents simultaneously
+but no deletion or statistics other than percentage completuion so far.
+
+How does it work?
+-----------------
+TorrentWindow uses Tkinter to produce a very simple GUI.
+It then produces messages for and accepts messages produced by a
+TorrentPatron component (also would work with TorrentClient but 
+TorrentPatron is preferred, see their respective files).
+"""
+
 from Kamaelia.UI.Tk.TkWindow import TkWindow
 from Axon.Ipc import producerFinished, shutdown
 import Tkinter, time
 from TorrentPatron import TorrentPatron
 from TorrentIPC import *
+
+
 class TorrentWindow(TkWindow):
     Inboxes = { 
-        "inbox" : "From TorrentPatron backend",
+        "inbox"   : "From TorrentPatron backend",
         "control" : "Tell me to shutdown",
     }
     Outboxes = {
-        "outbox" : "To TorrentPatron backend",
+        "outbox"  : "To TorrentPatron backend",
         "fetcher" : "To TorrentPatron backend via a resource fetcher, e.g. file reader or HTTP client",
         "fetchersignal" : "Shutdown resource fetcher",
         "signal" : "When I've shutdown"
@@ -21,6 +40,7 @@ class TorrentWindow(TkWindow):
         super(TorrentWindow, self).__init__()
         
     def setupWindow(self):
+        "Create the GUI controls and window for this application"
         self.entry = Tkinter.Entry(self.window)
         self.addtorrentbutton = Tkinter.Button(self.window, text="Add Torrent", command=self.addTorrent)
         self.window.title("Kamaelia BitTorrent Client")
@@ -32,15 +52,15 @@ class TorrentWindow(TkWindow):
         self.window.columnconfigure(1, weight=1)
 
     def addTorrent(self):
+        "Request the addition of a new torrent"
         torrenturl = self.entry.get()
         self.pendingtorrents.append(torrenturl.rsplit("/", 1)[-1])
         self.send(torrenturl, "fetcher") #forward on the torrent URL/path
         self.entry.delete(0, Tkinter.END)
 
-
     def main(self):
         while not self.isDestroyed():
-            time.sleep(0.05)
+            time.sleep(0.05) # reduces CPU usage but a timer component would be better
             yield 1
             if self.dataReady("control"):
                 msg = self.recv("control")
@@ -51,7 +71,7 @@ class TorrentWindow(TkWindow):
                 msg = self.recv("inbox")
                 if isinstance(msg, TIPCNewTorrentCreated):
                     torrentname = self.pendingtorrents.pop(0)
-                    labeltext = Tkinter.StringVar()
+                    labeltext = Tkinter.StringVar() # allow us to change the label's text on the fly
                     newlabel = Tkinter.Label(self.window, textvariable=labeltext)                    
                     self.torrents[msg.torrentid] = (torrentname, newlabel, labeltext)
                     labeltext.set(torrentname + " - 0%")
@@ -90,5 +110,4 @@ if __name__ == "__main__":
             ("backend", "outbox"): ("gui", "inbox")
         }
     ).run()
-            
 
