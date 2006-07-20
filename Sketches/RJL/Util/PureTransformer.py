9e3082c6a61f58cae62c51e27a16e53cda39e277
celestialryan <celestialryan@2682807a-6752-0410-967a-793f22d1dfe9>
2006-07-20 16:37:30 +0000

    Documentation.

--- Sketches/RJL/Util/PureTransformer.py
+++ Sketches/RJL/Util/PureTransformer.py
@@ -1,8 +1,26 @@
 from Axon.Component import component
 
+"""\
+=================
+Pure Transformer component
+=================
+
+This component applies a function specified at its creation to messages received (a filter).
+
+Example Usage
+-------------
+
+To read in lines of text, convert to upper case and then write to the console.
+pipeline(
+    ConsoleReader(),
+    PureTransformer(lambda x : x.upper()),
+    ConsoleEchoer()
+).run()
+"""
+
 class PureTransformer(component):
     def __init__(self, function=None):
-        super(PureTransformerComponent, self).__init__()
+        super(PureTransformer, self).__init__()
         if function:
             self.processMessage = function
         
