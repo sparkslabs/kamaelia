68e294b059d6f7594a232f02dd102336b3ddd7c4
celestialryan <celestialryan@2682807a-6752-0410-967a-793f22d1dfe9>
2006-07-21 15:12:12 +0000

    More packaging.

---
+++ Sketches/RJL/Kamaelia/Community/RJL/Examples/P2PStreamPeer/p2pstreampeer.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+#
+# (C) 2004 British Broadcasting Corporation and Kamaelia Contributors(1)
+#     All Rights Reserved.
+#
+# You may only modify and redistribute this under the terms of any of the
+# following licenses(2): Mozilla Public License, V1.1, GNU General
+# Public License, V2.0, GNU Lesser General Public License, V2.1
+#
+# (1) Kamaelia Contributors are listed in the AUTHORS file and at
+#     http://kamaelia.sourceforge.net/AUTHORS - please extend this file,
+#     not this notice.
+# (2) Reproduced in the COPYING file, and at:
+#     http://kamaelia.sourceforge.net/COPYING
+# Under section 3.5 of the MPL, we are using this text since we deem the MPL
+# notice inappropriate for this file. As per MPL/GPL/LGPL removal of this
+# notice is prohibited.
+#
+# Please contact us via: kamaelia-list-owner@lists.sourceforge.net
+# to discuss alternative licensing.
+# -------------------------------------------------------------------------
+"""\
+"""
+import time
+
+from Axon.ThreadedComponent import threadedcomponent
+from Axon.Component import component
+
+from Kamaelia.Util.PipelineComponent import pipeline
+from Kamaelia.Util.Graphline import Graphline
+from Kamaelia.Util.Console import ConsoleReader, ConsoleEchoer
+from Kamaelia.Util.Fanout import fanout
+
+from Kamaelia.Community.RJL.Kamaelia.Protocol.HTTP.IcecastClient import IcecastClient, IcecastDemux, IcecastStreamRemoveMetadata
+from Kamaelia.Community.RJL.Kamaelia.Protocol.HTTP.HTTPHelpers import HTTPMakePostRequest
+from Kamaelia.Community.RJL.Kamaelia.Protocol.HTTP.HTTPClient import SimpleHTTPClient
+
+from Kamaelia.Community.RJL.Kamaelia.File.WholeFileWriter import WholeFileWriter
+from Kamaelia.Community.RJL.Kamaelia.File.TriggeredFileReader import TriggeredFileReader
+
+from Kamaelia.Community.RJL.Kamaelia.Protocol.Torrent.TorrentMaker import TorrentMaker
+from Kamaelia.Community.RJL.Kamaelia.Protocol.Torrent.TorrentPatron import TorrentPatron
+from Kamaelia.Community.RJL.Kamaelia.Protocol.Torrent.TorrentIPC import *
+
+from Kamaelia.Community.RJL.Kamaelia.Util.PureTransformer import PureTransformer
+from Kamaelia.Community.RJL.Kamaelia.Util.DataSource import TriggeredSource
+from Kamaelia.Community.RJL.Kamaelia.Util.UnseenOnly import UnseenOnly
+
+
+from ChunkDistributor import ChunkDistributor
+
+
+
+
+
+
+from OnDemandIntrospector import OnDemandIntrospector
+
+#UTIL
+
+from LineSplit import LineSplit
+
+
+
+
+
+
+class CheapAndCheerfulClock(threadedcomponent):
+    def __init__(self, interval):
+        super(CheapAndCheerfulClock, self).__init__()
+        self.interval = interval
+
+    def main(self):
+        while 1:
+            self.send(True, "outbox")
+            time.sleep(self.interval)
+
+
+class StreamReconstructor(component):
+    def main(self):
+        torrents = []
+        while 1:
+            yield 1
+            while self.dataReady("inbox"):
+                msg = self.recv("inbox")
+                print msg
+                if isinstance(msg, TIPCNewTorrentCreated):
+                    torrents.append([msg.torrentid, msg.savefolder])
+                    
+                elif isinstance(msg, TIPCTorrentStatusUpdate):
+                    print msg.torrentid
+                    if msg.torrentid == torrents[0][0]:
+                        print msg.statsdictionary
+                        if msg.statsdictionary.get("fractionDone",0) == 1:
+                            self.send(torrents[0][1], "outbox")                        
+                            torrents.pop(0)
+            self.pause()
+
+if __name__ == '__main__':
+    partslist = "filelist.txt"
+    resourcefetcher = TriggeredFileReader #SimpleHTTPClient
+    
+    pipeline(
+        ConsoleReader(),
+        OnDemandIntrospector(),
+        ConsoleEchoer(),
+    ).activate()
+        
+    pipeline(
+        CheapAndCheerfulClock(30.0),
+        TriggeredSource(partslist),
+        resourcefetcher(),
+        LineSplit(),
+        UnseenOnly(),
+        PureTransformer(lambda x: x or None), #eradicate blank lines
+        resourcefetcher(),
+        TorrentPatron(),
+        StreamReconstructor(),
+        ConsoleEchoer()
+    ).run()
+    
