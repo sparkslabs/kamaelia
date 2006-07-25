6c43cbcc12e8b6940319d98771daa5f1288bf9aa
matth_rd <matth_rd@2682807a-6752-0410-967a-793f22d1dfe9>
2006-07-25 13:52:59 +0000

    Reduced chunk size further (again wierd choppiess).
    Very basic naive componentisation. Doesn't play nicely
    in all kinds of ways (doing all decoding then playing,
    bundling of all functionality into one place ...
    including filereading, and blocking behaviour during
    sound output)
    
    Matt

--- trunk/Sketches/MH/pymedia/pymedia_test.py
+++ trunk/Sketches/MH/pymedia/pymedia_test.py
@@ -14,42 +14,61 @@ import pymedia.muxer as muxer
 import pymedia.audio.acodec as acodec
 import pymedia.audio.sound as sound
 
-f=open(filename,"rb")
-data=f.read(99999999)
-
-dm = muxer.Demuxer(extension)
-frames = dm.parse(data)
-
-rawout=[]
-
-frame0 = True
-for frame in frames:
-    if frame0:
-        stream_index = frame[0]
-        dec = acodec.Decoder(dm.streams[stream_index])
-        raw = dec.decode(frame[1])
-        snd =sound.Output(raw.sample_rate, raw.channels, sound.AFMT_S16_LE)
-        print len(dm.streams),raw.channels, raw.sample_rate
-        print dm.streams
-        print dm.getHeaderInfo()
-        frame0 = False
-
-    rawout.append(str(raw.data))
-#    snd.play(raw.data)
-    raw = dec.decode(frame[1])
-
+from Axon.Component import component
+from Axon.Ipc import shutdownMicroprocess, producerFinished
 
 import time
+        
+class PyMediaAudioPlayer(component):
+    
+    def __init__(self,filename):
+        super(PyMediaAudioPlayer,self).__init__()
 
-#allraw = "".join(rawout)
-CHUNKSIZE=4096
+    def main(self):
+        
+        f=open(filename,"rb")
+        data=f.read(99999999)
+        
+        yield 1
+        
+        dm = muxer.Demuxer(extension)
+        frames = dm.parse(data)
+        
+        yield 1
+        rawout=[]
+        
+        frame0 = True
+        for frame in frames:
+            yield 1
+            if frame0:
+                stream_index = frame[0]
+                dec = acodec.Decoder(dm.streams[stream_index])
+                raw = dec.decode(frame[1])
+                snd =sound.Output(raw.sample_rate, raw.channels, sound.AFMT_S16_LE)
+                print len(dm.streams),raw.channels, raw.sample_rate
+                print dm.streams
+                print dm.getHeaderInfo()
+                frame0 = False
+        
+            rawout.append(str(raw.data))
+        #    snd.play(raw.data)
+            raw = dec.decode(frame[1])
+        
+        
+        #allraw = "".join(rawout)
+        CHUNKSIZE=2048
+        
+        print snd.getSpace()
+        for chunk in rawout:
+            #  print len(chunk), snd.getSpace()
+            for i in xrange(0,len(chunk),CHUNKSIZE):
+#                t=time.time()
+                snd.play(chunk[i:i+CHUNKSIZE])
+#                print time.time()-t, snd.getSpace()
+                yield 1
+          #  snd.play(chunk)
+          #  print "flob",time.time()
 
-print snd.getSpace()
-for chunk in rawout:
-#  print len(chunk), snd.getSpace()
-  for i in xrange(0,len(chunk),CHUNKSIZE):
-      t=time.time()
-      snd.play(chunk[i:i+CHUNKSIZE])
-      print time.time()-t, snd.getSpace()
-#  snd.play(chunk)
-#  print "flob",time.time()
+if __name__ == "__main__":
+    PyMediaAudioPlayer(filename).run()
+    
