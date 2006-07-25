be93dfc193be3a0bb75be601856a937d3d7f4420
matth_rd <matth_rd@2682807a-6752-0410-967a-793f22d1dfe9>
2006-07-25 16:23:34 +0000

    Put in a highly simplified sound output component that just takes format options at initialisation and expects pure raw audio samples
    Matt

--- Sketches/MH/pymedia/pymedia_test.py
+++ Sketches/MH/pymedia/pymedia_test.py
@@ -25,6 +25,24 @@ READSIZE=1000
 from Kamaelia.File.Reading import RateControlledFileReader
 from Kamaelia.Chassis.Pipeline import pipeline
 
+mapping_format_to_pymedia = {
+    'AC3'       : sound.AFMT_AC3,
+    'A_LAW'     : sound.AFMT_A_LAW,
+    'IMA_ADPCM' : sound.AFMT_IMA_ADPCM,
+    'MPEG'      : sound.AFMT_MPEG,
+    'MU_LAW'    : sound.AFMT_MU_LAW,
+    'S16_BE'    : sound.AFMT_S16_BE,
+    'S16_LE'    : sound.AFMT_S16_LE,
+    'S16_NE'    : sound.AFMT_S16_NE,
+    'S8'        : sound.AFMT_S8,
+    'U16_BE'    : sound.AFMT_U16_BE,
+    'U16_LE'    : sound.AFMT_U16_LE,
+    'U8'        : sound.AFMT_U8,
+}
+
+mapping_format_from_pymedia = dict([(v,k) for (k,v) in mapping_format_to_pymedia.items() ])
+
+
 class PyMediaAudioPlayer(component):
     
     Outboxes = { "outbox" : "audio samples in data structures",
@@ -66,7 +84,7 @@ class PyMediaAudioPlayer(component):
                     data['data'] = str(raw.data)
                     data['channels'] = raw.channels
                     data['sample_rate'] = raw.sample_rate
-                    data['format'] = sound.AFMT_S16_LE
+                    data['format'] = mapping_format_from_pymedia[sound.AFMT_S16_LE]
                     self.send(data,"outbox")
         
             while self.dataReady("control"):
@@ -111,9 +129,60 @@ class SoundOutput(component):
                 self.pause()
             yield 1
 
+class ExtractData(component):
+    def main(self):
+        snd = None
+        
+        shutdown=False
+        while self.anyReady() or not shutdown:
+            while self.dataReady("inbox"):
+                data = self.recv("inbox")
+                self.send(data['data'],"outbox")
+            
+            while self.dataReady("control"):
+                msg=self.recv("control")
+                if isinstance(msg, (producerFinished,shutdownMicroprocess)):
+                    shutdown=True
+                self.send(msg,"signal")
+                
+            if not shutdown:
+                self.pause()
+            yield 1
+
+class RawSoundOutput(component):
+    def __init__(self, sample_rate=44050, channels=2, format="S16_LE"):
+        super(RawSoundOutput,self).__init__()
+        
+        pformat = mapping_format_to_pymedia[format]
+        self.snd = sound.Output(sample_rate, channels, pformat)
+        
+    def main(self):
+        CHUNKSIZE=2048
+        shutdown=False
+        while self.anyReady() or not shutdown:
+            while self.dataReady("inbox"):
+                chunk = self.recv("inbox")
+                
+                for i in xrange(0,len(chunk),CHUNKSIZE):
+                    self.snd.play(chunk[i:i+CHUNKSIZE])
+                yield 1
+            
+            while self.dataReady("control"):
+                msg=self.recv("control")
+                if isinstance(msg, (producerFinished,shutdownMicroprocess)):
+                    shutdown=True
+                self.send(msg,"signal")
+                
+            if not shutdown:
+                self.pause()
+            yield 1
+
+
 if __name__ == "__main__":
     pipeline( RateControlledFileReader(filename,readmode="bytes",rate=999999,chunksize=1024),
               PyMediaAudioPlayer(extension),
-              SoundOutput(),
+#              SoundOutput(),
+              ExtractData(),
+              RawSoundOutput(),
             ).run()
     
