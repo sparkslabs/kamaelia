e195298de46b43e6bd79d9b80e0841e2aea4dae8
anagha_m <anagha_m@2682807a-6752-0410-967a-793f22d1dfe9>
2006-07-31 21:10:17 +0000

    added licence block

--- trunk/Sketches/AM/xtea/xtea.py
+++ trunk/Sketches/AM/xtea/xtea.py
@@ -1,4 +1,38 @@
+""" 
+XTEA Block Encryption Algorithm
+
+Author: Paul Chakravarti (paul_dot_chakravarti_at_mac_dot_com)
+License: Public Domain
+
+This module provides a Python implementation of the XTEA block encryption
+algorithm (http://www.cix.co.uk/~klockstone/xtea.pdf). 
+
+The module implements the basic XTEA block encryption algortithm
+(`xtea_encrypt`/`xtea_decrypt`) and also provides a higher level `crypt`
+function which symmetrically encrypts/decrypts a variable length string using
+XTEA in OFB mode as a key generator. The `crypt` function does not use
+`xtea_decrypt` which is provided for completeness only (but can be used
+to support other stream modes - eg CBC/CFB).
+
+This module is intended to provide a simple 'privacy-grade' Python encryption
+algorithm with no external dependencies. The implementation is relatively slow
+and is best suited to small volumes of data. Note that the XTEA algorithm has
+not been subjected to extensive analysis (though is believed to be relatively
+secure - see http://en.wikipedia.org/wiki/XTEA). For applications requiring
+'real' security please use a known and well tested algorithm/implementation.
+
+The security of the algorithm is entirely based on quality (entropy) and
+secrecy of the key. You should generate the key from a known random source and
+exchange using a trusted mechanism. In addition, you should always use a random
+IV to seed the key generator (the IV is not sensitive and does not need to be
+exchanged securely)
+
+
+"""
+
 import struct
+
+
 def xtea_encrypt(key,block,n=32):
     """
         Encrypt 64 bit data block using XTEA block cypher
@@ -34,3 +68,41 @@ def xtea_decrypt(key,block,n=32):
         v0 = (v0 - (((v1<<4 ^ v1>>5) + v1) ^ (sum + k[sum & 3]))) & mask
     return struct.pack("!2L",v0,v1)
 
+def xtea_encrypt_arbit(key,block):
+   #breakup the block into 8 byte
+   times = len(block)/8
+   index = 0
+   start = 0
+   enc_list = []
+   
+   while times != 0:
+      slice = block[index:index+8]
+      #print "slice is",slice
+      index = index+8
+      times = times - 1
+      enc_list.append(xtea_encrypt(key,slice))
+
+   return ''.join(enc_list)
+
+def xtea_decrypt_arbit(key,block):
+   #breakup the block into 8 byte
+   times = len(block)/8
+   index = 0
+   start = 0
+   enc_list = []
+   
+   while times != 0:
+      slice = block[index:index+8]
+      #print "slice is",slice
+      index = index+8
+      times = times - 1
+      enc_list.append(xtea_decrypt(key,slice))
+
+   return ''.join(enc_list)
+"""
+cipher = xtea_encrypt_arbit('1234567890123456','12345678ABCDEFGH')
+print cipher
+plain = xtea_decrypt_arbit('1234567890123456',cipher)
+print plain
+
+"""
