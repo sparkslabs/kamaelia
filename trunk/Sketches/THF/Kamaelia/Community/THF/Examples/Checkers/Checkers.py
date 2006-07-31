68bbc028ff9bc0c1c39980a8a150fb100a0ca653
thf_dev <thf_dev@2682807a-6752-0410-967a-793f22d1dfe9>
2006-07-31 12:44:33 +0000

    Initial version of checkers game. No game logic implemented yet.

---
+++ trunk/Sketches/THF/Kamaelia/Community/THF/Examples/Checkers/Checkers.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+#
+# (C) 2005 British Broadcasting Corporation and Kamaelia Contributors(1)
+#     All Rights Reserved.
+#
+# You may only modify and redistribute this under the terms of any of the
+# following licenses(2): Mozilla Public License, V1.1, GNU General
+# Public License, V2.0, GNU Lesser General Public License, V2.1
+#
+# (1) Kamaelia Contributors are listed in the AUTHORS file and at
+#     http://kamaelia.sourceforge.net/AUTHORS - please extend this file,
+#     not this notice.
+# (2) Reproduced in the COPYING file, and at:
+#     http://kamaelia.sourceforge.net/COPYING
+# Under section 3.5 of the MPL, we are using this text since we deem the MPL
+# notice inappropriate for this file. As per MPL/GPL/LGPL removal of this
+# notice is prohibited.
+#
+# Please contact us via: kamaelia-list-owner@lists.sourceforge.net
+# to discuss alternative licensing.
+# -------------------------------------------------------------------------
+"""\
+=====================
+Checkers game component
+=====================
+"""
+
+
+import Axon
+
+import sys
+sys.path.append("../../Kamaelia/UI/OpenGL/")
+
+from OpenGLDisplay import *
+from OpenGLComponent import *
+
+from CheckersBoard import CheckersBoard
+from CheckersPiece import CheckersPiece
+from CheckersInteractor import CheckersInteractor
+
+class Checkers(Axon.AdaptiveCommsComponent.AdaptiveCommsComponent):
+    Inboxes = {
+       "inbox": "not used",
+       "control": "ignored",
+    }
+    
+    Outboxes = {
+        "outbox": "not used",
+    }
+
+    def initialiseComponent(self):
+        # initialise display
+        display = OpenGLDisplay(viewerposition=(0,-10,0), lookat=(0,0,-15)).activate()
+        OpenGLDisplay.setDisplayService(display)
+    
+        # create board
+        self.board = CheckersBoard(position=(0,0,-15)).activate()
+        
+        # create black pieces
+        self.blackPieces = []
+        self.blackInteractors = []
+        for i in range(8):
+            for j in range(3):
+                if (i+j) %2 == 0:
+                    x = float(i)-3.5
+                    y = float(j)-3.5
+                    piece = CheckersPiece(position=(x, y, -15), colour=(0.6,0,0)).activate()
+                    self.blackPieces.append(piece)
+
+                    interactor = CheckersInteractor(victim=piece).activate()
+                    piece.link( (piece, "position"), (interactor, "position"))
+                    interactor.link( (interactor, "outbox"), (piece, "rel_position"))
+                    self.blackInteractors.append(interactor)
+
+                    
+        # create white pieces
+        self.whitePieces = []
+        self.whiteInteractors = []
+        for i in range(8):
+            for j in range(5,8):
+                if (i+j) %2 == 0:
+                    x = float(i)-3.5
+                    y = float(j)-3.5
+                    piece = CheckersPiece(position=(x, y, -15), colour=(0,0,0.6)).activate()
+                    self.whitePieces.append(piece)
+
+                    interactor = CheckersInteractor(victim=piece).activate()
+                    piece.link( (piece, "position"), (interactor, "position"))
+                    interactor.link( (interactor, "outbox"), (piece, "rel_position"))
+                    self.whiteInteractors.append(interactor)
+
+
+        return 1
+        
+        
+    def mainBody(self):
+        
+        return 1
+        
+if __name__=='__main__':
+    Checkers().activate()
+    Axon.Scheduler.scheduler.run.runThreads()
