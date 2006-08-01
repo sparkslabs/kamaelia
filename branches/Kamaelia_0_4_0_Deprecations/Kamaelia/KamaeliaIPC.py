7207f73f385870a762cda48d10e9d5e48e12f897
matth_rd <matth_rd@2682807a-6752-0410-967a-793f22d1dfe9>
2006-08-01 15:56:38 +0000

    Deprecated. Now a stub.
    Matt

--- branches/Kamaelia_0_4_0_Deprecations/Kamaelia/KamaeliaIPC.py
+++ branches/Kamaelia_0_4_0_Deprecations/Kamaelia/KamaeliaIPC.py
@@ -19,91 +19,18 @@
 # Please contact us via: kamaelia-list-owner@lists.sourceforge.net
 # to discuss alternative licensing.
 # -------------------------------------------------------------------------
+"""DEPRECATED"""
 
-from Axon.Ipc import producerFinished, notify
+from Kamaelia.IPC import *
 
-class socketShutdown(producerFinished):
-   """Message to indicate that the network connection has been closed."""
-   pass
-
-class newCSA(notify):
-   """Helper class to notify of new CSAs as they are created.  newCSA.object
-   will return the CSA."""
-   def __init__(self, caller, CSA):
-      super(newCSA, self).__init__(caller, CSA)
-   def handlesWriting(self):
-      return True
-
-class shutdownCSA(notify):
-   """Helper class to notify of new CSAs as they are created.  newCSA.object
-   will return the CSA."""
-   def __init__(self, caller, CSA):
-      super(shutdownCSA, self).__init__(caller, CSA)
-   def shutdown(self):
-      return True
-
-class newServer(notify):
-   """Helper class to notify of new CSAs as they are created.  newCSA.object
-   will return the CSA."""
-   def __init__(self, caller, CSA):
-      super(newServer, self).__init__(caller, CSA)
-   def handlesWriting(self):
-      return False
-
-#
-# Two new classes to simplify the selector.
-# 
-# Note that hasOOB is only to be set for SOCKETS, and relates to out of band
-# information regarding the socket. This is used to put the specific thing
-# to monitor into the exceptions set (places we get out of band info)
-#
-# For more help as to what's going on with exceptionals, look at
-#   $ man 2 select_tut
-#
-# This may get simplified further at some point to simply add in a newExceptionalReader
-# message (indeed I'm in two minds right now about that!)
-#
-
-
-class newWriter(notify):
-    """Helper class to notify of new CSAs as they are created.  newCSA.object
-    will return the CSA."""
-    def __init__(self, caller, CSA):
-        super(newWriter, self).__init__(caller, CSA)
-        self.hasOOB = False 
-
-class newReader(notify):
-   """Helper class to notify of new CSAs as they are created.  newCSA.object
-   will return the CSA."""
-   def __init__(self, caller, CSA):
-      super(newReader, self).__init__(caller, CSA)
-      self.hasOOB = False
-
-class newExceptional(notify):
-   """Helper class to notify of new CSAs as they are created.  newCSA.object
-   will return the CSA."""
-   def __init__(self, caller, CSA):
-      super(newExceptional, self).__init__(caller, CSA)
-      self.hasOOB = False
-
-class removeReader(notify):
-   """Helper class to notify of new CSAs as they are created.  newCSA.object
-   will return the CSA."""
-   def __init__(self, caller, CSA):
-      super(removeReader, self).__init__(caller, CSA)
-      self.hasOOB = False
-
-class removeWriter(notify):
-   """Helper class to notify of new CSAs as they are created.  newCSA.object
-   will return the CSA."""
-   def __init__(self, caller, CSA):
-      super(removeWriter, self).__init__(caller, CSA)
-      self.hasOOB = False
-
-class removeExceptional(notify):
-   """Helper class to notify of new CSAs as they are created.  newCSA.object
-   will return the CSA."""
-   def __init__(self, caller, CSA):
-      super(removeExceptional, self).__init__(caller, CSA)
-      self.hasOOB = False
-
+# USED TO CONTAIN:
+# class socketShutdown(producerFinished):
+# class newCSA(notify):
+# class shutdownCSA(notify):
+# class newServer(notify):
+# class newWriter(notify):
+# class newReader(notify):
+# class newExceptional(notify):
+# class removeReader(notify):
+# class removeWriter(notify):
+# class removeExceptional(notify):
