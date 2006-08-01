1e0f38183aaa4afcc00315bda7f237d624e0de93
matth_rd <matth_rd@2682807a-6752-0410-967a-793f22d1dfe9>
2006-08-01 15:27:25 +0000

    Deprecated. Turned into stubs.
    Matt

--- branches/Kamaelia_0_4_0_Deprecations/Kamaelia/SimpleServerComponent.py
+++ branches/Kamaelia_0_4_0_Deprecations/Kamaelia/SimpleServerComponent.py
@@ -19,56 +19,11 @@
 # Please contact us via: kamaelia-list-owner@lists.sourceforge.net
 # to discuss alternative licensing.
 # -------------------------------------------------------------------------
-"""
-Simple Server class.
+"""DEPRECATED"""
 
-Provides a framework for creating generic protocol handlers to
-deal with information coming in on a single port (and a single
-port only). This however covers a large array of server types.
-
-This code is based on the code used for testing the Internet Connection
-abstraction layer.
-
-EXTERNAL CONNECTORS
-      * inboxes : ["_oobinfo"]
-      * outboxes=None
-
-Strictly speaking _oobinfo isn't an external connector - it's used for plumbing
-internal components into the Simple Server. (The PLS sends messages to
-the Simple Server - such as "new connection" on this connector)
-
-In practice, this component provides no external connectors for your use.
-"""
-
-import Axon as _Axon
 from Kamaelia.Chassis.ConnectedServer import SimpleServer
 
-class simpleServerProtocol(_Axon.Component.component):
-   pass
+# This was a stub to start with
 
 if __name__ == '__main__':
-
-   from Axon.Scheduler import scheduler
-   class SimpleServerTestProtocol(simpleServerProtocol):
-      def __init__(self):
-         super(SimpleServerTestProtocol, self).__init__()
-         assert self.debugger.note("SimpleServerTestProtocol.__init__",1, "Starting test protocol")
-
-      def mainBody(self):
-         if self.dataReady("inbox"):
-            data = self.recv("inbox")
-            print "Got data", data
-            assert self.debugger.note("SimpleServerTestProtocol.mainBody",1, "NetServ : We were sent data - ")
-            assert self.debugger.note("SimpleServerTestProtocol.mainBody",1, "We should probably do something with it now? :-)")
-            assert self.debugger.note("SimpleServerTestProtocol.mainBody",1, "I know, let's sling it straight back at them :-)")
-            self.send(data,"outbox")
-         if self.dataReady("control"):
-            data = self.recv("control")
-            return 0
-         return 1
-
-      def closeDownComponent(self):
-         assert self.debugger.note("SimpleServerTestProtocol.closeDownComponent",1, "Closing down test protcol")
-
-   SimpleServer(protocol=SimpleServerTestProtocol).activate()
-   scheduler.run.runThreads(slowmo=0)
+    raise DeprecationWarning()
