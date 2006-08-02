43d1463d434aa767c21acc71058746776574b258
matth_rd <matth_rd@2682807a-6752-0410-967a-793f22d1dfe9>
2006-08-02 13:56:45 +0000

    Deprecated FilterComponent.* in favour of Filter.Filter
    Matt

--- branches/Kamaelia_0_4_0_Deprecations/Kamaelia/Util/FilterComponent.py
+++ branches/Kamaelia_0_4_0_Deprecations/Kamaelia/Util/FilterComponent.py
@@ -19,109 +19,12 @@
 # Please contact us via: kamaelia-list-owner@lists.sourceforge.net
 # to discuss alternative licensing.
 # -------------------------------------------------------------------------
-"""\
-===================================
-Simple framework for filtering data
-===================================
+"""DEPRECATED"""
 
-A framework for filtering a stream of data. Write an object providing a
-filter(...) method and plug it into a FilterComponent component.
+from Kamaelia.Util.Filter import Filter as FilterComponent
 
+# USED TO CONTAIN:
+# class FilterComponent(component):
 
+# __kamaelia_components__  = ( FilterComponent, )
 
-Example Usage
--------------
-Filters any non-strings from a stream of data::
-    
-    class StringFilter(object):
-        def filter(self, input):
-            if type(input) == type(""):
-                return input
-            else:
-                return None            # indicates nothing to be output
-
-    myfilter = FilterComponent(filter = StringFilter).activate()
-    
-
-
-How does it work?
------------------
-
-Initialize a FilterComponent component, providing an object with a filter(...)
-method.
-
-The method should take a single argument - the data to be filtered. It should
-return the result of the filtering/processing. If that result is None then
-the component outputs nothing, otherwise it outputs whatever the value is that
-was returned.
-
-Data received on the component's "inbox" inbox is passed to the filter(...)
-method of the object you provided. The result is output on the "outbox" outbox.
-
-If a producerFinished message is received on the "control" inbox then it is sent
-on out of the "signal" outbox. The component will then terminate.
-
-However, before terminating it will repeatedly call your object's filter(...)
-method, passing it an empty string ("") until the result returned is None.
-If not None, then whatever value the filter(...) method returned is output. This
-is to give your object a chance to flush any data it may have been buffering.
-
-Irrespective of whether your filtering object buffers any data from one call to
-the next, you must ensure that (eventually) calling it with an empty string ("")
-will result in None being returned.
-
-"""
-
-
-from Axon.Component import component
-class NullFilter(object):
-   """A filter class that filters nothing.  This is the null default for the FilterComponent."""
-   def filter(self, newtext):
-      return newtext
-      
-class FilterComponent(component):
-   """\
-   FilterComponent([filter]) -> new FilterComponent component.
-
-   Component that can modify and filter data passing through it. Plug your own
-   'filter' into it.
-   
-   Keyword arguments:
-   - filter  -- an object implementing a filter(data) method (default=NullFilter instance)
-   """
-
-   Inboxes  = { "inbox"   : "Data to be filtered",
-                "control" : "Shutdown signalling",
-              }
-   Outboxes = { "outbox" : "Filtered data",
-                "signal" : "Shutdown signalling",
-              }
-       
-   def __init__(self, filter = NullFilter()):
-      """x.__init__(...) initializes x; see x.__class__.__doc__ for signature"""
-      super(FilterComponent, self).__init__()
-      self.filter = filter
-      
-   def mainBody(self):
-      """Main loop body."""
-      if self.dataReady():
-         mes = self.recv()
-         outmes = self.filter.filter(mes)
-         if outmes is not None:
-            self.send(outmes)
-      if self.dataReady("control"):
-         mes = self.recv("control")
-         if isinstance(mes, producerFinished):
-            self.send(mes, "signal")
-            return 0
-      return 1
-            
-   def closeDownComponent(self):
-      """Flush any data remaining in the filter before shutting down."""
-      while 1:
-        outmes = self.filt.filter("")
-        if outmes is None:
-            break
-        self.send(outmes)
-
-__kamaelia_components__  = ( FilterComponent, )
