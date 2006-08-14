d9cb6ffa0abbeb998e711134598b175b7e665b2d
matth_rd <matth_rd@2682807a-6752-0410-967a-793f22d1dfe9>
2006-08-14 21:50:07 +0000

    Removed copy of Entuple component.
    Matt

--- trunk/Sketches/MH/Sketcher/MusicPlayer.py
+++ trunk/Sketches/MH/Sketcher/MusicPlayer.py
@@ -64,29 +64,7 @@ def RawAudioMixer():
                            readInterval   = 0.1
                          ),
 
-class Entuple(component):
-    def __init__(self, prefix=[], postfix=[]):
-        super(Entuple,self).__init__()
-        self.prefix = prefix
-        self.postfix = postfix
-    
-    def shutdown(self):
-        while self.dataReady("control"):
-            msg = self.recv("control")
-            self.send(msg,"signal")
-            if isinstance(msg, (producerFinished, shutdownMicroprocess)):
-                return True
-        return False
-        
-    def main(self):
-        while not self.shutdown():
-            while self.dataReady("inbox"):
-                data = self.recv("inbox")
-                entupled = self.prefix + [data] + self.postfix
-                self.send( entupled, "outbox" )
-            self.pause()
-            yield 1
-
+from Whiteboard.Entuple import Entuple
 if __name__=="__main__":
     
     from Kamaelia.Internet.TCPClient import TCPClient
