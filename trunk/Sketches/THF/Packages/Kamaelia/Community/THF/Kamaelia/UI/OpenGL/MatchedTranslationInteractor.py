324921ca8b79b08c64c39a831bbf1ef24fbe2c88
thf_dev <thf_dev@2682807a-6752-0410-967a-793f22d1dfe9>
2006-08-16 14:49:38 +0000

    Added documentation.

--- trunk/Sketches/THF/Packages/Kamaelia/Community/THF/Kamaelia/UI/OpenGL/MatchedTranslationInteractor.py
+++ trunk/Sketches/THF/Packages/Kamaelia/Community/THF/Kamaelia/UI/OpenGL/MatchedTranslationInteractor.py
@@ -23,6 +23,57 @@
 =====================
 Matched Translation Interactor
 =====================
+
+An interactor for moving OpenGLComponents corresponding to mouse
+movement along the X,Y plane.
+
+MatchedTranslationInteractor is a subclass of Interactor.
+
+Example Usage
+-------------
+
+The following example shows four SimpleCubes which can be moved by
+dragging your mouse:
+
+    o1 = SimpleCube(position=(6, 0,-30), size=(1,1,1)).activate()
+    i1 = MatchedTranslationInteractor(victim=o1).activate()
+
+    o2 = SimpleCube(position=(0, 0,-20), size=(1,1,1)).activate()
+    i2 = MatchedTranslationInteractor(victim=o2).activate()
+
+    o3 = SimpleCube(position=(-3, 0,-10), size=(1,1,1)).activate()
+    i3 = MatchedTranslationInteractor(victim=o3).activate()
+
+    o4 = SimpleCube(position=(15, 0,-40), size=(1,1,1)).activate()
+    i4 = MatchedTranslationInteractor(victim=o4).activate()
+
+    Axon.Scheduler.scheduler.run.runThreads()  
+
+How does it work?
+-----------------
+
+MatchedTranslationInteractor is a subclass of Interactor. It overrides
+the __ini__(), setup(), handleEvents() and frame() methods.
+
+The matched movement works by using the position of the controlled
+object and determine its X,Y-aligned plane. The amount of mouse movement
+is then calculated as if it was on this plane. This is done by
+intersecting the direction vector which is included in the mouse event
+with the plane to get the point of intersection. Then the distance
+between the newly generated point and the last point is calculated. The result
+is the actual amount of movement along the X and the Y axis.
+
+The interactor makes all the linkages it needs during initialisation.
+Because the interactor needs the actual position of the controlled
+component to be accurate all the time, it uses the components "position"
+outbox by default. If you don't want the interactor to make the
+linkages, you can set nolink=True as constructor argument. The following
+linkages are needed for the interactor to work (from the interactors
+point of view):
+
+    self.link( (self, "outbox"), (self.victim, "rel_position") )
+    self.link( (self.victim, "position"), (self, "inbox") )
+
 """
 
 
@@ -36,6 +87,9 @@ from Intersect import *
 from Interactor import *
 
 class MatchedTranslationInteractor(Interactor):
+    """\
+    The components has no special keyword arguments.
+    """
     
     def __init__(self, **argd):
         super(MatchedTranslationInteractor, self).__init__(**argd)
@@ -69,9 +123,11 @@ class MatchedTranslationInteractor(Interactor):
                 if event.type == pygame.MOUSEBUTTONDOWN and self.identifier in event.hitobjects:
                     if event.button == 1:
                         self.grabbed = True
+                        
                 if event.type == pygame.MOUSEBUTTONUP:
                     if event.button == 1:
                         self.grabbed = False
+                        
                 if event.type == pygame.MOUSEMOTION:
                     if self.grabbed == True:
                         if self.oldpoint is not None:
@@ -92,9 +148,6 @@ class MatchedTranslationInteractor(Interactor):
 if __name__=='__main__':
     from SimpleCube import *
     
-    display = OpenGLDisplay(fog=(20.0,50.0)).activate()
-    OpenGLDisplay.setDisplayService(display)
-
     o1 = SimpleCube(position=(6, 0,-30), size=(1,1,1), name="center").activate()
     i1 = MatchedTranslationInteractor(victim=o1).activate()
 
