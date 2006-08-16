74e6c7d9ef8e272f95bd064d5ec5c1d41ede3a5f
thf_dev <thf_dev@2682807a-6752-0410-967a-793f22d1dfe9>
2006-08-16 23:41:16 +0000

    Added documentation.

--- Sketches/THF/Packages/Kamaelia/Community/THF/Kamaelia/UI/OpenGL/Vector.py
+++ Sketches/THF/Packages/Kamaelia/Community/THF/Kamaelia/UI/OpenGL/Vector.py
@@ -23,7 +23,9 @@
 =====================
 3D Vector class
 =====================
-TODO
+
+A class for 3 dimensional vectors providing several methods for common
+vector operations.
 """
 
 from math import *
@@ -33,23 +35,56 @@ from math import *
 # Vector: used for handling 3D Vectors
 # =====================
 class Vector:
+    """\
+    Vector([x][,y][,z]) -> A new Vector object.
+    
+    Keyword arguments:
+    - x,y,z -- Initial values.
+    """
     def __init__(self, x=0.0, y=0.0, z=0.0):
         self.x = float(x)
         self.y = float(y)
         self.z = float(z)
 
     def zero(self):
+        """ Set all values to zero. """
         self.x = 0.0
         self.y = 0.0
         self.z = 0.0
         return self
         
     def invert(self):
+        """ Changes the sign of each vector component. """
         self.x = -self.x
         self.y = -self.y
         self.z = -self.z
         return self
 
+    def copy(self):
+        """ Returns a copy of the Vector object. """
+        return Vector(self.x,self.y,self.z)
+        
+    def toTuple(self):
+        """ Returns a tuple (x,y,z). """
+        return (self.x,self.y,self.z)
+    
+    def length(self):
+        """ Returns the length of the vector. """
+        return sqrt(self.x*self.x + self.y*self.y + self.z*self.z)
+    
+    def dot(self, other):
+        """ Returns the dot product between self and other. """
+        return self.x*other.x + self.y*other.y + self.z*other.z
+        
+    def cross(self, other):
+        """ Returns the cross product between self and other. """
+         return Vector(self.y*other.z - self.z*other.y, self.z*other.x - self.x*other.z, self.x*other.y - self.y*other.x)
+         
+    def norm(self):
+        """ Returns a normalised version of the vector. """
+        l = sqrt(self.x*self.x + self.y*self.y + self.z*self.z)
+        return Vector(self.x / l, self.y / l, self.z / l)
+        
     def __str__(self):
         return str([self.x,self.y,self.z])
 
@@ -62,25 +97,6 @@ class Vector:
         if self.x == other.x and self.y == other.y and self.z == other.z:
             return False
         return True
-        
-    def copy(self):
-        return Vector(self.x,self.y,self.z)
-        
-    def toTuple(self):
-        return (self.x,self.y,self.z)
-    
-    def length(self):
-        return sqrt(self.x*self.x + self.y*self.y + self.z*self.z)
-    
-    def dot(self, other):
-        return self.x*other.x + self.y*other.y + self.z*other.z
-        
-    def cross(self, other):
-         return Vector(self.y*other.z - self.z*other.y, self.z*other.x - self.x*other.z, self.x*other.y - self.y*other.x)
-         
-    def norm(self):
-        l = sqrt(self.x*self.x + self.y*self.y + self.z*self.z)
-        return Vector(self.x / l, self.y / l, self.z / l)
         
     def __mul__(self, factor):
         return Vector(self.x * factor, self.y * factor, self.z * factor)
