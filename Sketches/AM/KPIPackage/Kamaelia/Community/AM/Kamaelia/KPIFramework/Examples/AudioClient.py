c858b1177e0c749b9df1e9f8fcc92fb31f7cd9cd
anagha_m <anagha_m@2682807a-6752-0410-967a-793f22d1dfe9>
2006-08-17 17:38:16 +0000

    Removed Audio Related components after discussion with Matt

--- Sketches/AM/KPIPackage/Kamaelia/Community/AM/Kamaelia/KPIFramework/Examples/AudioClient.py
+++
@@ -1,8 +0,0 @@
-from AudioLib import *
-from Kamaelia.Util.PipelineComponent import pipeline
-from Kamaelia.Internet.TCPClient import TCPClient as _TCPClient
-
-pipeline(_TCPClient("127.0.0.1",1256),
-         AudioDecoder('mp3'),         
-         SoundOutput()
-        ).run()
