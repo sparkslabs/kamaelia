db58314e6833e46750cd7f67188ad155f4c8810f
anagha_m <anagha_m@2682807a-6752-0410-967a-793f22d1dfe9>
2006-08-17 15:37:07 +0000

    updated documentation

--- Sketches/AM/KPIPackage/Kamaelia/Community/AM/Kamaelia/KPIFramework/KPI/Server/Authenticator.py
+++ Sketches/AM/KPIPackage/Kamaelia/Community/AM/Kamaelia/KPIFramework/KPI/Server/Authenticator.py
@@ -50,9 +50,11 @@ from Kamaelia.Util.Backplane import subscribeTo
 from Kamaelia.Community.AM.Kamaelia.KPIFramework.KPI.Crypto import xtea
 
 class Authenticator(Axon.Component.component):
-    Inboxes = {"inbox" : "authentication and data packets"}
+    Inboxes = {"inbox" : "authentication and data packets",
+               "control" : "receive shutdown messages"}
     Outboxes = {"outbox" : "authentication",
-                "notifyuser" : "user notification"}
+                "notifyuser" : "user notification",
+                "signal" : "pass shutdown messages"}
 
     def __init__(self, kpidb):
         super(Authenticator,self).__init__()
@@ -65,16 +67,16 @@ class Authenticator(Axon.Component.component):
         data = self.recv("inbox")
         padding,userid = struct.unpack('!2L',
                     xtea.xtea_decrypt(kpidb.getRootKey(),data))
-        print "Authenticator received userid:", userid
+        #print "Authenticator received userid:", userid
         if kpidb.isValidUser(userid) == False:
-            print "Invalid UserID" # todo shutdown
+            #print "Invalid UserID" # todo shutdown
             return
 
         challenge = random.getrandbits(32)
         temp = struct.pack('!2L',0, challenge)
         userkey = kpidb.getKPIKeys().getKey(userid)
         data = xtea.xtea_encrypt(userkey, temp)
-        print data, challenge, userkey
+        #print data, challenge, userkey
         self.send(data, "outbox")
         yield 1
         while not self.dataReady("inbox"):
@@ -82,12 +84,12 @@ class Authenticator(Axon.Component.component):
         data = self.recv("inbox")
         temp = xtea.xtea_decrypt(userkey,data)
         padding, response = struct.unpack('!2L', temp)
-        print data, response
+        #print data, response
         if response == challenge + 1:
             self.send("SUCCESS", "outbox")
             yield 1
         else:
-            print "authenication failure"
+            #print "authenication failure"
             return # shutdown
 
         #new user added
--- Sketches/AM/KPIPackage/Kamaelia/Community/AM/Kamaelia/KPIFramework/KPI/Server/SessionKeyController.py
+++ Sketches/AM/KPIPackage/Kamaelia/Community/AM/Kamaelia/KPIFramework/KPI/Server/SessionKeyController.py
@@ -33,52 +33,58 @@ import struct
 from Kamaelia.Community.AM.Kamaelia.KPIFramework.KPI.Crypto import xtea
 
 class SessionKeyController(Axon.Component.component):
-   Inboxes = {"userevent" : "new user event"}
-   Outboxes = {"outbox" : "encrypted session key packets",
-                "notifykey" : "notify key"}
-
-   def __init__(self, kpikeys):
-       super(SessionKeyController,self).__init__()
-       self.kpikeys = kpikeys
+    Inboxes = {"userevent" : "new user event",
+               "control" : "receive shutdown messages"}
+    Outboxes = {"outbox" : "encrypted session key packets",
+                "notifykey" : "notify key",
+                "signal" : "pass shutdown messages"}
 
 
-   def main(self):
-       kpikeys = self.kpikeys
-       users = []
-       
-       while 1:
-           while not self.dataReady("userevent"):
-               yield 1
-           print "SC sending a key"
-           userid = self.recv("userevent")
-           #to avoid duplicate entries
-           try:
-               users.index(userid)
-           except ValueError:
-               users.append(userid)
-               users.sort()
-           #todo to send in a format
-           idkeymap = kpikeys.getCommonKeys(users)
-           sessionKey = self.getSessionKey()
-           print "SC->sessionkey", sessionKey
+    def __init__(self, kpikeys):
+        super(SessionKeyController,self).__init__()
+        self.kpikeys = kpikeys
 
-           #encrypt the the session key with common keys
-           for ID, key in idkeymap.iteritems():
-               idstr = struct.pack("!2L", 0, ID)
-               print "id,key", ID,len(key)
-               cipher = xtea.xtea_encrypt(key, sessionKey[:8])
-               cipher = cipher + xtea.xtea_encrypt(key, sessionKey[8:16])
-               data = idstr + cipher
-               self.send(data, "outbox")
 
-           self.send(sessionKey, "notifykey")
-           yield 1
+    def main(self):
+        kpikeys = self.kpikeys
+        users = []
 
-   def getSessionKey(self):
-       r1 = random.getrandbits(32)
-       r2 = random.getrandbits(32)
-       r3 = random.getrandbits(32)
-       r4 = random.getrandbits(32)
-       m = md5.new()
-       m.update(struct.pack("!4L", r1, r3, r4, r2))
-       return m.digest() 
+        while 1:
+            while not self.dataReady("userevent"):
+                yield 1
+            #print "SC sending a key"
+            userid = self.recv("userevent")
+            #to avoid duplicate entries
+            try:
+                users.index(userid)
+            except ValueError:
+                users.append(userid)
+                users.sort()
+
+            #todo to send in a format
+            idkeymap = kpikeys.getCommonKeys(users)
+            sessionKey = self.getSessionKey()
+            #print "SC->sessionkey", sessionKey
+
+            #notify the session key
+            self.send(sessionKey, "notifykey")
+
+            #encrypt the the session key with common keys
+            for ID, key in idkeymap.iteritems():
+                idstr = struct.pack("!2L", 0, ID)
+                #print "id,key", ID,len(key)
+                cipher = xtea.xtea_encrypt(key, sessionKey[:8])
+                cipher = cipher + xtea.xtea_encrypt(key, sessionKey[8:16])
+                data = idstr + cipher
+                self.send(data, "outbox")
+            yield 1
+
+
+    def getSessionKey(self):
+        r1 = random.getrandbits(32)
+        r2 = random.getrandbits(32)
+        r3 = random.getrandbits(32)
+        r4 = random.getrandbits(32)
+        m = md5.new()
+        m.update(struct.pack("!4L", r1, r3, r4, r2))
+        return m.digest() 
