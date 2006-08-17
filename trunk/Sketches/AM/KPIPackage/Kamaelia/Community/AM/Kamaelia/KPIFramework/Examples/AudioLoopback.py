47937c02918a9bb1d3f69ce7908a03baae1dfcb9
anagha_m <anagha_m@2682807a-6752-0410-967a-793f22d1dfe9>
2006-08-17 17:38:16 +0000

    Removed Audio Related components after discussion with Matt

--- trunk/Sketches/AM/KPIPackage/Kamaelia/Community/AM/Kamaelia/KPIFramework/Examples/AudioLoopback.py
+++
@@ -1,8 +0,0 @@
-from AudioLib import *
-from Kamaelia.Util.PipelineComponent import pipeline
-
-pipeline(AudioSource(10),
-         AudioEncoder('mp3'),
-         AudioDecoder('mp3'),         
-         SoundOutput()
-        ).run()
