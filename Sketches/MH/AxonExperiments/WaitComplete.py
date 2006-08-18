af32aba3253f77fc36c6d1190976c79a8ecda735
matth_rd <matth_rd@2682807a-6752-0410-967a-793f22d1dfe9>
2006-08-18 14:52:25 +0000

    A stripped down core axon, ready for some interesting experiments!
    Matt

---
+++ Sketches/MH/AxonExperiments/WaitComplete.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+
+# a little experiment to see if WaitComplete and reactivate behaviour can be
+# implemented again in the current optimised axon and, preferably making the
+# scheduler a little simpler in the process
+
+class Microprocess(object):
+    def __init__(self):
+        super(Microprocess,self).__init__()
+
+    def next(self):
+        return self.__thread.next()
+
+    def _microprocessGenerator(self,someobject, mainmethod="main"):
+       pc = someobject.__getattribute__(mainmethod)()
+       while 1:
+           # Continually try to run the code, and then release control
+           v = pc.next()
+           yield v
+
+    def main(self):
+        """STUB"""
+        yield 1
+        return
+
+    def activate(self, scheduler, mainmethod="main"):
+        self.scheduler = scheduler
+        self.__thread = self._microprocessGenerator(self,mainmethod)
+        self.scheduler.activateThread(self)
+        return self
+
+    def closeDownMicroprocess(self):
+        return None
+    
+    def pause(self):
+        self.scheduler.pauseThread(self)
+        
+    def unPause(self):
+        self.scheduler.wakeThread(self)
+        
+    
+    
+class Scheduler(object):
+    def __init__(self):
+        super(Scheduler,self).__init__()
+        self.runqueue = {}
+        
+    def activateThread(self, thread):
+        self.runqueue[thread] = 1
+        
+    def pauseThread(self, thread):
+        if thread in self.runqueue:
+            self.runqueue[thread] = 0
+        
+    def wakeThread(self, thread):
+        if thread in self.runqueue:
+            self.runqueue[thread] = 1
+        
+    def main(self):
+        while self.runqueue:
+            
+            for (thread,active) in self.runqueue.items():     # implicit copy of runqueue state
+                if active:
+                    try:
+                        retval = thread.next()
+                    except StopIteration:
+                        del self.runqueue[thread]
+
+
+class Component(Microprocess):
+    def __init__(self):
+        super(Component,self).__init__()
+        self.inboxes = { "inbox":[], "control":[] }
+        self.outboxes = { "outbox":None, "signal":None }
+        
+    def link(self, (src,srcbox), (dst,dstbox)):
+        src.outboxes[srcbox] = (dst,dstbox)
+        
+    def dataReady(self,boxname):
+        return len(self.inboxes[boxname])
+    
+    def recv(self,boxname):
+        return self.inboxes[boxname].pop(0)
+    
+    def send(self, value, boxname):
+        (dst,dstbox) = self.outboxes[boxname]
+        dst.inboxes[dstbox].append(value)
+        dst.unPause()
+    
+    
+    
+# --------------------------------------
+
+import time,sys
+
+class Source(Component):
+    def main(self):
+        for i in range(10,-1,-1):
+            t=time.time()
+            while time.time() < t+1.0:
+                yield 1
+                
+            self.send(i,"outbox")
+
+
+class Sink(Component):
+    def main(self):
+        done=False
+        while not done:
+            while self.dataReady("inbox"):
+                data = self.recv("inbox")
+                sys.stdout.write(str(data)+"\n")
+                sys.stdout.flush()
+                done = data==0
+            yield 1
+            
+            
+            
+scheduler = Scheduler()
+            
+src = Source().activate(scheduler)
+dst = Sink().activate(scheduler)
+src.link((src,"outbox"),(dst,"inbox"))
+
+
+scheduler.main()
