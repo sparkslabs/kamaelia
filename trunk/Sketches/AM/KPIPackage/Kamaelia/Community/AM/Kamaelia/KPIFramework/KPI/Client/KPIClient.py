54f96c341d36db6c8921b45827595eca1f394eef
anagha_m <anagha_m@2682807a-6752-0410-967a-793f22d1dfe9>
2006-08-19 14:27:45 +0000

    Added Documentation

--- trunk/Sketches/AM/KPIPackage/Kamaelia/Community/AM/Kamaelia/KPIFramework/KPI/Client/KPIClient.py
+++ trunk/Sketches/AM/KPIPackage/Kamaelia/Community/AM/Kamaelia/KPIFramework/KPI/Client/KPIClient.py
@@ -20,8 +20,12 @@
 # to discuss alternative licensing.
 # -------------------------------------------------------------------------
 """
+====================
+KPIClient
+====================
+
 KPIClient is client subsystem that encapsulates Authenticatee, Decryptor
-and DataSink
+and DataSink into a graphline component
 """
 
 from Kamaelia.Util.Graphline import *
@@ -29,8 +33,15 @@ from Authenticatee import Authenticatee
 from Decryptor import Decryptor
 from Kamaelia.Community.AM.Kamaelia.KPIFramework.KPI.DB.KPIUser import KPIUser
 
-#client side
 def KPIClient(config, datasink):
+    """\   KPIClient(config, datasink) -> returns a Graphline
+    KPIClient handles authentication and decryption
+    Keyword arguments:
+    - config    -- uses KPIUser instance for looking up user
+                    key from client config file
+    - datasink    -- any component with an inbox. the KPIClient sends
+        decrypted data to datasink's inbox
+    """      
     return Graphline(
         authenticatee = Authenticatee(KPIUser(configfile=config)),
         dec = Decryptor(),
