744eadf651bddcb866431fc66c0859866bb5800f
anagha_m <anagha_m@2682807a-6752-0410-967a-793f22d1dfe9>
2006-08-19 14:27:45 +0000

    Added Documentation

--- Sketches/AM/KPIPackage/Kamaelia/Community/AM/Kamaelia/KPIFramework/KPI/Client/Authenticatee.py
+++ Sketches/AM/KPIPackage/Kamaelia/Community/AM/Kamaelia/KPIFramework/KPI/Client/Authenticatee.py
@@ -21,21 +21,41 @@
 # -------------------------------------------------------------------------
 #
 """
-Authenticatee is client side component of authentication
-After successful authentication, it acts as pass through for
-the encrypted data. It decodes all key packets and data packets
-and notifies the decryptor
+=========================
+Authenticatee Component
+=========================
+Authenticatee is client side component of authentication. After successful
+authentication, it depacketizes the data and key packets
 
-TODO:
-currently uses xtea as the encryption algorithm for challenge response
-communication with authenticator. need to refactor to be able to plugin
-different encryption algos
-
-separate authenticatee and passthrough decoder into two components.
-make authenticatee signal about the authentication status
-
-Should be able to extend and override new authentication mechanisms
+How it works ?
+--------------
+How does it work ?
+------------------
+The authentication process is based on the Needham-Scroeder protocol.
+Authentication Sequence
+1. The Authenticatee sends its Userid encrypted with root key.
+2. The Authenticator obtains userid by decrypting the data received and
+   looks up for the key corresponding to the user id. 
+3. The Authenticator generates a random number, encrypts it
+   with a user's key and sends it to the authenticatee.
+4. The Authenticatee decrypts it, increments it by 1 and sends it back
+   to the Authenticator. 
+5. The Authenticator verifies if the number recieved is indeed the original
+   number incremented by 1.
+6. Authenticator sends SUCCESS Message
+7. Authenticatee sends back "OK" ack
+8. After succesful authentication, Authenticatee reads incoming packet's
+   header. if it is key packet, it decrypts and sends to "notifykey" outbox
+9. If its data packet, it sends data to "encout" outbox
+10. Note: Authenticator takes care of packets of different sizes. 
 """
+
+#TODO:
+#currently uses xtea as the encryption algorithm for challenge response
+#communication with authenticator. need to refactor to be able to plugin
+#various ciphers
+#separate authenticatee and depackizer into two components.
+#Should be able to extend and override new authentication mechanisms
 
 import struct
 import Axon
@@ -43,6 +63,12 @@ from Kamaelia.Community.AM.Kamaelia.KPIFramework.KPI.Crypto import xtea
 
 
 class Authenticatee(Axon.Component.component):
+    """\   Authenticator(kpiuser) -> new Authenticatee component
+    Handles authentication and depacketizing
+    Keyword arguments:
+    - kpiuser    -- uses KPIUser instance for looking up user
+                    key from client config file
+    """
     Inboxes = {"inbox" : "authentication and data packets",
                "control" : "receive shutdown messages"}
     Outboxes = {"outbox" : "authentication",
@@ -51,25 +77,27 @@ class Authenticatee(Axon.Component.component):
                "signal" : "pass shutdown messages"}
 
     def __init__(self, kpiuser):
+        """x.__init__(...) initializes x; see x.__class__.__doc__ for signature"""
         super(Authenticatee,self).__init__()
         self.kpiuser = kpiuser
 
     def main(self):
+        #encrypt user id with root key
         userid = self.kpiuser.getID()
         data = xtea.xtea_encrypt(self.kpiuser.getRootKey(),
                                  struct.pack('!2L',0, userid))
-        #print "encrypting user id with user key", self.kpiuser.getID(), self.kpiuser.getRootKey()
         self.send(data, "outbox")
         yield 1
 
         while not self.dataReady("inbox"):
             yield 1
         data = self.recv("inbox")
+        #receive challenge
         temp = xtea.xtea_decrypt(self.kpiuser.getUserKey(), data)
         padding, challenge = struct.unpack('!2L',temp)
         response = challenge+1
-        #print "received challenge",challenge
-        #print "sending response", response
+
+        #sending response encrypted with my key
         data = xtea.xtea_encrypt(self.kpiuser.getUserKey(),
                                  struct.pack('!2L',0, response))
         
@@ -86,11 +114,13 @@ class Authenticatee(Axon.Component.component):
             #print "authenication failure"
             return #shutdown
 
-        """Send ack to server from client to Server"""
+        #Send OK ack to authenticator
         self.send("OK", "outbox")
         yield 1
 
         #decode data
+        #the depacketizer has to modes
+        #reading header or reading body
         buffer = ''
         KEY = 0x20        
         DATA = 0x30
@@ -102,8 +132,6 @@ class Authenticatee(Axon.Component.component):
         while 1:
             while self.dataReady("inbox"):
                 data = self.recv("inbox")
-                #print "decoder", data
-
                 buffer = buffer + data
                 if mode == READ_HEADER :
                     if len(buffer) >= HEADER_SIZE:
@@ -114,22 +142,21 @@ class Authenticatee(Axon.Component.component):
                         #data from buffer -> body = buffer[HEADER_SIZE:data2read]
                         #this caused the client to be slower in receiving data
                         body = buffer[HEADER_SIZE:HEADER_SIZE+data2read]
-                        #print "packet type->", hex(packetType)
-                        #print "data 2 read", data2read
-                        #read all the data
+                        #read the body
                         if data2read <= len(body):
                             mode = READ_HEADER
                             if packetType == KEY:
+                                #key packet structure
+                                #8 bytes  - key ID with which session key was encrypted
+                                #16 bytes of encrypted session key
                                 padding,ID = struct.unpack("!2L", body[:8])
-                                #print "****ID****", ID
                                 try:
                                     key = self.kpiuser.getKey(ID)
+                                    #i have the key for the ID
                                     enckey = body[8:data2read]
-                                    #print "size of enckey", len(enckey)
                                     part1 = xtea.xtea_decrypt(key, enckey[:8])
                                     part2 = xtea.xtea_decrypt(key, enckey[8:16])
                                     sessionkey = part1 + part2
-                                    #print "decoded key", sessionkey
                                     self.send(sessionkey, "notifykey")
                                 except KeyError:
                                     pass #the key is not for me
@@ -148,20 +175,20 @@ class Authenticatee(Axon.Component.component):
                     if data2read <= len(body):
                         mode = READ_HEADER
                         if packetType == KEY:
+                            #key packet structure
+                            #8 bytes  - key ID with which session key was encrypted
+                            #16 bytes of encrypted session key                            
                             padding,ID = struct.unpack("!2L", body[:8])
-                            #print "****ID****", ID
                             try:
                                 key = self.kpiuser.getKey(ID)
                                 enckey = body[8:data2read]
                                 part1 = xtea.xtea_decrypt(key, enckey[:8])
                                 part2 = xtea.xtea_decrypt(key, enckey[8:16])
                                 sessionkey = part1 + part2
-                                #print "decoded key", sessionkey
                                 self.send(sessionkey, "notifykey")
                             except KeyError:
                                 pass #the key is not for me
                         elif packetType == DATA:
-                            #print "decoded data", body
                             self.send(body, "encout")
                         # remove the data read
                         buffer = buffer[data2read:len(buffer)]
--- Sketches/AM/KPIPackage/Kamaelia/Community/AM/Kamaelia/KPIFramework/KPI/Client/Decryptor.py
+++ Sketches/AM/KPIPackage/Kamaelia/Community/AM/Kamaelia/KPIFramework/KPI/Client/Decryptor.py
@@ -20,17 +20,44 @@
 # to discuss alternative licensing.
 # -------------------------------------------------------------------------
 """
-Decryptor decrypts the cipher data with the key provided 
+=====================
+Decryptor Component
+=====================
+Decryptor decrypts the encrypted data on its inbox with the key.The key
+is passed as constructor param or sent to its "keyevent" inbox
 
-TODO
-to be able plugin different data integrity algorithms 
+Example usage
+-------------
+key = "1234567890123456"
+pipeline (
+    chargen(),
+    Encryptor(key),
+    Decryptor(key),
+    consoleEchoer()
+).run()
+    
+How it works?
+--------------
+Decryptor uses xtea cipher for encryption. xtea is a block cipher whose block
+size is 8 bytes and key length is 16 bytes. The decryptor after decrypting
+data looks for looks for padding, if it finds padding, it strips
+the padding and sends the decrypted data to its outbox
+
 """
+#TODO
+#to be able plugin different ciphers
+#separate unpadding from decryption
 
 import Axon
 import struct
 from Kamaelia.Community.AM.Kamaelia.KPIFramework.KPI.Crypto import xtea
 
 class Decryptor(Axon.Component.component):
+    """\   Decryptor([key]) -> new Decryptor component.
+    Decrypts data and sends to outbox
+    Keyword arguments:
+    - key    -- key string (default="\0")
+    """    
     Inboxes = {"inbox" : "encrypted data packets",
                "keyevent": "key for decryption",
                "control": "receive shutdown messages"}
@@ -38,11 +65,13 @@ class Decryptor(Axon.Component.component):
                 "signal" : "pass shutdown messages"}
 
     def __init__(self, key="\0"):
+        """x.__init__(...) initializes x; see x.__class__.__doc__ for signature"""
         super(Decryptor,self).__init__()
         self.key = key
 
     def main(self):
         blocksize = 8
+        #MAGIC_STRING indicates that its preceding block is not padded
         MAGIC_STRING = blocksize * chr(0x80)
         while 1:
             yield 1
@@ -59,13 +88,20 @@ class Decryptor(Axon.Component.component):
                 #Unpad last byte with 0x80 followed by zero (null) bytes
                 if datalen > blocksize:
                     k = 0
+                    #if data contains more than two blocks
                     if datalen > 2*blocksize:
+                        #decrypt all the blocks
                         for i in range(0, datalen - 2*blocksize, blocksize):
                             block = data[i:i+blocksize]
+                            #since the data is not padding, append it to
+                            #dec string
                             dec = dec + xtea.xtea_decrypt(self.key,block)
                         k = i + blocksize
+                    #decrypt last two blocks
                     block1 = xtea.xtea_decrypt(self.key,data[k:k+blocksize])
                     block2 = xtea.xtea_decrypt(self.key,data[k+blocksize:datalen])
+                    #if the last block contains magic string
+                    #it indicates that there is no padding for block1
                     dec = dec + block1
                     if block2 == MAGIC_STRING:
                         pad = False
@@ -75,12 +111,14 @@ class Decryptor(Axon.Component.component):
                     block = xtea.xtea_decrypt(self.key,data)
 
                 if pad == True:
+                    #find the position of the last occurence of 0x80
                     rindex = block.rfind(chr(0x80))
                     if rindex != -1:
                         tmp = block[rindex:len(block)]
+                        #if the substring is equal to 0x80 followed null bytes
                         pad = chr(0x80) + (len(block)-rindex-1)*chr(0x00)
                         if(pad == tmp):
-                            #print "remove padding", pad, "padlen", len(pad)
+                            #remove padding
                             block = block[:rindex]
                     dec = dec + block
                 #print "decrypted data ",dec
