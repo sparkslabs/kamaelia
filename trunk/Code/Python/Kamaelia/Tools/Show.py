ba089a4d37183e236452db40360bf984752e2dfe
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2006-08-23 15:10:40 +0000

    Changed use of:     Kamaelia.Util.Graphline
    to:
        Kamaelia.Chassis.Graphline
    
    Michael.

--- trunk/Code/Python/Kamaelia/Tools/Show.py
+++ trunk/Code/Python/Kamaelia/Tools/Show.py
@@ -33,7 +33,7 @@ from Kamaelia.Visualisation.PhysicsGraph.chunks_to_lines import chunks_to_lines
 from Kamaelia.Visualisation.PhysicsGraph.lines_to_tokenlists import lines_to_tokenlists
 from Kamaelia.Visualisation.PhysicsGraph.TopologyViewerComponent import TopologyViewerComponent
 from Kamaelia.Util.Chooser import Chooser
-from Kamaelia.Util.Graphline import Graphline
+from Kamaelia.Chassis.Graphline import Graphline
 from Kamaelia.Util.Pipeline import Pipeline
 from Kamaelia.File.ReadFileAdaptor import ReadFileAdaptor
 from Kamaelia.UI.Pygame.KeyEvent import KeyEvent
