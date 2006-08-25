0b040b14c449947a4c69153b2677e1c852047dea
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2006-08-25 18:32:21 +0000

    Changed Kamaelia.Util.passThrough to a deprecation stub
    Michael.

--- trunk/Code/Python/Kamaelia/Kamaelia/Util/passThrough.py
+++ trunk/Code/Python/Kamaelia/Kamaelia/Util/passThrough.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python2.3
-#
-# (C) 2004 British Broadcasting Corporation and Kamaelia Contributors(1)
+# (C) 2006 British Broadcasting Corporation and Kamaelia Contributors(1)
 #     All Rights Reserved.
 #
 # You may only modify and redistribute this under the terms of any of the
@@ -19,109 +17,17 @@
 # Please contact us via: kamaelia-list-owner@lists.sourceforge.net
 # to discuss alternative licensing.
 # -------------------------------------------------------------------------
+"""\
+This is a deprecation stub, due for later removal.
+"""
 
-from Axon.Component import component
-from Axon.Ipc import producerFinished, shutdownMicroprocess
+import Kamaelia.Support.Deprecate as Deprecate
+from Kamaelia.Util.PassThrough import PassThrough as __PassThrough
 
+Deprecate.deprecationWarning("Use Kamaelia.Util.PassThrough instead of Kamaelia.Util.passThrough")
 
-class passThrough(component):
-   """\
-   """
-   Inboxes= {
-      "inbox" : "",
-      "control" : "",
-   }
-   Outboxes = {
-      "outbox" : "", 
-      "signal" : "",
-   }
-
-   Connections={ "inbox":"outbox","control":"signal" }
-   
-   def __init__(self, shutdownOn = [producerFinished,shutdownMicroprocess]):
-      """\
-      """
-      self.__super.__init__()  # !!!! Must happen, if this method exists
-      self.shutdownOn = shutdownOn
-
-   def mainBody(self):
-      """\
-      """
-      forwarded = 1
-      
-      for (inbox,outbox) in self.Connections.items():
-      
-         while self.dataReady(inbox):
-             forwarded += 1
-             data = self.recv(inbox)
-             self.send(data, outbox)
-             if inbox == "control":
-                for ipc in self.shutdownOn:
-                    if isinstance(data, ipc):
-                        return 0
-                    
-      self.pause()
-      return forwarded
-
-__kamaelia_components__  = ( passThrough, )
-
-      
-if __name__=="__main__":
-    from Axon.Component import scheduler
-    from ConsoleEcho import consoleEchoer
-    
-    class fruitSource(component):
-        def __init__(self):
-            self.outlist = ["apples\n","pears\n","grapes\n","bananas\n","oranges\n","cherrys\n","lemons\n","<end of list>\n"]
-            self.__super.__init__()
-
-        def main(self):
-            for i in self.outlist:
-                self.send(i,"outbox")
-                yield 1
-            self.send(producerFinished(), "signal")
-            yield 1
-
-    
-    class testComponent(component):
-        Inboxes=['_control']
-        Outboxes=['_signal']
-
-        def __init__(self):
-            self.__super.__init__()
-            
-            self.source = fruitSource()
-            self.passT   = passThrough()
-            self.dest   = consoleEchoer()
-            self.addChildren(self.source, self.passT, self.dest)
-            
-            self.link((self.source, "outbox"),  (self.passT, "inbox"))
-            self.link((self.passT,  "outbox"),  (self.dest,  "inbox"))
-            self.link((self.source, "signal"),  (self.passT, "control"))
-            self.link((self.passT , "signal"),  (self,       "_control"))
-            self.link((self,        "_signal"), (self.dest,  "control"))
-
-        def childComponents(self):
-            return [self.source, self.passT, self.dest]
-
-        def main(self):
-            done = False
-            while not done:
-                if self.dataReady("_control"):
-                    data = self.recv("_control")
-                    done = done or isinstance(data, producerFinished) or isinstance(data, shutdownMicroprocess)
-                    self.send(data, "_signal")
-                yield 1
-
-
-    r = scheduler()
-    t = testComponent()
-    t.activate()
-    cs = t.childComponents()
-    for c in cs:
-        c.activate()
-        
-    print "Should output 7 fruit, followed by '<end of list>' then terminate.\n"
-    
-    scheduler.run.runThreads(slowmo=0)
-    
+passThrough = Deprecate.makeClassStub(
+    __PassThrough,
+    "Use Kamaelia.Util.PassThrough:PassThrough instead of Kamaelia.Util.passThrough:passThrough",
+    "WARN"
+    )
