b67b8ab6e0d326bc0f15ac260e125eebb374cd9a
matth_rd <matth_rd@2682807a-6752-0410-967a-793f22d1dfe9>
2006-08-31 17:17:49 +0000

    First tranche of components, in a vaguely kamaelia repository like directory structure.
    Added components to wrap and unwrap raw audio data from 'packaging' that
    describes the format of the audio data.
    
    Added Raw and wrapped sound output components.
    
    Added code to test these (you'll have to find your own wav files)
    
    NOTE: the hard coded paths at the moment.
    
    
    Matt

---
+++ Sketches/MH/pymedia/Audio/PyMedia/Output.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+#
+# (C) 2004 British Broadcasting Corporation and Kamaelia Contributors(1)
+#     All Rights Reserved.
+#
+# You may only modify and redistribute this under the terms of any of the
+# following licenses(2): Mozilla Public License, V1.1, GNU General
+# Public License, V2.0, GNU Lesser General Public License, V2.1
+#
+# (1) Kamaelia Contributors are listed in the AUTHORS file and at
+#     http://kamaelia.sourceforge.net/AUTHORS - please extend this file,
+#     not this notice.
+# (2) Reproduced in the COPYING file, and at:
+#     http://kamaelia.sourceforge.net/COPYING
+# Under section 3.5 of the MPL, we are using this text since we deem the MPL
+# notice inappropriate for this file. As per MPL/GPL/LGPL removal of this
+# notice is prohibited.
+#
+# Please contact us via: kamaelia-list-owner@lists.sourceforge.net
+# to discuss alternative licensing.
+# -------------------------------------------------------------------------
+
+from Axon.Component import component
+from Axon.Ipc import shutdownMicroprocess, producerFinished
+import sys
+sys.path.append("/home/matteh/kamaelia/trunk/Sketches/MH/Timer")
+#from Axon.ThreadedComponent import threadedcomponent
+from ThreadedComponent import threadedcomponent
+
+
+import time
+from math import log
+
+import pymedia.muxer as muxer
+import pymedia.audio.acodec as acodec
+import pymedia.audio.sound as sound
+
+from Support.PyMedia.AudioFormats import mapping_format_to_pymedia
+from Support.PyMedia.AudioFormats import mapping_format_from_pymedia
+
+sys.path.append("/home/matteh/kamaelia/trunk/Sketches/MH/pymedia")
+from Audio.Output import Output as __Output
+
+def Output():
+    return __Output(RawOutput)
+
+
+class RawOutput(threadedcomponent):
+    def __init__(self, sample_rate=44100, channels=2, format="S16_LE"):
+        super(RawOutput,self).__init__()
+        
+        pformat = mapping_format_to_pymedia[format]
+        self.snd = sound.Output(sample_rate, channels, pformat)
+        
+        self.chunksize = sample_rate/40    # no idea why, but it seems we need to pass to pymedia chunks of a sufficiently short duration to prevent playback artefacts
+        mask = 4*channels-1
+        # round to nearest power of 2
+        self.chunksize = 2**int(log(self.chunksize)/log(2))
+        
+    def main(self):
+        CHUNKSIZE=self.chunksize
+        shutdown=False
+        while self.anyReady() or not shutdown:
+            while self.dataReady("inbox"):
+                chunk = self.recv("inbox")
+                
+                for i in range(0,len(chunk),CHUNKSIZE):
+                    self.snd.play(chunk[i:i+CHUNKSIZE])
+            
+            while self.dataReady("control"):
+                msg=self.recv("control")
+                if isinstance(msg, (producerFinished,shutdownMicroprocess)):
+                    shutdown=True
+                self.send(msg,"signal")
+                
+            if not shutdown:
+                self.pause()
+            
+        self.snd.stop()
