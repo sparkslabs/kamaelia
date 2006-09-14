4bcc6e2806b1c969cadd754cd22994ee521d4bb1
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2006-09-14 23:49:25 +0000

    Cleanups and notes.
    Michael.

--- trunk/Sketches/MPS/Compose/Compose/BuildViewer.py
+++ trunk/Sketches/MPS/Compose/Compose/BuildViewer.py
@@ -32,6 +32,9 @@ from Kamaelia.Visualisation.Axon.ExtraWindowFurniture import ExtraWindowFurnitur
 
 import time
 
+#
+# FIXME: How does this relate to the normal particle?
+#
 class ComponentParticle(Particle):
     """Version of Physics.Particle designed to represent components in a simple pipeline"""
 
@@ -40,29 +43,29 @@ class ComponentParticle(Particle):
         self.radius = 20
         self.labelText = name   # strip up to the first pipe only
         self.name = name
-        
+
         pygame.font.init()
         font = pygame.font.Font(None, 24)
         self.label = font.render(self.labelText, False, (0,0,0))
         self.left = 0
         self.top  = 0
         self.selected = False
-       
+
     def render(self, surface):
         """Rendering passes. A generator method that renders in multiple passes.
         Use yields to specify a wait until the pass the next stage of rendering
         should take place at.
-        
+
         Example, that renders bonds 'behind' the blobs.
             def render(self, surface):
                 yield 1
                 self.renderBonds(surface)        # render bonds on pass 1
                 yield 5 
                 self.renderSelf(surface)         # render 'blob' on pass 5
-        
+
          If another particle type rendered, for example, on pass 3, then it
          would be rendered on top of the bonds, but behind the blobs.
-         
+
          Use this mechanism to order rendering into layers.
          """
         sx = int(self.pos[0]) - self.left
@@ -101,15 +104,15 @@ class ComponentParticle(Particle):
 
         
         yield 2
-        
+
         if self.selected:
             pygame.draw.circle(surface, (255,255,128), (sx,sy), self.radius)
         else:
             pygame.draw.circle(surface, (192,192,192), (sx,sy), self.radius)
-            
+
         surface.blit(self.label, (sx - self.label.get_width()/2, sy - self.label.get_height()/2))
-        
-        
+
+
     def setOffset( self, (left,top) ):
         """Inform of a change to the coords of the top left of the drawing surface,
         so that this entity can render, as if the top left had moved
@@ -117,7 +120,6 @@ class ComponentParticle(Particle):
         self.left = left
         self.top  = top
 
-        
     def select( self ):
         """Tell this particle it is selected"""
         self.selected = True
@@ -126,12 +128,12 @@ class ComponentParticle(Particle):
         """Tell this particle it is selected"""
         self.selected = False
 
-def BuildViewer(screensize = (800,600), fullscreen = False, transparency = None):                                        
-#    laws = SimpleLaws(bondLength=100)
+def BuildViewer(screensize = (800,600), fullscreen = False, transparency = None):
+    laws = SimpleLaws(bondLength=100)
     return AxonVisualiser( screensize=screensize,
                            fullscreen=fullscreen,
                            caption = "The pipeline",
-#                           particleTypes = {"component":ComponentParticle},
+                           particleTypes = {"component":ComponentParticle},
 #                           extraDrawing = ExtraWindowFurniture(),
-#                           laws = laws
+                            laws = laws
                          )
