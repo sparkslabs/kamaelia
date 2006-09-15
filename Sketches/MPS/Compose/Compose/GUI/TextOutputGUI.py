73eba4fd849dd642404ac36bf28e0c3ad8202612
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2006-09-15 15:38:38 +0000

    Added a simple example using this component. This could actually be a very useful example component for a variety of reasons.
    Michael.

--- Sketches/MPS/Compose/Compose/GUI/TextOutputGUI.py
+++ Sketches/MPS/Compose/Compose/GUI/TextOutputGUI.py
@@ -75,3 +75,46 @@ class TextOutputGUI(TkWindow):
     def handleCloseWindowRequest(self):
         self.send( shutdownMicroprocess(self), "signal")
         self.window.destroy()
+
+if __name__ == "__main__":
+    from Kamaelia.Chassis.Pipeline import Pipeline
+    import Axon
+    import time
+    class Source(Axon.Component.component):
+        "A simple data source"
+        def __init__(self, data=None):
+            super(Source, self).__init__()
+            if data == None: data = []
+            self.data = data
+
+        def main(self):
+            for item in iter(self.data):
+                self.send(item, "outbox")
+                yield 1
+
+    class TimedPassthrough(Axon.ThreadedComponent.threadedcomponent):
+        def __init__(self, delay=1):
+            super(TimedPassthrough, self).__init__()
+            self.delay = delay
+        def main(self):
+            while 1:
+                 time.sleep(self.delay)
+                 if self.dataReady("inbox"):
+                     data = self.recv("inbox")
+                     self.send(data, "outbox")
+
+    Pipeline(
+        Source(["H\n", None, # None says "We're done. Anything you recieve after this replaces this text"
+                "Hel\n", None,
+                "Hello \n", None,
+                "Hello Wo\n", None,
+                "Hello World\n",
+        ]),
+        TimedPassthrough(0.1),
+        TextOutputGUI("Basic Display"),
+    ).run()
+
+
+
+
+
