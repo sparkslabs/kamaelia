417d22ebf3882ec736209960395685a4f3a1bc18
matth_rd <matth_rd@2682807a-6752-0410-967a-793f22d1dfe9>
2006-09-15 11:49:48 +0000

    Added some docs and renamed "Service" to "RegisterService" ... a better description of what it actually does.
    Matt

--- Sketches/MH/ServiceWrapper.py
+++ Sketches/MH/ServiceWrapper.py
@@ -1,15 +1,126 @@
 #!/usr/bin/env python
 
-# service wrapper
-# wrapper that turns something into a service
+# (C) 2006 British Broadcasting Corporation and Kamaelia Contributors(1)
+#     All Rights Reserved.
+#
+# You may only modify and redistribute this under the terms of any of the
+# following licenses(2): Mozilla Public License, V1.1, GNU General
+# Public License, V2.0, GNU Lesser General Public License, V2.1
+#
+# (1) Kamaelia Contributors are listed in the AUTHORS file and at
+#     http://kamaelia.sourceforge.net/AUTHORS - please extend this file,
+#     not this notice.
+# (2) Reproduced in the COPYING file, and at:
+#     http://kamaelia.sourceforge.net/COPYING
+# Under section 3.5 of the MPL, we are using this text since we deem the MPL
+# notice inappropriate for this file. As per MPL/GPL/LGPL removal of this
+# notice is prohibited.
+#
+# Please contact us via: kamaelia-list-owner@lists.sourceforge.net
+# to discuss alternative licensing.
+# -------------------------------------------------------------------------
+"""\
+================================================
+Components to help build Services (EXPERIMENTAL)
+================================================
 
+These components make it easier to build and use public services, registered
+with the Coordinating Assistant Tracker.
+
+Note: These components are EXPERIMENTAL and are likely to under go substantial
+change
+
+
+Register Service
+----------------
+
+A function that registers specified inboxes on a component as named services
+with the Coordinating Assistant Tracker (CAT). Returns the component, so can
+be dropped in where you would ordinarily use a component.
+
+
+
+Example Usage:
+~~~~~~~~~~~~~~
+
+Create and activate MyComponent instance, registering its "inbox" inbox with the
+CAT as a service called "MyService"::
+    
+    RegisterService( MyComponent(), {"MyService":"inbox"} ).activate()
+    
+    
+
+How does it work?
+~~~~~~~~~~~~~~~~~
+
+This method registers the component you provide with the CAT. It register the
+inboxes on it that you specify using the names that you specify.
+
+
+
+Subscibe To Service
+-------------------
+
+A component that connects to a public service and sends a fixed format message
+to it requesting to subscribe to a set of 'things' it provides. 
+
+
+
+Example Usage:
+~~~~~~~~~~~~~~
+
+Subscribe to a (fictional) "TV Channels Service", asking for three channels. The
+tv channel data is then recorded::
+    
+    pipeline(
+        SubscribeTo( "TV Channels Service", ["BBC ONE", "BBC TWO", "ITV"] ),
+        RecordChannels(),
+        ).run()
+
+The message sent to the "TV Channels Service" will be::
+    
+    ("ADD", ["BBC ONE", "BBC TWO", "ITV"], ( <theSubscribeToComponent>, "inbox" ) )
+
+
+
+How does it work?
+~~~~~~~~~~~~~~~~~
+
+Describe more detail here
+
+
+
+
+Connect To Service
+------------------
+
+A component that connects to a public service. Any data you send to its inbox
+gets sent to the service.
+
+
+
+Example Usage
+~~~~~~~~~~~~~
+::
+    
+    pipeline( MyComponentThatSendMessagesToService(),
+            ConnectTo("Name of service"),
+            ).run()
+
+
+
+How does it work?
+~~~~~~~~~~~~~~~~~
+
+Describe in more detail here.
+"""
 
 from Axon.AxonExceptions import ServiceAlreadyExists
 from Axon.CoordinatingAssistantTracker import coordinatingassistanttracker as CAT
 from Axon.Component import component
 from Axon.Ipc import shutdownMicroprocess, producerFinished
 
-def Service(component, services):
+def RegisterService(component, services):
     cat = CAT.getcat()
     for (name, boxname) in services.items():
         
@@ -138,15 +249,15 @@ if __name__ == "__main__":
                 yield 1
                 
     from Kamaelia.Util.Console import ConsoleEchoer
-    from Kamaelia.Chassis.Pipeline import pipeline
+    from Kamaelia.Chassis.Pipeline import Pipeline
     
-    pipeline( Subscribe("SERVICE1", 5),
+    Pipeline( Subscribe("SERVICE1", 5),
               ConsoleEchoer(),
             ).activate()
     
-    pipeline( Subscribe("SERVICE1", 1),
+    Pipeline( Subscribe("SERVICE1", 1),
               ConsoleEchoer(),
             ).activate()
     
-    Service(DummyService(), {"SERVICE1":"inbox"}).run()
+    RegisterService(DummyService(), {"SERVICE1":"inbox"}).run()
     
