93df3a6baa9ee5ed69d990291977b58e490550fb
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2006-09-19 10:59:27 +0000

    Fixed imports for sitting in repository.
    Michael.

--- Code/Python/Kamaelia/Kamaelia/Audio/Codec/PyMedia/Decoder.py
+++ Code/Python/Kamaelia/Kamaelia/Audio/Codec/PyMedia/Decoder.py
@@ -28,9 +28,9 @@ import pymedia.audio.acodec as acodec
 import pymedia.audio.sound as sound
 
 import sys,os
-sys.path.append(__file__[:1+__file__.rfind(os.sep)] + (".."+os.sep)*3)
-from Support.PyMedia.AudioFormats import codec2fileExt
-from Support.PyMedia.AudioFormats import pyMediaFormat2format
+# sys.path.append(__file__[:1+__file__.rfind(os.sep)] + (".."+os.sep)*3)
+from Kamaelia.Support.PyMedia.AudioFormats import codec2fileExt
+from Kamaelia.Support.PyMedia.AudioFormats import pyMediaFormat2format
 
 
 class Decoder(component):
