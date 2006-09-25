ef956ec77511d73a3a52b9ca30f39c3e5be8d358
matth_rd <matth_rd@2682807a-6752-0410-967a-793f22d1dfe9>
2006-09-25 15:51:15 +0000

    Bugfix (to put in release): DVB CRC checking algorithm broken - now fixed.
    Result of crc check was negated! Meaning valid data was marked as
    not matching! Doh! This is sufficiently significant a bug that I'd strongly
    suggest this fix should be put into the imminent Kamaelia 0.5 release.
    
    Matt

--- Code/Python/Kamaelia/Kamaelia/Support/DVB/CRC.py
+++ Code/Python/Kamaelia/Kamaelia/Support/DVB/CRC.py
@@ -23,23 +23,61 @@
 CRC algorithm used to verify the integrity of data in DVB transport streams.
 """
 
-# old slow CRC algorithm. 
-# ought to recode a faster algorithm sometime soon
+# # old slow CRC algorithm. 
+# # ought to recode a faster algorithm sometime soon
+# 
+# def crc32(data):
+#     poly = 0x4c11db7
+#     crc = 0xffffffff
+#     for byte in data:
+#         byte = ord(byte)
+#         for bit in range(7,-1,-1):  # MSB to LSB
+#             z32 = crc>>31    # top bit
+#             crc = crc << 1
+#             if ((byte>>bit)&1) ^ z32:
+#                 crc = crc ^ poly
+#             crc = crc & 0xffffffff
+#     return crc
+# 
+# def dvbcrc(data):
+#     return not crc32(data)
 
-
-def crc32(data):
-    poly = 0x4c11db7
-    crc = 0xffffffff
-    for byte in data:
-        byte = ord(byte)
+def __MakeCRC32(polynomial = 0x4c11db7,initial=0xffffffff):
+    """\
+    MakeCRC32([polynomial][,inital]) -> (string -> 32bit CRC of binary string data)
+    
+    Returns a function that calculatees the 32 bit CRC of binary data in a
+    string, using the specified CRC polynomial and initial value.
+    """
+    
+    # precalculate the effect on the CRC of processing a byte of data
+    # create a table of values to xor by, indexed by
+    # new_byte_of_data xor most-sig-byte of current CRC
+    xorvals = []
+    for x in range(0,256):   # x is the result of top byte of crc xored with new data byte
+        crc = x<<24
         for bit in range(7,-1,-1):  # MSB to LSB
             z32 = crc>>31    # top bit
             crc = crc << 1
-            if ((byte>>bit)&1) ^ z32:
-                crc = crc ^ poly
+            if z32:
+                crc = crc ^ polynomial
             crc = crc & 0xffffffff
-    return crc
+        xorvals.append(crc & 0xffffffff)   # only interested in bottom 24 bits
+    
+    # define the function that will do the crc, using the table we've just
+    # precalculated.
+    def fastcrc32(data):
+        crc = 0xffffffff
+        for byte in data:
+            byte = ord(byte)
+            xv = xorvals[byte ^ (crc>>24)]
+            crc = xv ^ ((crc & 0xffffff)<<8)
+        return crc
 
-def dvbcrc(data):
-    return not crc32(data)
+    return fastcrc32
+
+__dvbcrc = __MakeCRC32(polynomial = 0x04c11db7)
+
+dvbcrc = lambda data : not __dvbcrc(data)
+
 
