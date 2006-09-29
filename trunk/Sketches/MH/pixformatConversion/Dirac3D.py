f137460a21bd0ce6c57b44cfdef8a62ef181d448
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2006-09-29 23:28:13 +0000

    Now rotating the surface playing back the dirac video in 3D. This is mad crazy :-) Screenshot: http://kamaelia.sourceforge.net/t/Dirac3D.png
    Michael.

--- trunk/Sketches/MH/pixformatConversion/Dirac3D.py
+++ trunk/Sketches/MH/pixformatConversion/Dirac3D.py
@@ -9,16 +9,21 @@ from VideoSurface import VideoSurface, YUVtoRGB
 from Kamaelia.UI.PygameDisplay import PygameDisplay
 from Kamaelia.UI.OpenGL.OpenGLDisplay import OpenGLDisplay
 from Kamaelia.UI.OpenGL.PygameWrapper import PygameWrapper
+from Kamaelia.UI.OpenGL.SkyGrassBackground import SkyGrassBackground
+from Kamaelia.UI.OpenGL.Movement import SimpleRotator
 
 file = "../../../Code/Python/Kamaelia/Examples/VideoCodecs/Dirac/snowboard-jum-352x288x75.dirac.drc"
-framerate = 15
+framerate = 3
 
 # override pygame display service
 ogl_display = OpenGLDisplay.getDisplayService()
 PygameDisplay.setDisplayService(ogl_display[0])
 
+SkyGrassBackground(size=(5000,5000,0), position=(0,0,-100)).activate()
 screen = VideoSurface()
 screen__in_scene = PygameWrapper(wrap=screen, position=(0, 0,-5), rotation=(-30,15,3)).activate()
+rotator = SimpleRotator(amount=(0.0,0.0,0.5)).activate()
+rotator.link((rotator,"outbox"), (screen__in_scene,"rel_rotation"))
 
 Pipeline(
          ReadFileAdaptor(file, readmode="bitrate",
