395fac8bf6dac51327998ff8bd8a507f597fe042
matth_rd <matth_rd@2682807a-6752-0410-967a-793f22d1dfe9>
2006-11-23 15:07:09 +0000

    Upgraded to sensibly handle noSpaceInBox exceptions.
    Matt

--- trunk/Sketches/MH/Video/CropAndScale.py
+++ trunk/Sketches/MH/Video/CropAndScale.py
@@ -3,6 +3,7 @@
 
 from Axon.Component import component
 from Axon.Ipc import producerFinished, shutdownMicroprocess
+from Axon.AxonExceptions import noSpaceInBox
 
 import Image
 
@@ -14,25 +15,57 @@ class CropAndScale(component):
         self.newsize = newsize
         self.cropbounds = cropbounds[0], cropbounds[3], cropbounds[2], cropbounds[1]
 
-    def main(self):
-        i=0
+    def handleControl(self):
+        while self.dataReady("control"):
+            msg = self.recv("control")
+            if isinstance(msg, producerFinished) and not isinstance(self.shutdownMsg, shutdownMicroprocess):
+                self.shutdownMsg = msg
+            elif isinstance(msg, shutdownMicroprocess):
+                self.shutdownMsg = msg
+
+    def canStop(self):
+        self.handleControl()
+        return isinstance(self.shutdownMsg, (producerFinished,shutdownMicroprocess))
+
+    def mustStop(self):
+        self.handleControl()
+        return isinstance(self.shutdownMsg, shutdownMicroprocess)
+    
+    def waitSend(self,data,boxname):
         while 1:
-            while self.dataReady("inbox"):
-                frame = self.recv("inbox")
-                newframe = self.processFrame(frame)
-                self.send(newframe, "outbox")
-                print i
-                i+=1
+            try:
+                self.send(data,boxname)
+                return
+            except noSpaceInBox:
+                if self.mustStop():
+                    raise "STOP"
+                
+                self.pause()
+                yield 1
+                
+                if self.mustStop():
+                    raise "STOP"
 
-            while self.dataReady("control"):
-                msg = self.recv("control")
-                self.send(msg, "signal")
-                if isinstance(msg, (producerFinished, shutdownMicroprocess)):
-                    return
-
-            self.pause()
-            yield 1
-
+    def main(self):
+        self.shutdownMsg = None
+        i=0
+        try:
+            while 1:
+                while self.dataReady("inbox"):
+                    frame = self.recv("inbox")
+                    newframe = self.processFrame(frame)
+                    for _ in self.waitSend(newframe, "outbox"):
+                        yield _
+                    print i
+                    i+=1
+    
+                if self.canStop():
+                    raise "STOP"
+    
+                self.pause()
+                yield 1
+        except "STOP":
+            self.send(self.shutdownMsg,"signal")
 
 
     def processFrame(self, frame):
