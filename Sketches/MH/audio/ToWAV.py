5d8d80f44138d7676317ed2591129555b69e9e5b
matth_rd <matth_rd@2682807a-6752-0410-967a-793f22d1dfe9>
2006-11-23 15:02:48 +0000

    Added ability to handle noSpaceInBox exceptions.
    Matt

--- Sketches/MH/audio/ToWAV.py
+++ Sketches/MH/audio/ToWAV.py
@@ -4,6 +4,7 @@ from Axon.Component import component
 import string
 import struct
 from Axon.Ipc import producerFinished, shutdownMicroprocess
+from Axon.AxonExceptions import noSpaceInBox
 
 # based on ryan's PCMToWave component, modified to match WAVParser's formats for
 # conveying samplerate, channels etc
@@ -25,42 +26,76 @@ class WavWriter(component):
         self.samplingfrequency = sample_rate
         self.channels = channels
         
-    def main(self):
-        #we don't know the length yet, so we say the file lasts an arbitrary (long) time 
-        riffchunk = "RIFF" + struct.pack("<L", 0x0) + "WAVE"
-        
-        bytespersecond = self.bytespersample * self.channels * self.samplingfrequency
-        
-        formatchunk = "fmt "
-        formatchunk += struct.pack("<L", self.bitsPerSample)
-        formatchunk += struct.pack("<H", 0x01) #PCM/Linear quantization
-        formatchunk += struct.pack("<H", self.channels) 
-        formatchunk += struct.pack("<L", self.samplingfrequency)
-        formatchunk += struct.pack("<L", bytespersecond)
-        formatchunk += struct.pack("<H", self.bytespersample * self.channels)
-        formatchunk += struct.pack("<H", self.bitsPerSample)
-    
-        self.send(riffchunk, "outbox")
-        self.send(formatchunk, "outbox")
-        datachunkheader = "data" + struct.pack("<L", 0x0) #again, an arbitrary (large) value
-        self.send(datachunkheader, "outbox")
-        
-        running = True
-        while running:
-            yield 1
-            
-            while self.dataReady("inbox"): # we accept binary sample data in strings
-                sampledata = self.recv("inbox")
-                self.send(sampledata, "outbox")
-                
-            while self.dataReady("control"):
-                msg = self.recv("control")
-                if isinstance(msg, (producerFinished,shutdownMicroprocess)):
-                    self.send(msg,"signal")
-                    return
-                    
-            self.pause()
+    def handleControl(self):
+        while self.dataReady("control"):
+            msg = self.recv("control")
+            if isinstance(msg, producerFinished) and not isinstance(self.shutdownMsg, shutdownMicroprocess):
+                self.shutdownMsg = msg
+            elif isinstance(msg, shutdownMicroprocess):
+                self.shutdownMsg = msg
 
+    def canStop(self):
+        self.handleControl()
+        return isinstance(self.shutdownMsg, (producerFinished,shutdownMicroprocess))
+
+    def mustStop(self):
+        self.handleControl()
+        return isinstance(self.shutdownMsg, shutdownMicroprocess)
+    
+    def waitSend(self,data,boxname):
+        while 1:
+            try:
+                self.send(data,boxname)
+                return
+            except noSpaceInBox:
+                if self.mustStop():
+                    raise "STOP"
+                
+                self.pause()
+                yield 1
+                
+                if self.mustStop():
+                    raise "STOP"
+
+    def main(self):
+        self.shutdownMsg=None
+        
+        try:
+            #we don't know the length yet, so we say the file lasts an arbitrary (long) time 
+            riffchunk = "RIFF" + struct.pack("<L", 0x0) + "WAVE"
+            
+            bytespersecond = self.bytespersample * self.channels * self.samplingfrequency
+            
+            formatchunk = "fmt "
+            formatchunk += struct.pack("<L", self.bitsPerSample)
+            formatchunk += struct.pack("<H", 0x01) #PCM/Linear quantization
+            formatchunk += struct.pack("<H", self.channels) 
+            formatchunk += struct.pack("<L", self.samplingfrequency)
+            formatchunk += struct.pack("<L", bytespersecond)
+            formatchunk += struct.pack("<H", self.bytespersample * self.channels)
+            formatchunk += struct.pack("<H", self.bitsPerSample)
+        
+            datachunkheader = "data" + struct.pack("<L", 0x0) #again, an arbitrary (large) value
+            
+            for _ in self.waitSend(riffchunk + formatchunk + datachunkheader, "outbox"):
+                yield 1
+            
+            running = True
+            while running:
+                yield 1
+                
+                while self.dataReady("inbox"): # we accept binary sample data in strings
+                    sampledata = self.recv("inbox")
+                    for _ in self.waitSend(sampledata, "outbox"):
+                        yield 1
+                    
+                if self.canStop():
+                    raise "STOP"
+                        
+                self.pause()
+
+        except "STOP":
+            self.send(self.shutdownMsg,"signal")
 
 if __name__=="__main__":
     from Kamaelia.Chassis.Graphline import Graphline
