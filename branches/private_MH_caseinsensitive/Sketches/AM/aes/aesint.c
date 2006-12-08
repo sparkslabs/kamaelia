4f76523dbc6c988d45925a0db581cea59d9f33a2
matth_rd <matth_rd@2682807a-6752-0410-967a-793f22d1dfe9>
2006-12-08 13:02:31 +0000

    Removed this branch as its no longer needed - use trunk instead!
    Matt

--- branches/private_MH_caseinsensitive/Sketches/AM/aes/aesint.c
+++
@@ -1,8 +0,0 @@
-# include "aesint.h"
-
-int encrypt_aes(char* infile, char* outfile, char* enckey) {
-	return aes_enc(infile, outfile, enckey);
-}
-int decrypt_aes(char* infile, char* outfile, char* deckey) {
-	return aes_dec(infile, outfile, deckey);
-}
--- branches/private_MH_caseinsensitive/Sketches/MPS/Old/foo.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-
-# created merely to see if notification works!
-
-print "game over"# testing
-# testing
