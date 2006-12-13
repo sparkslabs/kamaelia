73974bbf4b05f160450628f94c9947bb27b45b0a
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2006-12-13 19:37:23 +0000

    Interesting little test case that causes *AXON* to crash. As a result checked in.
    The error thrown is this:
    
      File "/usr/local/lib/python2.4/site-packages/Axon/Component.py", line
    237, in __addChild
        child._callOnCloseDown.append(self.unpause)
    AttributeError: type object 'ExampleWrapper' has no attribute
    '_callOnCloseDown'
    
    Now, given this happens *with* or *without* a correctly initialising
    __init__ method in ExampleWrapper, this is particularly intriguing
    since it can be easily demonstrated that this attribute *DID* exist
    at some point :-)
    
    
    Michael.

---
+++ trunk/Sketches/MPS/HTTP/michael.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python
+
+import socket
+import Axon
+
+from Kamaelia.Chassis.ConnectedServer import SimpleServer
+from Kamaelia.Protocol.HTTP.HTTPServer import HTTPServer
+from Kamaelia.Protocol.HTTP.Handlers.Minimal import Minimal
+import Kamaelia.Protocol.HTTP.ErrorPages as ErrorPages
+
+homedirectory = "/media/usbdisk/SF/hotdocs/"
+indexfilename = "index.html"
+
+def requestHandlers(URLHandlers):
+    def createRequestHandler(request):
+        if request.get("bad"):
+            return ErrorPages.websiteErrorPage(400, request.get("errormsg",""))
+        else:
+            for (prefix, handler) in URLHandlers:
+                if request["raw-uri"][:len(prefix)] == prefix:
+                    request["uri-prefix-trigger"] = prefix
+                    request["uri-suffix"] = request["raw-uri"][len(prefix):]
+                    return handler(request)
+
+        return ErrorPages.websiteErrorPage(404, "No resource handlers could be found for the requested URL.")
+
+    return createRequestHandler
+
+class HelloHandler(Axon.Component.component):
+    def __init__(self, request):
+        super(HelloHandler, self).__init__()
+        self.request = request
+
+    def main(self):
+        resource = {
+           "type"           : "text/html",
+           "statuscode"     : "200",
+           "length": len("<html><body><h1>Hello World</h1><P> Game Over!! </body></html>"),
+        }
+        self.send(resource, "outbox"); yield 1
+        page = {
+          "data" : "<html><body><h1>Hello World</h1><P> Game Over!! </body></html>"
+        }
+        self.send(page, "outbox"); yield 1
+        self.send(Axon.Ipc.producerFinished(self), "signal")        
+        yield 1
+
+class Cat(Axon.Component.component):
+    def __init__(self, *args):
+        super(Cat, self).__init__()
+        self.args = args
+    def main(self):
+        self.send(args, "outbox")
+        self.send(Axon.Ipc.producerFinished(self), "signal")        
+        yield 1
+
+class ExampleWrapper(Axon.Component.component):
+    def __init__(self):
+        super(ExampleWrapper, self).__init__()
+ 
+   def main(self):
+        # Tell the browser the type of data we're sending!
+        resource = {
+           "type"           : "text/html",
+           "statuscode"     : "200",
+        }
+        self.send(resource, "outbox"); yield 1
+
+        # Send the header 
+        header = {
+          "data" : "<html><body>"
+        }
+        self.send(header, "outbox"); yield 1
+
+        # Wait for it....
+        while not self.dataReady("inbox"):
+#            self.pause()
+            yield 1
+
+        # Send the data we recieve as the page body
+        while self.dataReady("inbox"):
+            pageData = {
+               "data" : str(self.recv("inbox"))
+            }
+            self.send(pageData, "outbox"); yield 1
+
+        # send a footer
+        footer = {
+          "data" : "</body></html>"
+        }
+        self.send(footer, "outbox"); yield 1
+
+        # and shutdown nicely
+        self.send(Axon.Ipc.producerFinished(self), "signal")        
+        yield 1
+
+from Kamaelia.Chassis.Pipeline import Pipeline
+
+def OurHandler(request):
+    return Pipeline ( Cat(request), ExampleWrapper)
+
+
+
+def servePage(request):
+    return Minimal(request=request, 
+                   homedirectory=homedirectory, 
+                   indexfilename=indexfilename)
+
+def HTTPProtocol():
+    return HTTPServer(requestHandlers([
+                         ["/echo", OurHandler ],
+                         ["/hello", HelloHandler ],
+                         ["/", servePage ],
+                      ]))
+
+SimpleServer(protocol=HTTPProtocol,
+             port=8082,
+             socketOptions=(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  ).run()
+
+
+
+
+#        header = {
+#          "data" : "<html><body>"
+#        }
+#        body = {
+#          "data" : "<h1>Hello World</h1><P> Game Over!! "
+#        }
+#        footer = {
+#          "data" : "</body></html>"
+#        }
+#        self.send(header, "outbox"); yield 1
+#        self.send(body  , "outbox"); yield 1
+#        self.send(body  , "outbox"); yield 1
+#        self.send(footer, "outbox"); yield 1
