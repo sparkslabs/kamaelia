56e1db941569b4f7930f60e74a4d2dae940a4962
matth_rd <matth_rd@2682807a-6752-0410-967a-793f22d1dfe9>
2007-02-27 13:54:17 +0000

    Harmonised plain text and html renderers. Abstracted anchor and link generation into renderers. Abstracted file extensions.
    Matt

--- trunk/Sketches/MH/DocGen/renderPlaintext.py
+++ trunk/Sketches/MH/DocGen/renderPlaintext.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 
 class RenderPlaintext(object):
+    
+    extension = ".text"
+    
     def __init__(self, debug=False):
         super(RenderPlaintext,self).__init__()
         self.debug=debug
+
+    def itemList(self, items):
+        result = []
+        for item in items:
+            result.append("   "+ str(item[0])+ " : "+ str(item[1]))
+        return "\n  * ".join(result)+"\n"
 
     def heading(self, label, level=4):
         if level == 2: 
@@ -21,5 +30,14 @@ class RenderPlaintext(object):
         return "".join(L)
     def divider(self):
         return "\n"
+    def hardDivider(self):
+        return ("="*79)+"\n"
+    
+    def setAnchor(self,name):
+        return ''
+    
+    def linkToAnchor(self,name,text):
+        return text+' (see: '+name+')'
+    
     def start(self): return ""
     def stop(self): return ""
