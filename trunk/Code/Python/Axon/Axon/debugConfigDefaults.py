6a8f620c4db5683f64d7ae3ae5b655cffc5e97ef
matth_rd <matth_rd@2682807a-6752-0410-967a-793f22d1dfe9>
2007-04-01 11:49:20 +0000

    Added documentation strings
    Matt

--- trunk/Code/Python/Axon/Axon/debugConfigDefaults.py
+++ trunk/Code/Python/Axon/Axon/debugConfigDefaults.py
@@ -1,4 +1,34 @@
-#!/usr/bin/python
+#!/usr/bin/env python2.3
+#
+# (C) 2004 British Broadcasting Corporation and Kamaelia Contributors(1)
+#     All Rights Reserved.
+#
+# You may only modify and redistribute this under the terms of any of the
+# following licenses(2): Mozilla Public License, V1.1, GNU General
+# Public License, V2.0, GNU Lesser General Public License, V2.1
+#
+# (1) Kamaelia Contributors are listed in the AUTHORS file and at
+#     http://kamaelia.sourceforge.net/AUTHORS - please extend this file,
+#     not this notice.
+# (2) Reproduced in the COPYING file, and at:
+#     http://kamaelia.sourceforge.net/COPYING
+# Under section 3.5 of the MPL, we are using this text since we deem the MPL
+# notice inappropriate for this file. As per MPL/GPL/LGPL removal of this
+# notice is prohibited.
+#
+# Please contact us via: kamaelia-list-owner@lists.sourceforge.net
+# to discuss alternative licensing.
+# -------------------------------------------------------------------------
+"""\
+===============================
+Default debugging configuration
+===============================
+
+This file defines default configuration used by Axon.debug.debug when it cannot
+find a configuration file to load.
+
+The defaultConfig() method returns the default configuration.
+"""
 
 _tags = """debugTestClass.even
 debugTestClass.triple
@@ -83,6 +113,10 @@ ConnectedSocketAdapter.mainBody
 """
 
 def defaultConfig():
+   """\
+   Returns a default debugging configuration - a dictionary mapping section
+   names to (level, location) tuples
+   """
    debugConfig = {}
    for tag in _tags.split("\n"):
       debugConfig[tag] = (0,"default")
