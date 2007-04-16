5b6fac142c8eac985a82300f15a4934344d642f5
matth_rd <matth_rd@2682807a-6752-0410-967a-793f22d1dfe9>
2007-04-16 15:15:37 +0000

    Added documentation strings
    Matt

--- Sketches/MH/MobileReframe/MaxSpeedFileReader.py
+++ Sketches/MH/MobileReframe/MaxSpeedFileReader.py
@@ -19,12 +19,77 @@
 # Please contact us via: kamaelia-list-owner@lists.sourceforge.net
 # to discuss alternative licensing.
 # -------------------------------------------------------------------------
-#
+"""\
+==================================
+Reading a file as fast as possible
+==================================
+
+MaxSpeedFileReader reads a file in bytes mode as fast as it can; limited only
+by any size limit on the inbox it is sending the data to.
+
+This component is therefore useful for building systems that are self rate
+limiting - systems that are just trying to process data as fast as they can and
+are limited by the speed of the slowest part of the chain.
+
+
+
+Example Usage
+-------------
+
+Read "myfile" in in chunks of 1024 bytes. The rate is limited by the rate at
+which the consumer component can consume the chunks, since its inbox has a size
+limit of 5 items of data::
+
+    consumer = Consumer()
+    consumer.inboxes["inbox"].setSize(5)
+    
+    Pipeline( MaxSpeedFileReader("myfile", chunksize=1024),
+              consumer,
+            ).run()
+
+
+
+More details
+------------
+
+Specify a filename and chunksize and MaxSpeedFileReader will read bytes from
+the file in the chunksize you specified and send them out of its "outbox"
+outbox.
+
+If the destination inbox it is sending chunks to is size limited, then
+MaxSpeedFileReader will pause until space becomes available. This is how the
+speed at which the file is ingested is regulated - by the rate at which it is
+consumed.
+
+When the whole file has been read, this component will terminate and send a
+producerFinished() message out of its "signal" outbox.
+
+If a producerFinished message is received on the "control" inbox, this component
+will complete sending any data that may be waiting. It will then send the
+producerFinished message on out of its "signal" outbox and terminate.
+
+If a shutdownMicroprocess message is received on the "control" inbox, this
+component will immediately send it on out of its "signal" outbox and immediately
+terminate. It will not complete sending on any pending data.
+
+"""
 
 from Axon.Component import component
 from Axon.Ipc import producerFinished, shutdownMicroprocess
 
 class MaxSpeedFileReader(component):
+    """\
+    MaxSpeedFileReader(filename[,chunksize]) -> new MaxSpeedFileReader component.
+
+    Reads the contents of a file in bytes mode; sending it out as fast as it can
+    in chunks from the "outbox" outbox. The rate of reading is only limited by
+    any size limit of the destination inbox to which the data is being sent.
+    
+    Keyword arguments:
+
+    - filename   -- The filename of the file to read
+    - chunksize  -- Optional. The maximum number of bytes in each chunk of data read from the file and sent out of the "outbox" outbox (default=32768)
+    """
 
     def __init__(self, filename, chunksize=32768):
         super(MaxSpeedFileReader,self).__init__()
