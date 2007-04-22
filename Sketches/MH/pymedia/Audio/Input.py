58e35fc68cac8fe712fb754e591f056ce7023524
matth_rd <matth_rd@2682807a-6752-0410-967a-793f22d1dfe9>
2007-04-22 19:19:39 +0000

    Bugfixes to pymedia audio capture. Hopefully merge into main codebase soon.
    Wasn't dealing with:
    * when None is returned instead of audio data
    * handling noSpaceInBox exceptions - esp. important as its a threaded component now
    
    Matt

--- Sketches/MH/pymedia/Audio/Input.py
+++ Sketches/MH/pymedia/Audio/Input.py
@@ -19,9 +19,116 @@
 # Please contact us via: kamaelia-list-owner@lists.sourceforge.net
 # to discuss alternative licensing.
 # -------------------------------------------------------------------------
+"""\
+===========================
+Audio Capture using PyMedia
+===========================
+
+This component captures raw audio using the pymedia library, and outputs it
+out of its "outbox" outbox.
+
+
+
+Example Usage
+-------------
+
+Recording telephone quality audio to a file::
+    
+    Pipeline( Input(sample_rate=8000, channels=1, format="S16_LE"),
+              SimpleFileWriter("recording.raw"),
+            ).run()
+
+
+
+How does it work?
+-----------------
+
+Input uses the PyMedia library to capture audio from the currently selected
+audio capture device.
+
+It outputs to its "outbox" outbox raw binary audio data in strings.
+
+This component will terminate if a shutdownMicroprocess or producerFinished
+message is sent to the "control" inbox. The message will be forwarded on out of
+the "signal" outbox just before termination.
+"""
+
+from Axon.Component import component
+from Axon.Ipc import shutdownMicroprocess, producerFinished
 
 import sys,os
-sys.path.append(__file__[:1+__file__.rfind(os.sep)] + (".."+os.sep)*1)
+from Axon.ThreadedComponent import threadedcomponent
 
-from Audio.PyMedia.Input import Input as PyMediaInput
 
+import time
+from math import log
+
+import pymedia.audio.acodec as acodec
+import pymedia.audio.sound as sound
+
+from Kamaelia.Support.PyMedia.AudioFormats import format2PyMediaFormat
+
+
+class Input(threadedcomponent):
+    """\
+    Input([sample_rate][,channels][,format]) -> new Input component.
+    
+    Captures audio using the PyMedia library and sends the raw audio data out
+    of its "outbox" outbox.
+    
+    Keyword arguments:
+        
+    - sample_rate  -- Sample rate in Hz (default = 44100)
+    - channels     -- Number of channels (default = 2)
+    - format       -- Sample format (default = "S16_LE")
+    """
+    Outboxes = { "outbox" : "raw audio samples",
+                 "format" : "dictionary detailing sample_rate, sample_format and channels",
+                 "signal" : "Shutdown signalling",
+               }
+    
+    def __init__(self, sample_rate=44100, channels=2, format="S16_LE"):
+        """x.__init__(...) initializes x; see x.__class__.__doc__ for signature"""
+        super(Input,self).__init__()
+        
+        pformat = format2PyMediaFormat[format]
+        self.snd = sound.Input(sample_rate, channels, pformat)
+        
+        self.sample_rate = sample_rate
+        self.channels = channels
+        self.format = format
+        
+    def main(self):
+        self.snd.start()
+        
+        format = {
+            'channels'    : self.channels,
+            'sample_rate' : self.sample_rate,
+            'format'      : self.format,
+        }
+        self.send(format, "format")
+        
+        shutdown=False
+        while self.anyReady() or not shutdown:
+            while self.dataReady("control"):
+                msg=self.recv("control")
+                if isinstance(msg, (producerFinished,shutdownMicroprocess)):
+                    shutdown=True
+                self.send(msg,"signal")
+                
+            raw = self.snd.getData()
+            if raw and len(raw):
+                data = str(raw)
+                while data:
+                    try:
+                        self.send(data,"outbox")
+                        data=None
+                    except noSpaceInBox:
+                        self.pause()
+            else:
+                self.pause(0.01)
+                    
+        self.snd.stop()
+
+
+__kamaelia_components__ = ( Input, )
