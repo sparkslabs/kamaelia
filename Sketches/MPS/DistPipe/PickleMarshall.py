6eb316fe6f439f9a95605c6a3fa0f19cc15333c6
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2007-04-23 17:00:31 +0000

    changed to use pickle

--- Sketches/MPS/DistPipe/PickleMarshall.py
+++ Sketches/MPS/DistPipe/PickleMarshall.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import time
+import pickle
 from Kamaelia.Util.Backplane import *
 from Axon.ThreadedComponent import threadedcomponent
 from Kamaelia.Chassis.Pipeline import Pipeline
@@ -8,15 +9,16 @@ from Kamaelia.Util.Marshalling import *
 from Kamaelia.Util.Console import *
 
 class Serialiser(object):
-    def marshall(int): return str(int)
+    def marshall(item): return pickle.dumps(item)
     marshall = staticmethod(marshall)
 
-    def demarshall(string): return int(string)
+    def demarshall(item): return pickle.loads(item)
     demarshall = staticmethod(demarshall)
 
 Backplane("inprocesscomms").activate()
 
 class Producer(threadedcomponent):
+    # Lazy timed source
     def main(self):
         for i in xrange(1000):
             self.send(i, "outbox")
