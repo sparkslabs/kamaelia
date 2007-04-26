cac63bad93763943d4d96085d0cd927f2e7c9a14
matth_rd <matth_rd@2682807a-6752-0410-967a-793f22d1dfe9>
2007-04-26 13:21:23 +0000

    Corrections to documentation for ReST parsing. Plus forgot to remove prefab declaration on previous commit (oops!)
    Matt

--- Sketches/MH/RTP/SDP.py
+++ Sketches/MH/RTP/SDP.py
@@ -1,4 +1,156 @@
 #!/usr/bin/env python
+#
+# (C) 2006 British Broadcasting Corporation and Kamaelia Contributors(1)
+#     All Rights Reserved.
+#
+# You may only modify and redistribute this under the terms of any of the
+# following licenses(2): Mozilla Public License, V1.1, GNU General
+# Public License, V2.0, GNU Lesser General Public License, V2.1
+#
+# (1) Kamaelia Contributors are listed in the AUTHORS file and at
+#     http://kamaelia.sourceforge.net/AUTHORS - please extend this file,
+#     not this notice.
+# (2) Reproduced in the COPYING file, and at:
+#     http://kamaelia.sourceforge.net/COPYING
+# Under section 3.5 of the MPL, we are using this text since we deem the MPL
+# notice inappropriate for this file. As per MPL/GPL/LGPL removal of this
+# notice is prohibited.
+#
+# Please contact us via: kamaelia-list-owner@lists.sourceforge.net
+# to discuss alternative licensing.
+# -------------------------------------------------------------------------
+"""\
+==========================================
+Session Description Protocol (SDP) Support
+==========================================
+
+The SDPParser component parses Session Description Protocol (see RFC 4566) data
+sent to it as individual lines of text (not multiline strings) and outputs a
+dictionary containing the parsed session description.
+
+
+
+Example Usage
+-------------
+
+Fetch SDP data from a URL, parse it, and display the output::
+
+    Pipeline( OneShot("http://www.mysite.com/sessiondescription.sdp"),
+              SimpleHTTPClient(),
+              chunks_to_lines(),
+              SDPParser(),
+              ConsoleEchoer(),
+            ).run()
+
+If the session description at the URL provided is this::
+
+    v=0
+    o=jdoe 2890844526 2890842807 IN IP4 10.47.16.5
+    s=SDP Seminar
+    i=A Seminar on the session description protocol
+    u=http://www.example.com/seminars/sdp.pdf
+    e=j.doe@example.com (Jane Doe)
+    c=IN IP4 224.2.17.12/127
+    t=2873397496 2873404696
+    a=recvonly
+    m=audio 49170 RTP/AVP 0
+    m=video 51372 RTP/AVP 99
+    a=rtpmap:99 h263-1998/90000
+
+
+Then parsing will return this dictionary::
+
+    { 'protocol_version': 0,
+      'origin'     : ('jdoe', 2890844526, 2890842807, 'IN', 'IP4', '10.47.16.5'),
+      'sessionname': 'SDP Seminar',
+      'information': 'A Seminar on the session description protocol',
+      'connection' : ('IN', 'IP4', '224.2.17.12', '127', 1),
+      'time'       : [(2873397496L, 2873404696L, [])],
+      'URI'        : 'http://www.example.com/seminars/sdp.pdf',
+      'email'      : 'j.doe@example.com (Jane Doe)',
+      'attribute'  : ['recvonly'],
+      'media':
+          [ { 'media'     : ('audio', 49170, None, 'RTP/AVP', '0'),
+              'connection': ('IN', 'IP4', '224.2.17.12', '127', 1)
+            },
+            { 'media'     : ('video', 51372, None, 'RTP/AVP', '99'),
+              'connection': ('IN', 'IP4', '224.2.17.12', '127', 1),
+              'attribute' : ['rtpmap:99 h263-1998/90000']
+            }
+          ],
+    }
+
+
+    
+Behaviour
+---------
+
+Send individual lines as strings to SDPParser's "inbox" inbox. SDPParser cannot
+handle multiple lines in the same string.
+
+When SDPParser receives a producerFinished() message on its "control" inbox, or
+if it encounter another "v=" line then it knows it has reached the end of the
+SDP data and will output the parsed data as a dictionary to its "outbox" outbox.
+
+The SDP format does *not* contain any kind of marker to signify the end of a
+session description - so SDPParser only deduces this by being told that the
+producer/data source has finished, or if it encounters a "v=" line indicating
+the start of another session description.
+
+SDPParser can parse more than one session description, one after the other.
+
+If the SDP data is malformed AssertionError, or other exceptions, may be raised.
+SDPParser does not rigorously test for exact compliance - it just complains if
+there are glaring problems, such as fields appearing in the wrong sections!
+
+If a producerFinished or shutdownMicroprocess message is received on the
+"control" inbox then, once any pending data at the "inbox" inbox has been
+processed, this component will terminate. It will send the message on out of
+its "signal" outbox.
+
+Only if the message is a producerFinished message will it output the session
+description is has been parsing. A shutdownMicroprocess message will not result
+in it being output.
+
+
+
+Format of parsed output
+-----------------------
+
+The result of parsing SDP data is a dictionary mapping descriptive names of
+types to values:
+
+ ======  ======================  ======================================================================
+ Type    Dictionary key          Format of the value
+ ======  ======================  ======================================================================
+ v       "protocol_version"      version_number
+ o       "origin"                ("user", session_id, session_version, "net_type", "addr_type", "addr")
+ s       "sessionname"           "session name"
+ t & r   "time"                  (starttime, stoptime, [repeat,repeat, ...])
+                                    where repeat = (interval,duration,[offset,offset, ...])
+ a       "attribute"             "value of attribute"
+ b       "bandwidth"             (mode, bitspersecond)
+ i       "information"           "value"
+ e       "email"                 "email-address"
+ u       "URI"                   "uri"
+ p       "phone"                 "phone-number"
+ c       "connection"            ("net_type", "addr_type", "addr", ttl, groupsize)
+ z       "timezone adjustments"  [(adj-time,offset), (adj-time,offset), ...]
+ k       "encryption"            ("method","value")
+ ======  ======================  ======================================================================
+
+Note that 't' and 'r' lines are combined in the dictionary into a single
+"time" key containing both the start and end times specified in the 't' line
+and a list of any repeats specified in any 'r' lines present.
+
+Some lines are optional in SDP. If they are not included, then the parsed output
+will not contain the corresponding key.
+ 
+The formats of values are left unchanged by the parsing. For example, integers
+representing times are simply converted to integers, but the units used remain
+unchanged (ie. they will not be converted to unix time units).
+
+"""
 
 # Basic Parser for SDP data, as defined in RFC 4566
 #
@@ -12,7 +164,21 @@ from Axon.Ipc import producerFinished,shutdownMicroprocess
 
 import re
 
-class SdpParser(component):
+class SDPParser(component):
+    """\
+    SDPParser() -> new SDPParser component.
+
+    Parses Session Description Protocol data (see RFC 4566) sent to its "inbox"
+    inbox as individual strings for each line of the SDP data. Outputs a dict
+    containing the parsed data from its "outbox" outbox.
+    """
+
+    Inboxes = { "inbox"   : "SDP data in strings, each containing a single line",
+                "control" : "Shutdown signalling",
+              }
+    Outboxes = { "outbox" : "Parsed SDP data in a dictionary",
+                 "signal" : "Shutdown signalling",
+               }
 
     def handleControl(self):
         while self.dataReady("control"):
@@ -48,7 +214,7 @@ class SdpParser(component):
         try:
             for line in self.readline(): yield 1
             # self.readline() generator complete ... line now contains a line with something on it
-            type,key,value = parseline(line)
+            type,key,value = _parseline(line)
 
             while 1:
                 # begin by parsing the session section
@@ -81,7 +247,7 @@ class SdpParser(component):
                 
                     for line in self.readline(): yield 1
                     # self.readline() generator complete ... line now contains a line with something on it
-                    type,key,value = parseline(line)
+                    type,key,value = _parseline(line)
 
                 # we've hit an 'm' so its the end of the session section
                 assert(mandatory=="")
@@ -105,7 +271,7 @@ class SdpParser(component):
                     
                     for line in self.readline(): yield 1
                     # self.readline() generator complete ... line now contains a line with something on it
-                    type,key,value = parseline(line)
+                    type,key,value = _parseline(line)
                     
                     while type != "m" and type != "v":
                         mandatory=mandatory.replace(type,"")
@@ -120,7 +286,7 @@ class SdpParser(component):
                     
                         for line in self.readline(): yield 1
                         # self.readline() generator complete ... line now contains a line with something on it
-                        type,key,value = parseline(line)
+                        type,key,value = _parseline(line)
 
                     # end of media section
                     assert(mandatory=="")
@@ -151,7 +317,7 @@ class SdpParser(component):
         self.send(session,"outbox")
 
         
-def parseline(line):
+def _parseline(line):
     match = re.match("^(.)=(.*)",line)
     
     type,value = match.group(1), match.group(2)
@@ -251,26 +417,7 @@ def parseline(line):
         return type, 'unknown', value
 
 
-import sys
-sys.path.append("../MobileReframe")
-from OneShot import OneShot
-from chunks_to_lines import chunks_to_lines
-from Kamaelia.Protocol.HTTP.HTTPClient import SimpleHTTPClient
-from Kamaelia.Util.PureTransformer import PureTransformer
-from Kamaelia.Chassis.Pipeline import Pipeline
-
-    
-def GetRTPAddressFromSDP(sdp_url):
-    return \
-        Pipeline( OneShot(sdp_url),
-                  SimpleHTTPClient(),
-                  chunks_to_lines(),
-                  SdpParser(),
-                  PureTransformer(lambda session : \
-                      (session["connection"][2], session["media"][0]["media"][1])
-                  ),
-                )
-    
+__kamaelia_components__ = ( SDPParser, )
 
 
 if __name__ == "__main__":
@@ -314,7 +461,7 @@ v=0
 """.splitlines()
     
     Pipeline( DataSource(sdp),
-              SdpParser(),
+              SDPParser(),
               ConsoleEchoer(),
             ).run()
 
