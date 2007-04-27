91d9b803221a616157c13dcc3889b6112500a964
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2007-04-27 09:39:13 +0000

    Initial pass at a Networked Pipeline
    Michael.

--- trunk/Sketches/MPS/DistPipe/PickleMarshall.py
+++ trunk/Sketches/MPS/DistPipe/PickleMarshall.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python
 
-import time
+import re
+import sys
 import pickle
 from Kamaelia.Util.Backplane import *
-from Axon.ThreadedComponent import threadedcomponent
 from Kamaelia.Chassis.Pipeline import Pipeline
 from Kamaelia.Chassis.Graphline import Graphline
 from Kamaelia.Util.Marshalling import *
 from Kamaelia.Util.Console import *
 from Kamaelia.Internet.SingleServer import SingleServer
 from Kamaelia.Internet.TCPClient import TCPClient
+from ExampleClasses import Producer
 
 class Serialiser(object):
     def marshall(item): return pickle.dumps(item)
@@ -18,12 +19,26 @@ class Serialiser(object):
     def demarshall(item): return pickle.loads(item)
     demarshall = staticmethod(demarshall)
 
-class Producer(threadedcomponent):
-    # Lazy timed source
-    def main(self):
-        for i in xrange(1000):
-            self.send(range(i), "outbox")
-            time.sleep(1)
+def makeComponent(spec, uid=None):
+    """\
+    Takes spec of the form:
+       "importname:classname(arguments)"
+    and constructs it, eg
+       "Kamaelia.Util.Console:consoleEchoer()"
+    """
+    match = re.match("^([^:]*):([^(]*)(.*)$", spec)
+    (modulename, classname, arguments) = match.groups()
+    module = __import__(modulename, [], [], [classname])
+
+    try:
+        thecomponent = eval("module."+classname+arguments)   ### XXX Probably a gaping security hole!!!
+    except e:
+        print "Couldn't instantiate component: ",str(e)
+
+    if not uid is None:
+        thecomponent.id = eval(uid)
+    thecomponent.name = spec + "_" + str(thecomponent.id)
+    return thecomponent
 
 def NetworkOutbox(port):
     return Pipeline( Marshaller(Serialiser),
@@ -46,13 +61,32 @@ def NetworkLinkage(ip1, port1, ip2, port2):
                  ("PIPE", "signal"): ("PIPE", "control"),
               }
            )
-if 1:
-    Pipeline( Producer(),
+
+def LocalNetworkPipelineLength2(source, sink):
+    baseport = 1500
+    Pipeline( makeComponent(source),
               NetworkOutbox(1500)
             ).activate()
-            
+
     Pipeline( NetworkInbox(1501),
-              ConsoleEchoer()
+              makeComponent(sink)
+            ).activate()
+
+    return NetworkLinkage("127.0.0.1", 1500, "127.0.0.1", 1501)
+
+if 1:
+    LocalNetworkPipelineLength2(
+          "ExampleClasses:Producer()",
+          "Kamaelia.Util.Console:ConsoleEchoer()"
+    ).run()
+
+if 0:
+    Pipeline( makeComponent("ExampleClasses:Producer()"),
+              NetworkOutbox(1500)
+            ).activate()
+
+    Pipeline( NetworkInbox(1501),
+              makeComponent("Kamaelia.Util.Console:ConsoleEchoer()")
             ).activate()
 
     NetworkLinkage("127.0.0.1", 1500, "127.0.0.1", 1501).run()
