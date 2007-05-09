ec33d357ce381d27ff58969245217fdd3e789b5c
pitt2 <pitt2@2682807a-6752-0410-967a-793f22d1dfe9>
2007-05-09 12:12:56 +0000

    Fixed a box conflict that prevented wrapped components from communicating with children.
    Patrick.

--- Sketches/PT/testlikefile.py
+++ Sketches/PT/testlikefile.py
@@ -16,14 +16,12 @@ schedulerThread(slowmo = 0.01).start()
 
 theComponentToWrap = SimpleHTTPClient
 #theComponentToWrap = Reverser
-
 page = []
 p = likeFile(theComponentToWrap())
-time.sleep(0.5)
 p.put("http://google.com", "inbox")
 p.put("http://slashdot.org", "inbox")
-time.sleep(0.5)
-
-while True:
-    print p.get("outbox", False) # this doesn't ever return anything useful, False meaning "don't block on the queue"
-    time.sleep(1)
+p.put("http://whatismyip.org", "inbox")
+google = p.get("outbox", True) # block until page recieved
+slashdot = p.get("outbox", True) # block until page recieved
+whatismyip = p.get("outbox", True) # block until page recieved
+print "google is", len(google), "bytes long, and slashdot is", len(slashdot), "bytes long. Also, our IP address is:", whatismyip
