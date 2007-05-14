d807e923eb1b495b84381824694ca46ef1ec0de6
jlei <jlei@2682807a-6752-0410-967a-793f22d1dfe9>
2007-05-14 23:50:08 +0000

    sequencer components

---
+++ trunk/Sketches/JL/Sequencer/sequencer.py
@@ -0,0 +1,38 @@
+from Axon.Component import component
+
+class sequencer(component):
+    def __init__(self):
+        super(sequencer, self).__init__()
+        
+    def findNext(self):
+        return 1
+    
+    def printNext(self):
+        print self.findNext()
+        
+    def main(self):
+        for i in range(20):
+            yield 1
+            self.printNext()
+        yield -1
+        
+class fibonacciSequencer(sequencer):
+    def __init__(self):
+        super(fibonacciSequencer, self).__init__()
+        self.n = 0
+        self.n_1 = 1
+        
+    def findNext(self):
+        val = self.n
+        self.n = self.n_1
+        self.n_1 = val + self.n_1
+        return val
+        
+class naturals(sequencer):
+    def __init__(self):
+        super(naturals, self).__init__()
+        self.n = 0
+        
+    def findNext(self):
+        self.n = self.n + 1
+        return self.n
