9faa46449fa34056efb91a2565d01cfa32e9e44e
jlei <jlei@2682807a-6752-0410-967a-793f22d1dfe9>
2007-05-19 01:19:54 +0000

    made it look like a real component

--- trunk/Sketches/JL/Sequencer/sequencer.py
+++ trunk/Sketches/JL/Sequencer/sequencer.py
@@ -1,8 +1,14 @@
+#! /usr/bin/env python
 from Axon.Component import component
+from Axon.Ipc import shutdownMicroprocess, producerFinished
 
 class sequencer(component):
+#Inboxes: inbox, control
+#Outboxes: outbox, signal
+
     def __init__(self):
         super(sequencer, self).__init__()
+        self.run = True
         
     def findNext(self):
         return 1
@@ -11,10 +17,32 @@ class sequencer(component):
         print self.findNext()
         
     def main(self):
-        while True:
+        while self.run:
             yield 1
-            self.printNext()
-        
+            if self.dataReady("control"):
+                msg = self.recv("control")
+                print "received control message: ", msg
+                if isinstance(msg, (shutdownMicroprocess, producerFinished)):
+                    self.shutdown()
+                    break
+            if self.dataReady("inbox"):
+                msg = self.recv("inbox")
+                print "received inbox message: ", msg
+                if msg == "NEXT":
+                    self.send(self.findNext(), 'outbox')
+
+    def shutdown(self):
+        raise "Received control message"
+##        self._callOnCloseDown() #doesn't do anything right now
+##        self.run = False
+##        self.send(producerFinished(), "signal") #should be taken care of in _callOnCloseDown?
+
+    def run(self):
+        from Axon.Scheduler import scheduler
+        self.activate()
+        scheduler.run.runThreads()
+
+                
 class fibonacciSequencer(sequencer):
     def __init__(self):
         super(fibonacciSequencer, self).__init__()
@@ -45,3 +73,4 @@ class naturals(sequencer):
     def findNext(self):
         self.n = self.n + 1
         return self.n
+
