9278a5374e355e4880e4fcb855285c3305a82ece
matth_rd <matth_rd@2682807a-6752-0410-967a-793f22d1dfe9>
2007-05-23 16:25:26 +0000

    Added Class statement parsing. Some of the test cases are now being correctly resolved.
    Matt

--- trunk/Sketches/MH/DocGen/ImportTracking.py
+++ trunk/Sketches/MH/DocGen/ImportTracking.py
@@ -50,6 +50,7 @@ if __name__ == "__main__":
     root = AST.getChildren()[1]           # root statement node
 
     imports = {}
+    classes = {}
     
     def parse_From(node, imports):
         sourceModule, items = node.getChildren()
@@ -66,8 +67,21 @@ if __name__ == "__main__":
             if destName == None:
                 destName = name
             imports[destName] = name
+            
+    def parse_Class(node, imports,classes):
+        name = node.name
+        bases = node.bases
+        resolvedBases = []
+        print name,bases
+        for base in bases:
+            expBase = parseName(base)
+            print "!!",expBase,base
+            resolvedBase = matchToImport(imports,expBase)
+            resolvedBases.append(resolvedBase)
+        classes[name] = resolvedBases
+        imports[name] = name  # XXX LOCAL NAME, DOES IT NEED SCOPING CONTEXT?
     
-    def chaseThrough(node, imports):
+    def chaseThrough(node, imports,classes):
         for node in node.getChildren():
             if isinstance(node, ast.From):
                 # parse "from ... import"s to recognise what symbols are mapped to what imported things
@@ -76,6 +90,7 @@ if __name__ == "__main__":
                 # parse imports to recognise what symbols are mapped to what imported things
                 parse_Import(node, imports)
             elif isinstance(node, ast.Class):
+                parse_Class(node, imports,classes)
                 pass  # classes need to be parsed so we can work out base classes
             elif isinstance(node, ast.Assign):
                 pass  # parse assignments that map stuff thats been imported to new names
@@ -83,10 +98,32 @@ if __name__ == "__main__":
                 pass  # ignore everything else for the moment
         return
         
-    chaseThrough(root, imports)
+    def parseName(node):
+        if isinstance(node, ast.Name):
+            return node.name
+        elif isinstance(node, ast.Getattr):
+            return ".".join([parseName(node.expr), node.attrname])
+        else:
+            return node.__class__.__name__
+        
+    def matchToImport(imports,name):
+        # go through imports, if we find one that matches the root of the name
+        # then resolve it
+        for (importName,resolved) in imports.items():
+            if importName == name:
+                return resolved
+            else:
+                importName+="."
+                if importName == name[:len(importName)]:
+                    return ".".join([resolved, name[len(importName):]])
+        return name
+    
+    chaseThrough(root, imports, classes)
     
     import pprint
-    print "-----"
+    print "-----MAPPINGS:"
     pprint.pprint(imports)
+    print "-----CLASSES:"
+    pprint.pprint(classes)
     print "-----"
     
