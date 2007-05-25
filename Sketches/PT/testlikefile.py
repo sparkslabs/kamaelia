56536c8aab3cb72aaf6aa06ff1d27f131fdf9972
pitt2 <pitt2@2682807a-6752-0410-967a-793f22d1dfe9>
2007-05-25 15:24:25 +0000

    forgot to commit along with likefile. Syntactic changes.
    Patrick

--- Sketches/PT/testlikefile.py
+++ Sketches/PT/testlikefile.py
@@ -10,7 +10,7 @@ p = likeFile(SimpleHTTPClient())
 p.put("http://google.com", "inbox")
 p.put("http://slashdot.org", "inbox")
 p.put("http://whatismyip.org", "inbox")
-google = p.get("outbox", True) # block until page recieved
-slashdot = p.get("outbox", True) # block until page recieved
-whatismyip = p.get("outbox", True) # block until page recieved
+google = p.get("outbox")
+slashdot = p.get("outbox")
+whatismyip = p.get("outbox")
 print "google is", len(google), "bytes long, and slashdot is", len(slashdot), "bytes long. Also, our IP address is:", whatismyip
