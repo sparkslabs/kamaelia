7379d3c3320b16a751a874a8e0a39d2f9fcabfea
jlei <jlei@2682807a-6752-0410-967a-793f22d1dfe9>
2007-05-31 17:32:29 +0000

    Stricter parsing. Further parsing and message handling on hold until I come up with with a SimpleIRCClient that handles output from "command"

--- Sketches/JL/IRC/IRCClient.py
+++ Sketches/JL/IRC/IRCClient.py
@@ -19,13 +19,20 @@
 ## Please contact us via: kamaelia-list-owner@lists.sourceforge.net
 ## to discuss alternative licensing.
 ## -------------------------------------------------------------------------
-
+# This is the base version.
+# The purpose of this version is to see if a general sendCommand statement would
+#  work, and to improve display support for messages other than PRIVMSGs. 
+# The purpose of this version is to try out the IF model of message parsing.
+# The purpose of this version is to try out a more general IF model -- one
+#  general path for messages via IRC and one general path for messages received
+#  from the local user. It relays all received data to its "heard" outbox,
+#  in the form (msgtype, sender, recipient, body)
 
 import Axon as _Axon
 from Kamaelia.Internet.TCPClient import TCPClient
 from Kamaelia.Chassis.Graphline import Graphline
 from Axon.Ipc import producerFinished, shutdownMicroprocess
-
+import string
     
 class channel(object):
    """\
@@ -60,7 +67,7 @@ class IRC_Client(_Axon.Component.component):
       default in IRC. There are MANY ways this could be achieved.
    """
    Inboxes = {"inbox":"", "control":"", "talk":"", "topic":""}
-   Outboxes = {"outbox":"", "signal":"", "heard":"" }
+   Outboxes = {"outbox":"", "signal":"", "heard":"", "commands":"inbox for non PRIVMSGs" }
    def __init__(self, nick="kamaeliabot",
                       nickinfo="Kamaelia",
                       defaultChannel="#kamaeliatest"):
@@ -105,22 +112,20 @@ class IRC_Client(_Axon.Component.component):
              newtopic = self.recv("topic")
              self.channels[self.defaultChannel].topic(newtopic)
          elif self.dataReady("inbox"): #if received messages 
-            lines = self.recv()
-            if "\r" in lines:
-                lines.replace("\r","\n")
-            lines = lines.split("\n")
-            for data in lines:
-                if "PRIVMSG" in data:
-                    if data[0] == ":":
-                        data = data[1:]
-                    if ("VERSION" in data) and not seen_VERSION:
-                        seen_Version = True
-                    else:
-                        data = data[data.find(":")+1:]
-                        self.send(data, "heard")
-                elif "PING" in data:
-                    reply = "PONG" + data[data.find("PING")+4:]
-                    self.send(reply+"\r\n")
+             self.handleMessage(self.recv("inbox"))
+             
+#This is only around until I figure out why seen_VERSION is set.  
+##                if "PRIVMSG" in data:
+##                    if data[0] == ":":
+##                        data = data[1:]
+##                    if ("VERSION" in data) and not seen_VERSION:
+##                        seen_Version = True
+##                    else:
+##                        data = data[data.find(":")+1:]
+##                        self.send(data, "heard")
+##                elif "PING" in data:
+##                    reply = "PONG" + data[data.find("PING")+4:]
+##                    self.send(reply+"\r\n")
                     
          if data.find(self.nick) != -1:
             if data.find("LEAVE") != -1: #slash notation not implemented? 
@@ -133,6 +138,78 @@ class IRC_Client(_Axon.Component.component):
       self.channels[self.defaultChannel].leave()
       # print self.nick + "... is leaving\n" # Check with and IRC client instead.
 
+   def handleMessage(self, lines):
+        """handles incoming messages that don't originate from the user."""
+        if "\r" in lines:
+            lines.replace("\r","\n")
+        lines = lines.split("\n")
+        for one_line in lines:
+            if self.parseable(one_line):
+                data = self.parseIRCMessage(one_line)
+                print data
+                self.send(data, "heard")
+            elif len(one_line) > 0:
+                self.send('Unknown message format: ' + one_line, "heard")
+                
+            if data:
+                (msgtype, sender, recipient, body) = data
+                if msgtype == 'PRIVMSG':
+                    self.send(data, 'heard')
+                elif msgtype == 'PING' or 'PING' in body:
+                    if msgtype == 'PING':
+                        reply = ("PONG " + sender)
+                    else:
+                        reply = ("PONG " + body[body.find("PING")+4:])
+                    self.send(reply + '\r\n')
+                    self.send(data, 'commands')
+                
+                    
+
+
+   def parseable(self, line):
+        if len(line) > 0 and len(line.split()) <= 1 and line[0] == ':':
+            print "Malformed message:", line
+            return False
+        return len(line) > 0
+       
+   def parseIRCMessage(self, line):
+        """Assumes most lines in the format of :nick!username MSGTYPE recipient :message.
+           Returns a tuple (message type, sender, recipient, message body)."""
+        tokens = line.split()
+        sender = ""
+        recipient = ""
+        body = ""
+        try:
+            if tokens[0] == 'NOTICE':
+                msgtype = 'NOTICE'
+                recipient = tokens[1]
+                body = self.extractBody(tokens[2:])
+            elif tokens[0] == 'PING' or tokens[0] == 'PONG':    
+                msgtype = tokens[0]
+                sender = self.extractSender(tokens[1])
+            else:
+                sender = self.extractSender(tokens[0])
+                msgtype = tokens[1]
+                recipient = tokens[2]
+                if len(tokens) > 3:
+                    body = self.extractBody(tokens[3:])
+            return (msgtype, sender, recipient, body)
+        except IndexError:
+            print "Malformed or unaccounted-for message:", tokens
+
+   def extractSender(self, token):
+        if '!' in token:
+            return token[1:token.find('!')]
+        else:
+            return token[1:]
+
+   def extractBody(self, tokens):
+        body =  string.join(tokens, ' ')
+        if body[0] == ':':
+            return body[1:]
+        else:
+            return body
+        
    def shutdown(self):
        while self.dataReady("control"):
            msg = self.recv("control")
@@ -162,14 +239,15 @@ def SimpleIRCClientPrefab(host="127.0.0.1",
         )
 
 if __name__ == '__main__':
-   from Axon.Scheduler import scheduler
-   from Kamaelia.Util.Console import ConsoleReader
-   from Kamaelia.UI.Pygame.Ticker import Ticker
-   from Kamaelia.Chassis.Pipeline import Pipeline
-
-   Pipeline(
-       ConsoleReader(),
-       SimpleIRCClientPrefab(host="irc.freenode.net", nick="kamaeliabot", defaultChannel="#kamtest"),
-       Ticker(render_right = 800,render_bottom = 600),
-   ).run()
+    from Axon.Scheduler import scheduler
+    from Kamaelia.Util.Console import ConsoleReader
+    from Kamaelia.UI.Pygame.Ticker import Ticker
+    from Kamaelia.Chassis.Pipeline import Pipeline
+    from  Kamaelia.Util.PureTransformer import PureTransformer
+    Pipeline(
+        ConsoleReader(),
+        SimpleIRCClientPrefab(host="irc.freenode.net", nick="kamaeliabot", defaultChannel="#kamtest"),
+        PureTransformer(lambda x: str(x)),
+        Ticker(render_right = 800,render_bottom = 600),
+    ).run()
 
