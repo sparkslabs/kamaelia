55d4d4bec825ba42b66f71dee757a96614f6ddff
jlei <jlei@2682807a-6752-0410-967a-793f22d1dfe9>
2007-06-01 16:53:12 +0000

    separate prefab for ComplexIRCClient and brushed-up version of ircShakespeare

--- trunk/Sketches/JL/IRC/IRCClient.py
+++ trunk/Sketches/JL/IRC/IRCClient.py
@@ -29,11 +29,8 @@
 #  in the form (msgtype, sender, recipient, body)
 
 import Axon as _Axon
-from Kamaelia.Internet.TCPClient import TCPClient
-from Kamaelia.Chassis.Graphline import Graphline
 from Axon.Ipc import producerFinished, shutdownMicroprocess
 import string
-from Kamaelia.Util.Console import ConsoleEchoer
 
 class channel(object):
    """\
@@ -109,7 +106,6 @@ class IRC_Client(_Axon.Component.component):
       "Handling here is still in progress. :)"
       self.login(self.nick, self.nickinfo)
       self.channels[self.defaultChannel] = self.join(self.defaultChannel)
-      self.channels['CONSOLE'] = channel(ConsoleEchoer(), 'CONSOLE') #temporary hack
       seen_VERSION = False
 
       while not self.shutdown():
@@ -117,7 +113,6 @@ class IRC_Client(_Axon.Component.component):
          if self.dataReady("talk"):
             data = self.recv("talk")
             self.channels[self.defaultChannel].say(data)
-            self.channels['CONSOLE'].say(data) #temporary hack
             if data.find(self.nick) != -1:
                 if data.find("LEAVE") != -1:
                    break
@@ -223,47 +218,3 @@ class IRC_Client(_Axon.Component.component):
            if isinstance(msg, producerFinished) or isinstance(msg, shutdownMicroprocess):
                return True
        return False
-
-def ComplexIRCClientPrefab(host="127.0.0.1",
-                          port=6667,
-                          nick="kamaeliabot",
-                          nickinfo="Kamaelia",
-                          defaultChannel="#kamaeliatest",
-                          IRC_Handler=IRC_Client):
-    return Graphline(
-        CLIENT = TCPClient(host, port),
-        PROTO = IRC_Handler(nick, nickinfo, defaultChannel),
-        linkages = {
-              ("CLIENT" , "outbox") : ("PROTO" , "inbox"),
-              ("PROTO"  , "outbox") : ("CLIENT", "inbox"),
-              ("PROTO"  , "privmsg")  : ("SELF", "outbox"), #SELF refers to the Graphline. Passthrough linkage
-              ("SELF"  , "inbox") : ("PROTO" , "talk"), #passthrough
-              ("SELF"  , "topic") : ("PROTO" , "topic"), #passthrough
-              ("SELF"  , "control") : ("PROTO" , "control"), #passthrough
-              ("PROTO"  , "signal") : ("CLIENT", "control"),
-              ("CLIENT" , "signal") : ("SELF" , "signal"), #passthrough
-              ("PROTO", "nonPrivmsg") : ("SELF", "nonPrivmsg") #passthrough
-              }
-        )
-
-if __name__ == '__main__':
-    from Kamaelia.Util.Console import ConsoleReader
-    from Kamaelia.UI.Pygame.Ticker import Ticker
-    from Kamaelia.Chassis.Graphline import Graphline
-    from Kamaelia.Util.PureTransformer import PureTransformer
-    Graphline(
-        reader = ConsoleReader(),
-        irc = ComplexIRCClientPrefab(host="irc.freenode.net", nick="kamaeliabot", defaultChannel="#kamtest"),
-        stringer1 = PureTransformer(lambda x: str(x)),
-        stringer2 = PureTransformer(lambda x: str(x)),
-        display1 = Ticker(render_right = 400,render_bottom = 300),
-        display2 = Ticker(render_right = 400,render_bottom = 300, position = (440, 0)),
-        linkages = {
-            ("reader", "outbox") : ("irc", "inbox"),
-            ("irc", "outbox") : ("stringer1", "inbox"),
-            ("stringer1", "outbox") : ("display1", "inbox"),
-            ("irc", "nonPrivmsg") : ("stringer2", "inbox"),
-            ("stringer2", "outbox") : ("display2", "inbox")
-            }
-            
-    ).run()
