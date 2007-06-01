fe696263d53322cd4e4a017152c402ca21a6c9ed
jlei <jlei@2682807a-6752-0410-967a-793f22d1dfe9>
2007-06-01 20:28:24 +0000

    now echoes all data sent to the server to another outbox, 'sendCopy'

--- Sketches/JL/IRC/Prefab.py
+++ Sketches/JL/IRC/Prefab.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python
 from Kamaelia.Internet.TCPClient import TCPClient
 from Kamaelia.Chassis.Graphline import Graphline
-from Kamaelia.Util.Console import ConsoleEchoer
 from Kamaelia.Util.Fanout import Fanout
 from IRCClient import *
 
@@ -13,17 +12,20 @@ def ComplexIRCClientPrefab(host="127.0.0.1",
                           IRC_Handler=IRC_Client):
     return Graphline(
         CLIENT = TCPClient(host, port),
-        PROTO = IRC_Handler(nick, nickinfo, defaultChannel),
+        PROTO = IRC_Handler(nick, nickinfo, defaultChannel, sendAsString=True),
+        SPLIT = Fanout(["toGraphline", "toTCP"]),
         linkages = {
               ("CLIENT" , "outbox") : ("PROTO" , "inbox"),
-              ("PROTO"  , "outbox") : ("CLIENT", "inbox"),
+              ("PROTO"  , "outbox") : ("SPLIT", "inbox"),
               ("PROTO"  , "privmsg")  : ("SELF", "outbox"), #SELF refers to the Graphline. Passthrough linkage
               ("SELF"  , "inbox") : ("PROTO" , "talk"), #passthrough
               ("SELF"  , "topic") : ("PROTO" , "topic"), #passthrough
               ("SELF"  , "control") : ("PROTO" , "control"), #passthrough
               ("PROTO"  , "signal") : ("CLIENT", "control"),
               ("CLIENT" , "signal") : ("SELF" , "signal"), #passthrough
-              ("PROTO", "nonPrivmsg") : ("SELF", "nonPrivmsg") #passthrough
+              ("PROTO", "nonPrivmsg") : ("SELF", "nonPrivmsg"), #passthrough
+              ("SPLIT", "toGraphline") : ("SELF", "sentCopy"), #passthrough
+              ("SPLIT", "toTCP") : ("CLIENT", "inbox")
               }
         )
 
@@ -35,16 +37,14 @@ if __name__ == '__main__':
     Graphline(
         reader = ConsoleReader(),
         irc = ComplexIRCClientPrefab(host="irc.freenode.net", nick="kamaeliabot", defaultChannel="#kamtest"),
-        stringer1 = PureTransformer(lambda x: str(x)),
-        stringer2 = PureTransformer(lambda x: str(x)),
         display1 = Ticker(render_right = 400,render_bottom = 300),
         display2 = Ticker(render_right = 400,render_bottom = 300, position = (440, 0)),
+        display3 = Ticker(render_right = 400,render_bottom = 300, position = (0, 340)),
         linkages = {
             ("reader", "outbox") : ("irc", "inbox"),
-            ("irc", "outbox") : ("stringer1", "inbox"),
-            ("stringer1", "outbox") : ("display1", "inbox"),
-            ("irc", "nonPrivmsg") : ("stringer2", "inbox"),
-            ("stringer2", "outbox") : ("display2", "inbox")
+            ("irc", "outbox") : ("display1", "inbox"),
+            ("irc", "nonPrivmsg") : ("display2", "inbox"),
+            ("irc", "sentCopy") : ("display3", "inbox")
             }
             
     ).run()
