5fd36d2e2f8210f102fb4b3378fa61216c81a7b3
jlei <jlei@2682807a-6752-0410-967a-793f22d1dfe9>
2007-06-05 17:29:48 +0000

    some tweaks

--- trunk/Sketches/JL/IRC/NiceTickerPrefab.py
+++ trunk/Sketches/JL/IRC/NiceTickerPrefab.py
@@ -6,7 +6,7 @@ def NiceTickerPrefab(**other_ticker_args):
     """Ticker that displays black text on a white background, and transforms
     any non-string arguments passed to it into strings.
     Do not pass in keywords text_height, line_spacing, background_colour, or text_colour."""
-    return Pipeline(PureTransformer(lambda x: str(x)),
+    return Pipeline(PureTransformer(lambda x: '*!*' + str(x)),
              Ticker(text_height=16, line_spacing=4,
                     background_colour=(255, 255, 245), text_colour=(10,10,10),
                     outline_colour = (0,0,0),
