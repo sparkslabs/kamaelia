5c563b9aa399d67bf8e358fa68d36d396e0a102b
jlei <jlei@2682807a-6752-0410-967a-793f22d1dfe9>
2007-06-11 20:33:47 +0000

    another object-oriented version

--- Sketches/JL/IRC/plainPython/pygui.py
+++ Sketches/JL/IRC/plainPython/pygui.py
@@ -4,27 +4,26 @@ import pygame
 import sys
 import threading
 
-WHITE = (255, 255, 255)
-BLACK = (0,0,0)
-
 class textbox(object):
-    def __init__(self, screen_width=300, screen_height=200, text_height=14):
+    def __init__(self, screen_width=300, screen_height=200, text_height=14,
+                 background_color = (255,255,255), text_color=(0,0,0)):
         super(textbox, self).__init__()
         self.screen_width = screen_width
         self.screen_height = screen_height
         self.text_height = text_height
+        self.background_color = background_color
+        self.text_color = text_color
         self.linelen = 70
         
         pygame.init()
         self.screen = pygame.display.set_mode((screen_width, screen_height))
-        self.screen.fill(WHITE)
-        self.scratch = pygame.Surface((screen_width, screen_height))
-        self.scratch.fill(WHITE)
+        self.screen.fill(background_color)
+        self.scratch = self.screen.copy()
         self.font = pygame.font.Font(None, 14)
+        
         self.keepRect = pygame.Rect((0, text_height), (screen_width, screen_width-text_height))
         self.scrollingRect = pygame.Rect((0, 0), (screen_width, screen_height - text_height))
         self.writeRect = pygame.Rect((0, screen_height-text_height), (screen_width, text_height))
-        pygame.display.update()
 
     def update(self, text):
         while len(text) > self.linelen:
@@ -34,13 +33,12 @@ class textbox(object):
         self.updateLine(text)
             
     def updateLine(self, line):            
-        lineSurf = self.font.render(line, True, BLACK)    
-        self.screen.fill(WHITE)
+        lineSurf = self.font.render(line, True, self.text_color)    
+        self.screen.fill(self.background_color)
         self.screen.blit(self.scratch, self.scrollingRect, self.keepRect)
         self.screen.blit(lineSurf, self.writeRect)
-        self.scratch.fill(WHITE)
+        self.scratch.fill(self.background_color)
         self.scratch.blit(self.screen, self.screen.get_rect())
-        pygame.display.update()
 
 class inputThread(threading.Thread):
     def __init__(self, gui):
@@ -52,41 +50,38 @@ class inputThread(threading.Thread):
         while not self.done:
             msg = raw_input('You say: ')
             self.gui.update(msg)
+            if msg == 'exit':
+                pygame.event.post(pygame.event.Event(pygame.QUIT))
 
-class shutdownThread(threading.Thread):
+class UtilThread(threading.Thread):
     def __init__(self, threads):
-        super(shutdownThread, self).__init__()
+        super(UtilThread, self).__init__()
         self.threads = threads
-        self.done = False
         
     def checkForShutdown(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                print 'Press any key to shut down...'
-                for one_thread in self.threads:
-                    one_thread.done = True
-                self.done = True
+                return True
+        return False
                 
     def run(self):
-        while not self.done:
-            self.checkForShutdown()
-
-class refreshThread(threading.Thread):
-    done = False
-    def run(self):
-        while not self.done:
-            pygame.display.update()
+        while True:
+            if self.checkForShutdown():
+                for one_thread in self.threads:
+                    one_thread.done = True
+                    print 'Press any key to exit.'
+                break
+            else:
+                pygame.display.update()
 
 class ui(object):
     def __init__(self):
         super(ui, self).__init__()
         gui = textbox()
         input1 = inputThread(gui)
-        refresh = refreshThread()
-        shutdown = shutdownThread([input1, refresh])
+        util = UtilThread([input1])
         input1.start()
-        refresh.start()
-        shutdown.start()
+        util.start()
 
 done = False
 if __name__ == '__main__':
