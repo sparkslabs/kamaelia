2b90ec5ee0171e28757e0ab9bf1fa54f04a89887
jlei <jlei@2682807a-6752-0410-967a-793f22d1dfe9>
2007-06-11 19:11:52 +0000

    more object-oriented version

--- trunk/Sketches/JL/IRC/plainPython/pygui.py
+++ trunk/Sketches/JL/IRC/plainPython/pygui.py
@@ -1,4 +1,4 @@
-#pygame GUI
+#simple scrolling textbox using Pygame
 
 import pygame
 import sys
@@ -7,9 +7,9 @@ import threading
 WHITE = (255, 255, 255)
 BLACK = (0,0,0)
 
-class gui(threading.Thread):
+class textbox(object):
     def __init__(self, screen_width=300, screen_height=200, text_height=14):
-        super(gui, self).__init__()
+        super(textbox, self).__init__()
         self.screen_width = screen_width
         self.screen_height = screen_height
         self.text_height = text_height
@@ -26,7 +26,14 @@ class gui(threading.Thread):
         self.writeRect = pygame.Rect((0, screen_height-text_height), (screen_width, text_height))
         pygame.display.update()
 
-    def update(self, line):            
+    def update(self, text):
+        while len(text) > self.linelen:
+            cutoff = text.rfind(' ', 0, self.linelen)
+            self.updateLine(text[0:cutoff])
+            text = text[cutoff + 1:]
+        self.updateLine(text)
+            
+    def updateLine(self, line):            
         lineSurf = self.font.render(line, True, BLACK)    
         self.screen.fill(WHITE)
         self.screen.blit(self.scratch, self.scrollingRect, self.keepRect)
@@ -35,32 +42,53 @@ class gui(threading.Thread):
         self.scratch.blit(self.screen, self.screen.get_rect())
         pygame.display.update()
 
+class inputThread(threading.Thread):
+    def __init__(self, gui):
+        super(inputThread, self).__init__()
+        self.gui = gui
+        self.done = False
+        
     def run(self):
-        while not done:
-            msg = raw_input('> ')
-            while len(msg) > self.linelen:
-                cutoff = msg.rfind(' ', 0, self.linelen)
-                self.update(msg[0:cutoff])
-                msg = msg[cutoff + 1:]
-            self.update(msg)
+        while not self.done:
+            msg = raw_input('You say: ')
+            self.gui.update(msg)
 
 class shutdownThread(threading.Thread):
+    def __init__(self, threads):
+        super(shutdownThread, self).__init__()
+        self.threads = threads
+        self.done = False
+        
     def checkForShutdown(self):
-        global done
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 print 'Press any key to shut down...'
-                done = True
-
+                for one_thread in self.threads:
+                    one_thread.done = True
+                self.done = True
+                
     def run(self):
-        while not done:
+        while not self.done:
             self.checkForShutdown()
 
 class refreshThread(threading.Thread):
+    done = False
     def run(self):
-        while not done:
+        while not self.done:
             pygame.display.update()
 
+class ui(object):
+    def __init__(self):
+        super(ui, self).__init__()
+        gui = textbox()
+        input1 = inputThread(gui)
+        refresh = refreshThread()
+        shutdown = shutdownThread([input1, refresh])
+        input1.start()
+        refresh.start()
+        shutdown.start()
+
 done = False
-gui().start()
-shutdownThread().start()
+if __name__ == '__main__':
+    UI = ui()
+    
