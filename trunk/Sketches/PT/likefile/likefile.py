e392809f445827b2a3bb36180316bf092856a78b
pitt2 <pitt2@2682807a-6752-0410-967a-793f22d1dfe9>
2007-06-15 09:45:58 +0000

    syntax/usability changes and hooks for future features.
    Patrick

--- trunk/Sketches/PT/likefile/likefile.py
+++ trunk/Sketches/PT/likefile/likefile.py
@@ -46,6 +46,10 @@ from Axon.Ipc import producerFinished, shutdownMicroprocess
 import Queue, threading, time, copy, Axon
 queuelengths = 0
 
+class SchedulerShutdown(Exception):
+    """An exception used internally to provide a way of shutting down a thread."""
+    pass
+
 class dummyComponent(Axon.Component.component):
     """A dummy component. Functionality: None. Prevents the scheduler from dying immediately.
     Currently this object prevents the scheduler from cleanly exiting, but most components don't
@@ -66,7 +70,10 @@ class schedulerThread(threading.Thread):
         self.setDaemon(True) # Die when the caller dies
     def run(self):
         dummyComponent().activate() # to keep the scheduler from exiting immediately.
-        scheduler.run.runThreads(slowmo = self.slowmo)
+        try:
+            scheduler.run.runThreads(slowmo = self.slowmo)
+        except SchedulerShutdown:
+            pass
         schedulerThread.lock.release()
 
 
@@ -77,26 +84,27 @@ class componentWrapper(Axon.AdaptiveCommsComponent.AdaptiveCommsComponent):
         super(componentWrapper, self).__init__()
         self.queuelengths = queuelengths
         self.child = childcomponent
-        self.inqueues = dict() # queue for data traversing from, e.g, stdin to a component's inbox
-        self.outqueues = dict()# queue for data traversing from, e.g, stdout to a component's outbox
+        self.inqueues = dict()
+        self.outqueues = dict()
         self.addChildren(self.child)
 
-        # for now, these are hard-coded.
         # this means, e.g. our own outbox is linked to the child's inbox. childSink:parentSource
         # used to deliver information to a wrapped component from non-kamaelionic environments.
         self.childInboxMapping = { "inbox": "outbox", "control": "signal" }
         if extrainboxes:
-            for boxname in extrainboxes.iterkeys():
-                self.addOutbox(boxname)
-            self.childInboxMapping.update(extrainboxes)
+            if type(extrainboxes) == str(): extrainboxes = (extrainboxes,)
+            for boxname in extrainboxes:
+                realboxname = self.addOutbox(boxname)
+                self.childInboxMapping[realboxname] = boxname
 
         # this means, e.g. the child's outbox is linked to our own inbox. childSource:parentSink
         # used to retrieve information from a kamaelia system to a non-kamaelionic environment.
         self.childOutboxMapping = { "outbox": "inbox", "signal": "control" }
         if extraoutboxes:
-            for boxname in extraoutboxes.iterkeys():
-                self.addInbox(boxname)
-            self.childOutboxMapping.update(extraoutboxes)
+            if type(extraoutboxes) == str(): extraoutboxes = (extraoutboxes,)
+            for boxname in extraoutboxes:
+                realboxname = self.addInbox(boxname)
+                self.childOutboxMapping[realboxname] = boxname
 
         for childSink, parentSource in self.childInboxMapping.iteritems():
             self.inqueues[childSink] = Queue.Queue(self.queuelengths)
@@ -113,6 +121,7 @@ class componentWrapper(Axon.AdaptiveCommsComponent.AdaptiveCommsComponent):
     def main(self):
         self.child.activate()
         while True:
+            yield 1
             for childSink, parentSource in self.childInboxMapping.iteritems():
                 queue = self.inqueues[childSink]
                 # to aid a lack of confusion, this is where information would traverse from stdin to a child component's inbox.
@@ -124,8 +133,10 @@ class componentWrapper(Axon.AdaptiveCommsComponent.AdaptiveCommsComponent):
                         except noSpaceInBox, e:
                             raise "Box delivery failed despite box (earlier) reporting being not full. Is more than one thread directly accessing boxes?"
                         if isinstance(msg, shutdownMicroprocess) or isinstance(msg, producerFinished):
-                            return # relying on a child component to propogate a shutdown back to our own control inbox is potentially flawed.
-                    else: # if the component's inboxes are full, do something here. Preferably not succeed.
+                            return
+                            # relying on a child component to propogate a shutdown back to our own control inbox is potentially flawed.
+                    else:
+                        # if the component's inboxes are full, do something here. Preferably not succeed.
                         break
 
 
@@ -136,41 +147,46 @@ class componentWrapper(Axon.AdaptiveCommsComponent.AdaptiveCommsComponent):
                     if not queue.full():
                         msg = self.recv(parentSink)
                         queue.put_nowait(msg)
-                    else: break # permit a horrible backlog to build up inside our boxes. What could go wrong?
-            yield 1
+                    else:
+                        break
+                        # permit a horrible backlog to build up inside our boxes. What could go wrong?
+
 
 class LikeFile(object):
     """An interface to the message queues from a wrapped component, which is activated on a backgrounded scheduler."""
     def __init__(self, componenttowrap, extrainboxes = None, extraoutboxes = None):
         if schedulerThread.lock.acquire(False): 
             schedulerThread.lock.release()
-            raise "no running scheduler found! Did you end all running components?"
+            raise "no running scheduler found!"
         component = componentWrapper(componenttowrap, extrainboxes, extraoutboxes)
+        # if invalid boxes are requested, the link() call will fail verbosely in the above
+        # line. You may want to make this slightly more userfriendly.
         self.inqueues = copy.copy(component.inqueues)
         self.outqueues = copy.copy(component.outqueues)
-        # reaching into the component like this is threadsafe since it has not been activated yet, and __init__
-        # runs in the current thread
+        # reaching into the component like this is threadsafe since it has not been activated yet.
         self.component = component
         self.alive = False
 
-##    def addlinkage(self):
+##    def addInbox(self):
 ##        """This will add an additional linkage, to access a wrapped component's
 ##        boxes from the likefile interface. Can be called during execution."""
 
+
     def activate(self):
-        if self.alive: return # do nothing.
         """activates the component, etc."""
+        if self.alive:
+            return
         self.component.activate() # threadsafe, see note 1
-        del self.component # we should not retain a reference since subsequent operations will not be threadsafe.
         self.alive = True
 
     def get(self, boxname):
-        if self.alive: 
+        if self.alive:
             return self.outqueues[boxname].get()
         else: raise "shutdown was previously called!"
 
     def put(self, msg, boxname):
-        if self.alive: self.inqueues[boxname].put_nowait(msg)
+        if self.alive:
+            self.inqueues[boxname].put_nowait(msg)
         else: raise "shutdown was previously called!"
 
     def shutdown(self):
@@ -180,11 +196,13 @@ class LikeFile(object):
             self.inqueues["control"].put_nowait(Axon.Ipc.shutdown()) # legacy support.
             self.inqueues["control"].put_nowait(Axon.Ipc.producerFinished())
             self.inqueues["control"].put_nowait(Axon.Ipc.shutdownMicroprocess()) # should be last, this is what we honour
-        else: raise "shutdown was previously called!"
+        else:
+            raise "shutdown was previously called, or we were never activated."
         self.alive = False
 
     def __del__(self):
-        if self.alive: self.shutdown()
+        if self.alive:
+            self.shutdown()
 
 
 if __name__ == "__main__":
