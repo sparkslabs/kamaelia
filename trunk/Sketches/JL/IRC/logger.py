c6d23f864afeb28d0adb62f3856b47ef508a4917
jlei <jlei@2682807a-6752-0410-967a-793f22d1dfe9>
2007-07-01 21:43:23 +0000

    makes a new log file for every date.

--- trunk/Sketches/JL/IRC/logger.py
+++ trunk/Sketches/JL/IRC/logger.py
@@ -4,12 +4,15 @@ from Kamaelia.Util.Console import ConsoleReader, ConsoleEchoer
 from Kamaelia.File.Writing import SimpleFileWriter
 from Kamaelia.Chassis.Pipeline import Pipeline
 from Kamaelia.Chassis.Graphline import Graphline
+from Kamaelia.Chassis.Carousel import Carousel
 from Kamaelia.Util.PureTransformer import PureTransformer
+from Kamaelia.Util.Fanout import Fanout
 from Axon.Component import component
 from Axon.Ipc import WaitComplete
 import time
 from guitest import outformat
 from IRCClient import SimpleIRCClientPrefab
+
 
 class Logger(component):
     
@@ -17,6 +20,7 @@ class Logger(component):
                 "outbox" : "What we're interested in, the traffic over the channel",
                 "system" : "Messages directed toward the client, numeric replies, etc.",
                 "signal" : "Shutdown handling in the future",
+                "date" : "For sending out the date" #useful if your logfiles are going to be encapsulated in Carousels
                 }
         
     def __init__(self, channel, formatter=outformat, name="jinnaslogbot"):
@@ -24,36 +28,101 @@ class Logger(component):
         self.channel = channel
         self.format = formatter
         self.name = name
+        self.lastdatestring = self.currentDateString()
 
     def login(self):
         self.send(("NICK", self.name), "irc")
         self.send(("USER", self.name, self.name, self.name, self.name), "irc")
+        self.send(("PRIVMSG", 'nickserv', "identify abc123"), "irc")
         self.send(("JOIN", self.channel), "irc")
         
     def main(self):
         self.login()
+        self.send(self.lastdatestring, "date")
+        yield 1
+        
         while True:
+            if self.currentDateString() != self.lastdatestring:
+                self.lastdatestring = self.currentDateString()
+                self.send(self.lastdatestring, "date")
+                
             yield 1
+            
             while self.dataReady("inbox"):
                 data = self.recv("inbox")
                 formatted_data = self.format(data)
-                if data[2] == self.channel:
+                if data[2] == self.channel and formatted_data: #format might return None
                     self.send(formatted_data, "outbox")
-                else:
+                elif formatted_data:
+                    self.respond(data) 
                     self.send(formatted_data, "system")
 
-            
+    def respond(self, msg):
+        if msg[0] == 'PRIVMSG' and msg[2] == self.name:
+            words = msg[3].split()
+            replyLines = ""
+            if words[0] == 'logfile':
+                replyLines = ["feature in progress!"]
+            elif words[0] == 'help':
+                replyLines = ["Lines prefixed by [off] won't get recorded",
+                              "Name: %s   Channel: %s" % (self.name, self.channel)
+                              ]
+            elif words[0] == 'date':
+                replyLines = [self.currentDateString()]
+
+            if replyLines:
+                for reply in replyLines:
+                    self.send(('PRIVMSG', msg[1], reply), "irc")
+                    self.send("Reply: %s \n" % reply, "system")
+                
+    def currentDateString(self):
+       curtime = time.gmtime()
+       return time.strftime("%d-%m-%Y-%M", curtime)
+
+
+#unused code, but I wanted to have it in the repository anyway
+class DateChecker(component):
+    """Sends the new filename to its outbox every time there is a date change"""
+    def __init__(self):
+        super(DateChecker, self).__init__()
+        self.lastdatestring = self.currentDateString()
+
+    def main(self):
+        self.send(self.lastdatestring)
+        while True: 
+            yield 1
+            if self.currentDateString() != self.lastdatestring:
+                self.lastdatestring = self.currentDateString()
+                self.send(self.lastdatestring)
+
+    def currentDateString(self):
+        curtime = time.gmtime()
+        return time.strftime("%d-%m-%Y", curtime)
+#/unused code
+
+
+def LogFile(channel, prefix="", suffix=""):
+    def getfile(changybit):
+        return SimpleFileWriter(prefix+channel.lstrip('#')+changybit+suffix)
+    return getfile
+
+
 def LoggerPrefab(channel):
     return Graphline(irc = SimpleIRCClientPrefab(),
                      logger = Logger(channel),
-                     log = SimpleFileWriter("%s%i.log" % (channel[1:], time.time())),
-                     info = SimpleFileWriter("%s%i.info" % (channel[1:], time.time())),
+                     split = Fanout(("toLog", "toInfo")),
+                     log = Carousel(LogFile(channel, suffix=".log")),
+                     info = Carousel(LogFile(channel, suffix=".info")),
                      linkages = {("logger", "irc") : ("irc", "inbox"),
                                  ("irc", "outbox") : ("logger", "inbox"),
                                  ("logger", "outbox"): ("log", "inbox"),
                                  ("logger", "system"): ("info", "inbox"),
-                               }
-                     )
+
+                                 ("logger", "date") : ("split", "inbox"),
+                                 ("split", "toLog") : ("log", "next"),
+                                 ("split", "toInfo") : ("info", "next"),
+                                }
+                     ) 
     
 if __name__ == '__main__':
     LoggerPrefab('#kamtest').run()
