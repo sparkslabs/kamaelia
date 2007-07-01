b0f4433f4690bfef5443f57eab552573ec55ec92
pitt2 <pitt2@2682807a-6752-0410-967a-793f22d1dfe9>
2007-07-01 13:39:35 +0000

    Started to update tests due to stabilised external interface of likefile (for the moment).
    Patrick.

--- Sketches/PT/likefile/testlikefile.py
+++ Sketches/PT/likefile/testlikefile.py
@@ -3,26 +3,30 @@
 from likefile import LikeFile, schedulerThread
 from Axon.Component import component
 from Axon.Ipc import producerFinished, shutdownMicroprocess
-import unittest, random, Axon
+import unittest, random, Axon, threading, time
+
+scheduler = schedulerThread(slowmo=0.001)
+scheduler.start()
+
 
 class DyingShunt(component):
     """A component which passes all data through itself, and terminates on receipt of 
     shutdownMicroprocess() or producerFinished()"""
     Inboxes = { "inbox"   : "Input data",
                 "control" : "Control data",
-                "extra"   : "An additional nonstandard inbox",
+                "extrain"   : "An additional nonstandard inbox",
               }
     Outboxes = { "outbox" : "Input data is echoed here",
                  "signal" : "Control data is echoed here",
-                 "extra"  : "Extra data is echoed here",
+                 "extraout"  : "Extra data is echoed here",
                }
     def main(self):
         while True:
             yield 1
             while self.dataReady("inbox"):
                 self.send(self.recv("inbox"), "outbox")
-            while self.dataReady("extra"):
-                self.send(self.recv("extra"), "extra")
+            while self.dataReady("extrain"):
+                self.send(self.recv("extrain"), "extraout")
             while self.dataReady("control"):
                 data = self.recv("control")
                 self.send(data, "signal")
@@ -32,11 +36,11 @@ class DyingShunt(component):
 class Dummy(component):
     Inboxes = { "inbox"   : "Input data",
                 "control" : "Control data",
-                "extra"   : "An additional nonstandard inbox",
+                "extraout"   : "An additional nonstandard inbox",
               }
     Outboxes = { "outbox" : "Input data is echoed here",
                  "signal" : "Control data is echoed here",
-                 "extra"  : "Extra data is echoed here",
+                 "extrain"  : "Extra data is echoed here",
                }
     def main(self):
         while True:
@@ -45,6 +49,7 @@ class Dummy(component):
 class Test_DyingShunt(unittest.TestCase):
     """A test for the test dummy component used to test likefile. If this test passes, the behaviour of DyingShunt is assumed to always work."""
     def setUp(self):
+        self.oldRun = Axon.Scheduler.scheduler.run
         self.randlist = [random.random() for x in xrange(0, 10)]
         self.scheduler = Axon.Scheduler.scheduler()
         Axon.Scheduler.scheduler.run = self.scheduler
@@ -52,16 +57,17 @@ class Test_DyingShunt(unittest.TestCase):
         self.inSrc = Dummy()
         self.inSrc.link((self.inSrc,"outbox"), (self.shunt,"inbox"))
         self.inSrc.link((self.inSrc,"signal"), (self.shunt,"control"))
-        self.inSrc.link((self.inSrc,"extra"), (self.shunt,"extra"))
+        self.inSrc.link((self.inSrc,"extrain"), (self.shunt,"extrain"))
         self.outDest = Dummy()
         self.outDest.link((self.shunt,"outbox"), (self.outDest,"inbox"))
         self.outDest.link((self.shunt,"signal"), (self.outDest,"control"))
-        self.outDest.link((self.shunt,"extra"), (self.outDest,"extra"))
+        self.outDest.link((self.shunt,"extraout"), (self.outDest,"extraout"))
         self.run = self.scheduler.main()
         self.shunt.activate()
 
     def tearDown(self):
         del self.run, self.shunt, Axon.Scheduler.scheduler.run
+        Axon.Scheduler.scheduler.run = self.oldRun
     def runFor(self, iterations):
         for i in xrange(0, iterations):
             self.run.next()
@@ -69,13 +75,13 @@ class Test_DyingShunt(unittest.TestCase):
     def test_passthrough(self):
         for i in self.randlist:
             self.inSrc.send(i, "outbox")
-            self.inSrc.send(i, "signal")
-            self.inSrc.send(i, "extra")
+            self.inSrc.send(i + 1, "signal")
+            self.inSrc.send(i + 2, "extrain")
         self.runFor(20) # shouldn't terminate
         for i in self.randlist:
             self.failUnless(self.outDest.recv("inbox") == i)
-            self.failUnless(self.outDest.recv("control") == i)
-            self.failUnless(self.outDest.recv("extra") == i)
+            self.failUnless(self.outDest.recv("control") == i + 1)
+            self.failUnless(self.outDest.recv("extraout") == i + 2)
     def test_shutdown1(self):
         self.inSrc.send(shutdownMicroprocess(), "signal")
         self.failUnlessRaises(StopIteration, self.runFor, iterations = 10)
@@ -85,6 +91,25 @@ class Test_DyingShunt(unittest.TestCase):
         self.failUnlessRaises(StopIteration, self.runFor, iterations = 10)
         self.failUnless(isinstance(self.outDest.recv("control"), producerFinished)) # pass through the shutdown code
 
+
+class test_LikeFileClosure(unittest.TestCase):
+    def build(self):
+        self.component = LikeFile(DyingShunt())
+        self.component.activate()
+        time.sleep(0.1)
+
+    def collapse(self):
+        self.component.shutdown()
+        del self.component
+
+    def test_setup_shutdown(self):
+        """a test to make sure that there's no cruft left in a scheduler from the passing existence of
+        a likefile, assuming the wrapped component honours shutdown messages."""
+        numthreads, numcomponents = threading.activeCount, len(Axon.Scheduler.scheduler.run.threads)
+        self.build()
+        self.collapse()
+        time.sleep(0.1)
+        self.failUnless(numcomponents == len(Axon.Scheduler.scheduler.run.threads))
 
 if __name__ == "__main__":
     unittest.main()
