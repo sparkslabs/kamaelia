ca0e52248c0c8caadf9bf133288f4fed284fffb6
jlei <jlei@2682807a-6752-0410-967a-793f22d1dfe9>
2007-07-02 15:58:30 +0000

    reversion

--- Sketches/JL/IRC/logger.py
+++ Sketches/JL/IRC/logger.py
@@ -8,26 +8,34 @@ from guitest import outformat
 from IRCClient import SimpleIRCClientPrefab
 import time
 
-class BasicLogger(component):
+class Logger(component):
     
     Outboxes = {"irc" : "to IRC, for user responses and login",
                 "outbox" : "What we're interested in, the traffic over the channel",
                 "system" : "Messages directed toward the client, numeric replies, etc.",
                 "signal" : "Shutdown handling in the future",
+
+                "log_next" : "for the Log Carousel",
+                "info_next" : "for the Info Carousel"
                 }
 
-
-    def __init__(self, channel, formatter=outformat, name="jinnaslogbot"):
-        super(BasicLogger, self).__init__()
+    def __init__(self, channel, formatter=outformat, name="jinnaslogbot", logdir=""):
+        super(Logger, self).__init__()
         self.channel = channel
         self.format = formatter
         self.name = name
-        self.Responses = {"help" : ["Lines prefixed by [off] won't get recorded",
-                                  "Name: %s   Channel: %s" % (self.name, self.channel)
-                                  ],
-                          "dance" : ['\x01ACTION does the macarena\x01'],
-                         }
-        
+        self.logdir = logdir
+        self.logname = ""
+        self.infoname = ""
+
+        Graphline(log = Carousel(SimpleFileWriter),
+                  info = Carousel(SimpleFileWriter),
+                  logger = self,
+                  linkages = {("logger", "log_next") : ("log", "next"),
+                              ("logger", "info_next") : ("info", "next"),
+                              ("logger", "outbox") : ("log", "inbox"),
+                              ("logger", "system") : ("info", "inbox"),
+                              }).activate()
 
     def login(self):
         self.send(("NICK", self.name), "irc")
@@ -37,92 +45,66 @@ class BasicLogger(component):
         
     def main(self):
         self.login()
-        yield 1
-        
-        while True:
-            yield 1
-            while self.dataReady("inbox"):
-                data = self.recv("inbox")
-                formatted_data = self.format(data)
-                if data[2] == self.channel and formatted_data: #format might return None
-                    self.send(formatted_data, "outbox")
-                elif formatted_data:
-                    self.send(formatted_data, "system")
-                    self.respond(data) #changed order of these two lines. Wait and See. 
-
-    def respond(self, msg):
-        if msg[0] == 'PRIVMSG' and msg[2] == self.name:
-            replyLines = self.Responses.get(msg[3])
-            if replyLines:
-                for reply in replyLines:
-                    self.send(('PRIVMSG', msg[1], reply), "irc")
-                    self.send("Reply: %s \n" % reply, "system")
-
-
-class DateNamedLogger(BasicLogger):
-    Outboxes = {"irc" : "to IRC, for user responses and login",
-                "outbox" : "What we're interested in, the traffic over the channel",
-                "system" : "Messages directed toward the client, numeric replies, etc.",
-                "signal" : "Shutdown handling in the future",
-                "log_next" : "For requesting the next log file",
-                "info_next" : "For requesting the next system log file",
-            }
-
-    def __init__(self, channel, logdir="", **argd): 
-        super(DateNamedLogger, self).__init__(channel, **argd)
-        self.logdir = logdir
-        self.channel = channel
-        self.Responses['What else floats in water?'] = ['Churches!']
-        
-        subsystem = Graphline(log = Carousel(SimpleFileWriter),
-                              info = Carousel(SimpleFileWriter),
-                              irc = SimpleIRCClientPrefab(),
-                              dnl = self,
-                              linkages = {("irc", "outbox") : ("dnl", "inbox"),
-                                          ("dnl", "irc") : ("irc", "inbox"),
-                                          ("dnl", "outbox") : ("log", "inbox"),
-                                          ("dnl", "system") : ("info", "inbox"),
-                                          ("dnl", "log_next") : ("log", "next"),
-                                          ("dnl", "info_next") : ("info", "next"),
-                                          })
-        self.addChildren(subsystem)
-        subsystem.activate()
-
-    def main(self):
-        self.login()
         self.changeDate()
         yield 1
         
         while True:
-            if self.lastdatestring != self.currentDateString():
+            if self.currentDateString() != self.lastdatestring:
+                self.lastdatestring = self.currentDateString()
                 self.changeDate()
-            yield 1
-
+                
+            yield 1 
             while self.dataReady("inbox"):
                 data = self.recv("inbox")
                 formatted_data = self.format(data)
                 if data[2] == self.channel and formatted_data: #format might return None
                     self.send(formatted_data, "outbox")
                 elif formatted_data:
+                    self.respond(data) 
                     self.send(formatted_data, "system")
-                    self.respond(data)    
-            
+
+    def respond(self, msg):
+        if msg[0] == 'PRIVMSG' and msg[2] == self.name:
+            words = msg[3].split()
+            replyLines = ""
+            if words[0] == 'logfile':
+                replyLines = [self.logname]
+            elif words[0] == 'infofile':
+                replyLines = [self.infoname]
+            elif words[0] == 'help':
+                replyLines = ["Lines prefixed by [off] won't get recorded",
+                              "Name: %s   Channel: %s" % (self.name, self.channel)
+                              ]
+            elif words[0] == 'date':
+                replyLines = [self.currentDateString()]
+            elif words[0] == 'dance':
+                replyLines = ['\x01ACTION does the macarena\x01']
+
+            if replyLines:
+                for reply in replyLines:
+                    self.send(('PRIVMSG', msg[1], reply), "irc")
+                    self.send("Reply: %s \n" % reply, "system")
                 
+    def currentDateString(self):
+       curtime = time.gmtime()
+       return time.strftime("%d-%m-%Y", curtime)
+
     def changeDate(self):
         self.lastdatestring = self.currentDateString()
         self.logname = self.logdir+self.channel.lstrip('#')+self.lastdatestring+'.log'
         self.infoname = self.logdir+self.channel.lstrip('#')+self.lastdatestring+'.info'
         self.send(self.logname, "log_next")
         self.send(self.infoname, "info_next")
-        self.Responses['logfile'] = [self.logname]
-        self.Responses['infofile']= [self.infoname]
-        self.Responses['date'] = [self.lastdatestring]
-        
 
-    def currentDateString(self):
-       curtime = time.gmtime()
-       return time.strftime("%d-%m-%Y", curtime)
 
+
+def LoggerPrefab(channel):
+    return Graphline(irc = SimpleIRCClientPrefab(),
+                     logger = Logger(channel),
+                     linkages = {("logger", "irc") : ("irc", "inbox"),
+                                 ("irc", "outbox") : ("logger", "inbox"),
+                                }
+                     ) 
     
 if __name__ == '__main__':
-    DateNamedLogger('#kamtest').run()
+    LoggerPrefab('#kamtest').run()
