2f9fdaac5c4693bad970475b740e5f5bc7910ff9
tara- <tara-@2682807a-6752-0410-967a-793f22d1dfe9>
2007-07-03 07:59:54 +0000

    would probably help if i hadn't block commented an entire file...

--- Sketches/TG/cshard/cshardtest.py
+++ Sketches/TG/cshard/cshardtest.py
@@ -1,93 +1,93 @@
-from fshard import *
+from cshard import *
 
 """
 Code generation testing
 """
 
-# importmodules
-#~ imps = importmodules('lala', 'doo', 'ming', wheee = ['huphup', 'pop', 'pip'], nanoo = ('noom', ))
-#~ for line in imps:
-#~     print line,
+#~ # importmodules
+imps = importmodules('lala', 'doo', 'ming', wheee = ['huphup', 'pop', 'pip'], nanoo = ('noom', ))
+for line in imps:
+    print line,
 
-# indent
-#~ impsind = indent(imps, level = 1)
-#~ for line in impsind:
-#~     print line,
+#~ # setindent
+impsind = setindent(imps, level = 1)
+for line in impsind:
+    print line,
 
-#~ impsind = indent(imps, level = 2)
-#~ for line in impsind:
-#~     print line,
+impsind = setindent(imps, level = 2)
+for line in impsind:
+    print line,
 
-#~ impsind = indent(impsind, level = 1)  # additional level
-#~ for line in impsind:
-#~     print line,
+impsind = setindent(impsind)
+for line in impsind:
+    print line,
 
-# makeclass
-#~ for line in makeclass("CMagnaDoodle"):
-#~     print line,
-#~ for line in makeclass("CMagnaDoodle", []):
-#~     print line,
-#~ for line in makeclass("CMagnaDoodle", ['Axon.Component.component']):
-#~     print line,
-#~ for line in makeclass("CMagnaDoodle", ['Axon.Component.component', 'dummy']):
-#~     print line,
+#~ # makeclass
+for line in makeclass("CMagnaDoodle"):
+    print line,
+for line in makeclass("CMagnaDoodle", []):
+    print line,
+for line in makeclass("CMagnaDoodle", ['Axon.Component.component']):
+    print line,
+for line in makeclass("CMagnaDoodle", ['Axon.Component.component', 'dummy']):
+    print line,
 
-# makedoc
-#~ doc = "one line doc"
-#~ docs = makedoc(doc)
-#~ for line in docs:
-#~     print line,
+#~ # makedoc
+doc = "one line doc"
+docs = makedoc(doc)
+for line in docs:
+    print line,
 
-#~ doc = "manymany\nline\ndoc\ndoo doo doo"
-#~ docs = makedoc(doc)
-#~ for line in docs:
-#~     print line,
+doc = "manymany\nline\ndoc\ndoo doo doo"
+docs = makedoc(doc)
+for line in docs:
+    print line,
 
-# makeboxes
-#~ for line in makeboxes():
-#~     print line,
-#~ print
+#~ # makeboxes
+for line in makeboxes():
+    print line,
+print
 
-#~ for line in makeboxes(True, False):
-#~     print line,
-#~ print
+for line in makeboxes(True, False):
+    print line,
+print
 
-#~ for line in makeboxes(inboxes = False, default = True):
-#~     print line,
-#~ print
+for line in makeboxes(inboxes = False, default = True):
+    print line,
+print
 
-#~ for line in makeboxes(inboxes = True, default = False, doo = "useless box", dum = "twin"):
-#~     print line,
-#~ print
+for line in makeboxes(inboxes = True, default = False, doo = "useless box", dum = "twin"):
+    print line,
+print
 
-#~ for line in makeboxes(True, True, doo = "useless box", dum = "twin"):
-#~     print line,
-#~ print
+for line in makeboxes(True, True, doo = "useless box", dum = "twin"):
+    print line,
+print
 
-# getshard
-#~ from CDrawing import *
+#~ # getshard
+from CDrawing import *
 
-#~ for line in getshard(drawBG):
-#~     print line,
-#~ print
+for line in getshard(drawBG):
+    print line,
+print
 
-#~ for line in getshard(drawBG, 2):
-#~     print line,
-#~ print
+for line in getshard(drawBG, 2):
+    print line,
+print
 
-#~ for line in getshard(drawBG, 0):
-#~     print line,
-#~ print
+for line in getshard(drawBG, 0):
+    print line,
+print
 
-#~ for line in getshard(blitToSurface, 3):
-#~     print line,
-#~ print
+for line in getshard(blitToSurface, 3):
+    print line,
+print
 
-#~ for line in getshard(displaySetup):
-#~     print line,
-#~ print
+for line in getshard(displaySetup):
+    print line,
+print
 
-# annotateshard
+#~ # annotateshard
 from CDrawing import *
 for line in annotateshard(getshard(drawBG), "drawBG"):
     print line,
