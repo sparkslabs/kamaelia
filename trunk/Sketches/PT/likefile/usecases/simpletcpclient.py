e4159b5d19284b6cb2e6e305b35c276cae64fa21
pitt2 <pitt2@2682807a-6752-0410-967a-793f22d1dfe9>
2007-07-12 14:48:45 +0000

    Trivial tcp client example using likefile.
    Patrick

---
+++ trunk/Sketches/PT/likefile/usecases/simpletcpclient.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+# This is a quick example of using kamaelia as a general tcp client in your system.
+
+
+host = "irc.freenode.net"
+port = 6667
+
+import likefile, time
+from Kamaelia.Util.Console import ConsoleEchoer
+from Kamaelia.Internet.TCPClient import TCPClient
+likefile.schedulerThread(slowmo=0.01).start()
+
+print "what channel on freenode?"
+channel = raw_input(">>> ")
+
+client = likefile.LikeFile(TCPClient(host = host, port = port))
+client.activate()
+time.sleep(1)
+client.put("user likefile likefile likefile :likefile\n")
+client.put("nick likefile\n")
+client.put("JOIN %s\n" % channel)
+while True:
+    print client.get()
+
+
+
