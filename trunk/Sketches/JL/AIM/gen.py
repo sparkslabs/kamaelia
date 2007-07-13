357d2ab7f46bb5ab94293058d7db866220b87dd9
jlei <jlei@2682807a-6752-0410-967a-793f22d1dfe9>
2007-07-13 17:46:46 +0000

    working OSCARClient and example

--- trunk/Sketches/JL/AIM/gen.py
+++ trunk/Sketches/JL/AIM/gen.py
@@ -1,144 +1,133 @@
 #!/usr/bin/env python
 
-import md5
-import struct
 import pickle
-from aimUtil import *
+import md5
 from Axon.ThreadedComponent import threadedcomponent
-from Kamaelia.Internet.TCPClient import TCPClient
+from Axon.Component import component
+from Axon.Ipc import shutdownMicroprocess, WaitComplete, ipc
+from OSCARClient import OSCARClient
+from oscarutil import *
 
 screenname = 'kamaelia1'
 password = 'abc123'
-seq = getseqnum()
+CLIENT_ID_STRING = "KamaeliaAIM/0.01"
 
-class AuthCookieGetter(threadedcomponent):
+class AuthCookieGetter(component):
+    Outboxes = {"outbox" : "the only thing that comes out is the auth cookie and BOS server",
+                "strings" : "non-snac requests to AIM",
+                "snac" : "snacs to go out to AIM",
+                "signal" : "shutdown handling",
+                }
+    
+    def __init__(self):
+        super(AuthCookieGetter, self).__init__()
+        self.versionNumber = 1
+        self.debugger.addDebugSection("AuthCookieGetter.main", 5)
+        
     def main(self):
-    ### ============= initial connection =====================###
-        flap = '*' + channel1 + seq.next() + '\x00\x04' + '\x00\x00\x00\x01'
-        self.send(flap)
-        printHex(self.recv())
-    ###============ send client key request =====================###
-        snac_fam = 0x17
-        snac_sub = 0x06
-        snac_flags = 0
-        snac_reqid = 1
+        yield WaitComplete(self.handshake())
+        for value in self.getMD5key():
+            if value == 1: yield value
+            else: md5key = value
+        for value in self.getBOSandAuthCookie(md5key):
+            if value == 1: yield value
+            else: reply = value
+            
+        #save data, in case we need it for debugging later
+        fle = open("snac1703.dat", "wb")
+        pickle.dump(reply, fle)
+        fle.close()
+        snac_body = reply[FLAP_HEADER_LEN+SNAC_HEADER_LEN:]
+        parsed = readTLVs(snac_body)
+        BOS_server = parsed[0x05]
+        BOS_server, port = BOS_server.split(':')
+        port = int(port)    
+        auth_cookie = parsed[0x06]
+        self.send((BOS_server, port, auth_cookie))
+        self.send(shutdownMicroprocess(), "signal")
+    
+    def handshake(self):
+        data = struct.pack('!i', self.versionNumber)
+        self.send((CHANNEL1, data), "strings")
+        while not self.dataReady("inbox"):
+            yield 1
+        reply = self.recv() #should be server ack of new connection
+        assert self.debugger.note("AuthCookieGetter.main", 5, "received new connection ack")
 
-        tlv_type = 0x01
-        payload1 = chrs((len(screenname)), 2) + screenname 
-        payload1 = chrs(tlv_type, 2) + payload1
-
-        tlv_type = 0x4b
-        payload2 = chrs(0, 2)
-        payload2 = chrs(tlv_type, 2) + payload2
-
-        tlv_type = 0x5a
-        payload3 = chrs(0, 2)
-        payload3 = chrs(tlv_type, 2) + payload3
-
-        payload = payload1 + payload2 + payload3
-
-        snac = chrs(snac_fam, 2) + chrs(snac_sub, 2) + chrs(snac_flags, 2) + chrs(snac_reqid, 4) + payload
-
-        fieldLen = len(snac)
-        flap = '*' + channel2 + seq.next() + chrs(fieldLen, 2) + snac
-        self.send(flap)
-    ###============ get md5 key ================================###
-        reply = self.recv()
-        printHex(reply) 
-        md5len = reply[16:18]
-        md5key = reply[18:]
-    ###=============== send encrypted password ==================###
-        snac_fam = 0x17
-        snac_sub = 0x02
-        snac_flags = 0
-        snac_reqid = 0x02
+    def getMD5key(self):
+        zero = struct.pack('!H', 0)
         snac_body = ""
+        snac_body += TLV(0x01, screenname)
+        snac_body += TLV(0x4b, zero)
+        snac_body += TLV(0x5a, zero)
+        self.send(((0x17, 0x06), snac_body), "snac")
+        # get md5 key
+        while not self.dataReady("inbox"):
+            yield 1
+        reply = self.recv()
+        assert self.debugger.note("AuthCookieGetter.main", 5, "received md5 key")
+        md5key = reply[18:]
+        yield md5key
 
-        tlv_type = 0x01
-        payload = screenname
-        tlv = chrs(tlv_type, 2) + chrs(len(payload), 2) + payload
-        snac_body += tlv
+    def getBOSandAuthCookie(self, md5key):
+        snac_body = ""
+        snac_body += TLV(0x01, screenname)
+        snac_body += TLV(0x03, CLIENT_ID_STRING)
 
-        tlv_type = 0x03
-        client_id_string = "KamaeliaAIM/0.01"
-        payload = client_id_string
-        tlv = chrs(tlv_type, 2) + chrs(len(payload), 2) + payload
-        snac_body += tlv
-
-        ### Hash password ###
+        ### Digest password ###
         md5obj = md5.new()
         md5obj.update(md5key)
         md5obj.update(password)
         md5obj.update(AIM_MD5_STRING)
         password_hash = md5obj.digest()
-        snac_body = appendTLV(0x25, password_hash, snac_body)
-
+        snac_body += TLV(0x25, password_hash)
 
         client_id = 0x0109 #this value seems to work
-        snac_body = appendTLV(0x16, chrs(client_id, 2), snac_body)
+        snac_body += TLV(0x16, Double(client_id))
 
         major_version = 0
-        snac_body = appendTLV(0x17, chrs(major_version, 2), snac_body)
+        snac_body += TLV(0x17, Double(major_version))
 
         minor_version = 0
-        snac_body = appendTLV(0x18, chrs(minor_version, 2), snac_body)
+        snac_body += TLV(0x18, Double(minor_version))
 
         lesser_version = 1
-        snac_body = appendTLV(0x19, chrs(lesser_version, 2), snac_body)
+        snac_body += TLV(0x19, Double(lesser_version))
 
-        build_num = 3036
-        snac_body = appendTLV(0x1a, chrs(build_num, 2), snac_body)
+        build_num = 1
+        snac_body += TLV(0x1a, Double(build_num))
 
         distr_num = 0
-        snac_body = appendTLV(0x14, chrs(distr_num, 4), snac_body)
+        snac_body += TLV(0x14, Double(distr_num))
 
         language = 'en'
-        snac_body = appendTLV(0x0f, language, snac_body)
+        snac_body += TLV(0x0f, language)
 
         country = 'us'
-        snac_body = appendTLV(0x0e, country, snac_body)
+        snac_body += TLV(0x0e, country)
 
         ssiflag = 1
-        snac_body = appendTLV(0x4a, chrs(ssiflag, 1), snac_body)
-
-        snac = chrs(snac_fam, 2) + chrs(snac_sub, 2) + chrs(snac_flags, 2) + chrs(snac_reqid, 4) + snac_body
-        flap = '*' + channel2 + seq.next() + chrs(len(snac), 2) + snac
-        self.send(flap)
-        reply = self.recv()
+        snac_body += TLV(0x4a, Single(ssiflag))
+        self.send(((0x17, 0x02), snac_body), "snac")
         
-        #save data, in case we need it for debugging later
-        fle = open("snac1703.dat", "wb")
-        pickle.dump(reply, fle)
-        fle.close()
-    ###================= extract BOS server and auth cookie ========================= ###
-        snac_body = reply[flap_header_len+snac_header_len:]
-        #parse a string of TLV's, returns a dictionary. tlv_type: tlv_data
-
-        parsed = parseTLVstring(snac_body)
-        for p in parsed:
-            print p,
-            printHex(parsed[p])
-            
-        BOS_server = parsed[0x05]
-        BOS_server, port = BOS_server.split(':')
-        port = int(port)
-        auth_cookie = parsed[0x06]
-
-        #save for future reference
-        fle = open("bos_auth.dat", "wb")
-        pickle.dump((BOS_server, port, auth_cookie), fle)
-        fle.close()
-
+        while not self.dataReady("inbox"):
+            yield 1
+        reply = self.recv()
+        assert self.debugger.note("AuthCookieGetter.main", 5, "received BOS/auth cookie")
+        yield reply
 
 
 from Kamaelia.Chassis.Graphline import Graphline
+from Kamaelia.Util.Console import ConsoleEchoer
 
-server = auth_server
-port = port
 Graphline(auth = AuthCookieGetter(),
-          tcp = TCPClient(server, port),
-          linkages = {("auth", "outbox") : ("tcp", "inbox"),
-                      ("tcp", "outbox") : ("auth", "inbox"),
+          oscar = OSCARClient(),
+          echo = ConsoleEchoer(),
+          linkages = {("auth", "strings") : ("oscar", "strings"),
+                      ("auth", "snac") : ("oscar", "snac"),
+                      ("oscar", "outbox") : ("auth", "inbox"),
+                      ("auth", "outbox") : ("echo", "inbox"),
+                      ("auth", "signal") : ("oscar", "control"),
                       }
           ).run()
           
