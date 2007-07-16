607f23ab1fb9ef14bfaa67071877da30f0c6eccb
pitt2 <pitt2@2682807a-6752-0410-967a-793f22d1dfe9>
2007-07-16 14:49:39 +0000

    Added simple examples of vorbis decoder components.
    Patrick.

---
+++ Sketches/PT/likefile/usecases/simpleoggradioplayer.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from likefile import LikeFile, schedulerThread
+from Kamaelia.Codec.Vorbis import VorbisDecode, AOAudioPlaybackAdaptor
+from Kamaelia.Chassis.Pipeline import Pipeline
+from Kamaelia.File.ReadFileAdaptor import ReadFileAdaptor
+from Kamaelia.Internet.TCPClient import TCPClient
+import ao
+schedulerThread(slowmo=0.001).start()
+
+filename = "./snail.ogg"
+
+playStream = LikeFile(Pipeline(VorbisDecode(), AOAudioPlaybackAdaptor()))
+playStream.activate()
+# set of components for playing the stream back.
+
+host = "bbc.kamaelia.org"
+port = 1500
+
+client = LikeFile(TCPClient(host = host, port = port))
+client.activate()
+# component to grab a stream from the internet
+
+filedump = open("streamdump.ogg", "w+b")
+
+# Play the ogg data in the background
+while True:
+    data = client.get()
+    filedump.write(data)
+    # log the stream to disk
+    playStream.put(data)
+    # and play it.
+
+# this could all be done entirely within kamaelia but using likefile
+# makes it easier to hook in external programs.
