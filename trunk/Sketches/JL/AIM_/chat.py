a9806cad03ef7d4187ba500d2ae21d3bea15bb46
jlei <jlei@2682807a-6752-0410-967a-793f22d1dfe9>
2007-08-04 03:21:19 +0000

    receives IMs properly

--- trunk/Sketches/JL/AIM_/chat.py
+++ trunk/Sketches/JL/AIM_/chat.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 from OSCARClient import *
+from oscarutil import *
 from Kamaelia.Internet.TCPClient import TCPClient
 from Axon.Component import component
 
@@ -18,7 +19,9 @@ class ChatManager(SNACExchanger):
 
     def __init__(self):
         super(ChatManager, self).__init__()
-        self.debugger.addDebugSection("ChatManager.main", 7)
+        debugSections = {"ChatManager.main" : 7,
+                        "ChatManager.receiveMessage" : 5}
+        self.debugger.addDebug(**debugSections)
 
     def main(self):
         while True:
@@ -33,10 +36,46 @@ class ChatManager(SNACExchanger):
                     buddyinfo = {"name" : buddy}
                     self.send(("buddy online", buddyinfo), "heard")
                 elif kind == (0x04, 0x07):
-                    msgid, msgchan, l = struct.unpack('!QHB', body[:11])
-                    sender = body[11:11+l]
-                    assert self.debugger.note("ChatManager.main", 7, "message from " + sender)
-                    self.send(("message", sender, "we're working on the parsing algorithm!"), "heard")
+                    self.receiveMessage(body)
                 else:
                     print "unknown message", header
-                    
+
+    def receiveMessage(self, body):
+        msgid, msgchan, l = struct.unpack('!QHB', body[:11])
+        sender = body[11:11+l]
+        assert self.debugger.note("ChatManager.receiveMessage", 7, "message from %s on channel %i" % (sender, msgchan))
+        TLVchain = body[11 + l + 4:]
+        parsed = readTLVs(TLVchain)
+        if msgchan == 1:
+            fragments = readTLVs(parsed[0x02])
+            msgText = fragments[0x0101]
+            charSetID, charSubset = struct.unpack("!HH", msgText[:4]) #ignoring these for now, but they might come in handy
+            message = msgText[4:]
+        elif msgchan == 2:
+            message = "%s requesting unsupported function" % sender
+        elif msgchan == 3:
+            message = parsed[0x05][8:]
+            
+        self.send(("message", sender, message), "heard")
+
+if __name__ == '__main__':
+    from Kamaelia.Chassis.Graphline import Graphline
+    from Kamaelia.Util.Console import ConsoleEchoer
+    import sys
+    sys.path.append('..')
+    from likefile import *
+    
+    flap = open('/home/jlei/aim/snacs/0407').read()
+    class Chargen(component):
+        def main(self):
+            self.send((2, flap[6:]))
+            yield 1
+            
+    p = Graphline(chargen = Chargen(),
+                  cm = ChatManager(),
+                  ce = ConsoleEchoer(),
+                  linkages = {("chargen", "outbox") : ("cm", "inbox"),
+                              ("cm", "heard") : ("ce", "inbox"),
+                              }
+                  )
+    p.run()
