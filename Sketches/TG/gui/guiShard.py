d6d79647536dd14d204045bee3a6815329632b34
tara- <tara-@2682807a-6752-0410-967a-793f22d1dfe9>
2007-08-06 13:27:31 +0000

    added to shard child layouts

--- Sketches/TG/gui/guiShard.py
+++ Sketches/TG/gui/guiShard.py
@@ -4,8 +4,11 @@ from pygame.rect import Rect
 
 from random import randint
 
+nicecolours = [c for k, c in colours.items() if not ('grey' in k or 'gray' in k or 'black' in k)]
+darkcolours = [x for x in nicecolours if x[0]+x[1]+x[2] < 500]
+
 def pickColour():
-    return colours.values()[randint(0, len(colours)-1)]
+    return darkcolours[randint(0, len(darkcolours)-1)]
 
 class guiShard(object):
     def __init__(self, floating, parent, row, cols):
@@ -18,27 +21,41 @@ class guiShard(object):
         self.row = row
         self.cols = cols
         
+        self.rect = self.makeRect()
+        
+        self.parent = parent
+        self.children = []
+    
+    def draw(self, surface):
+        rect(surface, self.colour, self.rect, 0)
+        for c in self.children:
+            c.draw(surface)
+    
+    def makeRect(self):
         x = self.grid.colCoord(self.cols[0])
         y = self.grid.rowCoord(self.row)
         width = self.grid.xspacing*len(cols)
         height = self.grid.yspacing
-        self.rect = Rect(x, y, width, height)
-        
-        self.parent = parent
-        self.children = [] # dict? need occupied cols info?
+        return Rect(x, y, width, height)
     
-    def draw(self, surface):
-        rect(surface, self.colour, self.rect, 0)
-        for s in self.children:
-            s.draw(surface)
+    def resize(self, newcols):
+        self.cols = newcols
+        self.rect = self.makeRect()
+    
+    def calculateCols(self, newcol):
+        return [newcol]
     
     def add(self, floating, row, col):
         if row == self.row:
-            cols = [col]  # work out which cols it should cover
+            cols = self.calculateCols(col)
             self.children += [guiShard(floating, self, row+1, cols)]
         else:
-            #find correct child and call add() on it
-            pass
+            for c in self.children:
+                if col in c.cols:
+                    c.add(floating, row, col)
+                    return
+            print 'no child covers col', col
+            
 
 
 if __name__ == '__main__':
