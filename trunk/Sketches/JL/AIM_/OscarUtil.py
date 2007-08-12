cb76f134f45cec9e242abc6aa8270cd936d63f27
jlei <jlei@2682807a-6752-0410-967a-793f22d1dfe9>
2007-08-12 23:42:09 +0000

    added an error codes dictionary and credits for it

--- trunk/Sketches/JL/AIM_/OscarUtil.py
+++ trunk/Sketches/JL/AIM_/OscarUtil.py
@@ -25,7 +25,11 @@ OSCAR Utility functions
 This file includes functions for packing and unpacking binary data and defines
 some constants useful for dealing with OSCAR.
 
-This is the first of two utility modules. OscarUtil2 is the other. 
+This is the first of two utility modules. OscarUtil2 is the other.
+
+Credit goes to Alexandr Shutko for errorCodes dictionary (and for writing an
+excellent guide on the OSCAR protocol). You can find the original table at
+http://iserverd.khstu.ru/oscar/auth_failed.html
 """
 import struct
 
@@ -72,7 +76,40 @@ class selfClass(object):
         snac = SNAC(fam, sub, text)
         printWireshark(snac)
 
-
+errorCodes = {0x0001 :   'Invalid nick or password',
+              0x0002 :	  'Service temporarily unavailable',
+              0x0003 :	  'All other errors',
+              0x0004 :	  'Incorrect nick or password, re-enter',
+              0x0005 :	  'Mismatch nick or password, re-enter',
+              0x0006 :	  'Internal client error (bad input to authorizer)',
+              0x0007 :	  'Invalid account',
+              0x0008 :	  'Deleted account',
+              0x0009 :	  'Expired account',
+              0x000A :	  'No access to database',
+              0x000B :	  'No access to resolver',
+              0x000C :	  'Invalid database fields',
+              0x000D :	  'Bad database status',
+              0x000E :	  'Bad resolver status',
+              0x000F :	  'Internal error',
+              0x0010 :	  'Service temporarily offline',
+              0x0011 :	  'Suspended account',
+              0x0012 :	  'DB send error',
+              0x0013 :	  'DB link error',
+              0x0014 :	  'Reservation map error',
+              0x0015 :	  'Reservation link error',
+              0x0016 :	  'The users num connected from this IP has reached the maximum',
+              0x0017 :	  'The users num connected from this IP has reached the maximum (reservation)',
+              0x0018 :	  'Rate limit exceeded (reservation). Please try to reconnect in a few minutes',
+              0x0019 :	  'User too heavily warned',
+              0x001A :	  'Reservation timeout',
+              0x001B :	  'You are using an older version of ICQ. Upgrade required',
+              0x001C :	  'You are using an older version of ICQ. Upgrade recommended',
+              0x001D :	  'Rate limit exceeded. Please try to reconnect in a few minutes',
+              0x001E :	  'Can\'t register on the ICQ network. Reconnect in a few minutes',
+              0x0020 :	  'Invalid SecurID',
+              0x0022 :	  'Account suspended because of your age (age < 13)',
+              }
+              
 """Lots of constants defined below"""
 #How many bytes (2 ASCII chars) each variable is
 RATE_ID_WIDTH = 2
