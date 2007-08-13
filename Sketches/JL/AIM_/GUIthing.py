c298e639cda0ce54b44fb7301de7de2ce1daa32a
jlei <jlei@2682807a-6752-0410-967a-793f22d1dfe9>
2007-08-13 22:47:35 +0000

    final checkin before branch

--- Sketches/JL/AIM_/GUIthing.py
+++ Sketches/JL/AIM_/GUIthing.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+#the locations of AIMHarness and IRC.Text may change.
+
 from AIMHarness import AIMHarness
 from IRC.Text import TextDisplayer, Textbox
 from Kamaelia.Chassis.Pipeline import Pipeline
@@ -8,10 +10,19 @@ from Kamaelia.Util.PureTransformer import PureTransformer
 def sendTo(recipient, text):
     return ("message", recipient, text)
 
-def GUIthing(screenname, password, buddyname):
+def outformat(data, buddyname):
+    if data[0] == "buddy online" and data[1]["name"] ==  buddyname:
+        return "%s is online" % buddyname
+    elif data[0] == "message" and data[1] == buddyname:
+        return "%s: %s" % (buddyname, data[2])
+    elif data[0] == "error":
+        ": ".join(data)
+
+def SimpleAIMClient(screenname, password, buddyname):
     Pipeline(Textbox(position=(0, 400)),
              PureTransformer(lambda text: sendTo(buddyname, text)),
              AIMHarness(screenname, password),
+             PureTransformer(lambda tup: outformat(tup, buddyname)),
              TextDisplayer()
              ).run()
 
@@ -32,4 +43,4 @@ if __name__ == '__main__':
 
     args = parseArgs(sys.argv[1:])
     print sys.argv
-    GUIthing(*args).run()
+    SimpleAIMClient(*args).run()
