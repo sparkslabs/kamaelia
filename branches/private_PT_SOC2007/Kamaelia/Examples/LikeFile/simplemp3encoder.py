cd31afc501b9fe34ab964fe3cec0dd5fd4f3dbec
jlei <jlei@2682807a-6752-0410-967a-793f22d1dfe9>
2007-08-20 16:57:36 +0000

    fixed some obvious bugs but it's still hanging up on 'data=enc.get()'

--- branches/private_PT_SOC2007/Kamaelia/Examples/LikeFile/simplemp3encoder.py
+++ branches/private_PT_SOC2007/Kamaelia/Examples/LikeFile/simplemp3encoder.py
@@ -1,21 +1,42 @@
 #!/usr/bin/env python
+#
+# (C) 2004 British Broadcasting Corporation and Kamaelia Contributors(1)
+#     All Rights Reserved.
+#
+# You may only modify and redistribute this under the terms of any of the
+# following licenses(2): Mozilla Public License, V1.1, GNU General
+# Public License, V2.0, GNU Lesser General Public License, V2.1
+#
+# (1) Kamaelia Contributors are listed in the AUTHORS file and at
+#     http://kamaelia.sourceforge.net/AUTHORS - please extend this file,
+#     not this notice.
+# (2) Reproduced in the COPYING file, and at:
+#     http://kamaelia.sourceforge.net/COPYING
+# Under section 3.5 of the MPL, we are using this text since we deem the MPL
+# notice inappropriate for this file. As per MPL/GPL/LGPL removal of this
+# notice is prohibited.
+#
+# Please contact us via: kamaelia-list-owner@lists.sourceforge.net
+# to discuss alternative licensing.
+# -------------------------------------------------------------------------
 
 # An example of using likefile to pass audo data for on the fly compression.
 
 import Axon.likefile, time
 from Kamaelia.Audio.Codec.PyMedia.Encoder import Encoder
 from Kamaelia.Internet.TCPClient import TCPClient
-likefile.schedulerThread(slowmo=0.01).start()
+Axon.likefile.schedulerThread(slowmo=0.01).start()
 
 infile = "./stereo.wav"
 outfile = "./outfile.mp3"
 
-enc = likefile.LikeFile(Encoder("mp3", bitrate=128000, sample_rate=44100, channels=2))
+enc = Axon.likefile.LikeFile(Encoder("mp3", bitrate=128000, sample_rate=44100, channels=2))
 
+input1 = open(infile, "r+b")
 output = open(outfile, "w+b")
 
 while True:
-    data = output.read(1024)
+    data = input1.read(1024)
     print "about to sleep"
     time.sleep(1)
     print "slept"
