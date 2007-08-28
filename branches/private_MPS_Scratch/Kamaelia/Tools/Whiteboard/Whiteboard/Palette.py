35a5059b6aab9a7856c52224924a8a25d5632cee
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2007-08-28 16:30:53 +0000

    This is a personal scratch branch which I'll be using for general dev rather than trunk. (I have a bad tendency to make personal changes on my local trunk which causes problems for me when I want to merge changes :-)
    This also has the advantage that I can use the same changed scratch
    branch on both machines I use!
    
    :)
    
    Michael.

---
+++ branches/private_MPS_Scratch/Kamaelia/Tools/Whiteboard/Whiteboard/Palette.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+#
+# (C) 2007 British Broadcasting Corporation and Kamaelia Contributors(1)
+#     All Rights Reserved.
+#
+# You may only modify and redistribute this under the terms of any of the
+# following licenses(2): Mozilla Public License, V1.1, GNU General
+# Public License, V2.0, GNU Lesser General Public License, V2.1
+#
+# (1) Kamaelia Contributors are listed in the AUTHORS file and at
+#     http://kamaelia.sourceforge.net/AUTHORS - please extend this file,
+#     not this notice.
+# (2) Reproduced in the COPYING file, and at:
+#     http://kamaelia.sourceforge.net/COPYING
+# Under section 3.5 of the MPL, we are using this text since we deem the MPL
+# notice inappropriate for this file. As per MPL/GPL/LGPL removal of this
+# notice is prohibited.
+#
+# Please contact us via: kamaelia-list-owner@lists.sourceforge.net
+# to discuss alternative licensing.
+# -------------------------------------------------------------------------
+#
+
+from Kamaelia.UI.Pygame.Button import Button
+from Kamaelia.Chassis.Graphline import Graphline
+
+colours = { "black" :  (0,0,0), 
+            "red" :    (192,0,0),
+            "orange" : (192,96,0),
+            "yellow" : (160,160,0),
+            "green" :  (0,192,0),
+            "turquoise" : (0,160,160),
+            "blue": (0,0,255),
+            "purple" : (192,0,192),
+            "darkgrey" : (96,96,96),
+            "lightgrey" :(192,192,192),
+          }
+
+def buildPalette(cols, order, topleft=(0,0), size=32):
+    buttons = {}
+    links = {}
+    pos = topleft
+    i=0
+    # Interesting/neat trick MPS
+    for col in order:
+        buttons[col] = Button(caption="", position=pos, size=(size,size), bgcolour=cols[col], msg=cols[col])
+        links[ (col,"outbox") ] = ("self","outbox")
+        pos = (pos[0] + size, pos[1])
+        i=i+1
+    return Graphline( linkages = links,  **buttons )
