0700fb0b1dbff0b5f3aeab1d875fb9104d32433f
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2007-09-11 17:09:17 +0000

    Move constant out of the class
    Michael

--- branches/private_MPS_Scratch/Kamaelia/Kamaelia/Protocol/HTTP/HTTPServer.py
+++ branches/private_MPS_Scratch/Kamaelia/Kamaelia/Protocol/HTTP/HTTPServer.py
@@ -132,6 +132,48 @@ from Axon.Component import component
 from Axon.ThreadedComponent import threadedcomponent
 
 from Kamaelia.Protocol.HTTP.HTTPParser import *
+MapStatusCodeToText = {
+        "100" : "100 Continue",
+        "200" : "200 OK",
+        "302" : "302 Found",
+        "304" : "304 Non Modified",        
+        "400" : "400 Bad Request",
+        "401" : "401 Unauthorised",
+        "401" : "403 Forbidden",
+        "404" : "404 Not Found",
+                        
+        #UNCOMMON RESPONSES
+        "201" : "201 Created",
+        "202" : "202 Accepted", # AKA non-commital response
+        "203" : "203 Non-Authoritative Information",
+        "204" : "204 No Content",
+        "205" : "205 Reset Content",
+        "206" : "206 Partial Content",
+        "300" : "300 Multiple Choices",
+        "301" : "301 Moved Permanently",
+        "303" : "303 See Other",
+        "305" : "305 Use Proxy",
+        "307" : "307 Temporary Redirect",
+        "402" : "402 Payment Required",
+        "405" : "405 Method Not Allowed",
+        "406" : "406 Not Acceptable",
+        "407" : "407 Proxy Authentication Required",
+        "408" : "408 Request Timeout",
+        "409" : "409 Conflict",
+        "410" : "410 Gone",
+        "411" : "411 Length Required",
+        "412" : "412 Precondition Failed",
+        "413" : "413 Request Entity Too Large",
+        "414" : "414 Request-URI Too Long",
+        "415" : "415 Unsupported Media Type",
+        "416" : "416 Requested Range Not Satisfiable",
+        "417" : "417 Expectation Failed",
+        "500" : "500 Internal Server Error",
+        "501" : "501 Not Implemented",
+        "502" : "502 Bad Gateway",
+        "503" : "503 Service Unavailable",
+        "505" : "HTTP Version Not Supported"
+    }
 
 def currentTimeHTTP():
     "Get the current date and time in the format specified by HTTP/1.1"
@@ -250,48 +292,6 @@ class HTTPRequestHandler(component):
         "_handlersignal" : "Signals for the request handler"
     }
 
-    MapStatusCodeToText = {
-        "100" : "100 Continue",
-        "200" : "200 OK",
-        "302" : "302 Found",
-        "304" : "304 Non Modified",        
-        "400" : "400 Bad Request",
-        "401" : "401 Unauthorised",
-        "401" : "403 Forbidden",
-        "404" : "404 Not Found",
-                        
-        #UNCOMMON RESPONSES
-        "201" : "201 Created",
-        "202" : "202 Accepted", # AKA non-commital response
-        "203" : "203 Non-Authoritative Information",
-        "204" : "204 No Content",
-        "205" : "205 Reset Content",
-        "206" : "206 Partial Content",
-        "300" : "300 Multiple Choices",
-        "301" : "301 Moved Permanently",
-        "303" : "303 See Other",
-        "305" : "305 Use Proxy",
-        "307" : "307 Temporary Redirect",
-        "402" : "402 Payment Required",
-        "405" : "405 Method Not Allowed",
-        "406" : "406 Not Acceptable",
-        "407" : "407 Proxy Authentication Required",
-        "408" : "408 Request Timeout",
-        "409" : "409 Conflict",
-        "410" : "410 Gone",
-        "411" : "411 Length Required",
-        "412" : "412 Precondition Failed",
-        "413" : "413 Request Entity Too Large",
-        "414" : "414 Request-URI Too Long",
-        "415" : "415 Unsupported Media Type",
-        "416" : "416 Requested Range Not Satisfiable",
-        "417" : "417 Expectation Failed",
-        "500" : "500 Internal Server Error",
-        "501" : "501 Not Implemented",
-        "502" : "502 Bad Gateway",
-        "503" : "503 Service Unavailable",
-        "505" : "HTTP Version Not Supported"
-    }
     
     def debug(self, msg):
         self.send(msg, "debug")
@@ -313,7 +313,7 @@ class HTTPRequestHandler(component):
         elif not isinstance(resource.get("statuscode"), str):
             resource["statuscode"] = "500"
                     
-        statustext = self.MapStatusCodeToText.get(resource["statuscode"], "500 Internal Server Error")
+        statustext = MapStatusCodeToText.get(resource["statuscode"], "500 Internal Server Error")
 
         if (protocolversion == "0.9"):
             header = ""        
