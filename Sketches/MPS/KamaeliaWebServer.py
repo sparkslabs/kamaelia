d42f54c6cebbc55f23641a02f3338e2ede6b3f25
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2007-09-20 16:09:47 +0000

    Changed to use MoreComplexServer (battletested in the ground of greylisting :-)
    For debugging, our app now sorts the keys before displaying them.
    
    WSGI/CGI compliance:
        app_name added
        Header munging into CGI vars
        All required, and useful environment vars for CGI (not WSGI yet)
        added, many required ones have useful/valid values.
    
    The way the WSGI handler is configured has been extended slightly.
    Still not set in stone.
    
    
    Michael.

--- Sketches/MPS/KamaeliaWebServer.py
+++ Sketches/MPS/KamaeliaWebServer.py
@@ -10,6 +10,8 @@ import Axon
 # Import the server framework, the HTTP protocol handling, the minimal request handler, and error handlers
 
 from Kamaelia.Chassis.ConnectedServer import SimpleServer
+from Kamaelia.Chassis.ConnectedServer import MoreComplexServer
+
 from Kamaelia.Protocol.HTTP.HTTPServer import HTTPServer
 
 Axon.Box.ShowAllTransits = False
@@ -67,7 +69,7 @@ def simple_app(environ, start_response):
     response_headers = [('Content-type','text/html'),('Pragma','no-cache')]
     start_response(status, response_headers)
     yield '<P> My Own Hello World!\n'
-    for i in environ:
+    for i in sorted(environ.keys()):
         yield "<li>%s: %s\n" % (i, environ[i])
     yield "<li> Date:" + time.ctime()
 
@@ -93,8 +95,9 @@ class _WSGIHandler(Axon.ThreadedComponent.threadedcomponent):
     """Choosing to run the WSGI app in a thread rather than the same
        context, this means we don't have to worry what they get up
        to really"""
-    def __init__(self, request, app):
+    def __init__(self, app_name, request, app):
         super(_WSGIHandler, self).__init__()
+        self.app_name = app_name
         self.request = request
         self.environ = request
         self.app = app
@@ -103,7 +106,49 @@ class _WSGIHandler(Axon.ThreadedComponent.threadedcomponent):
         self.status = status
         self.response_headers = response_headers
 
+    def munge_headers(self):
+        for header in self.environ["headers"]:
+            cgi_varname = "HTTP_"+header.replace("-","_").upper()
+            self.environ[cgi_varname] = self.environ["headers"][header]
+        pprint.pprint(self.environ)
+        pprint.pprint(self.environ["headers"])
+
     def main(self):
+        required = "*** REQUIRED FIX THIS ***"
+        headers = self.environ["headers"]
+        self.environ["REQUEST_METHOD"] = required  # Required
+        self.environ["SCRIPT_NAME"] = self.app_name  # Portion of URL that relates to the application object. May be empty. (eg /cgi-bin/test.pl)
+        self.environ["PATH_INFO"] = self.environ["uri-suffix"]  # Remainder of request path after "SCRIPT_NAME", designating a content path may be empty.
+        
+        if self.environ["uri-suffix"].find("?") != -1:
+            self.environ["QUERY_STRING"] = self.environ["uri-suffix"][self.environ["uri-suffix"].find("?")+1:]
+        else:
+            self.environ["QUERY_STRING"] = ""
+#        self.environ["QUERY_STRING"] = required    # Portion of request URL that follows the ? - may be empty or absent
+        self.environ["CONTENT_TYPE"] = headers.get("content-type","") # Contents of an HTTP_CONTENT_TYPE field - may be absent or empty
+        self.environ["CONTENT_LENGTH"] = headers.get("content-length","") # Contents of an HTTP_CONTENT_LENGTH field - may be absent or empty
+        self.environ["SERVER_NAME"] = required     # Server name published to the outside world
+        self.environ["SERVER_PORT"] = required     # Server port published to the outside world
+        self.environ["SERVER_PROTOCOL"] = required # Version of protocol client _sent us_ (what they would like back)
+
+        consider = " **CONSIDER ADDING THIS -- eg: "
+        self.environ["SERVER_ADDR"] = consider + "192.168.2.9"
+        self.environ["HTTP_REFERER"] = consider + "-"
+        self.environ["SERVER_ADMIN"] = consider + "[no address given]"
+        self.environ["SERVER_SIGNATURE"] = consider + "...."
+        self.environ["SERVER_SOFTWARE"] = consider + "Apache/1.3.33 (Darwin)"
+        self.environ["SCRIPT_FILENAME"] = consider + "/usr/local/httpd/sites/com.thwackety/cgi/test.pl"
+        self.environ["DOCUMENT_ROOT"] = consider + "/usr/local/httpd/sites/com.thwackety/docs"
+        self.environ["REQUEST_URI"] = consider + "/cgi-bin/test.pl"
+        self.environ["SCRIPT_URL"] = consider + "/cgi-bin/test.pl"
+        self.environ["SCRIPT_URI"] = consider + "http://thwackety.com/cgi-bin/test.pl"
+        self.environ["REMOTE_ADDR"] = consider + "192.168.2.5"
+        self.environ["REMOTE_PORT"] = consider + "56669"
+        self.environ["DATE"] = consider + "Sat Sep 15 15:42:25 2007" #####
+        self.environ["PATH"] = consider + "/bin:/sbin:/usr/bin:/usr/sbin:/usr/libexec:/System/Library/CoreServices"
+        self.environ["GATEWAY_INTERFACE"] = consider + "CGI/1.1"
+
+        self.munge_headers()
         R = [ x for x in self.app(self.environ, self.start_response) ]
         resource = {
            "statuscode" : self.status,
@@ -117,25 +162,58 @@ class _WSGIHandler(Axon.ThreadedComponent.threadedcomponent):
             self.send(page, "outbox")
         self.send(Axon.Ipc.producerFinished(self), "signal")
 
-def WSGIHandler(app):
+def WSGIHandler(app_name, app):
     def R(request):
-        return _WSGIHandler(request,app)
+        return _WSGIHandler(app_name, request,app)
     return R
 
 def HTTPProtocol():
-    def foo(self):
+    def foo(self,**argd):
         print self.routing
-        return HTTPServer(requestHandlers(self.routing))
+        return HTTPServer(requestHandlers(self.routing),**argd)
     return foo
 
 # Finally we create the actual server and run it.
 
-class WebServer(SimpleServer):
+class WebServer(MoreComplexServer):
     routing = [
-               ["/", WSGIHandler(HTML_WRAP(simple_app)) ],
+               ["/wsgi", WSGIHandler("/wsgi", HTML_WRAP(simple_app)) ],
               ]
     protocol=HTTPProtocol()
     port=8082
     socketOptions=(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 WebServer().run()
+
+"""
+Changed Webserver to use the newer MoreComplexServer:
+   * Requried change to HTTPServer
+   * HTTPParser
+
+IPs now in request object passed out for a handler with keys
+   * peer, peerip
+   * localip, localport
+
+
+
+
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
