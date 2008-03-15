183b44423cfed47fa87ed9731195a15068de2e6a
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2008-03-15 23:53:12 +0000

    2 _standard_ Kamaelia components, running in seperate processes. One sending messages to the other. The "proof" is that they are both pygame based, meaning if they weren't in separate processes, it simply would not work.
    Michael.

--- Sketches/MPS/pprocess/MultiPipeline.py
+++ Sketches/MPS/pprocess/MultiPipeline.py
@@ -31,22 +31,22 @@ class ProcessWrapComponent(object):
         t = 0
         while 1:
             if time.time() - t > 0.2:
-                print "Wrapper!"
+#                print "Wrapper!"
                 t = time.time()
 
             if self.exchange.ready(0):
                 chan = self.exchange.ready(0)[0]
                 D = chan._receive()
-                print chan,D
+#                print chan,D
                 self.ce.put(*D)
 
             D = self.ce.anyReady()
             if D:
                 for boxname in D:
                     D = self.ce.get(boxname)
-                    print "DATA FROM COMPONENT",boxname, D
+#                    print "DATA FROM COMPONENT",boxname, D
                     self.channel._send((D, boxname))
-                print D
+#                print D
             yield 1
 
 import Axon
@@ -59,28 +59,49 @@ class Flooble(Axon.Component.component):
         t = 0
         while 1:
             if time.time() - t > 0.2:
-                print self.tag
+#                print self.tag
                 t = time.time()
             if self.dataReady("inbox"):
-                print "WOOOOOOOOOOOOOOOOOOOO", self.tag, self.recv("inbox")
+#                print "WOOOOOOOOOOOOOOOOOOOO", self.tag, self.recv("inbox")
                 self.send("OK, let's reply..."+self.tag, "outbox")
             yield 1
 
-X = ProcessWrapComponent(Flooble("Flooble"))
-Y = ProcessWrapComponent(Flooble("Bazzle"))
+from Kamaelia.Util.Console import ConsoleReader, ConsoleEchoer
+
+# X = ProcessWrapComponent(Flooble("Flooble"))
+# Y = ProcessWrapComponent(Flooble("Bazzle"))
+
+from Kamaelia.UI.Pygame.Text import TextDisplayer, Textbox
+
+X = ProcessWrapComponent(Textbox(position=(20, 340),
+                                 text_height=36,
+                                 screen_width=900,
+                                 screen_height=400,
+                                 background_color=(130,0,70),
+                                 text_color=(255,255,255)))
+#Y = ProcessWrapComponent(ConsoleEchoer())
+
+Y = ProcessWrapComponent( TextDisplayer(position=(20, 90),
+                                        text_height=36,
+                                        screen_width=400,
+                                        screen_height=540,
+                                        background_color=(130,0,70),
+                                        text_color=(255,255,255)) )
+
 
 exchange = pprocess.Exchange()
-chan = X.activate()
+chan1 = X.activate()
 chan2 = Y.activate()
 
-exchange.add(chan)
+exchange.add(chan1)
 exchange.add(chan2)
 
-print chan
+# print chan
 
 while 1:
-    chan._send(("hello","inbox"))
     for chan in exchange.ready(0):
-        D = chan._receive()
-        print "PASSED BACK OUT FROM COMPONENT IN OTHER PROCESS", D
-    time.sleep(1)
+        if chan == chan1:
+            D = chan._receive()
+            if D[1] == "outbox":
+                chan2._send( (D[0], "inbox") )
+    time.sleep(0.1)
