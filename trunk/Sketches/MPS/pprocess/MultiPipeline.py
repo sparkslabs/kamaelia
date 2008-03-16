2e526963e1440298c6bab2541f6cc9d840e2bf63
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2008-03-16 20:49:10 +0000

    We now also support arbitrary graphlines, where the nodes in the graphline are all running as seperate processes and can all communicate with each other. (Internally, the parent node forms a MiniAxon style postman which means this is actually a star topology.)
    Michael.

--- trunk/Sketches/MPS/pprocess/MultiPipeline.py
+++ trunk/Sketches/MPS/pprocess/MultiPipeline.py
@@ -69,44 +69,56 @@ def ProcessPipeline(*components):
             dest[0]._send( (D[0], dest[1] ) )
         time.sleep(0.1)
 
+
+def ProcessGraphline(**graphline_spec):
+    chans = []
+    count = 0
+    component_to_chan = {}
+    mappings = {}
+
+    exchange = pprocess.Exchange()
+    for comp in graphline_spec:
+        if comp != "linkages":
+            A = ProcessWrapComponent( graphline_spec[comp] )
+            chan = A.activate()
+            chans.append( chan )
+            exchange.add(chan )
+            component_to_chan[comp] = chan
+            print comp, chan
+            count += 1
+
+    linkages = graphline_spec.get("linkages", {})
+    for source in linkages:
+        sink = linkages[source]
+        mappings[ component_to_chan[source[0]], source[1] ] = component_to_chan[sink[0]], sink[1]
+
+    while 1:
+        for chan in exchange.ready(0):
+            D = chan._receive()
+            dest = mappings[ ( chan, D[1] ) ]
+            dest[0]._send( (D[0], dest[1] ) )
+        time.sleep(0.001)
+
 # --- End Support code ---------------------------------------------------------
 
-# Client code that uses the support code
+if __name__ == "__main__":
+    from Kamaelia.UI.Pygame.Text import TextDisplayer, Textbox
 
-from Kamaelia.UI.Pygame.Text import TextDisplayer, Textbox
-
-from Kamaelia.Chassis.Pipeline import Pipeline
-import sys
-
-if len(sys.argv) <2:
-    ProcessPipeline(
-                Textbox(position=(20, 340),
+    ProcessGraphline(
+                component_one = Textbox(position=(20, 340),
                                  text_height=36,
                                  screen_width=900,
                                  screen_height=200,
                                  background_color=(130,0,70),
                                  text_color=(255,255,255)),
-                TextDisplayer(position=(20, 90),
+                component_two = TextDisplayer(position=(20, 90),
                                         text_height=36,
                                         screen_width=900,
                                         screen_height=200,
                                         background_color=(130,0,70),
                                         text_color=(255,255,255)),
-                )
-else:
-    Pipeline(
-                Textbox(position=(20, 340),
-                                 text_height=36,
-                                 screen_width=900,
-                                 screen_height=200,
-                                 background_color=(130,0,70),
-                                 text_color=(255,255,255)),
-                TextDisplayer(position=(20, 90),
-                                        text_height=36,
-                                        screen_width=900,
-                                        screen_height=200,
-                                        background_color=(130,0,70),
-                                        text_color=(255,255,255))
-                ).run()
-
-
+                linkages = {
+                    ("component_one", "outbox") : ("component_two", "inbox"),
+                    ("component_one", "signal") : ("component_two", "control"),
+                }
+    )
