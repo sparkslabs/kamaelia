0bbfe9b9ce5e2a9ca8239738badfe0790315286b
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2008-03-16 00:34:53 +0000

    Code cleanup
    Michael

--- trunk/Sketches/MPS/pprocess/MultiPipeline.py
+++ trunk/Sketches/MPS/pprocess/MultiPipeline.py
@@ -4,6 +4,7 @@ import Axon.LikeFile
 import pprocess
 import time
 
+# --- Support code, this will go back into the library. ---------------------------------------------------------
 class ProcessWrapComponent(object):
     def __init__(self, somecomponent):
         self.exchange = pprocess.Exchange()
@@ -31,45 +32,23 @@ class ProcessWrapComponent(object):
         t = 0
         while 1:
             if time.time() - t > 0.2:
-#                print "Wrapper!"
                 t = time.time()
 
             if self.exchange.ready(0):
                 chan = self.exchange.ready(0)[0]
                 D = chan._receive()
-#                print chan,D
                 self.ce.put(*D)
 
             D = self.ce.anyReady()
             if D:
                 for boxname in D:
                     D = self.ce.get(boxname)
-#                    print "DATA FROM COMPONENT",boxname, D
                     self.channel._send((D, boxname))
-#                print D
             yield 1
 
-import Axon
+# --- End Support code ---------------------------------------------------------
 
-class Flooble(Axon.Component.component):
-    def __init__(self, tag):
-        super(Flooble, self).__init__()
-        self.tag = tag
-    def main(self):
-        t = 0
-        while 1:
-            if time.time() - t > 0.2:
-#                print self.tag
-                t = time.time()
-            if self.dataReady("inbox"):
-#                print "WOOOOOOOOOOOOOOOOOOOO", self.tag, self.recv("inbox")
-                self.send("OK, let's reply..."+self.tag, "outbox")
-            yield 1
-
-from Kamaelia.Util.Console import ConsoleReader, ConsoleEchoer
-
-# X = ProcessWrapComponent(Flooble("Flooble"))
-# Y = ProcessWrapComponent(Flooble("Bazzle"))
+# Client code that uses the support code
 
 from Kamaelia.UI.Pygame.Text import TextDisplayer, Textbox
 
@@ -79,7 +58,6 @@ X = ProcessWrapComponent(Textbox(position=(20, 340),
                                  screen_height=400,
                                  background_color=(130,0,70),
                                  text_color=(255,255,255)))
-#Y = ProcessWrapComponent(ConsoleEchoer())
 
 Y = ProcessWrapComponent( TextDisplayer(position=(20, 90),
                                         text_height=36,
@@ -88,15 +66,15 @@ Y = ProcessWrapComponent( TextDisplayer(position=(20, 90),
                                         background_color=(130,0,70),
                                         text_color=(255,255,255)) )
 
-
+#
+# The following code will get wrapped up as a utility function (or probably as a utility component)
+#
 exchange = pprocess.Exchange()
 chan1 = X.activate()
 chan2 = Y.activate()
 
 exchange.add(chan1)
 exchange.add(chan2)
-
-# print chan
 
 while 1:
     for chan in exchange.ready(0):
