b88008a7e91d056a4fe3990a769633a35b979850
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2008-05-11 22:59:03 +0000

    Needed for simplifying packaging up applications.
    Michael.

--- branches/private_MPS_ERModeller/Axon/setup.py
+++ branches/private_MPS_ERModeller/Axon/setup.py
@@ -29,8 +29,9 @@ setup(name = "Axon",
       author_email = "ms_@users.sourceforge.net",
       url = "http://kamaelia.sourceforge.net/",
       license = "Copyright (c)2004 BBC & Kamaelia Contributors, All Rights Reserved. Use allowed under MPL 1.1, GPL 2.0, LGPL 2.1",
-      packages = ["Axon",
-                  ""],
+      packages = [
+                  "Axon", ### START LAST
+                  ],
       long_description = """
 Axon is a software component system. In Axon, components are active and
 reactive, independent processing nodes responding to a CSP-like environment.
