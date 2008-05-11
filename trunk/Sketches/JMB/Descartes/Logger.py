476c31f006073643784ef5042b0e816e619145e4
amnorvend <amnorvend@2682807a-6752-0410-967a-793f22d1dfe9>
2008-05-11 07:29:46 +0000

    Set up some file structuring

---
+++ trunk/Sketches/JMB/Descartes/Logger.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Logger.py
+"""
+import datetime
+
+def Log(prefix, message):
+    output = "%s %s:  %s" % (datetime.now().isoformat(), prefix, message)
+    log_file = open("des.log", "a")
+    log_file.write(output)
+    log_file.close()
