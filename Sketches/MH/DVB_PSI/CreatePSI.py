928a8555bdef8c4d1befe5352f609592e8e3086b
matth_rd <matth_rd@2682807a-6752-0410-967a-793f22d1dfe9>
2008-05-11 22:59:27 +0000

    Added most of code for generating an EIT table, except for the generation of descriptors (todo).
    Not yet tested (actually, not yet even run!)
    
    Matt

--- Sketches/MH/DVB_PSI/CreatePSI.py
+++ Sketches/MH/DVB_PSI/CreatePSI.py
@@ -29,10 +29,10 @@ not yet tested ... or kamaelia-ised!
 
 
 """
+from Kamaelia.Support.DVB.CRC import __dvbcrc as doDvbCRC
 
 
-
-class SerialiseEIT(object):
+class SerialiseEITSection(object):
     """\
     EIT PSI section dictionary structure in ... binary PSI table section out
     """
@@ -41,7 +41,108 @@ class SerialiseEIT(object):
         self.sectionPacketiser = sectionPacketiser
       
     def serialise(self, section):
-        pass
+        data = []
+        
+        if section["current"]:
+            currentNextFlag=1
+        else:
+            currentNextFlag=0
+        
+        data.insert( chr(section["table_id"]) )
+        seclen_index = len(data) # note where the section length field will be inserted once we know its value
+        data.insert( chr((section["service_id"] >> 8) & 0xff) )
+        data.insert( chr((section["service_id"]     ) & 0xff) )
+        data.insert( chr(((section["version"] & 0x1f) << 1) + currentNextFlag) )
+        data.insert( chr(section["section"]) )
+        data.insert( chr(section["last_section"]) )
+        data.insert( chr((section["transport_stream_id"] >> 8) & 0xff) )
+        data.insert( chr((section["transport_stream_id"]     ) & 0xff) )
+        data.insert( chr((section["original_network_id"] >> 8) & 0xff) )
+        data.insert( chr((section["original_network_id"]     ) & 0xff) )
+        try:
+            data.insert( chr((section["segment_last_section_number")) )
+        except KeyError:
+            data.insert( chr((section["last_section_number")) )
+        try:
+            data.insert( chr((section["last_table_id")) )
+        except KeyError:
+            data.insert( chr((section["table_id")) )
+        
+        # now do events
+        events = []
+        for event in section["events"]:
+            mjd, utc = createMJDUTC(*event["starttime"])
+            dur = createBCDtime(*event["duration"])
+            flags = ((event["running_status"] & 0x7) << 5)
+            if event["free_CA_mode"]:
+                flags += 0x10
+            
+            events.insert( chr((event["event_id"] >> 8) & 0xff) \
+                         + chr((event["event_id"]     ) & 0xff) \
+                         + chr((mjd >> 8) & 0xff)  \
+                         + chr((mjd     ) & 0xff)  \
+                         + chr((utc >> 16) & 0xff) \
+                         + chr((utc >> 8 ) & 0xff) \
+                         + chr((utc      ) & 0xff) \
+                         + chr((dur >> 16) & 0xff) \
+                         + chr((dur >> 8 ) & 0xff) \
+                         + chr((dur      ) & 0xff) \
+                         )
+                         
+            elen_index = len(events)  # note where flags and descriptor_loop_length will be inserted
+            
+            # add descriptors
+            descriptors = serialiseDescriptors(event["descriptors"]
+            events.insert(descriptors)
+            descriptors_loop_length = len(descriptors)
+            
+            # now we know how long the descriptor loop is, we write the event's length
+            events.insert(elen_index,
+                chr(flags + (descriptors_loop_length >> 8) & 0x0f) \
+              + chr(        (descriptors_loop_length >> 8) & 0xff) \
+            )
+
+        # add events onto the end of the packet we're building
+        data.extend(events)
+        
+        # calculate total length of section
+        sectionLength = reduce(lambda total,nextStr: total+nextStr, data, 0)
+        sectionLength -= 1  # doesn't include bytes up to and including the section length field itself (which hasn't been inserted yet)
+        sectionLength += 4 # lets not forget the CRC
+        
+        data[seclen_index] = chr(0x80 + ((sectionLength >> 8) & 0x0f)) + chr(sectionLength & 0xff)
+        
+        # now we've assembled everything, calc the CRC, then write the CRC value at the end
+        data = data.join("")
+        crcval = doDvbCRC(data)
+        crc = chr((crcval >> 24) & 0xff) \
+            + chr((crcval >> 16) & 0xff) \
+            + chr((crcval >> 8 ) & 0xff) \
+            + chr((crcval      ) & 0xff)
+        
+        self.sectionPacketiser.packetise(data + crc)
+
+
+
+def createMJDUTC(year,month,day,hour,minute,second):
+    if month == 1 or month == 2:
+        L = 1
+    else:
+        L = 0
+    MJD = 14956 + day + int( (year - L) * 365.25) + int ( (month + 1 + L * 12) * 30.6001 )
+    
+    return MJD, createBCDtime(hour,minute,second)
+
+def createBCDtime(hour,minute,second):
+    HHMMSS = 0
+    for digit in "%02d%02d%02d" % (hour,minute,second):
+        HHMMSS = (HHMMSS<<4) + ord(digit)-ord("0")
+    return HHMMSS
+
+
+def serialiseDescriptors(descriptors):
+    data = []
+    pass
 
 
 class PacketiseTableSections(object):
@@ -70,7 +171,7 @@ class PacketiseTableSections(object):
         # first packet
         chunkLen = min(bytesLeft, 184-1-startOffset)  # -1 for the pointer_field
         payload.insert(section[sStart:sStart+chunkLen)
-        print self.tsPacketiser.packetise(payload.join(""), true, chr(0xff))
+        print self.tsPacketiser.packetise(payload.join(""), True, chr(0xff))
         sStart+=chunkLen
         bytesLeft-=chunkLen
 
@@ -83,7 +184,7 @@ class PacketiseTableSections(object):
             # subsequent packets
             chunkLen = min(bytesLeft, 184)  # -1 for the pointer_field
             payload.insert(section[sStart:sStart+chunkLen)
-            print self.tsPacketiser.packetise(payload.join(""), false, chr(0xff))
+            print self.tsPacketiser.packetise(payload.join(""), False, chr(0xff))
             sStart+=chunkLen
             bytesLeft-=chunkLen
 
@@ -94,7 +195,7 @@ class MakeTransportStreamPackets(object):
     """\
     Payloads in ... transport stream packets out
     """
-    def __init__(self, pid, scrambling=0, priority=false):
+    def __init__(self, pid, scrambling=0, priority=False):
         super(MakeTransportStreamPackets,self).__init__()
         self.pid = pid
         self.scrambling = scrambling
@@ -103,7 +204,7 @@ class MakeTransportStreamPackets(object):
         self.continuityCounter = 0
       
       
-    def packetise(self, payload, startIndicator=false, stuffingByte=chr(0xff)):
+    def packetise(self, payload, startIndicator=False, stuffingByte=chr(0xff)):
         packet = []
     
         pidAndFlags = self.pid & 0x1fff
