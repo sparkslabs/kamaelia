5c085b88d3c4c33b7a0348caa8f4a357eb72ce6d
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2008-05-11 11:50:58 +0000

    *YAWN*

--- branches/private_MPS_PymediaBugWorkaround/OS_Specific/Axon/Series60/Axon/ThreadedComponent.py
+++ branches/private_MPS_PymediaBugWorkaround/OS_Specific/Axon/Series60/Axon/ThreadedComponent.py
@@ -8,7 +8,7 @@
 #            Added simple trace statements into the code.
 #
 #
-# (C) 2004 British Broadcasting Corporation and Kamaelia Contributors(1)
+# Copyright (C) 2004 British Broadcasting Corporation and Kamaelia Contributors(1)
 #     All Rights Reserved.
 #
 # You may only modify and redistribute this under the terms of any of the
