e61bfb262394495e0a986200b47202112039a64c
toplch <toplch@2682807a-6752-0410-967a-793f22d1dfe9>
2008-05-13 22:21:32 +0000

    Topology initial import (Util/Console2Topology.py, Util/File2Topology.py (not work so far), Util/Files/Example.tp); Subclipse test

---
+++ Sketches/CL/Topology/src/Util/Console2Topology.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+"""\
+==================================
+Draw Topology from Console Command
+==================================
+
+Example Usage
+-------------
+1. Add one node
+ADD NODE TCPClient TCPClient auto -
+ADD NODE VorbisDecode VorbisDecode auto -
+2. add one link
+ADD LINK TCPClient VorbisDecode
+3. Delete one node/ link/ all 
+DEL NODE TCPClient
+ADD LINK TCPClient VorbisDecode
+DEL ALL
+"""
+
+from Kamaelia.Util.Console import ConsoleReader
+from Kamaelia.Visualisation.PhysicsGraph.lines_to_tokenlists import lines_to_tokenlists
+from Kamaelia.Visualisation.PhysicsGraph.TopologyViewer import TopologyViewer
+from Kamaelia.Chassis.Pipeline import Pipeline
+
+Pipeline(
+    ConsoleReader(">>> "),
+    lines_to_tokenlists(),
+    TopologyViewer(),
+).run()
