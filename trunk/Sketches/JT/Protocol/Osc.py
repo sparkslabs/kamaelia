32f16508788ae128d5f4465bfb38bf0825c97aae
orphans <orphans@2682807a-6752-0410-967a-793f22d1dfe9>
2008-05-15 20:50:09 +0000

    Start to add documentation to the OSC component

--- trunk/Sketches/JT/Protocol/Osc.py
+++ trunk/Sketches/JT/Protocol/Osc.py
@@ -1,7 +1,47 @@
+#! /usr/bin/env python
+# TODO: - Document inboxes and outboxes
+#       - Make the component shutdown on the appropriate signals
+#       - Work out how to make vim word wrap the docs :D
+#       - Test the test case
+"""
+===========
+OSC Creator
+===========
+
+This component is for creating Open Sound Control (OSC) messages.  Send data in the form of an (address, arguments) tuple to the "inbox" inbox to create a message.  Pick up data from the "outbox" outbox to receive the message as an OSC packet - a binary representation of the message.
+
+
+Example Usage
+-------------
+Creating an OSC message with the OSC arguments 1, 2, 3 and an OSC address pattern /OscTest/TestMessage ready for dispatch over a UDP socket.
+
+Pipeline(OneShot(("/TestMessage", (1, 2, 3))), Osc("/OscTest"))
+
+
+How does it work?
+-----------------
+The component receives a tuple, (address, arguments) on its "inbox" inbox, where arguments can either be a tuple, list or a single argument.  It then proceeds to create the OSC address pattern.
+
+If the address does not already have a leading forward slash, one is prepended.  If an address prefix has been supplied when the component is initialised this is also prepended, forming a complete OSC address pattern.  This address pattern is of the form /prefix/address.  Note that both prefix and address can contain further forward slashes, for example a complete address pattern could read /MyApp/MyButtons/AButton/Pressed.
+
+With the address pattern created, the component creates an OSCMessage object (which is defined as part of the pyOSC library).  It then appends the arguments using the object's append method, and sends an OSC packet (created using the toBinary() method) to the "outbox" outbox.
+"""
+
 import OSC
 import Axon
 
 class Osc(Axon.Component.component):
+    """\
+    Osc([addressPrefix]) -> new Osc component.
+
+    Creates OSC packets from data received on the "inbox" inbox.
+
+    Keyword arguments:
+
+    - addressPrefix -- A prefix to add to address pattern of each OSC Message.
+                       The first character must be a forward slash.
+    """
+
     def __init__(self, addressPrefix = None):
         super(Osc, self).__init__()
         self.addressPrefix = addressPrefix
@@ -24,4 +64,6 @@ class Osc(Axon.Component.component):
 if __name__ == "__main__":
     from Kamaelia.Chassis.Pipeline import Pipeline
     from Kamaelia.Internet.UDP import SimplePeer
-    Pipeline(Osc("/OscTest"), SimplePeer(receiver_addr="127.0.0.1", receiver_port=2000)).run()
+    from Kamaelia.Util.OneShot import OneShot
+    Pipeline(OneShot(("/TestMessage", (1, 2, 3))), Osc("/OscTest"),
+             SimplePeer(receiver_addr="127.0.0.1", receiver_port=2000)).run()
