11f64c81a4259b3df0a202044b20631fe7776552
matth_rd <matth_rd@2682807a-6752-0410-967a-793f22d1dfe9>
2008-05-18 14:08:43 +0000

    Test code for capturing and examining existing EIT data
    Matt

---
+++ trunk/Sketches/MH/DVB_PSI/showEIT.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+from Kamaelia.Device.DVB.Parse.ParseEventInformationTable import *
+from Kamaelia.Device.DVB.Parse.ParseTimeAndDateTable import *
+from Kamaelia.Device.DVB.Parse.PrettifyTables import *
+from Kamaelia.Device.DVB.SoftDemux import DVB_SoftDemuxer
+from Kamaelia.Device.DVB.Parse.ReassemblePSITables import ReassemblePSITables
+
+from Kamaelia.Chassis.Pipeline import Pipeline
+from Kamaelia.File.Reading import RateControlledFileReader
+from Kamaelia.Util.Console import ConsoleEchoer
+
+import sys
+sys.path.append("svn/trunk/Sketches/MH/Introspection")
+from Profiling import FormattedProfiler
+
+#TS_FILE = "2008-05-16 11.27.13 MUX1_EIT_TOT_TDT.ts"
+import sys
+
+if len(sys.argv) != 2:
+    print "Usage:"
+    print "    %s <ts_file_containing_eit>" % sys.argv[0]
+    print
+    sys.exit(1)
+    
+TS_FILE = sys.argv[1]
+
+
+from Kamaelia.Chassis.Graphline import Graphline
+from Kamaelia.Util.Splitter import Plug, PlugSplitter
+
+def Tee(*destinations):
+  splitter = PlugSplitter()
+  plugs = {}
+  i = 1
+  for dest in destinations:
+      plugs[str(i)] = Plug(splitter, dest)
+      i = i + 1
+
+  return Graphline(splitter = splitter, 
+    linkages = {
+      ("","inbox") : ("splitter", "inbox"),
+      ("","control") : ("splitter", "control"),
+      ("splitter", "outbox") : ("", "outbox"),
+      ("splitter", "signal") : ("", "signal")
+    },
+    **plugs
+    )
+
+Pipeline( FormattedProfiler(10.0, 1.0),
+          ConsoleEchoer(),
+        ).activate()
+
+
+Pipeline( RateControlledFileReader(TS_FILE, "bytes", rate=25000, chunksize=188, allowchunkaggregation=True),
+#          Tee( 
+#              Pipeline( DVB_SoftDemuxer({0x14 : ["outbox"]}),
+#                        ReassemblePSITables(),
+#                        ParseTimeAndDateTable(),
+#                        PrettifyTimeAndDateTable(),
+#                        ConsoleEchoer(),
+#                      ),
+#          ),
+          DVB_SoftDemuxer({0x12 : ["outbox"]}),
+          ReassemblePSITables(),
+          ParseEventInformationTable_Subset(actual_presentFollowing=True),
+          PrettifyEventInformationTable(),
+          ConsoleEchoer()
+        ).run()
