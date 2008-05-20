9ab69d4522fed8c4e3745c62f3d08317260fa064
amnorvend <amnorvend@2682807a-6752-0410-967a-793f22d1dfe9>
2008-05-20 20:11:52 +0000

    added refcounting to the logger

--- trunk/Sketches/JMB/Logger/Log.py
+++ trunk/Sketches/JMB/Logger/Log.py
@@ -14,14 +14,15 @@ class Logger(component):
         self.logname = logname
         self.bplane = Backplane(logname)
         self.subscriber = SubscribeTo(logname)
+        self.refcount = 0
         
         self.bplane.activate()
         self.subscriber.activate()
 
         #add the components as children
         self.addChildren(self.subscriber, self.bplane)
-
         self.link((self.subscriber,  'outbox'),  (self,  'inbox'))
+        self.link((self, 'signal'), (self.bplane, 'control'))
 
     def main(self):
         not_done = True
@@ -34,34 +35,55 @@ class Logger(component):
                 file.write(msg)
                 file.close()
                 
-            while self.dataReady('control'):
+            while self.dataReady('control'):    #TODO:  Move refcounting to the backplane somehow
                 msg = self.recv('control')
-                self.send(msg, 'signal')
-                if isinstance(msg, (producerFinished, shutdownMicroprocess)):
+                if isinstance(msg, (producerFinished)):
+                    self.refcount -= 1
+                    if not self.refcount:
+                        not_done = False
+                        self.shutdown(msg)
+                if isinstance(msg, (newComponent)):
+                    self.refcount += 1
+                if isinstance(msg, (shutdownMicroprocess)):
                     not_done = False
-            
+                    self.shutdown(msg)
             if not_done:
                 self.pause()
                 yield 1
         
-            
+    def shutdown(self, msg):
+        self.send(msg, 'signal')
+        print 'shutting down logger!'
+        print 'dataReady("inbox") = ' + str(self.dataReady('inbox'))
+        self.removeChild(self.bplane)
+        self.removeChild(self.subscriber)
 if __name__ == '__main__':
     from Kamaelia.Util.Backplane import PublishTo
     
     class Producer(component):
-        def __init__(self, message):
+        Inboxes = {'inbox' : 'NOT USED',
+                    'control' : 'receive shutdown messages',}
+        Outboxes = {'outbox' : 'push data out',
+                    'signal' : 'send shutdown messages',
+                    'logger-signal' : 'send shutdown messages to the logger',}
+        def __init__(self, message, logger, times):
             super(Producer, self).__init__()
             self.message = message
+            self.times = times
+            self.link((self, 'logger-signal'), (logger, 'control'))
+            self.send(newComponent(), 'logger-signal')
             
         def main(self):
-            for i in xrange(50):
+            for i in xrange(self.times):
                 self.send(self.message, 'outbox')
                 print 'sent %s' % (self.message)
                 yield 1
             
             self.send(producerFinished(), 'signal')
+            self.send(producerFinished(), 'logger-signal')
             
     log = Logger('blah.log')
     log.activate()
     
-    Pipeline(Producer('hello\n'), PublishTo('blah.log')).run()
+    Pipeline(Producer('hello\n', log, 50), PublishTo('blah.log')).activate()
+    Pipeline(Producer('bye\n', log, 75), PublishTo('blah.log')).run()
