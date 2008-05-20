218cccb675e60c242854b91882d1d4141aed7928
matth_rd <matth_rd@2682807a-6752-0410-967a-793f22d1dfe9>
2008-05-20 08:31:09 +0000

    Fixed syntax error (missing colon suffix on conditional statement)
    Matt

--- Code/Python/Kamaelia/Kamaelia/Internet/TCPClient.py
+++ Code/Python/Kamaelia/Kamaelia/Internet/TCPClient.py
@@ -240,7 +240,7 @@ class TCPClient(Axon.Component.component):
              # This is a windows error indicating the connection has already been made.
              self.connecting = 0 # as with the no exception case.
              return True
-         elif hasattr(errno, "WSAEINVAL")
+         elif hasattr(errno, "WSAEINVAL"):
             if errorno == errno.WSAEINVAL:
                 # If we are on windows, this will be the error instead of EALREADY
                 # above.
