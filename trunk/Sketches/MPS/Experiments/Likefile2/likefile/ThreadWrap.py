ebffaf005d84e225799ad9a9ae18bab412b40335
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2008-06-01 01:36:34 +0000

    Basic thin threading wrapping around a component. Currently does a number of things that are suboptimal, but can be dealt with.
    What I *think* this gives me is a thread which is seperate from
    the component being run by ThreadWrap, meaning it will operate
    completely async from the component above. This *should* mean
    that if we add threadsafe Queues for sending messages to the
    component on the inside, that this should result in messages
    passing safely into the threadwrap component and thence safely
    to the wrapped component. (ie essentially what Likefile was
    trying to do)
    
    Unlike Likefile this takes the same approach for handling component
    shutdown, as shown by the testcases at the bottom.
    
    
    Michael.

--- trunk/Sketches/MPS/Experiments/Likefile2/likefile/ThreadWrap.py
+++ trunk/Sketches/MPS/Experiments/Likefile2/likefile/ThreadWrap.py
@@ -19,104 +19,42 @@
 # Please contact us via: kamaelia-list-owner@lists.sourceforge.net
 # to discuss alternative licensing.
 # -------------------------------------------------------------------------
-"""\
-==================================
-Wiring up components in a Pipeline
-==================================
-
-The Pipeline component wires up a set of components in a linear chain (a
-Pipeline) and encapsulates them as a single component.
-
-
-
-Example Usage
--------------
-
-A simple pipeline of 4 components::
-
-    Pipeline(MyDataSource(...),
-             MyFirstStageOfProcessing(...),
-             MySecondStageOfProcessing(...),
-             MyDestination(...),
-            ).run()
-
-
-How does it work?
------------------
-A Pipeline component gives you a way of wiring up a system of components in a
-chain and then encapsulating the whole as a single component. The inboxes of
-this component pass through to the inboxes of the first component in the
-Pipeline, and the outboxes of the last component pass through to the outboxes
-of the Pipeline component.
-
-The components you specify are registered as children of the Pipeline
-component. When Pipeline is activate, all children are wired up and activated.
-
-For the components in the Pipeline, "outbox" outboxes are wired to "inbox"
-inboxes, and "signal" outboxes are wired to "control" inboxes. They are wired
-up in the order in which you specify them - data will flow through the chain
-from first component to last.
-
-The "inbox" and "control" inboxes of the Pipeline component are wired to
-pass-through to the "inbox" and "control" inboxes (respectively) of the first
-component in the Pipeline chain.
-
-The "outbox" and "signal" outboxes of the last component in the Pipeline chain
-are wired to pass-through to the "outbox" and "signal" outboxes (respectively)
-of the Pipeline component.
-
-During runtime, the Pipeline component monitors the child components. It will
-terminate if, and only if, *all* the child components have also terminated.
-
-NOTE that if your child components create additional components themselves, the
-Pipeline component will not know about them. It only monitors the components it
-was originally told about.
-
-Pipeline does not intercept any of its inboxes or outboxes. It ignores whatever
-traffic flows through them.
-"""
 
 # component that creates and encapsulates a Pipeline of components, connecting
 # their outbox to inbox, and signal to control to form the Pipeline chain.
 
-from Axon.Scheduler import scheduler as _scheduler
-import Axon as _Axon
+import Axon
+import time
 
-component = _Axon.Component.component
+class ThreadWrap(Axon.ThreadedComponent.threadedcomponent):
+   Inboxes = {
+       "inbox":"From the outside world",
+       "control":"From the outside world",
 
+       "_inbox":"From the component to go to the outside world",
+       "_control":"From the component to go to the outside world",
+   }
+   Outboxes = {
+       "outbox":"To the outside world",
+       "signal":"To the outside world",
 
-class Pipeline(component):
-   """\
-   Pipeline(\*components) -> new Pipeline component.
-
-   Encapsulates the specified set of components and wires them up in a chain
-   (a Pipeline) in the order you provided them.
-   
-   Keyword arguments:
-   
-   - components  -- the components you want, in the order you want them wired up
-   """
-   def __init__(self, *components):
+       "_outbox":"From the outside world to go to the component",
+       "_signal":"From the outside world to go to the component",
+   }
+   def __init__(self, someComponent):
       """x.__init__(...) initializes x; see x.__class__.__doc__ for signature"""
-      super(Pipeline,self).__init__()
-      self.components = list(components)
+      super(ThreadWrap,self).__init__()
+      self.comp = someComponent
 
    def main(self):
       """Main loop."""
-      self.addChildren(*self.components)
-      Pipeline = self.components[:]
-      source = Pipeline[0]
-      del Pipeline[0]
-      while len(Pipeline)>0:
-         dest = Pipeline[0]
-         del Pipeline[0]
-         self.link((source,"outbox"), (dest,"inbox"))
-         self.link((source,"signal"), (dest,"control"))
-         source = dest
-      self.link((self,"inbox"), (self.components[0],"inbox"), passthrough=1)
-      self.link((self,"control"), (self.components[0],"control"), passthrough=1)
-      self.link((self.components[-1],"outbox"), (self,"outbox"), passthrough=2)
-      self.link((self.components[-1],"signal"), (self,"signal"), passthrough=2)
+      self.addChildren(self.comp)
+
+      self.link((self,"_outbox"), (self.comp,"inbox"))
+      self.link((self,"_signal"), (self.comp,"control"))
+      self.link((self.comp,"outbox"), (self,"_inbox"))
+      self.link((self.comp,"signal"), (self,"_control"))
+
       for child in self.children:
           child.activate()
 
@@ -125,9 +63,21 @@ class Pipeline(component):
 
       # becuase they are children, if they terminate, we'll be woken up
       while not self.childrenDone():
-          self.pause()
-          yield 1
 
+          # We manually forward the data here. There are probably nicer methods, but for the
+          # moment, lets stick to brute force/clarity
+
+          time.sleep(0.01) # so that we're not totally spinning
+
+          while self.dataReady("inbox"):
+              self.send(self.recv("inbox"), "_outbox")
+          while self.dataReady("control"):
+              self.send(self.recv("control"), "_signal")
+
+          while self.dataReady("_inbox"):
+              self.send(self.recv("_inbox"), "outbox")
+          while self.dataReady("_control"):
+              self.send(self.recv("_control"), "signal")
 
 
    def childrenDone(self):
@@ -139,78 +89,28 @@ class Pipeline(component):
                self.removeChild(child)   # deregisters linkages for us
 
        return 0==len(self.childComponents())
-
-import Kamaelia.Support.Deprecate as Deprecate
-
-pipeline = Deprecate.makeClassStub(
-    Pipeline,
-    "Use Kamaelia.Chassis.Pipeline:Pipeline instead of Kamaelia.Chassis.Pipeline:pipeline",
-    "WARN"
-    )
-
-__kamaelia_components__  = ( Pipeline, )
                   
 if __name__=="__main__":
-    from Axon.Component import scheduler
-    from Kamaelia.Util.Console import ConsoleEchoer
-    from Kamaelia.Util.PassThrough import PassThrough
-    
-    from Axon.Ipc import producerFinished, shutdownMicroprocess
-    
-    class fruitSource(component):
-        def __init__(self):
-            self.outlist = ["apples\n","pears\n","grapes\n","bananas\n","oranges\n","cherrys\n","lemons\n","<end of list>\n"]
-            self.__super.__init__()
-
+    from Kamaelia.Util.Console import ConsoleReader, ConsoleEchoer
+    from Kamaelia.Chassis.Pipeline import Pipeline
+    import time
+    class Waiter(Axon.Component.component):
         def main(self):
-            for i in self.outlist:
-                self.send(i,"outbox")
+            print "RUNNING"
+            t = time.time()
+            while time.time()-t >2:
                 yield 1
-            self.send(producerFinished(self), "signal")
-            yield 1
-
-    
-    class testComponent(component):
-        Inboxes=['_control']
-        Outboxes=['_signal']
-
-        def __init__(self):
-            self.__super.__init__()
+            print "DONE"
             
-            self.source = fruitSource()
-            self.pipe   = Pipeline(PassThrough([]))
-            self.dest   = ConsoleEchoer()
-            self.addChildren(self.source, self.pipe, self.dest)
-            
-            self.link((self.source, "outbox"),  (self.pipe, "inbox"))
-            self.link((self.source, "signal"),  (self.pipe, "control"))
-            
-            self.link((self.pipe,   "outbox"),  (self.dest, "inbox"))
-            self.link((self.pipe,   "signal"),  (self,      "_control"))
-            
-            self.link((self,        "_signal"), (self.dest, "control"))
+    # All the following run as you would expect at this stage
+    if 0:
+        Waiter().run()
 
-        def childComponents(self):
-            return [self.source, self.pipe, self.dest]
+    if 0:
+        ThreadWrap( Waiter() ).run()
 
-        def main(self):
-            done = False
-            while not done:
-                if self.dataReady("_control"):
-                    data = self.recv("_control")
-                    done = done or isinstance(data, producerFinished) or isinstance(data, shutdownMicroprocess)
-                    self.send(data, "_signal")
-                yield 1
-
-
-    r = scheduler()
-    t = testComponent()
-    t.activate()
-    cs = t.childComponents()
-    for c in cs:
-        c.activate()
-        
-    print "Should output 7 fruit, followed by '<end of list>'.\n"
-    
-    scheduler.run.runThreads(slowmo=0)
-    
+    if 1:
+        Pipeline(
+            ThreadWrap(ConsoleReader()),
+            ConsoleEchoer(),
+        ).run()
