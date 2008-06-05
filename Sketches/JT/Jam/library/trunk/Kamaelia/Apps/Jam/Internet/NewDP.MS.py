0bd69cc86f8a03ad7294942543f564d0bd01836e
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2008-06-05 12:58:25 +0000

    Demo of fix

---
+++ Sketches/JT/Jam/library/trunk/Kamaelia/Apps/Jam/Internet/NewDP.MS.py
@@ -0,0 +1,62 @@
+import socket
+import Axon
+
+from Kamaelia.Internet.Selector import Selector
+from Kamaelia.IPC import newReader, newWriter
+
+class UDPReceiver(Axon.Component.component):
+    Inboxes  = {"inbox"   : "NOT USED",
+                "control"    : "Recieve shutdown messages",
+                "ReadReady"  : "Receive messages indicating data is ready to be read from the socket"}
+
+    Outboxes = {"outbox"          : "Data received from the socket",
+                "signal"          : "Signals receiver is shutting down",
+                "_selectorSignal" : "For communication to the selector"}
+
+    def __init__(self, local_addr, local_port):
+        super(UDPReceiver, self).__init__(self)
+        self.local = (local_addr, local_port)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM,
+                                  socket.IPPROTO_UDP)
+        
+    def main(self):
+        #TODO: Binding should handle errors gracefully
+        self.sock.bind(self.local)
+        selectorService, selectorShutdownService, newSelector = Selector.getSelectorServices()
+        if newSelector:
+            newSelector.activate()
+        yield 1
+        
+
+        self.link((self, "_selectorSignal"), selectorService)
+        self.send(newReader(self, ((self, "ReadReady"), self.sock)), "_selectorSignal")
+      
+        # TODO: Make me shutdown nicely
+        while 1:
+            if self.dataReady("ReadReady"):
+#            if 1:
+                data = True
+                while data:
+                    data = self.safeRecv(1024)
+                    if data:
+                        print "data", repr(data)
+                        self.send(data, "outbox")
+            else:
+                print newSelector
+            self.pause()
+            yield 1
+
+    def safeRecv(self, size):
+        try:
+            data = self.sock.recvfrom(size)
+            if data:
+                return data
+        except socket.error, socket.msg:
+            (errorno, errmsg) = socket.msg.args
+            if errorno == errno.EAGAIN or errorno == errno.EWOULDBLOCK:
+                self.send(newReader(self, "ReadReady"), self.sock)
+        return None
+
+                    
+if __name__ == "__main__":
+    UDPReceiver("127.0.0.1", 2000).run()
