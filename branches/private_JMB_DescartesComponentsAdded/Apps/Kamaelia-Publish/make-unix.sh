0b7741851e64e6a0ca415b18e19176a4243b28b3
amnorvend <amnorvend@2682807a-6752-0410-967a-793f22d1dfe9>
2008-06-06 17:42:11 +0000

    Tweaked the build toolchain so that it doesn't have to remove .svns from any working copy data.

--- branches/private_JMB_DescartesComponentsAdded/Apps/Kamaelia-Publish/make-unix.sh
+++ branches/private_JMB_DescartesComponentsAdded/Apps/Kamaelia-Publish/make-unix.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-scripts/Publish.notar.sh
-python byte-compile.py urls.py main.py ServerConfig.py
-zip kpublish.zip urls.py urls.pyc main.py main.pyc ServerConfig.py ServerConfig.pyc Kamaelia Axon WsgiApps
+scripts/publish.prepare.sh #assemble everything we need in the assembly directory
+#python byte-compile.py urls.py main.py ServerConfig.py
+
+(
+cd assembly
+echo "Removing any previously compiled modules"
+rm -rf assembly/*.pyc
+echo "Creating executable"
+zip kpublish.zip urls.py main.py ServerConfig.py Kamaelia Axon WsgiApps $@
 cat zipheader.unix kpublish.zip > kpublish
+mkdir ../dist
+mv kpublish ../dist
+)
