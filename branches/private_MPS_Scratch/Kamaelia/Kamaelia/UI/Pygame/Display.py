2deb33a5244749281a485ebb40c83e6ff1953c0e
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2008-06-08 17:26:36 +0000

    Handle when Pygame isn't built with mixer support

--- branches/private_MPS_Scratch/Kamaelia/Kamaelia/UI/Pygame/Display.py
+++ branches/private_MPS_Scratch/Kamaelia/Kamaelia/UI/Pygame/Display.py
@@ -596,7 +596,10 @@ class PygameDisplay(Axon.AdaptiveCommsComponent.AdaptiveCommsComponent):
    def main(self):
       """Main loop."""
       pygame.init()
-      pygame.mixer.quit()
+      try:
+          pygame.mixer.quit()
+      except NotImplementedError:
+          pass # If it's not implemented, it not closing isn't a problem because it doesn't need/can't be
       display = pygame.display.set_mode((self.width, self.height), self.fullscreen|pygame.DOUBLEBUF)
       eventsource = _PygameEventSource().activate()
       self.addChildren(eventsource)
