d3eb9c52f08cde82dddd58c34c2ceec10b98d605
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2008-06-09 00:58:49 +0000

    Branch deleted - version number of last version in MergedBranchesList.txt, if needed for restoration

--- branches/merged/private_MH_20070618_docgen_improvements/Sketches/PT/testhttpclient.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-from Kamaelia.Protocol.HTTP.HTTPClient import SimpleHTTPClient
-from Kamaelia.Util.Console import ConsoleReader, ConsoleEchoer
-from Kamaelia.Chassis.Pipeline import Pipeline
-
-Pipeline(ConsoleReader(">>> ", ""),SimpleHTTPClient(),ConsoleEchoer()).run()
