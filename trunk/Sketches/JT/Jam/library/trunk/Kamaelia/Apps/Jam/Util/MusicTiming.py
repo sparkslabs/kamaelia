8d5ee7e171a847f0608a1609d1c7d5281ec28f7a
orphans <orphans@2682807a-6752-0410-967a-793f22d1dfe9>
2008-06-11 14:38:04 +0000

    Add documentation to MusicTimingComponent and clean up the indentation in a couple of places

--- trunk/Sketches/JT/Jam/library/trunk/Kamaelia/Apps/Jam/Util/MusicTiming.py
+++ trunk/Sketches/JT/Jam/library/trunk/Kamaelia/Apps/Jam/Util/MusicTiming.py
@@ -1,15 +1,72 @@
 #! /usr/bin/env python
 #TODO:
-# * Document me
+# * Add timing compensation
 # * Test timing code hard
+"""
+======================
+Music Timing Component
+======================
+
+This component provides a scheduler and a musical "clock", which keeps time in
+beats and bars.  It is designed to be subclassed for use in creating components
+to be used in the creation of music.
+
+Example usage
+-------------
+
+A simple class which prints the beat and bar every half-beat
+
+class HalfBeatPrint(MusicTimingComponent):
+    def __init__(self):
+        super(HalfBeatPrint, self).__init__()
+        self.lastHalfBeatTime = self.startTime
+        self.halfBeatLength = self.beatLength/2
+        # Use priority=2 so beat/bar rolls over first
+        self.sched.enterabs(self.lastHalfBeatTime + self.halfBeatLength, 2,
+                            self.printPosition, ())
+
+    def printPosition(self):
+        print "Beat: %i, Bar: %i" % (self.beat, self.bar)
+        self.lastHalfBeatTime = self.lastHalfBeatTime + self.halfBeatLength
+        # Reschedule the event for the next half-beat
+        self.sched.enterabs(self.lastHalfBeatTime + self.halfBeatLength, 2,
+                            self.printPosition, ())
+   
+How does it work?
+-----------------
+
+When initialized the component creates a number of variables relating to the
+time.  It then creates a scheduler from python's sched module.  An event is
+scheduled to call updateBeat() after a beat has passed.  updateBeat() updates
+the various timing variables, and then schedules a new event to wake the
+component after another beat has passed.  When subclassed the child can add
+events to the scheduler allowing it to sleep until something needs doing (e.g.
+a note needs playing).
+
+Currently there is no compensation for the lack of real-time scheduling, so
+events are processed slightly later than the time which is specified to the
+scheduler.
+
+"""
+
 import time
 import sched
 
-from math import floor
 from Axon.ThreadedComponent import threadedcomponent
 
 class MusicTimingComponent(threadedcomponent):
+    """
+    MusicTimingComponent() -> new MusicTimingComponent component.
+
+    A threaded component consisting of a scheduler and a number of continually
+    updating variables which give the time since the component was initialized
+    in music timing (bars, beats etc)
+    """
     def __init__(self, bpm=120, beatsPerBar=4, beatUnit=4, loopBars=4):
+        """
+        x.__init__(...) initializes x; see x.__class__.__doc__ for signature
+        """
+
         super(MusicTimingComponent, self).__init__()
         self.bpm = bpm
         self.beatsPerBar = beatsPerBar
@@ -28,13 +85,23 @@ class MusicTimingComponent(threadedcomponent):
 
         self.sched = sched.scheduler(time.time, time.sleep)
 
-        self.sched.enterabs(self.lastBeatTime + self.beatLength, 1, self.updateBeat, ())
+        self.sched.enterabs(self.lastBeatTime + self.beatLength, 1,
+                            self.updateBeat, ())
 
     def main(self):
+        """
+        Main loop
+        """
+        #TODO: Make me shutdown properly
         while 1:
+            # Keep running events until the component is supposed to shutdown
             self.sched.run()
             
     def updateBeat(self):
+        """
+        Progress the timing by one beat, and update the number of bars, and
+        the bar position within the loop
+        """
         if self.beat != self.beatsPerBar - 1:
             self.beat += 1
         else:
@@ -45,7 +112,9 @@ class MusicTimingComponent(threadedcomponent):
             else:
                 self.loopBar = 0
         self.lastBeatTime += self.beatLength
-        self.sched.enterabs(self.lastBeatTime + self.beatLength, 1, self.updateBeat, ())
+        # Call this function again when the next beat has passed
+        self.sched.enterabs(self.lastBeatTime + self.beatLength, 1,
+                            self.updateBeat, ())
 
 if __name__ == "__main__":
     MusicTimingComponent().run()
