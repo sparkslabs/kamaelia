4756fd6887b30bba177ab00338cf299cb1542f98
toplch <toplch@2682807a-6752-0410-967a-793f22d1dfe9>
2008-06-11 23:28:06 +0000

    Topology Update: add RelationTopology functions 1. Add link label to show relation and link arrow to show the direction of relation 2. Make shapes, colors, pictures, sizes and labels of entities updatable 3. Make it able to receive inputs from python console or from a file or from both.

--- trunk/Sketches/CL/Topology/src/RelationTopology/Util/Particles.py
+++ trunk/Sketches/CL/Topology/src/RelationTopology/Util/Particles.py
@@ -3,9 +3,11 @@
 import pygame
 from Kamaelia.Support.Particles import Particle as BaseParticle
 
+from pygame.transform import smoothscale
+
 class GenericParticle(BaseParticle):
     """\
-    A generic particle type with picture, shape, color and size customable
+    A generic particle type with picture, shape, color and size customable and updatable
     Adapted from MPS.Experiments.360.Creator.IconComponent and .PPostbox
     """
    
@@ -13,62 +15,134 @@ class GenericParticle(BaseParticle):
                  color=None, width=None, height=None, radius=None):
         super(GenericParticle,self).__init__(position=position, ID = ID )
         self.type = type
+        pygame.font.init()
         self.set_label(name)
         #self.name = name
-        self.picture = pic
-        self.shape = shape
-        #self.color = color        
+        self.picture = None
+        self.shape = None
+        self.radius = None
+        self.width = None
+        self.height = None
+        
+        self.set_radius(radius)
+        self.set_width(width)
+        self.set_height(height)
+        
+        self.color = None        
         self.left = 0
         self.top = 0
-        self.selected = False        
+        self.selected = False
+        self.bondedRelations = {}
+
         if pic is not None:
-            self.picture = pygame.image.load(pic).convert()
-            if (width is not None) and (height is not None):
-                self.width = int(width)
-                self.height = int(height)
-                from pygame.transform import smoothscale           
-                self.picture = smoothscale(self.picture, (self.width,self.height))
-            else:
-                self.width = self.picture.get_width()
-                self.height = self.picture.get_height()
-            from math import sqrt
-            self.radius = sqrt(self.width*self.width+self.height*self.height)/2
+            self.set_pic(pic)
+            
         else:
-            if color is not None:
-                if isinstance(color, str):
-                    colorMap = {'blue': (128,128,255), 'pink': (255,128,128)}
+            self.set_color(color)
+            self.set_shape(shape)
+                       
+        
+    def updateAttrs(self, **attrs):
+        for key,value in attrs.iteritems():
+            methodName = 'set_' + key
+            #print methodName
+            if hasattr(self,methodName):
+                method = getattr(self,methodName)
+                #print method
+                method(value)
+    
+    def set_width(self, width):
+        if width is not None:
+            self.width = int(width)
+            if self.height is None:
+                self.height = self.width
+            if self.picture is not None:
+                self.height = self.picture.get_height()
+                self.picture = smoothscale(self.picture, (self.width,self.height))
+            self.radius = ((self.width*self.width+self.height*self.height)**0.5)/2
+            #print self.radius
+        else:
+            self.width = width
+
+    
+    def set_height(self, height):
+        if height is not None:
+            self.height = int(height)
+            if self.width is None:
+                self.width = self.height
+            if self.picture is not None:
+                self.width = self.picture.get_width()
+                self.picture = smoothscale(self.picture, (self.width,self.height))
+            self.radius = ((self.width*self.width+self.height*self.height)**0.5)/2
+            #print self.radius
+        else:
+            self.height = height
+        
+                        
+    def set_radius(self, radius):
+        if radius is not None:
+            self.radius = int(radius)
+        else:
+            self.radius = radius
+        
+    def set_color(self, color):
+        if color is not None:
+            if isinstance(color, str):
+                colorMap = {'blue': (128,128,255), 'pink': (255,128,128)}
+                if colorMap.has_key(color):
                     self.color = colorMap[color]
                 else:
-                    self.color = color
+                    print 'Only '+colorMap.keys().__str__()+' available' 
             else:
-                self.color = (255,128,128)
-            if shape == 'circle':
-                if radius is not None:
-                    self.radius = int(radius)
-                else:
-                    self.radius = 30
-            else:
-                if (width is not None) and (height is not None):
-                    self.width = int(width)
-                    self.height = int(height)
-                else:
-                    self.width = 60
-                    self.height = 60
-                from math import sqrt
-                self.radius = sqrt(self.width*self.width+self.height*self.height)/2             
-        
+                self.color = color
+        else:
+            self.color = (255,128,128)
         
     def set_label(self, newname):
         self.name = newname
-        pygame.font.init()
-        font = pygame.font.Font(None, 20)
+        #pygame.font.init()
+        font = pygame.font.Font(None, 20)        
         self.slabel   = font.render(self.name, True, (0,0,0))
         self.slabelxo = - self.slabel.get_width()/2
         self.slabelyo = - self.slabel.get_height()/2
         description = self.type+" : "+self.name
         self.desclabel = font.render( description, True, (0,0,0), (255,255,255))
         
+    def set_pic(self, pic):
+        self.picture = pygame.image.load(pic).convert()
+        if (self.width is not None) and (self.height is not None):
+            self.picture = smoothscale(self.picture, (self.width,self.height))
             
+        else:
+            self.width = self.picture.get_width()
+            self.height = self.picture.get_height()
+        #print self.width, self.height    
+        self.radius = ((self.width*self.width+self.height*self.height)**0.5)/2
+        #print self.radius
+        self.shape = None
+        self.color = None
+        
+    def set_shape(self, shape):
+        if shape is not None:
+            self.shape = shape
+            self.picture = None
+            if self.color is None:
+                self.color = (255,128,128)
+            if shape == 'circle':
+                self.width = None
+                self.height = None
+                if self.radius is None:
+                    self.radius = 30
+            else:
+                if self.width is None:
+                    self.width = 60
+                if self.height is None:
+                    self.height = 60
+                self.radius = ((self.width*self.width+self.height*self.height)**0.5)/2
+            
+            
+        
+    
     def render(self, surface):
         """\
         Multi-pass rendering generator.
@@ -87,36 +161,50 @@ class GenericParticle(BaseParticle):
             pygame.draw.line(surface, (128,128,255), (x,y),  (px,py) )
             # draw a pwetty arrow on the line, showing the direction
             arrow_colour = (0,160,0)
-            #mid = ( (x+px*2)/3, (y+py*2)/3 )
             
             direction = ( (px-x), (py-y) )
             length    = ( direction[0]**2 + direction[1]**2 )**0.5
             
             # Make the position of the arrow relative to the intersection
-            # between arc and the bond line rather than the centre of particle 
+            # between arc and the bond line rather than the centre of particle
+            #print self.radius 
             intersection_x = x - self.radius*(x-px)/length
             intersection_y = y - self.radius*(y-py)/length
             intersection_px = px - p.radius*(px-x)/length
             intersection_py = py - p.radius*(py-y)/length
-            mid = ( (intersection_x+intersection_px*2)/3, (intersection_y+intersection_py*2)/3 )
-            
+            intersection_relationLabel = ( (2*intersection_x+intersection_px)/3, 
+                                           (2*intersection_y+intersection_py)/3 )
+            arrowHead = ( (intersection_x+intersection_px*2)/3, (intersection_y+intersection_py*2)/3 )
             direction = [ 6*n / length for n in direction ]
             
             norm      = ( -direction[1], direction[0] )
             
-            leftarrow  = ( mid[0] - direction[0] - norm[0], mid[1] - direction[1] - norm[1] )
-            rightarrow = ( mid[0] - direction[0] + norm[0], mid[1] - direction[1] + norm[1] )
+            leftarrow  = ( arrowHead[0] - direction[0] - norm[0], arrowHead[1] - direction[1] - norm[1] )
+            rightarrow = ( arrowHead[0] - direction[0] + norm[0], arrowHead[1] - direction[1] + norm[1] )
             
-            pygame.draw.line(surface, arrow_colour, mid, leftarrow  )
-            pygame.draw.line(surface, arrow_colour, mid, rightarrow )
+            pygame.draw.line(surface, arrow_colour, arrowHead, leftarrow  )
+            pygame.draw.line(surface, arrow_colour, arrowHead, rightarrow )
+            
+            # Add text on the line to show the relation
+            if self.bondedRelations.has_key(p):
+                #print 'has'
+                font = pygame.font.Font(None, 15)
+                self.relationLabel = font.render(" "+self.bondedRelations[p]+" ", True, (0,0,0), )
+                relationLabel_pos = (intersection_relationLabel[0]-self.relationLabel.get_width()/2,  
+                                     intersection_relationLabel[1]-self.relationLabel.get_height()/2)
+                #print relationLabel_pos,self.bondedRelations[p]
+                surface.blit(self.relationLabel, relationLabel_pos)  
         yield 2
         if self.picture is not None:
             picture_rect = surface.blit(self.picture, (x- self.width/2, y - self.height/2))
             surface.blit(self.slabel, (x - self.slabel.get_width()/2, y + self.height/2) )
+            
             if self.selected:
+                
                 pygame.draw.rect(surface, (0,0,0), picture_rect, 2)
                 surface.blit(self.desclabel, (72,16) )
         else:
+            
             if self.shape == 'circle':
                 pygame.draw.circle(surface, self.color, (x,y), self.radius)
                 if self.selected:
@@ -125,6 +213,7 @@ class GenericParticle(BaseParticle):
             else:
                 pygame.draw.rect(surface, self.color, (x- self.width/2, y - self.height/2, self.width, self.height))
                 if self.selected:
+                    
                     pygame.draw.rect(surface, (0,0,0), (x- self.width/2, y - self.height/2, self.width, self.height), 2)
                     surface.blit(self.desclabel, (72,16) )
             surface.blit(self.slabel, (x - self.slabel.get_width()/2, y - self.slabel.get_height()/2) )
