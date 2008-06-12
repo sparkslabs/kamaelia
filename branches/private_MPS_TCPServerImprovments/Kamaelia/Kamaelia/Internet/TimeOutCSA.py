d953cd85d9d88303b84152cf4c7fcc1596fddaea
amnorvend <amnorvend@2682807a-6752-0410-967a-793f22d1dfe9>
2008-06-12 17:46:28 +0000

    Added legal notice to TimeOutCSA.py

--- branches/private_MPS_TCPServerImprovments/Kamaelia/Kamaelia/Internet/TimeOutCSA.py
+++ branches/private_MPS_TCPServerImprovments/Kamaelia/Kamaelia/Internet/TimeOutCSA.py
@@ -1,4 +1,23 @@
 #!/usr/bin/python
+# Copyright (C) 2008 British Broadcasting Corporation and Kamaelia Contributors(1)
+#     All Rights Reserved.
+#
+# You may only modify and redistribute this under the terms of any of the
+# following licenses(2): Mozilla Public License, V1.1, GNU General
+# Public License, V2.0, GNU Lesser General Public License, V2.1
+#
+# (1) Kamaelia Contributors are listed in the AUTHORS file and at
+#     http://kamaelia.sourceforge.net/AUTHORS - please extend this file,
+#     not this notice.
+# (2) Reproduced in the COPYING file, and at:
+#     http://kamaelia.sourceforge.net/COPYING
+# Under section 3.5 of the MPL, we are using this text since we deem the MPL
+# notice inappropriate for this file. As per MPL/GPL/LGPL removal of this
+# notice is prohibited.
+#
+# Please contact us via: kamaelia-list-owner@lists.sourceforge.net
+# to discuss alternative licensing.
+# -------------------------------------------------------------------------
 
 import time
 import Axon
@@ -8,6 +27,9 @@ from Kamaelia.IPC import shutdownCSA
 from Kamaelia.Chassis.Graphline import Graphline
 
 class ResettableSender(Axon.ThreadedComponent.threadedcomponent):
+    """
+
+    """
     timeout=5
     message="NEXT"
     debug = False
@@ -164,28 +186,28 @@ if __name__ == "__main__":
     from Kamaelia.Util.Console import *
     from Kamaelia.Chassis.Pipeline import Pipeline
     EchoServer().run()
-    
+
     sys.exit(0)
-    
+
     if 0:
         ConsoleReader_inactivity=NoActivityTimeout(ConsoleReader, timeout=1, debug=True)
         ConsoleReader_ConfigurableInactivity=ExtendedInactivity(ConsoleReader)
-    
+
         Pipeline(
             ConsoleReader_ConfigurableInactivity(timeout=1, debug=True),
             ConsoleEchoer(),
         ).run()
-    
+
         Pipeline(
             ConsoleReader_inactivity(),
             ConsoleEchoer(),
         ).run()
-    
+
         Pipeline(
             InactivityChassis(ConsoleReader(), timeout=1, debug=True),
             ConsoleEchoer(),
         ).run()
-    
+
         Pipeline(
             NoActivityTimeout(ConsoleReader, timeout=1, debug=True)(),
             ConsoleEchoer(),
