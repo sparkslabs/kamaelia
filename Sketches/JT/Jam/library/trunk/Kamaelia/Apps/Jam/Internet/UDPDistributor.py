8a56150a7189428625634527a97ba6fe61c8c53d
orphans <orphans@2682807a-6752-0410-967a-793f22d1dfe9>
2008-06-16 16:14:57 +0000

    Make UDP distributor component actually work

--- Sketches/JT/Jam/library/trunk/Kamaelia/Apps/Jam/Internet/UDPDistributor.py
+++ Sketches/JT/Jam/library/trunk/Kamaelia/Apps/Jam/Internet/UDPDistributor.py
@@ -1,33 +1,51 @@
 import Axon
+import OSC
+
+from Kamaelia.Chassis.Pipeline import Pipeline
+from Kamaelia.Util.OneShot import OneShot
+from Kamaelia.Apps.Jam.Protocol.Osc import Osc
+from Kamaelia.Apps.Jam.Internet.NewDP import UDPSender
 
 class UDPDistributor(Axon.AdaptiveCommsComponent.AdaptiveCommsComponent):
+    Inboxes = {"inbox" : "",
+               "control" : "",
+               "addPeer" : "",
+              }
+    Outboxes = {"outbox" : "",
+                "signal" : "",
+               }
     def __init__(self):
         super(UDPDistributor, self).__init__()
+        # List of peers needs to exist higher up the pipeline so we can use it
+        # to connect to others (i.e. peer select widget).  Maybe a backplane
+        # system?
         self.peers = []
         self.senders = {}
         self.acceptNewPeers = True
 
-    def createSender(address, port):
+    def createSender(self, address, port):
         self.addOutbox("outbox_%s" % address)
-        sender = UDPSender(remote_addr=address, remote_port=port).activate()
-        self.link(self, "outbox_%s" % address, sender, "inbox")
+        sender = UDPSender(receiver_addr=address, receiver_port=port).activate()
+        self.link((self, "outbox_%s" % address), (sender, "inbox"))
         self.senders[address] = sender
 
-    def sendPeerList(address, port):
-        # Bit ugly - means we end up creating two UDP senders then destroying
-        # one of them.  Maybe OK for now...
-        p = Pipeline(OneShot(("/Jam/PeerList", self.peers)),
-                     Osc(), UDPSender(address, port)).activate()
+    def sendPeerList(self, address, port):
+        # FIXME: Ugly - manual creation of OSC bundle = not very Kamaelia-ific
+        # Better to have a pipeline here with the OSC component in
+        bundle = OSC.OSCBundle("/Jam/PeerList", 0)
+        bundle.append(self.peers)
+        self.send(bundle.getBinary(), "outbox_%s" % address)
 
 
-    def main():
+
+    def main(self):
         while 1:
             if self.dataReady("addPeer"):
                 address, port = self.recv("addPeer")
                 if self.acceptNewPeers:
-                    self.peers.append((address, port))
                     self.createSender(address, port)
                     self.sendPeerList(address, port)
+                    self.peers.append((address, port))
             if self.dataReady("inbox"):
                 data = self.recv("inbox")
                 for peer in self.peers:
@@ -36,6 +54,3 @@ class UDPDistributor(Axon.AdaptiveCommsComponent.AdaptiveCommsComponent):
                 self.pause()
             yield 1
 
-                
-                
-            
