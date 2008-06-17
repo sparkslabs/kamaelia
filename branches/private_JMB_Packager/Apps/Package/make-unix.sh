e4e89bb5415f7bd8bd71a9e4011d859301576b57
amnorvend <amnorvend@2682807a-6752-0410-967a-793f22d1dfe9>
2008-06-17 20:33:55 +0000

    Made it possible to name output file.

--- branches/private_JMB_Packager/Apps/Package/make-unix.sh
+++ branches/private_JMB_Packager/Apps/Package/make-unix.sh
@@ -7,15 +7,21 @@
 #source files, byte compiled modules, and optimized modules as well as any other
 #files that were named in include-files at the command line.  After this, the script
 #will concatenate this zip file with zipheader.unix creating the executable (which
-#will be moved to the zip directory).  
+#will be moved to the zip directory).
 #
-#If clean was specified at the command line, the script will remove the assembly directory 
+#If clean was specified at the command line, the script will remove the assembly directory
 #once it is finished.  It is recommended that you remove the assembly directory prior to
 #running this script again.
 
-prepare.sh #assemble everything we need in the assembly directory
+if [ -z "$1" ]
+then
+    echo "You must provide an executable name to compile your script!"
+    exit 1
+fi
 
-if [ "$1" = "clean" ]
+./prepare.sh #assemble everything we need in the assembly directory
+
+if [ "$2" = "clean" ]
 then
     CLEANUP=$1
     shift 1
@@ -31,14 +37,14 @@ echo ">Removing any previously compiled modules"
 rm -rfv assembly/*.pyc
 echo ">Creating executable"
 find . -name "*.py"|zip -@ kpublish.zip
-cat zipheader.unix kpublish.zip > kpublish
+cat zipheader.unix kpublish.zip > $1
 
 if [ ! -d ../dist ]
 then
     mkdir ../dist
 fi
 
-mv kpublish ../dist
+mv $1 ../dist
 )
 
 if [ "$CLEANUP" = "clean" ]
