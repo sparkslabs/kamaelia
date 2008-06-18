bd041416da906565143599219f7a3f714de05ea9
orphans <orphans@2682807a-6752-0410-967a-793f22d1dfe9>
2008-06-18 13:23:30 +0000

    Make UDP dispatcher track which peers we are connected to seperately from the list of all peers in the network

--- trunk/Sketches/JT/Jam/library/trunk/Kamaelia/Apps/Jam/Internet/UDPDistributor.py
+++ trunk/Sketches/JT/Jam/library/trunk/Kamaelia/Apps/Jam/Internet/UDPDistributor.py
@@ -1,3 +1,4 @@
+import sets
 import Axon
 import OSC
 
@@ -10,46 +11,49 @@ class UDPDistributor(Axon.AdaptiveCommsComponent.AdaptiveCommsComponent):
     Inboxes = {"inbox" : "",
                "control" : "",
                "addPeer" : "",
+               "peerSet" : "",
               }
     Outboxes = {"outbox" : "",
                 "signal" : "",
                }
     def __init__(self):
         super(UDPDistributor, self).__init__()
-        # List of peers needs to exist higher up the pipeline so we can use it
-        # to connect to others (i.e. peer select widget).  Maybe a backplane
-        # system?
-        self.peers = []
+        self.peers = sets.Set()
+        self.connectedPeers = sets.Set()
         self.senders = {}
         self.acceptNewPeers = True
 
     def createSender(self, address, port):
-        self.addOutbox("outbox_%s" % address)
+        boxName = "outbox_%s_%s" % (address, port)
+        self.addOutbox(boxName)
         sender = UDPSender(receiver_addr=address, receiver_port=port).activate()
-        self.link((self, "outbox_%s" % address), (sender, "inbox"))
+        self.link((self, boxName), (sender, "inbox"))
         self.senders[address] = sender
 
     def sendPeerList(self, address, port):
         # FIXME: Ugly - manual creation of OSC bundle = not very Kamaelia-ific
         # Better to have a pipeline here with the OSC component in
         bundle = OSC.OSCBundle("/Jam/PeerList", 0)
-        bundle.append(self.peers)
-        self.send(bundle.getBinary(), "outbox_%s" % address)
-
-
+        bundle.append(list(self.peers))
+        self.send(bundle.getBinary(), "outbox_%s_%s" % (address, port))
 
     def main(self):
         while 1:
             if self.dataReady("addPeer"):
                 address, port = self.recv("addPeer")
-                if self.acceptNewPeers:
-                    self.createSender(address, port)
-                    self.sendPeerList(address, port)
-                    self.peers.append((address, port))
+                if not (address, port) in self.connectedPeers:
+                    if self.acceptNewPeers:
+                        print "New peer connected - %s:%s" % (address, port)
+                        self.createSender(address, port)
+                        self.sendPeerList(address, port)
+                        self.peers.add((address, port))
+                        self.connectedPeers.add((address, port))
+            if self.dataReady("peerSet"):
+                self.peers.update(self.recv("peerSet"))
             if self.dataReady("inbox"):
                 data = self.recv("inbox")
                 for peer in self.peers:
-                    self.send(data, "outbox_%s" % peer[0])
+                    self.send(data, "outbox_%s_%s" % peer)
             if not self.anyReady():
                 self.pause()
             yield 1
