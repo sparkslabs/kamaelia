9b377aa14503c5e34c55b24114e68b950630c3a3
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2008-06-19 21:16:42 +0000

    Push the correct MANIFEST.in up
    Michael

--- trunk/Code/Python/Apps/ReleasePackaging/DistBuild/Release.build.sh
+++ trunk/Code/Python/Apps/ReleasePackaging/DistBuild/Release.build.sh
@@ -39,5 +39,7 @@ egrep -A 1000 START ../../../Axon/setup.py|egrep -B1000 LAST >> ../setup.py
 egrep -A 1000 START ../../../Kamaelia/setup.py|egrep -B1000 LAST >> ../setup.py
 egrep -A1000 "# REPLACE" setup.py.src |grep -v "# REPLACE" >> ../setup.py
 
+cp Release.MANIFEST.in ../MANIFEST.in
+
 cd ..
 python setup.py sdist
