ee6e3f76c845da754658a2cc321fd29aaf69ac4a
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2008-06-20 01:10:47 +0000

    Change over to use new likefile
    Michael.

--- branches/private_MPS_LikeFileRewrite/Kamaelia/Examples/LikeFile/simpleoggradioplayer.py
+++ branches/private_MPS_LikeFileRewrite/Kamaelia/Examples/LikeFile/simpleoggradioplayer.py
@@ -1,32 +1,43 @@
 #!/usr/bin/env python
 
-from Axon.LikeFile import likefile, background
+from Axon.background import background
+from Axon.LikeFile import LikeFile
 from Kamaelia.Codec.Vorbis import VorbisDecode, AOAudioPlaybackAdaptor
 from Kamaelia.Chassis.Pipeline import Pipeline
 from Kamaelia.File.ReadFileAdaptor import ReadFileAdaptor
 from Kamaelia.Internet.TCPClient import TCPClient
+import time
+import Queue
 import ao
 background(slowmo=0.001).start()
 
 filename = "./snail.ogg"
 
-playStream = likefile(Pipeline(VorbisDecode(), AOAudioPlaybackAdaptor()))
+playStream = LikeFile(Pipeline(VorbisDecode(), AOAudioPlaybackAdaptor())).activate()
 # set of components for playing the stream back.
 
 host = "bbc.kamaelia.org"
 port = 1500
 
-client = likefile(TCPClient(host = host, port = port))
+client = LikeFile(TCPClient(host = host, port = port)).activate()
 # component to grab a stream from the internet
 
 filedump = open("streamdump.ogg", "w+b")
 
+def get_item(handle):
+    while 1:
+        try:
+            X = handle.get("outbox")
+            return X
+        except Queue.Empty:
+            time.sleep(0.001)
+
 # Play the ogg data in the background
 while True:
-    data = client.get()
+    data = get_item(client)
     filedump.write(data)
     # log the stream to disk
-    playStream.put(data)
+    playStream.put(data,"inbox")
     # and play it.
 
 # this could all be done entirely within kamaelia but using likefile
