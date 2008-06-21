ecb18ba05a216d04228ad8f88aafddb8ee60982d
orphans <orphans@2682807a-6752-0410-967a-793f22d1dfe9>
2008-06-21 09:59:25 +0000

    Move NewDP.py to UDP_ng.py to match Kamaelia namespace

--- trunk/Sketches/JT/Jam/application/trunk/ConnectionTest.py
+++ trunk/Sketches/JT/Jam/application/trunk/ConnectionTest.py
@@ -1,7 +1,9 @@
-from Kamaelia.Apps.Jam.Internet.NewDP import SimplePeer
+from Kamaelia.Apps.Jam.Internet.UDP_ng import SimplePeer, UDPSender
 from Kamaelia.Util.Console import ConsoleEchoer
 from Kamaelia.Chassis.Pipeline import Pipeline
 from Kamaelia.Apps.Jam.Protocol.Osc import Osc, DeOsc
 from Kamaelia.Util.OneShot import OneShot
 
-Pipeline(OneShot(("/Jam/Connect", 2005)), Osc(), SimplePeer(localaddr="127.0.0.1", localport=2005, receiver_addr="127.0.0.1", receiver_port=2001), DeOsc(0), ConsoleEchoer()).run()
+Pipeline(OneShot(("/Jam/Connect", 2005)), Osc(),
+#         UDPSender(receiver_addr="127.0.0.1", receiver_port=2001)).run()
+         SimplePeer(localaddr="127.0.0.1", localport=2005, receiver_addr="127.0.0.1", receiver_port=2001), DeOsc(0), ConsoleEchoer()).run()
