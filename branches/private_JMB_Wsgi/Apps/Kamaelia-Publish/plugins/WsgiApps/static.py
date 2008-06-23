f2208857447c08e18cf1280352ca012295f0c3fa
amnorvend <amnorvend@2682807a-6752-0410-967a-793f22d1dfe9>
2008-06-23 22:31:32 +0000

    Added ability to translate ~ into home directory.

--- branches/private_JMB_Wsgi/Apps/Kamaelia-Publish/plugins/WsgiApps/static.py
+++ branches/private_JMB_Wsgi/Apps/Kamaelia-Publish/plugins/WsgiApps/static.py
@@ -1,5 +1,6 @@
 from support.la_static import Cling
-import pdb
+import os
 
 def static_app(environ, start_response):
+    environ['kp.static_path'] = os.path.expanduser(environ['kp.static_path'])
     return Cling(environ['kp.static_path'], index_file=environ['kp.index_file']) (environ, start_response)
