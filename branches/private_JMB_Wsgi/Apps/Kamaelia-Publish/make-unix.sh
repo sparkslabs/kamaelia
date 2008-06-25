ee29bfcdcea72876610cab3683386f5e619605e4
amnorvend <amnorvend@2682807a-6752-0410-967a-793f22d1dfe9>
2008-06-25 05:36:32 +0000

    added logic to add configuration data to zip.

--- branches/private_JMB_Wsgi/Apps/Kamaelia-Publish/make-unix.sh
+++ branches/private_JMB_Wsgi/Apps/Kamaelia-Publish/make-unix.sh
@@ -32,6 +32,9 @@ echo ">Removing any previously compiled modules"
 rm -rfv assembly/*.pyc
 echo ">Creating executable"
 find . -name "*.py"|zip -@ kpublish.zip
+find . -name "*.cfg"|zip -@g9 kpublish.zip
+find . -name "*.ini"|zip -@g9 kpublish.zip
+zip kpublish.zip data/.kp
 #find . -name "*.pyc"|zip -@g kpublish.zip
 #find . -name "*.pyo"|zip -@g kpublish.zip
 cat zipheader.unix kpublish.zip > kpublish
