5ead4dcae360d6a51b465173c653be9a68bfd51f
davbo <davbo@2682807a-6752-0410-967a-793f22d1dfe9>
2008-06-25 16:42:07 +0000

    Added button for Eyedropper tool

--- trunk/Sketches/DK/Kamaelia-Paint/App/XYPad.py
+++ trunk/Sketches/DK/Kamaelia-Paint/App/XYPad.py
@@ -196,9 +196,11 @@ class XYPad(Axon.Component.component):
         circleb = Button(caption="Circle",position=(10,10), msg = (("Tool", "Circle"),)).activate()
         lineb = Button(caption="Line",position=(10,50), msg = (("Tool", "Line"),)).activate()
         bucketb = Button(caption="Bucket",position=(10,90), msg = (("Tool", "Bucket"),)).activate()
+        eyeb = Button(caption="Eyedropper",position=(10,130), msg = (("Tool", "Eyedropper"),)).activate()
         self.link( (circleb,"outbox"), (self,"outbox"), passthrough = 2 )
         self.link( (lineb,"outbox"), (self,"outbox"), passthrough = 2 )
         self.link( (bucketb,"outbox"), (self,"outbox"), passthrough = 2 )
+        self.link( (eyeb,"outbox"), (self,"outbox"), passthrough = 2 )
         
         
         
