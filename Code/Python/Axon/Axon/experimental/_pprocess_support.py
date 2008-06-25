ed86859191ce08ff892f6ab2f7fa926d7618e948
matth_rd <matth_rd@2682807a-6752-0410-967a-793f22d1dfe9>
2008-06-25 14:36:50 +0000

    Docstring syntax bugfixes
    Matt

--- Code/Python/Axon/Axon/experimental/_pprocess_support.py
+++ Code/Python/Axon/Axon/experimental/_pprocess_support.py
@@ -19,28 +19,29 @@
 # Please contact us via: kamaelia-list-owner@lists.sourceforge.net
 # to discuss alternative licensing.
 # -------------------------------------------------------------------------
-"""
-=================================================
-likefile - file-like interaction with components.
-=================================================
+"""\
+================================================
+likefile - file-like interaction with components
+================================================
 
 likefile is a way to run Axon components with code that is not Axon-aware. It
 does this by running the scheduler and all associated microprocesses in a
 separate thread, and using a custom component to communicate if so desired.
 
 
+
 Using this code
 ---------------
 
 With a normal kamaelia system, you would start up a component and start
-running the Axon scheduler as follows, either:
+running the Axon scheduler as follows, either::
 
     from Axon.Scheduler import scheduler
     component.activate()
     scheduler.run.runThreads()
     someOtherCode()
 
-or simply:
+or simply::
 
     component.run()
     someOtherCode()
@@ -49,14 +50,14 @@ In both cases, someOtherCode() only run when the scheduler exits. What do you
 do if you want to (e.g.) run this alongside another external library that has
 the same requirement?
 
-Well, first we start the Axon scheduler in the background as follows:
+Well, first we start the Axon scheduler in the background as follows::
 
     from likefile import background
     background().start()
 
 The scheduler is now actively running in the background, and you can start
 components on it from the foreground, in the same way as you would from inside
-kamaelia (don't worry, activate() is threadsafe):
+kamaelia (don't worry, activate() is threadsafe)::
 
     component.activate()
     someOtherCode()
@@ -67,7 +68,7 @@ interact with this component from someOtherCode?
 
 In this case, we use 'likefile', instead of activating. This is a wrapper
 which sits around a component and provides a threadsafe way to interact
-with it, whilst it is running in the backgrounded sheduler:
+with it, whilst it is running in the backgrounded scheduler::
 
     from Axon.LikeFile import likefile
     wrappedComponent = likefile(component)
@@ -76,7 +77,7 @@ with it, whilst it is running in the backgrounded sheduler:
 Now, wrappedComponent is an instance of the likefile wrapper, and you can
 interact with "component" by calling get() on wrappedComponent, to get data
 from the outbox on "component", or by calling put(data) to put "data" into
-the inbox of "component" like so:
+the inbox of "component" like so::
 
     p = likefile( SimpleHTTPClient() )
     p.put("http://google.com")
@@ -86,7 +87,7 @@ the inbox of "component" like so:
 
 for both get() and put(), there is an optional extra parameter boxname,
 allowing you to interact with different boxes, for example to send a message
-with the text "RELOAD" to a component's control inbox, you would do:
+with the text "RELOAD" to a component's control inbox, you would do::
 
     wrappedComponent.put("RELOAD", "control")
     wrappedComponent.get("signal")
@@ -95,12 +96,13 @@ Finally, likefile objects have a shutdown() method that sends the usual Axon
 IPC shutdown messages to a wrapped component, and prevents further IO.
 
 
-Advanced likefile usage.
-------------------------
+
+Advanced likefile usage
+-----------------------
 
 likefile has some optional extra arguments on creation, for handling custom
 boxes outside the "basic 4". For example, to wrap a component with inboxes
-called "secondary" and "tertiary" and an outbox called "debug", You would do:
+called "secondary" and "tertiary" and an outbox called "debug", You would do::
 
     p = likefile( componentMaker, 
                   extraInboxes = ("secondary", "tertiary"),
@@ -112,7 +114,7 @@ It may be the case that the component you are trying to wrap will link its own
 inbox/outbox/signal/control, and this will result in a BoxAlreadyLinkedToDestination
 exception. To stop likefile from wrapping the default 4 boxes, pass the parameter 
 wrapDefault = False. Note that you will need to manually wrap every box you want to use,
-for example to wrap a component that has its own linkages for signal/control:
+for example to wrap a component that has its own linkages for signal/control::
 
     p = likefile( myComponent, 
                   wrapDefault = False,
@@ -125,39 +127,41 @@ for example to wrap a component that has its own linkages for signal/control:
 
 Diagram of likefile's functionality
 -----------------------------------
-likefile is constructed from components like so:
 
 
-     +----------------------------------+
-     |             likefile             |
-     +----------------------------------+
-          |                      / \ 
-          |                       |
-      InQueues                 OutQueues
-          |                       |
-+---------+-----------------------+---------+
-|        \ /                      |         |
-|    +---------+               +--------+   |
-|    |  Input  |   Shutdown    | Output |   |
-|    | Wrapper | ------------> |        |   |
-|    | (thread)|   Message     |Wrapper |   |
-|    +---------+               +--------+   |
-|         |                      / \        |
-|         |                       |         |
-|     Inboxes                 Outboxes      |
-|         |                       |         |
-|        \ /                      |         |
-|    +----------------------------------+   |
-|    |      the wrapped component       |   |
-|    +----------------------------------+   |
-|                                           |
-|    +----------------------------------+   |
-|    |       Some other component       |   | 
-|    |     that was only activated      |   |
-|    +----------------------------------+   |
-|                                           |
-|  AXON SCHEDULED COMPONENTS                |
-+-------------------------------------------+
+likefile is constructed from components like so::
+
+
+         +----------------------------------+
+         |             likefile             |
+         +----------------------------------+
+              |                      / \ 
+              |                       |
+          InQueues                 OutQueues
+              |                       |
+    +---------+-----------------------+---------+
+    |        \ /                      |         |
+    |    +---------+               +--------+   |
+    |    |  Input  |   Shutdown    | Output |   |
+    |    | Wrapper | ------------> |        |   |
+    |    | (thread)|   Message     |Wrapper |   |
+    |    +---------+               +--------+   |
+    |         |                      / \        |
+    |         |                       |         |
+    |     Inboxes                 Outboxes      |
+    |         |                       |         |
+    |        \ /                      |         |
+    |    +----------------------------------+   |
+    |    |      the wrapped component       |   |
+    |    +----------------------------------+   |
+    |                                           |
+    |    +----------------------------------+   |
+    |    |       Some other component       |   | 
+    |    |     that was only activated      |   |
+    |    +----------------------------------+   |
+    |                                           |
+    |  AXON SCHEDULED COMPONENTS                |
+    +-------------------------------------------+
 
 
 
@@ -185,9 +189,11 @@ DEFIN = ["inbox", "control"]
 DEFOUT = ["outbox", "signal"]
 
 def addBox(names, boxMap, addBox): # XXX REVIEW: Using the function name as a parameter name
-        """Add an extra wrapped box called name, using the addBox function provided
+        """\
+        Add an extra wrapped box called name, using the addBox function provided
         (either self.addInbox or self.addOutbox), and adding it to the box mapping
-        which is used to coordinate message routing within component wrappers."""
+        which is used to coordinate message routing within component wrappers.
+        """
         for boxname in names:
             if boxname in boxMap:
                 raise ValueError, "%s %s already exists!" % (direction, boxname) # XXX REVIEW: *direction* doesn't actually exist. If this appeared in any other line besides a "raise..." line this would be a problem.
