7bcf740cc1ddea1f9ba157f1ca48b2e0d3c097f7
amnorvend <amnorvend@2682807a-6752-0410-967a-793f22d1dfe9>
2008-06-26 03:20:46 +0000

    Removing Kamaelia-Publish data that was duplicated from branch.

--- trunk/Sketches/JMB/Kamaelia-Publish/byte-compile.py
+++
@@ -1,3 +0,0 @@
-import py_compile
-
-py_compile.main()
--- trunk/Sketches/JMB/Kamaelia-Publish/scripts/package_kamaelia.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-CURR_DIR=`pwd`
-
-cd ../../Kamaelia/
-python setup.py sdist --formats=gztar -d "${CURR_DIR}/dist"
-cd $CURR_DIR
