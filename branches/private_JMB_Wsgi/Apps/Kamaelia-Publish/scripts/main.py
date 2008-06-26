c144a0316acc8122817102df7197fc23ecf844d2
amnorvend <amnorvend@2682807a-6752-0410-967a-793f22d1dfe9>
2008-06-26 17:12:24 +0000

    Added python path processing capability to the server config file.

--- branches/private_JMB_Wsgi/Apps/Kamaelia-Publish/scripts/main.py
+++ branches/private_JMB_Wsgi/Apps/Kamaelia-Publish/scripts/main.py
@@ -12,18 +12,41 @@ sys.path.insert(0, sys.argv[0] + '/data')
 
 def normalizeWsgiVars(WsgiConfig):
     WsgiConfig['wsgi_ver'] = tuple(WsgiConfig['wsgi_ver'].split('.'))
+    
+def normalizeUrlList(url_list):
+    for dict in url_list:
+        if not dict.get('kp.app_object'):
+            dict['kp.app_object'] = 'application'
+            
+def processServerConfig(ServerConfig):
+    if ServerConfig.get('pypath-append'):
+        path_append = ServerConfig['pypath-append'].split(':')
+        sys.path.extend(path_append)
+    
+    if ServerConfig.get('pypath-prepend'):
+        path_prepend = ServerConfig['pypath-prepend'].split(':')
+        path_prepend.reverse()
+        for path in path_prepend:
+            sys.path.insert(0, path)
+    
+    #print 'sys.path-'
+    #print sys.path
 
 def main():
     configs = ParseConfigFile('~/.kp', DictFormatter())
     ServerConfig = configs['SERVER']
     WsgiConfig = configs['WSGI']
-    from pprint import pprint
+    
+    processServerConfig(ServerConfig)
+    
+    #from pprint import pprint
     #pprint(WsgiConfig)
     normalizeWsgiVars(WsgiConfig)
 
     sys.path.append(WsgiConfig['log'])
 
     url_list = ParseConfigFile(WsgiConfig['url_list'], [DictFormatter(), UrlListFormatter()])
+    normalizeUrlList(url_list)
 
     log = Log.LogWriter(WsgiConfig['log'], wrapper=Log.nullWrapper)
 
