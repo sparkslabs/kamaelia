55401874e7a6ab1361f8fc0bc212c5eaef07def8
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2008-06-27 21:53:21 +0000

    Little "toy" experiment. Relies on the code in Michael's SpeakNLearn branch - or the version of kamaelia in the tarball for Speak N Write.
    What does it do? Well it's a first off the bat, ad hoc test of "scribble
    a circle" to "add an unconfigured Kamaelia node" - to allow
    (potentially) _drawing_ kamaelia systems (literally).
    
    
    Michael.

---
+++ trunk/Sketches/MPS/Compose2/ScribbleNLink.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python
+
+import random
+import Axon
+
+from Kamaelia.Util.Backplane import *
+from Kamaelia.Chassis.Pipeline import Pipeline
+from Kamaelia.Chassis.Graphline import Graphline
+
+from Kamaelia.File.UnixProcess import UnixProcess
+
+from Kamaelia.UI.Pygame.Display import PygameDisplay
+from Kamaelia.UI.Pygame.Image import Image
+from Kamaelia.UI.Pygame.Text import TextDisplayer, Textbox
+from Kamaelia.Util.Console import ConsoleEchoer, ConsoleReader
+
+from Kamaelia.Apps.Whiteboard.Canvas import Canvas
+from Kamaelia.Apps.SpeakNLearn.Gestures.StrokeRecogniser import StrokeRecogniser
+from Kamaelia.Apps.SpeakNLearn.Gestures.Pen import Pen
+
+from Kamaelia.Visualisation.Axon.AxonVisualiserServer import AxonVisualiser, text_to_token_lists
+
+pgd = PygameDisplay( width=1024, height=500 ).activate()
+PygameDisplay.setDisplayService(pgd)
+
+Backplane("STROKES").activate()
+Backplane("AXONVIS").activate()
+bgcolour = (255,255,180)
+
+node_add_template = \
+"""ADD NODE %(nodeid)s non_config randompos component
+ADD NODE %(nodeid)s.o.outbox "outbox" randompos outbox
+ADD NODE %(nodeid)s.o.signal "signal" randompos outbox
+ADD NODE %(nodeid)s.i.inbox "inbox" randompos inbox
+ADD NODE %(nodeid)s.i.control "control" randompos inbox
+ADD LINK %(nodeid)s %(nodeid)s.o.outbox
+ADD LINK %(nodeid)s %(nodeid)s.o.signal
+ADD LINK %(nodeid)s %(nodeid)s.i.inbox
+ADD LINK %(nodeid)s %(nodeid)s.i.control
+"""
+
+class NodeAdder(Axon.Component.component):
+    def main(self):
+        nodeid = 0
+        while True:
+            for message in self.Inbox("inbox"):
+                if message in [ "o", "u" ]:
+                     thisis = str(nodeid)
+                     nodeid = nodeid +1
+                     nodedef = node_add_template % { "nodeid": nodeid }
+                     self.send(nodedef, "outbox")
+                else:
+                   print repr(message)
+            yield 1
+
+Pipeline(
+         ConsoleReader(),
+         PublishTo("AXONVIS"),
+).activate()
+
+Pipeline(
+         SubscribeTo("AXONVIS"),
+         text_to_token_lists(),
+         AxonVisualiser(caption="Axon / Kamaelia Visualiser",screensize=(1024,500), position=(0,0), transparency=(255,255,255))
+).activate()
+
+Pipeline(
+     SubscribeTo("STROKES"),
+     NodeAdder(),
+     PublishTo("AXONVIS"),
+).activate()
+
+
+Pipeline(
+         SubscribeTo("STROKES"),
+         ConsoleEchoer(),
+).activate()
+
+Graphline(
+           CANVAS  = Canvas( position=(0,0),
+                             size=(1024,500),
+                             bgcolour = bgcolour,
+                           ),
+           PEN     = Pen(bgcolour = bgcolour),
+           STROKER = StrokeRecogniser(),
+           OUTPUT  = PublishTo("STROKES"),
+           linkages = {
+               ("CANVAS",  "eventsOut") : ("PEN", "inbox"),
+               ("PEN", "outbox")        : ("CANVAS", "inbox"),
+               ("PEN", "points")        : ("STROKER", "inbox"),
+               ("STROKER", "outbox")    : ("OUTPUT", "inbox"),
+               ("STROKER", "drawing")   : ("CANVAS", "inbox"),
+               ("STROKER", "outbox")    : ("OUTPUT", "inbox"),
+               },
+        ).run()
+        
