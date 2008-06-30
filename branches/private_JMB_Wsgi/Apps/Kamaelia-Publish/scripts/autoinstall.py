cba872f7888f2254a44154fd51be557f559ff6f8
amnorvend <amnorvend@2682807a-6752-0410-967a-793f22d1dfe9>
2008-06-30 21:40:28 +0000

    Autoinstall ALMOST works now.

--- branches/private_JMB_Wsgi/Apps/Kamaelia-Publish/scripts/autoinstall.py
+++ branches/private_JMB_Wsgi/Apps/Kamaelia-Publish/scripts/autoinstall.py
@@ -1,121 +1,105 @@
-import sys, zipfile, re
+import sys, zipfile, re, os, tarfile, cStringIO, shutil
+
 import Axon
 from Axon.Component import component
 from Axon.Ipc import producerFinished, shutdownMicroprocess
+from Kamaelia.Chassis.Pipeline import Pipeline
 
-def autoinstall():
-    prompt_text = 'It does not appear that Kamaelia Publish has been installed on your computer yet.  Would you like to do so now? [y/n]'
+from console_io import prompt_yesno
+
+def autoinstall(zip, dir):
+    prompt_text = 'It does not appear that Kamaelia Publish has been installed.  Would you like to do so now? [y/n]'    
     if not prompt_yesno(prompt_text):
         print 'Kamaelia Publish must be installed to continue.  Halting.'
         sys.exit(1)
-            
-    zip = zipfile.ZipFile(sys.argv[0], 'r')
     
-    corrupt = zip.testzip()
-    if not corrupt:
-        print 'The following files appear to be corrupted: \n', corrupt
-        if not prompt_yesno('Would you like to continue anyway? [y/n]'):
-            print "Halting!"
-            sys.exit(1)
-            
-    namelist = zip.namelist()
+    tar_mem = cStringIO.StringIO( zip.read('data/kpuser.tar') )
+    kpuser_file = tarfile.open(fileobj=tar_mem, mode='r')
+    kpuser_file.extractall(path=dir)
     
+    kpuser_file.close()
+    tar_mem.close()
+    
+    shutil.move(dir + '/data/kpuser', dir + '/kpuser')
+    shutil.move(dir + '/data/kp.ini', dir + '/kp.ini')
 
-def prompt_yesno(text):
-    """
-    Just a generic function to determine if the user wants to continue or not.
-    Will repeat if input is unrecognizable.
-    """
-    user_input = raw_input(text)
-    
-    if user_input[0] == 'y' or input[0] == 'Y':
-        return True
-    elif user_input[0] == 'n' or input[0] == 'N':
-        return False
-    else:
-        print 'Unrecognizable input.  Please try again'
-        return prompt_continue()
-    
 class AutoInstallBase(component):
+    MethodText = 'handle_'
+    ShutDownConditions = (producerFinished, shutdownMicroprocess)
+    DataInZip = 'data/kpuser'
     def __init__(self, **argd):
-        super(AutoInstallBase, self).__init__(argd)
+        super(AutoInstallBase, self).__init__(**argd)
+        self.not_done = True
         
-    def getInbox(self, box='inbox'):
+    def main(self):
+        while self.not_done:
+            for box in self.inboxes:
+                method_name = self.MethodText + box
+                if hasattr(self, method_name):
+                    print type(self)
+                    self.method = getattr(self, method_name)
+                    [self.method(x) for x in self.getInbox(box)]
+                    
+            if (not self.anyReady()) and self.not_done:
+                self.pause()
+                
+            yield 1
+        
+    def getInbox(self, name='inbox'):
         while self.dataReady(name):
             yield self.recv(name)
+            
+    def handle_control(signal):
+        if isinstance(signal, self.ShutDownConditions):
+            self.not_done = False
+            self.send(signal, 'signal')
     
 class NameSelector(component):
-    def __init__(self, namelist, regex):
+    DataInZip = 'data/kpuser'
+    def __init__(self, namelist, **argd):
         self.inamelist = iter(namelist)
-        self.regex = regex
-        super(name_selector, self).__init__()
+        super(NameSelector, self).__init__(argd)
+        if not self.DataInZip:
+            raise "You must define a DataInZip argument to determine where installation data is located!"
+        self.regex = re.compile(self.DataInZip)
         
     def main(self):
         not_done = True
         while True:
             try:
-                item = inamelist.next()
+                item = self.inamelist.next()
             except StopIteration:
                 break
             
-            if regex.search(item):
+            if self.regex.search(item):
                 self.send(item)
                 
             yield 1
             
         self.send(producerFinished(self), 'signal')
         
-class FileExtractor(AutoInstallBase ):
-    def __init__(self, zip):
-        self.zip = zip
-        super(FileExtractor, self).__init__()
-        
-    def main(self):
-        self.not_done = True  #variable is changed in self.handle_ctl_item
-        
-        while self.not_done:
-            [self.handle_inbox_item(x) for x in self.getInbox('inbox')]          
-            [self.handle_ctl_item(x) for x in self.getInbox('control')]
-                    
-            if not self.anyReady() and not_done:
-                self.pause()
-                
-            yield 1
+class FileExtractor(AutoInstallBase):
+    zip = None
+    def __init__(self, **argd):
+        super(FileExtractor, self).__init__(**argd)
+        if not self.zip:
+            raise "Zip file must be specified!"
             
-    def handle_inbox_item(self, item):
-        data = self.zip.read(name)
-        self.send((name, data))
-        
-    def handle_ctl_itm(self, item):
-        if isinstance(item, producerFinished):
-            self.not_done = False
-        if isinstance(item, shutdownMicroprocess):
-            self.not_done = False
-            
-_kpuser_location = 'data/kpuser'
+    def handle_inbox(self, item):
+        data = self.zip.read(item)
+        self.send((item, data))
             
 class FileWriter(AutoInstallBase):
-    def __init__(self, path, regex):
-        self.path = path
-        self.regex = regex
-        super(FileWriter, self).__init__()
-        
-    def main(self):
-        self.not_done = False
-        
-        while self.not_done:  #self.not_done is set in handleControlBox
-            [self.handleInbox(x) for x in self.getInbox()]
-            [self.handleControlBox(x) for x in self.getInbox('control')]
-            
-            if not self.anyReady() and not_done:
-                self.pause()
-                
-            yield 1
-            
-    def handleInbox(tup):
+    DataInZip = 'data/kpuser'
+    InstallPath = "~/kpuser"
+    def __init__(self, **argd):
+        super(FileWriter, self).__init__(**argd)
+        self.InstallPath = os.path.expanduser(self.InstallPath)
+
+    def handle_inbox(self, tup):
         name, data = tup
         
-        name = name.replace(_kpuser_location, self.path)
+        name = name.replace(self.DataInZip, self.InstallPath)
 
         ext = name.rsplit('.', 1)[-1]
         if ext == 'ini' or ext == 'kp': #these extensions are non-binary
@@ -127,6 +111,6 @@ class FileWriter(AutoInstallBase):
         file.write(data)
         file.close()
         
-    def handleControlBox(signal):
+    def handle_control(signal):
         if isinstance(signal, (producerFinished, shutdownMicroprocess)):
             self.not_done = False
