6d4ef96a8edf619cc07747f9155cd1b9983d550f
amnorvend <amnorvend@2682807a-6752-0410-967a-793f22d1dfe9>
2008-06-30 01:09:02 +0000

    Added commented out debugging code

--- branches/private_JMB_Wsgi/Apps/Kamaelia-Publish/plugins/WsgiApps/static.py
+++ branches/private_JMB_Wsgi/Apps/Kamaelia-Publish/plugins/WsgiApps/static.py
@@ -3,4 +3,6 @@ import os
 
 def static_app(environ, start_response):
     environ['kp.static_path'] = os.path.expanduser(environ['kp.static_path'])
+    #from pprint import pprint
+    #pprint(environ)
     return Cling(environ['kp.static_path'], index_file=environ['kp.index_file']) (environ, start_response)
