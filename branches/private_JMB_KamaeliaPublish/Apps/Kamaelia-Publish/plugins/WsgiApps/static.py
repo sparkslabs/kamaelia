f3e223d6ce621246aa0180e8a92d0474f7456175
amnorvend <amnorvend@2682807a-6752-0410-967a-793f22d1dfe9>
2008-07-01 21:08:56 +0000

    Removing old branch.

--- branches/private_JMB_KamaeliaPublish/Apps/Kamaelia-Publish/plugins/WsgiApps/static.py
+++
@@ -1,5 +0,0 @@
-from support.la_static import Cling
-import pdb
-
-def static_app(environ, start_response):
-    return Cling(environ['kp.static_path'], index_file=environ['kp.index_file']) (environ, start_response)
