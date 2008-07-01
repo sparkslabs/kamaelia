f355aaeae41d2ce20371b70cc497f29faed54f58
nctrun <nctrun@2682807a-6752-0410-967a-793f22d1dfe9>
2008-07-01 01:55:07 +0000

    * Major not-working changes to try to use Handle instead of "old" LikeFile. * This is mainly a "backup" of this version, since in the new branch I'm going to use LikeFile to start testing other code as soon as possible.

--- Sketches/PO/KamPlanet/SimpleSample.py
+++ Sketches/PO/KamPlanet/SimpleSample.py
@@ -9,17 +9,23 @@ class SimpleComponent(Axon.Component.component):
             "control"  : "signal messages are placed here", 
         }
     def main(self):
+        print "Starting SimpleComponent..."
         while True:
+            print "yet another iteration..."
             while self.dataReady('inbox'):
                 data = self.recv('inbox')
                 self.send(data, 'outbox')
+                print "SimpleComponent::inbox...", data
             while self.dataReady('numbers'):
                 data = self.recv('numbers')
                 self.send(str(data), 'outbox')
+                print "SimpleComponent::numbers...", data
             while self.dataReady('control'):
                 data = self.recv('control')
                 self.send(data, 'signal')
+                print "SimpleComponent::control...", data
                 return
             if not self.anyReady():
+                print "SimpleComponent::pausing..."
                 self.pause()
             yield 1
