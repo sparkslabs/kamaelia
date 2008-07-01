63a4b6a41768170f540ce40f00f20295859001cb
nctrun <nctrun@2682807a-6752-0410-967a-793f22d1dfe9>
2008-07-01 01:55:07 +0000

    * Major not-working changes to try to use Handle instead of "old" LikeFile. * This is mainly a "backup" of this version, since in the new branch I'm going to use LikeFile to start testing other code as soon as possible.

--- trunk/Sketches/PO/KamPlanet/test/test_SimpleSample.py
+++ trunk/Sketches/PO/KamPlanet/test/test_SimpleSample.py
@@ -29,8 +29,12 @@ from SimpleSample import SimpleComponent
 
 class SimpleSampleTestCase(KamTestCase.KamTestCase):
     def setUp(self):
+        print "setUp..."
         self.simpleSample = SimpleComponent()
         self.initializeSystem(self.simpleSample)
+        
+    def tearDown(self):
+        print "tearDown..."
         
     def testForwardsNumbers(self):
         self.put(5, 'numbers')
