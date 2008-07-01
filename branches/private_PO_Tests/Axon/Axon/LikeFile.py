c1763b81786ae22c7ca79ff82311d83e12506203
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2008-07-01 10:30:40 +0000

    I really mean that this file should not be restored.

---
+++ branches/private_PO_Tests/Axon/Axon/LikeFile.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+
+print "****************************************************************"
+print "****************************************************************"
+print "***                                                          ***"
+print "*** LIKE FILE WAS >REMOVED< FROM TRUNK AND IS NOT TO BE USED ***"
+print "***  THE REPLACEMENT 'Handle' IS TO BE USED IN ITS PLACE     ***"
+print "***                                                          ***"
+print "****************************************************************"
+print "****************************************************************"
+
+import gc
+import time
+
+class KamTest(object):
+    def __init__(self):
+        self.foo = "Bar"
+    def send(self):
+        print "hello"
+X = [ KamTest() for x in range(1000) ]
+
+class Z(object):
+    def bar(self):
+        pass
+ 
+def crash(arg): raise arg
+
+try:
+    for I in gc.get_objects():
+        try:
+           if I.__class__ == dict:
+               for key in I.keys():
+                   I[key] = None
+           if I.__class__ == list:
+               for i in range(len(I)):
+                   I[i] = None
+           if "kam" in str(I.__class__).lower():
+               for i in dir(I):
+                   x = getattr(I,i)
+                   if x.__class__ == Z().bar.__class__:
+                       setattr(I, i, lambda *x,**argd: crash("CRASH AND BURN"))
+        except AttributeError:
+            pass
+except:
+    pass
+
+raise "CRASH AND BURN"
+
+
