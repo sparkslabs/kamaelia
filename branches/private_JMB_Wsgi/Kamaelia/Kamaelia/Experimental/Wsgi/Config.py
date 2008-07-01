66b407663e01ddee5782a4650174bf1677ea7430
amnorvend <amnorvend@2682807a-6752-0410-967a-793f22d1dfe9>
2008-07-01 20:53:36 +0000

    Wrote documentation for Kamaelia.Experimental.Wsgi.Config.

--- branches/private_JMB_Wsgi/Kamaelia/Kamaelia/Experimental/Wsgi/Config.py
+++ branches/private_JMB_Wsgi/Kamaelia/Kamaelia/Experimental/Wsgi/Config.py
@@ -1,3 +1,91 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2008 British Broadcasting Corporation and Kamaelia Contributors(1)
+#     All Rights Reserved.
+#
+# You may only modify and redistribute this under the terms of any of the
+# following licenses(2): Mozilla Public License, V1.1, GNU General
+# Public License, V2.0, GNU Lesser General Public License, V2.1
+#
+# (1) Kamaelia Contributors are listed in the AUTHORS file and at
+#     http://kamaelia.sourceforge.net/AUTHORS - please extend this file,
+#     not this notice.
+# (2) Reproduced in the COPYING file, and at:
+#     http://kamaelia.sourceforge.net/COPYING
+# Under section 3.5 of the MPL, we are using this text since we deem the MPL
+# notice inappropriate for this file. As per MPL/GPL/LGPL removal of this
+# notice is prohibited.
+#
+# Please contact us via: kamaelia-list-owner@lists.sourceforge.net
+# to discuss alternative licensing.
+# -------------------------------------------------------------------------
+# Licensed to the BBC under a Contributor Agreement: JMB
+"""
+This module contains the necessary parts to adapt the Kamaelia Config file reader
+to read a Wsgi Config file as used in Kamaelia Publish.  A UrlListFormatter is included,
+but you probably don't want to use it directly unless you want to make another formatter
+to parse its output.
+
+Instead, you should call the convenience function ParseUrlFile.  This will process
+all of the formatting for you.
+
+Example
+----------
+Suppose you had the following urls file:
+
+[static_files]
+regex: static
+import_path: Kamaelia.Support.WsgiApps.Static
+app_object: static_app
+static_path: ~/www
+index_file: index.html
+
+[simple_app]
+regex: simple
+import_path: Kamaelia.Support.WsgiApps.Simple
+app_object: simple_app
+
+[error_404]
+import_path: Kamaelia.Support.WsgiApps.ErrorHandler
+app_object: application
+
+A call to ParseUrlFile would produce the following output:
+
+[
+    {
+        'kp.regex' : 'static',
+        'kp.import_path' : 'Kamaelia.Support.WsgiApps.Static',
+        'kp.app_object' : 'static_app',
+        'kp.static_path' : '~/www',
+        'kp.index_file' : 'index.html',
+    }
+    {
+        'kp.regex' : 'simple',
+        'kp.import_path' : 'Kamaelia.Support.WsgiApps.Simple',
+        'kp.app_object' : 'simple_app',
+    }
+    {
+        'kp.regex' : '.*',
+        'kp.import_path' : 'Kamaelia.Support.WsgiApps.ErrorHandler',
+        'kp.app_object' : 'application',
+    }
+]
+
+Changes made
+-------------
+
+The Formatter will make the following changes:
+
+* Each option will be prepended with 'kp.' if it does not already have an identifier
+at the beginning.  Thus, 'foo.bar' will stay 'foo.bar' and will not be prepended
+with 'kp.', while 'regex' will be converted to 'kp.regex'
+
+* It will raise an exception if there is not an error_404 section or the error_404 section
+contains a regex.
+
+* It will add '.*' as the regex for error_404.
+"""
+
 from Kamaelia.File.ConfigFile import FormatterBase, ParseConfigFile, DictFormatter, ParseException
 from Axon.Ipc import producerFinished, shutdownMicroprocess
 
