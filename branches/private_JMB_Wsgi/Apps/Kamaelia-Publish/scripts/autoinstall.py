3224a800b2800b0aba4dc9405d13ee3e0aa3fe84
amnorvend <amnorvend@2682807a-6752-0410-967a-793f22d1dfe9>
2008-07-02 01:41:50 +0000

    Just some commenting and file shuffling.

--- branches/private_JMB_Wsgi/Apps/Kamaelia-Publish/scripts/autoinstall.py
+++ branches/private_JMB_Wsgi/Apps/Kamaelia-Publish/scripts/autoinstall.py
@@ -1,4 +1,31 @@
-import sys, zipfile, re, os, tarfile, cStringIO, shutil
+#!/usr/bin/env python
+#
+# Copyright (C) 2008 British Broadcasting Corporation and Kamaelia Contributors(1)
+#     All Rights Reserved.
+#
+# You may only modify and redistribute this under the terms of any of the
+# following licenses(2): Mozilla Public License, V1.1, GNU General
+# Public License, V2.0, GNU Lesser General Public License, V2.1
+#
+# (1) Kamaelia Contributors are listed in the AUTHORS file and at
+#     http://kamaelia.sourceforge.net/AUTHORS - please extend this file,
+#     not this notice.
+# (2) Reproduced in the COPYING file, and at:
+#     http://kamaelia.sourceforge.net/COPYING
+# Under section 3.5 of the MPL, we are using this text since we deem the MPL
+# notice inappropriate for this file. As per MPL/GPL/LGPL removal of this
+# notice is prohibited.
+#
+# Please contact us via: kamaelia-list-owner@lists.sourceforge.net
+# to discuss alternative licensing.
+# -------------------------------------------------------------------------
+# Licensed to the BBC under a Contributor Agreement: JMB
+"""
+This module contains the functionality for autoinstall of necessary config files.
+At this point, it installs everything to a predefined location, but that should hopefully
+change at some point.
+"""
+import sys, zipfile, os, tarfile, cStringIO
 
 import Axon
 from Axon.Component import component
@@ -19,95 +46,3 @@ def autoinstall(zip, dir):
     
     kpuser_file.close()
     tar_mem.close()
-
-class AutoInstallBase(component):
-    MethodText = 'handle_'
-    ShutDownConditions = (producerFinished, shutdownMicroprocess)
-    DataInZip = 'data/kpuser'
-    def __init__(self, **argd):
-        super(AutoInstallBase, self).__init__(**argd)
-        self.not_done = True
-        
-    def main(self):
-        while self.not_done:
-            for box in self.inboxes:
-                method_name = self.MethodText + box
-                if hasattr(self, method_name):
-                    print type(self)
-                    self.method = getattr(self, method_name)
-                    [self.method(x) for x in self.getInbox(box)]
-                    
-            if (not self.anyReady()) and self.not_done:
-                self.pause()
-                
-            yield 1
-        
-    def getInbox(self, name='inbox'):
-        while self.dataReady(name):
-            yield self.recv(name)
-            
-    def handle_control(signal):
-        if isinstance(signal, self.ShutDownConditions):
-            self.not_done = False
-            self.send(signal, 'signal')
-    
-class NameSelector(component):
-    DataInZip = 'data/kpuser'
-    def __init__(self, namelist, **argd):
-        self.inamelist = iter(namelist)
-        super(NameSelector, self).__init__(argd)
-        if not self.DataInZip:
-            raise "You must define a DataInZip argument to determine where installation data is located!"
-        self.regex = re.compile(self.DataInZip)
-        
-    def main(self):
-        not_done = True
-        while True:
-            try:
-                item = self.inamelist.next()
-            except StopIteration:
-                break
-            
-            if self.regex.search(item):
-                self.send(item)
-                
-            yield 1
-            
-        self.send(producerFinished(self), 'signal')
-        
-class FileExtractor(AutoInstallBase):
-    zip = None
-    def __init__(self, **argd):
-        super(FileExtractor, self).__init__(**argd)
-        if not self.zip:
-            raise "Zip file must be specified!"
-            
-    def handle_inbox(self, item):
-        data = self.zip.read(item)
-        self.send((item, data))
-            
-class FileWriter(AutoInstallBase):
-    DataInZip = 'data/kpuser'
-    InstallPath = "~/kpuser"
-    def __init__(self, **argd):
-        super(FileWriter, self).__init__(**argd)
-        self.InstallPath = os.path.expanduser(self.InstallPath)
-
-    def handle_inbox(self, tup):
-        name, data = tup
-        
-        name = name.replace(self.DataInZip, self.InstallPath)
-
-        ext = name.rsplit('.', 1)[-1]
-        if ext == 'ini' or ext == 'kp': #these extensions are non-binary
-            mode = 'w'
-        else:
-            mode = 'wb'
-
-        file = open(name, mode)
-        file.write(data)
-        file.close()
-        
-    def handle_control(signal):
-        if isinstance(signal, (producerFinished, shutdownMicroprocess)):
-            self.not_done = False
--- branches/private_JMB_Wsgi/Apps/Kamaelia-Publish/scripts/console_io.py
+++ branches/private_JMB_Wsgi/Apps/Kamaelia-Publish/scripts/console_io.py
@@ -1,3 +1,28 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2008 British Broadcasting Corporation and Kamaelia Contributors(1)
+#     All Rights Reserved.
+#
+# You may only modify and redistribute this under the terms of any of the
+# following licenses(2): Mozilla Public License, V1.1, GNU General
+# Public License, V2.0, GNU Lesser General Public License, V2.1
+#
+# (1) Kamaelia Contributors are listed in the AUTHORS file and at
+#     http://kamaelia.sourceforge.net/AUTHORS - please extend this file,
+#     not this notice.
+# (2) Reproduced in the COPYING file, and at:
+#     http://kamaelia.sourceforge.net/COPYING
+# Under section 3.5 of the MPL, we are using this text since we deem the MPL
+# notice inappropriate for this file. As per MPL/GPL/LGPL removal of this
+# notice is prohibited.
+#
+# Please contact us via: kamaelia-list-owner@lists.sourceforge.net
+# to discuss alternative licensing.
+# -------------------------------------------------------------------------
+# Licensed to the BBC under a Contributor Agreement: JMB
+"""This is just a source file for miscellaneous console IO functions (in case you
+weren't able to guess by the title :) ).
+
 def prompt_yesno(text):
     """
     Just a generic function to determine if the user wants to continue or not.
@@ -14,6 +39,7 @@ def prompt_yesno(text):
         return prompt_yesno(text)
     
 def prompt_corrupt(corrupt):
+    """This is really just a convenience method for prompt_yesno."""
     print 'The following files appear to be corrupted: \n', corrupt, \
         '\n There may be more corrupted files.'
     if not prompt_yesno('Would you like to continue anyway? [y/n]'):
