693525a8ec75d95e6a14c44b1ec93c37ea1372c4
orphans <orphans@2682807a-6752-0410-967a-793f22d1dfe9>
2008-07-03 09:50:54 +0000

    Change build script and setup file template to reflect changes to setupcombiner.py

--- Sketches/JT/Jam/distbuild/build.sh
+++ Sketches/JT/Jam/distbuild/build.sh
@@ -33,27 +33,11 @@ else
 fi
 
 echo "Creating setup file"
-cp setup.py.src setup.py
 
-# Get stuff from Axon and Kamaelia - this doesn't follow the naming
-packages=`egrep -A 1000 START ../../../../Code/Python/Axon/setup.py|egrep -B1000 LAST`
-packages="${packages}\n`egrep -A 1000 START ../../../../Code/Python/Kamaelia/setup.py|egrep -B1000 LAST`"
-scripts=""
-data="" 
-
-setup_files=("../library/trunk/setup.py" "../application/trunk/setup.py")
-for file in ${setup_files[@]}
-do
-    packages="${packages}\n`egrep -A 1000 STARTPACKAGES $file|egrep -B1000 LASTPACKAGES`"
-    scripts="${scripts}\n`egrep -A 1000 STARTSCRIPTS $file|egrep -B1000 LASTSCRIPTS`"
-    data="${data}\n`egrep -A 1000 STARTDATA $file|egrep -B1000 LASTDATA`"
-done;
-
-./revomit.py "$packages" "$scripts" "$data" setup.py
-
+./revomit.py setup.py.src ../../../../Code/Python/Axon/setup.py ../../../../Code/Python/Kamaelia/setup.py ../library/trunk/setup.py ../application/trunk/setup.py setup.py
+echo "Copying setup file"
 mv setup.py ../build/setup.py
-
-
+echo "Building sdist"
 cd ../build
 python setup.py sdist --formats=gztar,zip
 
