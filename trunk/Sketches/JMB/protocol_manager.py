569079af68de87523e3542f2c62906992d43740e
amnorvend <amnorvend@2682807a-6752-0410-967a-793f22d1dfe9>
2008-07-04 00:59:58 +0000

    ALMOST works.  Got the system to echo back part of the response.

--- trunk/Sketches/JMB/protocol_manager.py
+++ trunk/Sketches/JMB/protocol_manager.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
+from pprint import pprint, pformat
+
 import Axon
 from Axon.AdaptiveCommsComponent import AdaptiveCommsComponent
+from Axon.Component import component
+from Axon.Ipc import producerFinished, shutdownMicroprocess
 from Kamaelia.Chassis.Graphline import Graphline
+
+from headstock.api.message import Message
 
 class ProtocolManager(AdaptiveCommsComponent):
     """
@@ -23,54 +29,119 @@ class ProtocolManager(AdaptiveCommsComponent):
         if not self.Protocol:
             raise BadProtocol('You must specify a protocol to use the Protocol Manager!')
         try:
-            Protocol
+            self.Protocol()
         except TypeError:
             raise BadProtocol('ProtocolManager.Protocol is not callable.')
         
-        self.message_store = {} #to be indexed by the outbox associated with the protocol
+        self.tracked_components = {}
+        self.marked_components = set()
+        
+    def main(self):
+        self.not_done = True
+        while self.not_done:
+            [self._processMainInbox(msg) for msg in self.igetInbox('inbox') if msg.bodies]
+            self._processProtocols()
+                        
+            if not self.anyReady() and self.not_done:
+                self.pause()
+                
+            yield 1
+            [self._processMainControlBox(msg) for msg in self.igetInbox('control')]
+                
+    def _processMainInbox(self, msg):
+        protocol_component = self.Protocol(peer=msg.from_jid)
+        protocol_component.activate()
+        out_boxname = self.addOutbox("out_%s" % (protocol_component.name))
+        #print 'added ' + out_boxname
+        in_boxname = self.addInbox("in_%s" % (protocol_component.name))
+        #print 'added ' + in_boxname
+        ctl_boxname = self.addInbox("ctl_%s" % (protocol_component.name))
+        #print 'added ' + ctl_boxname
+
+        tracked_data =  {'in' : in_boxname,
+                         'ctl' : ctl_boxname,
+                         'out' : out_boxname,
+                         'msg' : msg,}
+
+        self.tracked_components[protocol_component.name] = tracked_data
+
+        self.link((self, out_boxname), (protocol_component, 'inbox'))
+        self.link((protocol_component, 'outbox'), (self, in_boxname))
+        self.link((protocol_component, 'signal'), (self, ctl_boxname))
+    
+        buffer = [str(body) for body in msg.bodies]
+        request = ''.join(buffer)
+        print 'received ' + request
+
+        self.send(request, out_boxname)
+        
+    def _processMainControlBox(self, msg):
+        msg = self.recv('control')
+        if isinstance(msg, (producerFinished, shutdownMicroprocess)):
+            self.not_done = False
+            
+    def _processProtocols(self):        
+        for proto_name in self.tracked_components:
+            proto_data = self.tracked_components[proto_name]
+            #print 'processing ' + proto_data['in']
+            #print 'box contents: '
+            #pprint(self.inboxes[proto_data['in']])
+            self._processProtoInbox(proto_data['in'], proto_data['msg'])
+            #print 'processing ' + proto_data['ctl']
+            #print 'box contents: '
+            #pprint(self.inboxes[proto_data['ctl']])
+            self._processProtoControl(proto_data['ctl'], proto_name)
+            
+        self._clearMarkedComponents()
+            
+    def _processProtoInbox(self, boxname, orig_msg):
+        for msg in self.igetInbox(boxname):
+            #print 'proto_inbox received "%s"' % (msg)
+            #print 'boxname=' + boxname
+            #print 'orig_msg=' + repr(orig_msg)
+            out = '%s %s' % (orig_msg.from_jid, msg)
+            self.send(out, 'outbox')
+            
+    def _processProtoControl(self, boxname, proto_name):
+        for msg in self.igetInbox(boxname):
+            if isinstance(msg, (producerFinished, shutdownMicroprocess)):
+                self.marked_components.add(proto_name)
+                #print 'marking %s' % (repr(proto))
+            
+    def _clearMarkedComponents(self):
+        #pprint(self.tracked_components)
+        #pprint(self.marked_components)
+        for proto in self.marked_components:
+            #print 'clearing %s' % (repr(proto))
+            data = self.tracked_components[proto]
+            self.unlink(proto)
+            self.deleteInbox(data['in'])
+            self.deleteInbox(data['ctl'])
+            self.deleteOutbox(data['out'])
+            del self.tracked_components[proto]
+            
+        self.marked_components.clear()
+    
+    def igetInbox(self, name='inbox'):
+        while self.dataReady(name):
+            yield self.recv(name)
+                
+class Echoer(component):
+    def __init__(self, **argd):
+        super(Echoer, self).__init__(**argd)
         
     def main(self):
         not_done = True
         while not_done:
-            while self.dataReady('control'):
-                msg = self.recv('control')
-                if isinstance(msg, (producerFinished, shutdownMicroprocess)):
-                    not_done = False
-                    
             while self.dataReady('inbox'):
-                msg = self.recv('inbox')
-
-
+                self.send(self.recv('inbox'))
+                not_done = False
                 
+            yield 1
                 
-                
-    def _processMainInbox(self, msg):
-        buffer = [str(body) for body in msg.bodies]
-        request = ''.join(buffer)
-                
-        protocol_component = self.Protocol(peer=msg.sender)
-        out_boxname = self.addOutbox("out_%s" % (protocol_component.name))
-        in_boxname = self.addInbox("in_%s" % (protocol_component.name))
-        ctl_boxname = self.addInbox("ctl_%s" % (protocol_component.name))
+        self.send(producerFinished(self), 'signal')
         
-        inboxes = {'in' : in_boxname,
-                   'ctl' : ctl_boxname}
-        
-        Graphline(
-            this=self,
-            proto=protocol_component,
-            linkages={
-                ('this', out_boxname) : ('proto', 'inbox'),
-                ('proto', 'signal') : ('this', ctl_boxname),
-                ('proto', 'outbox') : ('this', in_boxname)
-            }
-        ).activate()
-        
-        self.trackResourceInformation(protocol_component, inboxes, out_boxname, msg)
-    
-    def getInbox(self, name='inbox'):
-        while self.dataReady(name):
-            yield self.recv(name)
-                
+    def __repr__(self):
+        return '<%s>' % (self.name)
         
 class BadProtocol(Exception): pass
