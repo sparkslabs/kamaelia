c1f027c122b74883d017e99471053f3351a608f9
orphans <orphans@2682807a-6752-0410-967a-793f22d1dfe9>
2008-07-07 17:47:36 +0000

    Include sub-packages in the library setup file.  This makes the __init__.py files install correctly.

--- Sketches/JT/Jam/library/trunk/setup.py
+++ Sketches/JT/Jam/library/trunk/setup.py
@@ -2,21 +2,17 @@
 from distutils.core import setup
 setup(name="Kamaelia Jam Library",
       version = "0.1a1",
+      platforms = ["any"],
       packages = [
                   "Axon",
+                  "Kamaelia",
+                  "Kamaelia.Apps",
+                  "Kamaelia.Apps.Jam",
                   "Kamaelia.Apps.Jam.Internet", #STARTPACKAGES
                   "Kamaelia.Apps.Jam.Protocol",
+                  "Kamaelia.Apps.Jam.Support",
                   "Kamaelia.Apps.Jam.Support.Data",
                   "Kamaelia.Apps.Jam.UI",
                   "Kamaelia.Apps.Jam.Util", #LASTPACKAGES
                  ],
-      py_modules = [
-                    "Kamaelia.Apps.__init__",
-                    "Kamaelia.Apps.Jam.__init__",
-                    "Kamaelia.Apps.Jam.Internet.__init__",
-                    "Kamaelia.Apps.Jam.Protocol.__init__",
-                    "Kamaelia.Apps.Jam.UI.__init__",
-                    "Kamaelia.Apps.Jam.Util.__init__",
-                   ]
       )
-
