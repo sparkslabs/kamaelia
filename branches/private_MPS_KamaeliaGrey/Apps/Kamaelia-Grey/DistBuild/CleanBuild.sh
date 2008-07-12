b011b8c60c0595a48f9d0b7ec338c966b86d6dea
ms_ <ms_@2682807a-6752-0410-967a-793f22d1dfe9>
2008-07-12 18:06:16 +0000

    Slightly cleaned up to only clean up stuff that will be in this tree.
    Michael.

--- branches/private_MPS_KamaeliaGrey/Apps/Kamaelia-Grey/DistBuild/CleanBuild.sh
+++ branches/private_MPS_KamaeliaGrey/Apps/Kamaelia-Grey/DistBuild/CleanBuild.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd ..
-rm -rf AUTHORS Axon COPYING dist/ Docs/ Examples/ Kamaelia MANIFEST Tools/ Axon.CHANGELOG Kamaelia.CHANGELOG  setup.py Axon.README Kamaelia.README MANIFEST.in
+rm -rf Axon/ dist/ Kamaelia/ AUTHORS COPYING MANIFEST setup.py
