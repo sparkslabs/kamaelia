a1ddcde282fb49147ef1ab8149f4cdb8a3e7549e
sparks.m <sparks.m@2682807a-6752-0410-967a-793f22d1dfe9>
2008-07-21 13:03:24 +0000

    testing speed

--- Sketches/MPS/IRCSpeakerBot.py
+++ Sketches/MPS/IRCSpeakerBot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+
 import time
 import Axon
 from Kamaelia.Chassis.Pipeline import Pipeline
--- Sketches/MPS/ScrollyTkMenu.py
+++ Sketches/MPS/ScrollyTkMenu.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+
 from Tkinter import *
 
 def _defaultCallback(*args):
