3003e66df8740300d82b1022ddaa9685d401d8c5
orphansandoligarchs <orphansandoligarchs@2682807a-6752-0410-967a-793f22d1dfe9>
2008-07-22 13:39:18 +0000

    Add Synth component for creating polyphonic synths from voices

---
+++ Sketches/JT/Jam/library/trunk/Kamaelia/Apps/Jam/Audio/Synth.py
@@ -0,0 +1,24 @@
+import Axon
+from Kamaelia.Apps.Jam.Audio.Polyphony import Polyphoniser
+from Kamaelia.Apps.Jam.Audio.Mixer import MonoMixer
+
+class Synth(Axon.Component.component):
+    polyphony = 8
+
+    def __init__(self, voiceGenerator, **argd):
+        super(Synth, self).__init__(**argd)
+        polyphoniser = Polyphoniser(polyphony=self.polyphony).activate()
+        mixer = MonoMixer(channels=self.polyphony).activate()
+        self.link((self, "inbox"), (polyphoniser, "inbox"), passthrough=1)
+        self.link((mixer, "outbox"), (self, "outbox"), passthrough=2)
+
+        for index, voice in enumerate(voiceGenerator()):
+            voice = voice.activate()
+            self.link((polyphoniser, "voice%i" % index), (voice, "inbox"))
+            self.link((voice, "outbox"), (mixer, "in%i" % index))
+
+    def main(self):
+        while 1:
+            if not self.anyReady():
+                self.pause()
+            yield 1
