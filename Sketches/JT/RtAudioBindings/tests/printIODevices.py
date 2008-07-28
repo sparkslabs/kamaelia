598c619fea7186e71055c8a7fa6176d44bbff35f
orphansandoligarchs@gmail.com <orphansandoligarchs@gmail.com@2682807a-6752-0410-967a-793f22d1dfe9>
2008-07-28 18:54:18 +0000

    Make printIODevices test script print in a nicer format

--- Sketches/JT/RtAudioBindings/tests/printIODevices.py
+++ Sketches/JT/RtAudioBindings/tests/printIODevices.py
@@ -3,5 +3,9 @@ import RtAudio
 
 io = RtAudio.RtAudio()
 for i in range(io.getDeviceCount()):
-    print io.getDeviceInfo(i)
+    info = io.getDeviceInfo(i)
+    print "%i: %s" % (i, info["name"])
+    for key in info:
+        if key != "name":
+            print "\t%s : %s" % (key, info[key])
 
