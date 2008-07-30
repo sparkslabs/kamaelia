c74fedbc1e4c71f0b1470a105564c00ca3a6f1a8
Chong.Liu@hotmail.co.uk <Chong.Liu@hotmail.co.uk@2682807a-6752-0410-967a-793f22d1dfe9>
2008-07-30 21:31:33 +0000

    Topology3D Update: use recurrence or iteration to fetch N layers of FOAF data * N layers follow the uri of knows tag * FOAF data fetched are name and img * The iteration is only correct for N<=2, though it could be changed to support N>2, it may be not needed since the recurrence can do the job very well * TODO: transform it to Kamaelia component and send data to TopologyViewer3D

--- Sketches/CL/Topology3D/Experiments/RDFParser_withLibRDF.py
+++ Sketches/CL/Topology3D/Experiments/RDFParser_withLibRDF.py
@@ -1,38 +1,141 @@
 import RDF
 
-model = RDF.Model()
-parser = RDF.Parser()
-#uri = "http://bigasterisk.com/foaf.rdf"
-uri = "http://jibbering.com/foaf.rdf"
-parser.parse_into_model(model, uri)
+#===============================================================================
+# model = RDF.Model()
+# parser = RDF.Parser()
+# #uri = "http://bigasterisk.com/foaf.rdf"
+# #uri = "http://jibbering.com/foaf.rdf"
+# #uri = "http://www.perceive.net/xml/foaf.rdf"
+# uri = "http://fooshed.net/foaf.rdf"
+# parser.parse_into_model(model, uri)
+# 
+# #query = """
+# #SELECT ?name
+# #WHERE {
+# #  ?x foaf:name ?name
+# #}
+# #""" 
+# 
+# 
+# query = """
+# SELECT DISTINCT ?aname ?aimg ?bname ?bimg ?seeAlso 
+# WHERE {
+#  ?a foaf:aname ?aname . ?a foaf:img ?aimg .
+#  ?a foaf:knows ?b . 
+#  OPTIONAL { ?b foaf:name ?name } .
+#  OPTIONAL { ?b foaf:img ?bimg } .
+#  OPTIONAL { ?b rdfs:seeAlso ?seeAlso }
+# }
+# """
+# 
+# sparql = """
+# PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+# PREFIX owl: <http://www.w3.org/2002/07/owl#>
+# PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+# %s""" %query
+# 
+# 
+# q = RDF.Query(sparql, query_language="sparql")
+# results = q.execute(model)
+# #print results
+# 
+# 
+# for result in results:
+#    print result['name'], ':', result['aimg'], result['seeAlso']
+#===============================================================================
 
-#query = """
-#SELECT ?name
-#WHERE {
-#  ?x foaf:name ?name
-#}
-#""" 
-
-query = """
-SELECT DISTINCT ?name ?knows ?seeAlso ?img
-WHERE {
-  ?a foaf:knows ?knows .
-  OPTIONAL { ?knows foaf:name ?name } .
-  OPTIONAL { ?knows foaf:img ?img } .
-  OPTIONAL { ?knows rdfs:seeAlso ?seeAlso }
-}
-"""
-
-sparql = """
+PREFIX = """
 PREFIX foaf: <http://xmlns.com/foaf/0.1/>
 PREFIX owl: <http://www.w3.org/2002/07/owl#>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-%s""" %query
+"""
 
+def make_query(rdf, query):
+    model = RDF.Model()
+    parser = RDF.Parser()
+    parser.parse_into_model(model, rdf)
+    sparql = """
+    %s
+    %s""" % (PREFIX, query)
+    q = RDF.Query(sparql, query_language="sparql")
+    return q.execute(model)
 
-q = RDF.Query(sparql, query_language="sparql")
-results = q.execute(model)
-#print results
+counter2 = counter = 0
+def fetch_data(rdf_uri, layer=0, MAX_LAYER = 2):
+    if layer == MAX_LAYER:
+        return
+    else:
+        global counter, counter2
+        counter += 1
+        print "--- The ", layer, " layer ---"
+        query1 = """
+        SELECT DISTINCT ?name ?img
+        WHERE { ?x foaf:name ?name .
+        OPTIONAL { ?x foaf:img ?img }
+        }
+        """
+        results = make_query(rdf_uri, query1)
+        result = results.next()
+        print result['name'], ':', result['img']
+        
+        print "*Knows*"
+        query2 = """
+        SELECT DISTINCT ?name ?img ?seeAlso
+        WHERE {
+        ?a foaf:knows ?b . ?b foaf:name ?name .
+        OPTIONAL { ?b foaf:img ?img } .
+        OPTIONAL { ?b rdfs:seeAlso ?seeAlso }
+        }
+        """
+        results = make_query(rdf_uri, query2)
+ #        import copy
+ #        results2 = copy.deepcopy(results) 
+        for result in results:
+            counter2 += 1
+            print result['name'], ':', result['img']
+        results = make_query(rdf_uri, query2)
+        for result in results:
+            uri = result['seeAlso']
+            if uri:
+                uri = uri._get_uri()
+                fetch_data(uri, layer+1)
+    
+fetch_data("http://fooshed.net/foaf.rdf")
+print counter, counter2 
 
-for result in results:
-    print result['name'], result['seeAlso']
+#MAX_LAYER = 2
+#rdf_uris = ["http://fooshed.net/foaf.rdf"] 
+#for x in xrange(MAX_LAYER):
+#    print "--- The ", x, " layer ---"
+#    print "--------------------------------"
+#    for rdf_uri in rdf_uris:
+#        counter += 1
+#        query1 = """
+#        SELECT DISTINCT ?name ?img
+#        WHERE { ?x foaf:name ?name .
+#        OPTIONAL { ?x foaf:img ?img }
+#        }
+#        """
+#        results = make_query(rdf_uri, query1)
+#        result = results.next()
+#        print result['name'], ':', result['img']
+#        rdf_uris = []
+#        
+#        print "*Knows*"
+#        query2 = """
+#        SELECT DISTINCT ?name ?img ?seeAlso
+#        WHERE {
+#        ?a foaf:knows ?b . ?b foaf:name ?name .
+#        OPTIONAL { ?b foaf:img ?img } .
+#        OPTIONAL { ?b rdfs:seeAlso ?seeAlso }
+#        }
+#        """
+#        results = make_query(rdf_uri, query2)
+#        for result in results:
+#            counter2 += 1
+#            uri = result['seeAlso']
+#            if uri:
+#                uri = uri._get_uri()
+#                rdf_uris.append(uri)
+#            print result['name'], ':', result['img']
+#print counter, counter2 
