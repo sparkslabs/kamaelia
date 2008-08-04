9f042865529e24191a9fc9ac808c55538912e59a
orphansandoligarchs@gmail.com <orphansandoligarchs@gmail.com@2682807a-6752-0410-967a-793f22d1dfe9>
2008-08-04 12:54:50 +0000

    Update RTOutput to use the new buffer api, and actually work almost decently :)

--- trunk/Sketches/JT/Jam/library/trunk/Kamaelia/Apps/Jam/Audio/RTOutput.py
+++ trunk/Sketches/JT/Jam/library/trunk/Kamaelia/Apps/Jam/Audio/RTOutput.py
@@ -1,33 +1,33 @@
 import Axon
 import RtAudio
+import numpy
 
 class RTOutput(Axon.Component.component):
-    channels = 2
-    type = 0x2 # INT16 - will add these into binding
+    outputDevice = 0
     sampleRate = 44100
     bufferSize = 1024
 
     def __init__(self, **argd):
         super(RTOutput, self).__init__(**argd)
         self.io = RtAudio.RtAudio()
-        self.io.showWarnings()
-        self.io.openStream(0, self.channels, 0, 0, 2, 0, self.type,
-                           self.sampleRate, self.bufferSize,
-                           self.callback, None)
-        self.buffer = None
+        self.io.openStream(self.outputDevice, self.sampleRate, self.bufferSize)
         self.io.startStream()
-
-    def callback(self, outputBuffer, inputBuffer, bufferSize, streamTime,
-                 extraData):
-        print "ZOMGGGGGGGGGGGGGGGGGG"
-        outputBuffer *= 0
-        outputBuffer += self.buffer
-        return 5
 
     def main(self):
         while 1:
             if self.dataReady("inbox"):
-                self.buffer = self.recv("inbox")
+                if self.io.needWrite() >= self.bufferSize:
+                    self.io.write(self.recv("inbox"))
+                else:
+                    print "Overflow"
+                    self.recv("inbox")
             if not self.anyReady():
                 self.pause()
             yield 1
+
+if __name__ == "__main__":
+    from Kamaelia.Apps.Jam.Audio.SineSynth import SineOsc
+    from Kamaelia.Chassis.Pipeline import Pipeline
+    from Kamaelia.Util.Console import ConsoleEchoer
+    Pipeline(SineOsc(), RTOutput(outputDevice=2)).run()
+
