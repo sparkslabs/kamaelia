7dbcb98cb6d75fd4cb2181dea35b24d62273444f
orphansandoligarchs@gmail.com <orphansandoligarchs@gmail.com@2682807a-6752-0410-967a-793f22d1dfe9>
2008-08-06 14:36:05 +0000

    Change RTOutput so it waits rather than throwing data away when there is an overflow, and add a SchedulingComponent version.  Not sure which is better at the moment, so we'll keep both in.

--- trunk/Sketches/JT/Jam/library/trunk/Kamaelia/Apps/Jam/Audio/RTOutput.py
+++ trunk/Sketches/JT/Jam/library/trunk/Kamaelia/Apps/Jam/Audio/RTOutput.py
@@ -1,8 +1,10 @@
 import Axon
+from Axon.SchedulingComponent import SchedulingComponent
 import RtAudio
 import numpy
+import time
 
-class RTOutput(Axon.Component.component):
+class RTOutput(Axon.ThreadedComponent.threadedcomponent):
     outputDevice = 0
     sampleRate = 44100
     bufferSize = 1024
@@ -16,14 +18,42 @@ class RTOutput(Axon.Component.component):
     def main(self):
         while 1:
             if self.dataReady("inbox"):
-                if self.io.needWrite() >= self.bufferSize:
-                    self.io.write(self.recv("inbox"))
-                else:
-                    print "Overflow"
-                    self.recv("inbox")
+                while not self.io.needWrite() >= self.bufferSize:
+                    time.sleep(0.001)
+                self.io.write(self.recv("inbox"))
             if not self.anyReady():
                 self.pause()
-            yield 1
+
+class SchedulingRTOutput(SchedulingComponent):
+    outputDevice = 0
+    sampleRate = 44100
+    internalBufferSize = 1024
+    outputBufferSize = 1024
+
+
+    def __init__(self, **argd):
+        super(SchedulingRTOutput, self).__init__(**argd)
+        self.io = RtAudio.RtAudio()
+        self.io.openStream(self.outputDevice, self.sampleRate,
+                           self.outputBufferSize)
+        self.period = float(self.internalBufferSize)/self.sampleRate
+        self.lastSendTime = time.time()
+        self.scheduleAbs("Send", self.lastSendTime + self.period)
+        self.io.startStream()
+
+    def main(self):
+        while 1:
+            if self.dataReady("event"):
+                if self.dataReady("inbox"):
+                    while not self.io.needWrite() >= self.internalBufferSize:
+                        time.sleep(0.001)
+                    data = self.recv("inbox")
+                    self.io.write(data)
+                    # TODO: Maybe correct for the sleeping above here - test me
+                    self.lastSendTime += self.period
+                    self.scheduleAbs("Send", self.lastSendTime + self.period)
+            else:
+                self.pause()
 
 if __name__ == "__main__":
     from Kamaelia.Apps.Jam.Audio.SineSynth import SineOsc
