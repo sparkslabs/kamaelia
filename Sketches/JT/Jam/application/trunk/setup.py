8d00d5710a0b3542d902d86588b156e3a5c57008
orphansandoligarchs@gmail.com <orphansandoligarchs@gmail.com@2682807a-6752-0410-967a-793f22d1dfe9>
2008-08-06 14:44:45 +0000

    Add four samples for use in Jam's sampler

--- Sketches/JT/Jam/application/trunk/setup.py
+++ Sketches/JT/Jam/application/trunk/setup.py
@@ -5,10 +5,13 @@ setup(name = "Kamaelia Jam Application",
       scripts = [
                  "jam", #STARTSCRIPTS LASTSCRIPTS
                 ],
-      data_files = [("share/kamaelia/jam",
-                     [
-                      "PD/PureJam.pd", #STARTDATA LASTDATA
-                     ]
-                    )
-                   ]
+      data_files = [("share/kamaelia/jam/pd", ["PD/PureJam.pd"]), #STARTDATA
+                    ("share/kamaelia/jam/samples",
+                     ["Samples/12910_sweet_trip_mm_clap_mid.wav",
+                      "Samples/12911_sweet_trip_mm_hat_cl.wav",
+                      "Samples/12912_sweet_trip_mm_hat_op.wav",
+                      "Samples/12914_sweet_trip_mm_kick_lo.wav",
+                      "Samples/__details_and_attribution.txt"
+                     ]) #LASTDATA
+                    ]
       )
