2080fbdf66a81d3372cdb0c9e97595ca41eec15a
Chong.Liu@hotmail.co.uk <Chong.Liu@hotmail.co.uk@2682807a-6752-0410-967a-793f22d1dfe9>
2008-08-07 11:11:57 +0000

    private_CL_Topology3D Update: add JSONDecoder and simple documentations to JSON.py

--- branches/private_CL_Topology3D/Kamaelia/Kamaelia/Codec/JSON.py
+++ branches/private_CL_Topology3D/Kamaelia/Kamaelia/Codec/JSON.py
@@ -1,9 +1,19 @@
+"""\
+=========================
+JSON serialisation coder
+=========================
+
+This component encode data to serialisable JSON format and 
+decode serialised JSON data.
+"""
+
 import cjson
 
 from Axon.Component import component
 from Axon.Ipc import producerFinished, shutdownMicroprocess
 
 class JSONEncoder(component):
+    """ Kamaelia component to encode data using JSON coding """
     def __init__(self):
         """x.__init__(...) initializes x; see x.__class__.__doc__ for signature"""
         super(JSONEncoder, self).__init__()
@@ -37,6 +47,65 @@ class JSONEncoder(component):
         self.send(self.shutdown_mess,"signal")
         
         
+#===============================================================================
+# if __name__ == "__main__":
+#    from Kamaelia.Util.DataSource import DataSource
+#    from Kamaelia.Util.Console import ConsoleReader,ConsoleEchoer
+#    from Kamaelia.Chassis.Graphline import Graphline
+#    
+#    # Data can be from both DataSource and console inputs
+#    Graphline(
+#        CONSOLEREADER = ConsoleReader('>>>'),
+#        DATASOURCE = DataSource(["['foo', {'bar': ('baz', None, 1.0, 2)}]"]),
+#        JSONENCODER = JSONEncoder(),
+#        CONSOLEECHOER = ConsoleEchoer(),
+#    linkages = {
+#        ("CONSOLEREADER","outbox") : ("JSONENCODER","inbox"),
+#        ("DATASOURCE","outbox") : ("JSONENCODER","inbox"),   
+#        ("JSONENCODER","outbox")  : ("CONSOLEECHOER","inbox"),     
+#    }
+# ).run()
+#===============================================================================
+
+
+
+class JSONDecoder(component):
+    """ Kamaelia component to decode data encoded by JSON coding """
+    def __init__(self):
+        """x.__init__(...) initializes x; see x.__class__.__doc__ for signature"""
+        super(JSONDecoder, self).__init__()
+        
+    def shutdown(self):
+        """ shutdown method: define when to shun down"""
+        while self.dataReady("control"):
+            message = self.recv("control")
+            if isinstance(message, producerFinished) or isinstance(message, shutdownMicroprocess):
+                self.shutdown_mess = message
+                return True
+        return False
+      
+    def main(self):
+        """ main method: do stuff """
+        
+        # Put all codes within the loop, so that others can be run even it doesn't shut down
+        while not self.shutdown():
+            while not self.anyReady():
+                self.pause()
+                yield 1
+    
+            while self.dataReady("inbox"):
+                serialisedData = self.recv("inbox").strip()
+                if serialisedData: # Ignore empty data
+                    data = cjson.decode(serialisedData)
+                    self.send(data, "outbox")
+            
+            yield 1
+            
+        self.send(self.shutdown_mess,"signal")
+
+__kamaelia_components__ = ( JSONEncoder, JSONDecoder )        
+
+        
 if __name__ == "__main__":
     from Kamaelia.Util.DataSource import DataSource
     from Kamaelia.Util.Console import ConsoleReader,ConsoleEchoer
@@ -47,10 +116,12 @@ if __name__ == "__main__":
         CONSOLEREADER = ConsoleReader('>>>'),
         DATASOURCE = DataSource(["['foo', {'bar': ('baz', None, 1.0, 2)}]"]),
         JSONENCODER = JSONEncoder(),
+        JSONDECODER = JSONDecoder(),
         CONSOLEECHOER = ConsoleEchoer(),
     linkages = {
         ("CONSOLEREADER","outbox") : ("JSONENCODER","inbox"),
-        ("DATASOURCE","outbox") : ("JSONENCODER","inbox"),   
-        ("JSONENCODER","outbox")  : ("CONSOLEECHOER","inbox"),     
+        ("DATASOURCE","outbox") : ("JSONENCODER","inbox"),
+        ("JSONENCODER","outbox")  : ("JSONDECODER","inbox"), 
+        ("JSONDECODER","outbox")  : ("CONSOLEECHOER","inbox"),     
     }
 ).run()
