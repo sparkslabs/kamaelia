b8bfd93fba641364417d6d56043f8aafa5958b3b
orphansandoligarchs@gmail.com <orphansandoligarchs@gmail.com@2682807a-6752-0410-967a-793f22d1dfe9>
2008-08-10 14:44:25 +0000

    Remove mixer section from synth code to reflect the components directly calling pygame mixer methods.

--- trunk/Sketches/JT/Jam/library/trunk/Kamaelia/Apps/Jam/Audio/Synth.py
+++ trunk/Sketches/JT/Jam/library/trunk/Kamaelia/Apps/Jam/Audio/Synth.py
@@ -1,6 +1,5 @@
 import Axon
 from Kamaelia.Apps.Jam.Audio.Polyphony import Polyphoniser
-from Kamaelia.Apps.Jam.Audio.Mixer import MonoMixer
 
 class Synth(Axon.Component.component):
     polyphony = 8
@@ -9,14 +8,11 @@ class Synth(Axon.Component.component):
     def __init__(self, voiceGenerator, **argd):
         super(Synth, self).__init__(**argd)
         polyphoniser = self.polyphoniser(**argd).activate()
-        mixer = MonoMixer(channels=self.polyphony, **argd).activate()
         self.link((self, "inbox"), (polyphoniser, "inbox"), passthrough=1)
-        self.link((mixer, "outbox"), (self, "outbox"), passthrough=2)
 
         for index, voice in enumerate(voiceGenerator()):
             voice = voice.activate()
             self.link((polyphoniser, "voice%i" % index), (voice, "inbox"))
-            self.link((voice, "outbox"), (mixer, "in%i" % index))
 
     def main(self):
         while 1:
