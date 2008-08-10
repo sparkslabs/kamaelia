2418ebf1b9c5867d77092f7f2e4d4cfdc257cf03
amnorvend <amnorvend@2682807a-6752-0410-967a-793f22d1dfe9>
2008-08-10 05:23:01 +0000

    Initial checkin of branch.

---
+++ branches/private_JMB_KamaeliaWebServe/Kamaelia/Kamaelia/Protocol/RTP/__init__.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2004 British Broadcasting Corporation and Kamaelia Contributors(1)
+#     All Rights Reserved.
+#
+# You may only modify and redistribute this under the terms of any of the
+# following licenses(2): Mozilla Public License, V1.1, GNU General
+# Public License, V2.0, GNU Lesser General Public License, V2.1
+#
+# (1) Kamaelia Contributors are listed in the AUTHORS file and at
+#     http://kamaelia.sourceforge.net/AUTHORS - please extend this file,
+#     not this notice.
+# (2) Reproduced in the COPYING file, and at:
+#     http://kamaelia.sourceforge.net/COPYING
+# Under section 3.5 of the MPL, we are using this text since we deem the MPL
+# notice inappropriate for this file. As per MPL/GPL/LGPL removal of this
+# notice is prohibited.
+#
+# Please contact us via: kamaelia-list-owner@lists.sourceforge.net
+# to discuss alternative licensing.
+# -------------------------------------------------------------------------
+"""\
+===========
+RTP support
+===========
+
+Components implementing aspects of the RTP protocol and defined in `RFC 3550`_
+and `RFC 3551`_.
+
+* **Kamaelia.Protocol.RTP.RTP** implements components that perform basic
+  framing and deframing of packets.
+
+.. _`RFC 3550`: http://tools.ietf.org/html/rfc3550
+.. _`RFC 3551`: http://tools.ietf.org/html/rfc3551
+
+
+"""
