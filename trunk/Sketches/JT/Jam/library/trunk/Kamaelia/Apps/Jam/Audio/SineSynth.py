a6ec13e594d777e50265260e0edcc89a117283b4
orphansandoligarchs@gmail.com <orphansandoligarchs@gmail.com@2682807a-6752-0410-967a-793f22d1dfe9>
2008-08-11 14:09:02 +0000

    Roll back SineSynth to r5209 before the hacks to try and smooth out playback - they were due to a bug in pygame.

--- trunk/Sketches/JT/Jam/library/trunk/Kamaelia/Apps/Jam/Audio/SineSynth.py
+++ trunk/Sketches/JT/Jam/library/trunk/Kamaelia/Apps/Jam/Audio/SineSynth.py
@@ -1,7 +1,7 @@
 import pygame
+import numpy
 import Numeric
 import time
-import Axon
 from Axon.SchedulingComponent import SchedulingComponent
 from Kamaelia.Apps.Jam.Audio.Synth import Synth
 
@@ -13,9 +13,7 @@ class SineOsc(SchedulingComponent):
 
     def __init__(self, **argd):
         super(SineOsc, self).__init__(**argd)
-        ### HACK - Quick test to see whether decreasing the period will
-        ### make playback smoother in SineVoice.
-        self.period = float(self.bufferSize)/self.sampleRate/2
+        self.period = float(self.bufferSize)/self.sampleRate
         self.phase = 0
         self.lastSendTime = time.time()
         self.scheduleAbs("Send", self.lastSendTime + self.period)
@@ -61,15 +59,10 @@ class SineOsc(SchedulingComponent):
             if not self.anyReady():
                 self.pause()
 
-class SineVoice(Axon.Component.component):
-    sampleRate = 44100
-    bufferSize = 1024
-    # The longest a note can be, for use in an ugly hack to prevent buffering
-    # problems
-    maxNoteLength = 4 * 4 * 60 / 120 # beats * bars * (60 secs) / bpm
-
+class SineVoice(SineOsc):
     def __init__(self, **argd):
         super(SineVoice, self).__init__(**argd)
+        self.on = False
         if not pygame.mixer.get_init():
             pygame.mixer.init(self.sampleRate, -16, 1, self.bufferSize)
             pygame.mixer.set_num_channels(0)
@@ -77,51 +70,35 @@ class SineVoice(Axon.Component.component):
         pygame.mixer.set_num_channels(numChannels)
         self.channel = pygame.mixer.Channel(numChannels - 1)
 
-    def generateSample(self, frequency, amplitude):
-        """
-        Generates one sample of a sine wave with a given frequency, amplitude
-        and phase offset
-        """
-        # Working from the formula y(t) = Asin(wt + c)
-        numSamples = self.sampleRate/frequency
-        # w
-        angularFreq = 2 * Numeric.pi * frequency
-        # t
-        sampleLength = 1.0/self.sampleRate
-        # wt for each sample
-        sample = Numeric.arange(numSamples) * angularFreq * sampleLength
-        # sin(wt + c) for each sample
-        sample = Numeric.sin(sample)
-        # Asin(wt + c) for each sample
-        sample *= amplitude
-        return sample
-
     def main(self):
         while 1:
             if self.dataReady("inbox"):
                 address, arguments = self.recv("inbox")
                 address = address.split("/")[-1]
                 if address == "On":
+                    self.on = True
                     noteNumber, frequency, velocity = arguments
                     self.frequency = frequency
                     self.amplitude = velocity
-                    sample = self.generateSample(self.frequency,
-                                                 self.amplitude * (2**15 - 1))
-                    sample = sample.astype("i")
-                    # Hacky - Resize the single sample to the length of the
-                    # all the bars.  Prevents a whole multitude of buffering
-                    # issues, at the expense of memory and not being a great
-                    # big horrible hack...
-                    numSamples = self.sampleRate * self.maxNoteLength
-                    sample = Numeric.resize(sample, (numSamples,))
-
-                    self.sound = pygame.sndarray.make_sound(sample)
-                    self.channel.play(self.sound)
                 elif address == "Off":
-                    self.channel.stop()
+                    self.on = False
+                
+            if self.dataReady("event"):
+                self.recv("event")
+                if self.on:
+                    while self.channel.get_queue() == None:
+                        sample, phase = self.generateSample(self.frequency,
+                                                            self.amplitude,
+                                                            self.phase)
+                        self.phase = phase
+                        sample *= 2**15-1
+                        sample = sample.astype("int16")
+                        sample = Numeric.asarray(sample)
+                        self.channel.queue(pygame.sndarray.make_sound(sample))
+                self.lastSendTime += self.period
+                self.scheduleAbs("Send", self.lastSendTime + self.period)
             if not self.anyReady():
                 self.pause()
-            yield 1
 
 def SineSynth(polyphony=8, **argd):
     def voiceGenerator():
