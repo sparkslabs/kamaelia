6e7478791af9bfdfe8c56d7774e11649a8569377
sparks.m <sparks.m@2682807a-6752-0410-967a-793f22d1dfe9>
2008-10-05 00:18:26 +0000

    Added in core documentation & metadata

--- branches/private_MPS_KamaeliaGrey/Kamaelia/Kamaelia/Apps/Grey/WakeableIntrospector.py
+++ branches/private_MPS_KamaeliaGrey/Kamaelia/Kamaelia/Apps/Grey/WakeableIntrospector.py
@@ -1,12 +1,45 @@
 
-#
-# This component grabs a list of all running/runnable components whenever it
-# receives a message on its inbox "inbox". This list is then sorted, and
-# noted to a logfile.
-#
-# TODO: In retrospect, it may've been nicer to split the introspection from
-#       the logging.
-#
+"""\
+===============================
+On Demand/Wakeable Introspector
+===============================
+
+This component grabs a list of all running/runnable components whenever it
+receives a message on its inbox "inbox". This list is then sorted, and
+noted to a logfile.
+
+
+Example Usage
+-------------
+
+This component is intended to be used with PeriodicWakeup, as follows::
+    Pipeline(
+         PeriodicWakeup(interval=20),
+         WakeableIntrospector(logfile="/tmp/trace"),
+    )
+
+How does it work?
+-----------------
+
+This component uses the fact that we can ask the scheduler for a list of
+running componenents, takes this, sorts it and dumps the result to a
+logfile.
+
+It then sits quietly waking for a message (any message) on the inbox "inbox".
+
+Termination
+-----------
+
+This component is not well behaved regarding termination, in that it does
+not have any shutdown conditions.
+
+TODO
+----
+
+In retrospect, it may've been nicer to split the introspection from the
+logging. Better termination/shutdown would be a good idea.
+
+"""
 
 import Axon
 
@@ -31,3 +64,5 @@ class WakeableIntrospector(Axon.Component.component):
                 self.pause()
                 yield 1
             while self.dataReady("inbox"): self.recv("inbox")
+
+__kamaelia_components__  = ( WakeableIntrospector, )
