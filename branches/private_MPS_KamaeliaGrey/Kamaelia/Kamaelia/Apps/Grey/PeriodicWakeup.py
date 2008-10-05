7fc5daee9e0ca87e47f906c11d79ce270021e50a
sparks.m <sparks.m@2682807a-6752-0410-967a-793f22d1dfe9>
2008-10-05 00:46:19 +0000

    Added docs & metadata

--- branches/private_MPS_KamaeliaGrey/Kamaelia/Kamaelia/Apps/Grey/PeriodicWakeup.py
+++ branches/private_MPS_KamaeliaGrey/Kamaelia/Kamaelia/Apps/Grey/PeriodicWakeup.py
@@ -1,16 +1,48 @@
-
-#
-# Simple component. Useful in its own right, so in a separate file. Probably
-# a candidate at some point for scavenging into the main tree.
-#
-#
-
 import time
 import Axon
 
+"""\
+================================
+Simple Periodic Sender Component
+================================
+
+Simply sends a message every X seconds.
+
+
+Example Usage
+-------------
+Used as follows::
+
+    PeriodicWakeup()
+
+It will send the message "tick" to the outbox "outbox" every 300 seconds.
+
+To configure the delay or message, modify this::
+    
+    PeriodicWakeup(message="tock", delay=1)
+
+Termination
+-----------
+This component does not at present terminate. It should
+
+How does it work?
+-----------------
+
+This is just a threaded component can calls time.sleep
+
+Todo
+----
+
+Add in termination/shutdown code.
+Shift into the main code tree.
+
+"""
 class PeriodicWakeup(Axon.ThreadedComponent.threadedcomponent):
     interval = 300
+    message = "tick"
     def main(self):
         while 1:
             time.sleep(self.interval)
-            self.send("tick", "outbox")
+            self.send(self.message, "outbox") # sleeper must awaken
+
+__kamaelia_components__  = ( PeriodicWakeup, )
