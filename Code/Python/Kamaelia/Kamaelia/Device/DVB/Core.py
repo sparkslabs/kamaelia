e6080d8f28c613ab7163a205f1ba32ec7845953d
sparks.m <sparks.m@2682807a-6752-0410-967a-793f22d1dfe9>
2008-10-27 22:24:19 +0000

    check the IRC logs...

--- Code/Python/Kamaelia/Kamaelia/Device/DVB/Core.py
+++ Code/Python/Kamaelia/Kamaelia/Device/DVB/Core.py
@@ -214,6 +214,8 @@ def tune_DVBT(fe, frequency, feparams={}):
 def tune_DVB(fe, frequency, feparams={}):
     # Build the tuning parameters (DVB-T)
     print dir(fe)
+    print fe.__class__
+    print type(fe)
     if fe.t == dvb3.frontend.OFDMParameters:
         params = dvb3.frontend.OFDMParameters(
             frequency = frequency * 1000 * 1000,
