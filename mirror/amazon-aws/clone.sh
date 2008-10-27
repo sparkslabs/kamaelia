5dbaf115de713b53d3ddb23ff943989a722822f1
sparks.m <sparks.m@2682807a-6752-0410-967a-793f22d1dfe9>
2008-10-27 18:44:16 +0000

    latest version of the clone script

--- mirror/amazon-aws/clone.sh
+++ mirror/amazon-aws/clone.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-rm -rf tmp
+rm -rf original
 hg clone http://freehg.org/u/ben/kamaelia-aws/
-mv kamaelia-aws tmp
+mv kamaelia-aws original
 
 (
+   echo "grabbing"
+   pwd
    cd original
    tar zcvf ../current.tar.gz . --exclude=.hg --exclude .hgignore
 )
 ( 
+   echo "ungrabbing"
+   pwd
    cd trunk
    tar zxvf ../current.tar.gz
    svn status 2>&1 |tee status
    grep "^?" status|while read q filename; do svn add $filename; done
    svn ci -m "Mirroring Changes to Kamaelia-AWS"
 )
-rm current.tar.gz
+
+rm -rf current.tar.gz
 rm -rf tmp
+rm -rf original
