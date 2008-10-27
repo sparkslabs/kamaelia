471d08403e0c32eced78632bb9822d093bb1c9e2
sparks.m <sparks.m@2682807a-6752-0410-967a-793f22d1dfe9>
2008-10-27 18:58:18 +0000

    Better cloning
    Michael.

--- mirror/amazon-aws/clone.sh
+++ mirror/amazon-aws/clone.sh
@@ -5,19 +5,20 @@ hg clone http://freehg.org/u/ben/kamaelia-aws/
 mv kamaelia-aws original
 
 (
+   cd original
    echo "grabbing"
    pwd
-   cd original
    tar zcvf ../current.tar.gz . --exclude=.hg --exclude .hgignore
 )
 ( 
+   cd trunk
    echo "ungrabbing"
    pwd
-   cd trunk
    tar zxvf ../current.tar.gz
-   svn status 2>&1 |tee status
-   grep "^?" status|while read q filename; do svn add $filename; done
+   svn status 2>&1 |egrep -v my_checkout_status_file| tee my_checkout_status_file
+   grep "^?" my_checkout_status_file|grep -v my_checkout_status_file |while read q filename; do svn add $filename; done
    svn ci -m "Mirroring Changes to Kamaelia-AWS"
+   rm -rf my_checkout_status_file
 )
 
 rm -rf current.tar.gz
