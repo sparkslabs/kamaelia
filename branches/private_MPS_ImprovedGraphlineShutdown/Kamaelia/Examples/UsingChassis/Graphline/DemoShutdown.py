5c7560c198be24e60491128bae42a86bb9179fd3
matthammond.org <matthammond.org@2682807a-6752-0410-967a-793f22d1dfe9>
2008-10-28 00:32:57 +0000

    Fleshed out demo to show/test variety of scenarios now covered. (see IRC discussions from this evening)
    Will probably refactor as beginnings of a new graphline test suite shortly (not really appropriate as a demo as it stands)
    
    Matt

--- branches/private_MPS_ImprovedGraphlineShutdown/Kamaelia/Examples/UsingChassis/Graphline/DemoShutdown.py
+++ branches/private_MPS_ImprovedGraphlineShutdown/Kamaelia/Examples/UsingChassis/Graphline/DemoShutdown.py
@@ -4,6 +4,7 @@ import time
 import Axon
 from Kamaelia.Chassis.Graphline import Graphline
 from Kamaelia.Chassis.Pipeline import Pipeline
+from Kamaelia.Util.Console import ConsoleEchoer
 
 class Pinger(Axon.ThreadedComponent.threadedcomponent):
     def main(self):
@@ -24,8 +25,11 @@ class Waiter(Axon.Component.component):
             yield 1
         msg = self.recv("control")
         print self.name, "shutting down having recieved:", msg
-        self.send("passedon", "signal")
+        self.send(msg, "signal")
         yield 1
+
+print "If the graphline's 'control' and 'signal' boxes aren't wired to anything"
+print "Graphline will make sure all child components shutdown and will pass it on"
 
 Pipeline(
     Pinger(),
@@ -36,5 +40,76 @@ Pipeline(
         linkages = {
             ("TO_SHUTDOWN1", "signal") : ("TO_SHUTDOWN2", "control"),
         }
-    )
+    ),
+    ConsoleEchoer(),
+).run()
+
+print "Success!"
+print 
+print "Now a 2nd test (slightly different)..."
+
+Pipeline(
+    Pinger(),
+    Graphline(
+        TO_SHUTDOWN1 = Waiter(),
+        TO_SHUTDOWN2 = Waiter(),
+        linkages = {
+            ("self", "control") : ("TO_SHUTDOWN1", "control"),
+            ("TO_SHUTDOWN1", "signal") : ("TO_SHUTDOWN2", "control"),
+            ("TO_SHUTDOWN2", "signal") : ("self", "signal"),
+        }
+    ),
+    ConsoleEchoer(),
+).run()
+
+print 
+print "Now a 3rd test (slightly different)..."
+
+Pipeline(
+    Pinger(),
+    Graphline(
+        TO_SHUTDOWN1 = Waiter(),
+        TO_SHUTDOWN2 = Waiter(),
+        linkages = {
+#            ("self", "control") : ("TO_SHUTDOWN1", "control"),
+            ("TO_SHUTDOWN1", "signal") : ("TO_SHUTDOWN2", "control"),
+            ("TO_SHUTDOWN2", "signal") : ("self", "signal"),
+        }
+    ),
+    ConsoleEchoer(),
+).run()
+
+
+print 
+print "Now a 4th test (slightly different)..."
+
+Pipeline(
+    Pinger(),
+    Graphline(
+        TO_SHUTDOWN1 = Waiter(),
+        TO_SHUTDOWN2 = Waiter(),
+        linkages = {
+            ("self", "control") : ("TO_SHUTDOWN1", "control"),
+            ("TO_SHUTDOWN1", "signal") : ("TO_SHUTDOWN2", "control"),
+#            ("TO_SHUTDOWN2", "signal") : ("self", "signal"),
+        }
+    ),
+    ConsoleEchoer(),
+).run()
+
+print
+print "Now this one will fail to terminate ..."
+
+Pipeline(
+    Pinger(),
+    Graphline(
+        TO_SHUTDOWN1 = Waiter(),
+        TO_SHUTDOWN2 = Waiter(),
+        linkages = {
+            ("self", "control") : ("TO_SHUTDOWN1", "control"),
+#            ("TO_SHUTDOWN1", "signal") : ("TO_SHUTDOWN2", "control"),
+            ("TO_SHUTDOWN2", "signal") : ("self", "signal"),
+        }
+    ),
+    ConsoleEchoer(),
 ).run()
