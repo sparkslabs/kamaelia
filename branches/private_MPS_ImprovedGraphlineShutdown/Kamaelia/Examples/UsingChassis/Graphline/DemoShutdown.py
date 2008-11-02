a9b735fd39669bbc250702f1994e09de792ce8d1
sparks.m <sparks.m@2682807a-6752-0410-967a-793f22d1dfe9>
2008-11-02 01:14:35 +0000

    Simplified demo shutdown example

---
+++ branches/private_MPS_ImprovedGraphlineShutdown/Kamaelia/Examples/UsingChassis/Graphline/DemoShutdown.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+
+import time
+import Axon
+from Kamaelia.Chassis.Graphline import Graphline
+from Kamaelia.Chassis.Pipeline import Pipeline
+from Kamaelia.Util.Console import ConsoleEchoer
+
+class Pinger(Axon.ThreadedComponent.threadedcomponent):
+    tosend = [ ]
+    box = "signal"
+    delay = 0.3
+    def main(self):
+        i = 0
+        for i in self.tosend:
+            time.sleep(1.0)
+            self.send(Axon.Ipc.producerFinished(), self.box)
+            print "PINGER: sent", i
+    
+class Waiter(Axon.Component.component):
+    def main(self):
+        print "WAITER", self.id, "waiting"
+        while not self.dataReady("control"):
+            if not self.anyReady(): self.pause()
+            yield 1
+        msg = self.recv("control")
+        print "WAITER", self.id, "shutting down having recieved:", msg
+        self.send(msg, "signal")
+        yield 1
+
+print """
+Basic Acceptance Test
+
+This example creates 3 components inside a graphline that wait for shutdown
+from the outside world When they get this passed onto them, they shutdown,
+causing the graphline (and pipeline) to shutdown
+
+      Pipeline(
+          Pinger(tosend=[Axon.Ipc.producerFinished()],box="signal"),
+          Graphline(
+              TO_SHUTDOWN1 = Waiter(),
+              TO_SHUTDOWN2 = Waiter(),
+              TO_SHUTDOWN3 = Waiter(),
+              linkages = {}
+          ),
+      ).run()
+"""
+
+Pipeline(
+    Pinger(tosend=[Axon.Ipc.producerFinished()]),
+    Graphline(
+        TO_SHUTDOWN1 = Waiter(),
+        TO_SHUTDOWN2 = Waiter(),
+        TO_SHUTDOWN3 = Waiter(),
+        linkages = {}
+    ),
+).run()
