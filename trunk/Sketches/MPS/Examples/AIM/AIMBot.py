628147cf463be36c27aa36e3e3a601ba34955c26
sparks.m <sparks.m@2682807a-6752-0410-967a-793f22d1dfe9>
2008-11-16 02:22:11 +0000

    For debugging and testing more interesting systems...
    Michael

---
+++ trunk/Sketches/MPS/Examples/AIM/AIMBot.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+##
+## (C) 2008 British Broadcasting Corporation and Kamaelia Contributors(1)
+##     All Rights Reserved.
+##
+## You may only modify and redistribute this under the terms of any of the
+## following licenses(2): Mozilla Public License, V1.1, GNU General
+## Public License, V2.0, GNU Lesser General Public License, V2.1
+##
+## (1) Kamaelia Contributors are listed in the AUTHORS file and at
+##     http://kamaelia.sourceforge.net/AUTHORS - please extend this file,
+##     not this notice.
+## (2) Reproduced in the COPYING file, and at:
+##     http://kamaelia.sourceforge.net/COPYING
+## Under section 3.5 of the MPL, we are using this text since we deem the MPL
+## notice inappropriate for this file. As per MPL/GPL/LGPL removal of this
+## notice is prohibited.
+##
+## Please contact us via: kamaelia-list-owner@lists.sourceforge.net
+## to discuss alternative licensing.
+## -------------------------------------------------------------------------
+
+import cgi
+import time
+import Axon
+from Kamaelia.Protocol.AIM.AIMHarness import AIMHarness
+from Kamaelia.Chassis.Graphline import Graphline
+from Kamaelia.File.UnixProcess import UnixProcess
+
+def sendTo(recipient, text):
+    return ("message", recipient, text)
+
+def outformat(data, buddyname):
+    buddyname = buddyname.lower()
+    if data[0] == "buddy online" and data[1]["name"].lower() ==  buddyname:
+        return "%s is online" % data[1]["name"]
+    elif data[0] == "message" and data[1].lower() == buddyname:
+        return "%s: %s" % (data[1], data[2])
+    elif data[0] == "error":
+        return ": ".join(data)
+
+def ConsoleUser():
+    return UnixProcess("/home/zathras/tmp/rules_test.py")
+
+
+class AIMUserTalkAdapter(Axon.ThreadedComponent.threadedcomponent):
+    Inboxes = {
+        "from_user" : "We receive plain text from the user here. Initially we just pass on messages almost as is",
+        "from_aim" : "We recieive 'raw' AIM messages here",
+        "control" : "We expect shutdown messages here",
+    }
+    Outboxes = {
+        "to_user" : "We send plain text to the user here.",
+        "to_aim" : "We send formatted messages here, which are for aim, targetted to a particular user",
+        "signal" : "We pass on shutdown messages here",
+    }
+    user = "zathmadscientist"
+    ratelimit = 0.5
+    def main(self):
+
+        while not self.dataReady("control"):
+            for message in self.Inbox("from_user"):
+                 message = cgi.escape(message,1)
+                 lines = message.split("\n")
+                 for line in lines:
+                     time.sleep(self.ratelimit)
+                     message = sendTo(self.user, line ) # FIXME: Crap name
+                     self.send(message, "to_aim")
+
+            for message in self.Inbox("from_aim"):
+                 if len(message) == 3:
+                     if message[0] == "message":
+                         text = message[2]+"\n"
+                         self.send(text, "to_user")
+
+            if not self.anyReady():
+                self.pause()
+        if self.dataReady("control"):
+            self.send(self.recv("control"), "signal")
+        else:
+           self.send(Axon.Ipc.ProducerFinished(), "signal")
+
+def UltraBot(screenname, password):
+    print "ULTRABOT STARTING UP"
+    print "For the moment, ultrabot may not say anything when it's told anything"
+
+    return Graphline(
+               AIM = AIMHarness(screenname, password),
+
+               ADAPTER = AIMUserTalkAdapter(),
+               USER = ConsoleUser(),
+               linkages = {               
+                   ("AIM", "outbox"): ("ADAPTER","from_aim"),
+                   ("USER","outbox"):       ("ADAPTER","from_user"),
+
+                   ("ADAPTER","to_aim"):  ("AIM", "inbox"),
+                   ("ADAPTER","to_user"): ("USER","inbox"),
+               }
+           )
+
+if __name__ == '__main__':
+
+    bot_id = "kamaelian"
+    bot_password = "iamgrey"
+
+    UltraBot(bot_id, bot_password).run()
+    
