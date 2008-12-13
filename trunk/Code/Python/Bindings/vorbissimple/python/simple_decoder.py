684655e6478887a1b8d7ab768d1966ae69852e29
sparks.m <sparks.m@2682807a-6752-0410-967a-793f22d1dfe9>
2008-12-13 01:33:13 +0000

    merge of branch private_MH_EnhancementsForDVB-C

--- trunk/Code/Python/Bindings/vorbissimple/python/simple_decoder.py
+++ trunk/Code/Python/Bindings/vorbissimple/python/simple_decoder.py
@@ -5,7 +5,7 @@ import sys
 import time
 
 x=vorbissimple.vorbissimple()
-f=open("/home/zathras/Documents/Media/Ogg/PopularClassics/2/audio_01.ogg","r",0)
+f=open("/usr/local/backup/Music/PopularClassics/4/audio_01.ogg","r",0)
 #f=open("/home/zathras/Documents/Media/Ogg/KR-Tomb-of-Horus.ogg","r",0)
 time.sleep(0.01)
 
