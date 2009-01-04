04e02a9283800c70a7c03e71dda5d010be49db5b
amnorvend <amnorvend@2682807a-6752-0410-967a-793f22d1dfe9>
2009-01-04 19:30:48 +0000

    I'm an idiot.  I branched against trunk instead of trunk/Code/Python.  Deleting branch so I can re-create it.

--- branches/private_JMB_HTTPSupport/Other/Community/MPS/Demo/Kamaelia/__init__.py
+++
@@ -1,1 +0,0 @@
-" stub to allow import"
--- branches/private_JMB_HTTPSupport/Sketches/TG/old_shard/cshard/CShutdownHandling.py
+++
@@ -1,9 +0,0 @@
-"""
-Code adapted slightly from MagnaDoodle.py and moved into separate module
-"""
-def handleShutdown(self):
-    while self.dataReady("control"):
-        cmsg = self.recv("control")
-        if isinstance(cmsg, producerFinished) or isinstance(cmsg, shutdownMicroprocess):
-            self.send(cmsg, "signal")
-            return True  # done = True
