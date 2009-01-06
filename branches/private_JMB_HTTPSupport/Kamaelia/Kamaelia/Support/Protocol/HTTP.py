acea062a62935fbfc2076f3846f75965d4488d1d
amnorvend <amnorvend@2682807a-6752-0410-967a-793f22d1dfe9>
2009-01-06 02:06:55 +0000

    Removed contributor agreement line.

--- branches/private_JMB_HTTPSupport/Kamaelia/Kamaelia/Support/Protocol/HTTP.py
+++ branches/private_JMB_HTTPSupport/Kamaelia/Kamaelia/Support/Protocol/HTTP.py
@@ -19,7 +19,6 @@
 # Please contact us via: kamaelia-list-owner@lists.sourceforge.net
 # to discuss alternative licensing.
 # -------------------------------------------------------------------------
-# Licensed to the BBC under a Contributor Agreement: JMB
 """
 This is a module of utility functions to be used with the HTTP server.
 
