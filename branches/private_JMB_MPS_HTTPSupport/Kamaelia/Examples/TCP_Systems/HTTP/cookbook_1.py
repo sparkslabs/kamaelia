afa5fd83f59fc14fc26874af046cd7ec8e86ec4b
sparks.m <sparks.m@2682807a-6752-0410-967a-793f22d1dfe9>
2009-01-07 23:46:15 +0000

    Changed doc string into a string that's printed out when the example is run. Also, shifted things about slightly as a result. Added a note about what happens when the application is run, and what to expect

--- branches/private_JMB_MPS_HTTPSupport/Kamaelia/Examples/TCP_Systems/HTTP/cookbook_1.py
+++ branches/private_JMB_MPS_HTTPSupport/Kamaelia/Examples/TCP_Systems/HTTP/cookbook_1.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python
-"""
-This example demonstrates using the Minimal file handler for
-serving static web content.
-
-System Requirements
--------------------
-This example requires a UNIX operating system.
-"""
-
 
 # Import socket to get at constants for socketOptions
 import socket
@@ -22,6 +13,32 @@ from Kamaelia.Support.Protocol.HTTP import HTTPProtocol
 
 homedirectory = "/srv/www/htdocs/"
 indexfilename = "index.html"
+
+print \
+"""
+About
+-----
+This example demonstrates using the Minimal file handler for
+serving static web content.
+
+System Requirements
+-------------------
+This example requires a UNIX operating system.
+
+What it does
+------------
+It runs a basic webserver on http://127.0.0.1:8082/ which looks for any files to
+serve from the docroot. This is hardcoded in this file as being %s . The correct
+mimetype is also served.
+
+Specifically, it starts up a ServerCore instance which starts an HTTPProtocol handler
+when a client connects on port 8082. The HTTPProtocol handler is configured using
+a simplistic request routing table that matches "/" against the Minimal handler -
+which just reads an appropriate file from a preconfigured location and serves that
+back to the user.
+
+""" % (homedirectory, )
+
 
 #Here we define our routing.  This tells us that the root of the server will run
 #the minimal request handler, a static file server.
