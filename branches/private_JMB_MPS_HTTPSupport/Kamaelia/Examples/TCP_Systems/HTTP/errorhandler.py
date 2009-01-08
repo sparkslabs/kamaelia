ff4d0d518f4eed8a14d1dfabfc2a2fc3c3544c2a
sparks.m <sparks.m@2682807a-6752-0410-967a-793f22d1dfe9>
2009-01-08 00:00:19 +0000

    Extended the docs for this slightly and spit the usage out to the usage when it's run :-)

--- branches/private_JMB_MPS_HTTPSupport/Kamaelia/Examples/TCP_Systems/HTTP/errorhandler.py
+++ branches/private_JMB_MPS_HTTPSupport/Kamaelia/Examples/TCP_Systems/HTTP/errorhandler.py
@@ -1,8 +1,17 @@
 #! /usr/bin/env python
-"""
+__doc__ = """
 This module demonstrates the use of the error handler
 HTTP component.  When run, it will pull up nothing
 but 404s.
+
+To see it in action, point your browser at:
+   http://127.0.0.1:8082/
+
+Or anything you want:
+   http://127.0.0.1:8082/FooBarBaz
+   http://127.0.0.1:8082/ThisDoesntExist
+   http://127.0.0.1:8082/NorThis
+   http://127.0.0.1:8082/OrThis   
 """
 #We need this for the socket options.
 import socket
@@ -19,6 +28,8 @@ from Kamaelia.Support.Protocol.HTTP import HTTPProtocol
 from Kamaelia.Protocol.HTTP.ErrorPages import ErrorPageHandler
 
 routing = []
+
+print __doc__
 
 #Create the server and run it.
 ServerCore(
