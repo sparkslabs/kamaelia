52a9d1ece2b675842f4d5499687bc9deb73216d4
matthammond.org <matthammond.org@2682807a-6752-0410-967a-793f22d1dfe9>
2009-01-08 23:35:46 +0000

    wrote docstrings for Axon.background. Removed defunct reference to Axon.Likefile in main index and pointed to Axon.background instead
    Matt

--- Code/Python/Axon/Axon/background.py
+++ Code/Python/Axon/Axon/background.py
@@ -20,10 +20,83 @@
 # to discuss alternative licensing.
 # -------------------------------------------------------------------------
 """
+Running an Axon system in a separate thread
+===========================================
 
-Out of the original code for background, this seems the most likely to
-remain intact.
+The background class makes it easy to run an Axon system in a separate
+thread (in effect: in the background).
 
+This simplifies integration of Axon/Kamaelia code into other python code.
+
+
+
+Example Usage
+-------------
+
+At its simplest, you could run a Kamaelia task independently in the background
+- such as a simple network connection, that dumps received data into a thread
+safe queue, after de-chunking it into lines of text. To do this:
+
+1. We implement a simple component to collect the data::
+
+    from Axon.background import background
+    from Axon.Component import component
+
+    class Receiver(component):
+        def __init__(self, queue):
+            super(Bucket,self).__init__()
+            self.q = queue
+            
+        def main(self):
+            while 1:
+                while self.dataReady("inbox"):
+                    self.q.put(self.recv("inbox"))
+                self.pause()
+                yield 1
+    
+2. Then we create a background object and call its start() method::
+
+    from Axon.background import background
+    
+    background().start()
+
+3. Finally, we create and activate our Kamaelia pipeline of components,
+   including the receiver component we've just written, passing it a thread-safe
+   queue to put the data into::
+    
+    from Kamaelia.Chassis.Pipeline import Pipeline 
+    from Kamaelia.Internet.TCPClient import TCPClient
+    from Kamaelia.Visualisation.PhysicsGraph import chunks_to_lines
+    from Queue import Queue
+
+    queue = Queue()
+
+    Pipeline(
+        TCPClient("my.server.com", 1234),
+        chunks_to_lines(),
+        Receiver(queue)
+    ).activate()
+
+We can now fetch items of data, from the queue when they arrive::
+
+    received_line = queue.get()
+
+
+
+Behavour
+--------
+
+Create one of these and start it running by calling its start() method.
+    
+After that, any components you activate will default to using this
+scheduler.
+
+Only one instance can be used within a given python interpreter.
+
+The background thread is set as a "daemon" thread. This means that if your
+program exits, this background thread will be killed too. If it were not a
+daemon, then it would prevent the python interpreter terminating until the
+components running in it had all terminated too.
 """
 
 
@@ -34,7 +107,17 @@ import threading
 import CoordinatingAssistantTracker as cat
 
 class background(threading.Thread):
-    """A python thread which runs a scheduler. Takes the same arguments at creation that scheduler.run.runThreads accepts."""
+    """\
+    A python thread which runs the Axon Scheduler. Takes the same arguments
+    at creation that Axon.Scheduler.scheduler.run.runThreads accepts.
+    
+    Create one of these and start it running by calling its start() method.
+    
+    After that, any components you activate will default to using this
+    scheduler.
+    
+    Only one instance can be used within a given python interpreter.
+    """
     lock = threading.Lock()
     def __init__(self,slowmo=0,zap=False):
         if not background.lock.acquire(False):
@@ -43,6 +126,10 @@ class background(threading.Thread):
         threading.Thread.__init__(self)
         self.setDaemon(True) # Die when the caller dies
         self.zap = zap
+        
+    """\
+    The internal body code of the thread. Call start() to begin execution.
+    """
     def run(self):
         if self.zap:
             X = scheduler()
