c870dc338e4bbe3c50ab83e79d161f96d6551287
sparks.m <sparks.m@2682807a-6752-0410-967a-793f22d1dfe9>
2009-07-04 12:48:29 +0000

    correct release directory

--- Code/Python/Apps/Europython09/DistBuild/upload.sh
+++ Code/Python/Apps/Europython09/DistBuild/upload.sh
@@ -5,4 +5,4 @@
 version=`grep version setup.py.src|sed -e 's/^.*= "//; s/",.*//'`
 echo "Uploading Europython distribution $version"
 
-scp ../dist/* michaels@132.185.142.2:/srv/www/sites/edit.kamaelia.org/docs/release/
+scp ../dist/* michaels@132.185.142.2:/srv/www/sites/www.kamaelia.org/docs/release/
