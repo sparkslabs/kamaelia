8d2d29b86e208b013121b8c9375227bc54c6a1be
sparks.m <sparks.m@2682807a-6752-0410-967a-793f22d1dfe9>
2009-07-20 11:29:08 +0000

    unsynced change

--- branches/private_DK_PaintDev/Kamaelia/Kamaelia/Apps/GSOCPaint/Button.py
+++ branches/private_DK_PaintDev/Kamaelia/Kamaelia/Apps/GSOCPaint/Button.py
@@ -78,164 +78,16 @@ display surface it requested.
 """
 
 import pygame
-import Axon
-from Axon.Ipc import producerFinished
-from Kamaelia.UI.GraphicDisplay import PygameDisplay
 
-class Button(Axon.Component.component):
-   """\
-   Button(...) -> new Button component.
+from Kamaelia.UI.Pygame.Button import Button
 
-   Create a button widget in pygame, using the Pygame Display service. Sends a
-   message out of its outbox when clicked.
 
-   Keyword arguments (all optional):
-   
-   - caption      -- text (default="Button <component id>")
-   - position     -- (x,y) position of top left corner in pixels
-   - margin       -- pixels margin between caption and button edge (default=8)
-   - bgcolour     -- (r,g,b) fill colour (default=(224,224,224))
-   - fgcolour     -- (r,g,b) text colour (default=(0,0,0))
-   - msg          -- sent when clicked (default=("CLICK",self.id))
-   - key          -- if not None, pygame keycode to trigger click (default=None)
-   - transparent  -- draw background transparent if True (default=False)
-   - size         -- None or (w,h) in pixels (default=None)
-   """
-   
-   Inboxes = { "inbox"    : "Receive events from Pygame Display",
-               "control"  : "For shutdown messages",
-               "callback" : "Receive callbacks from Pygame Display"
-             }
-   Outboxes = { "outbox" : "button click events emitted here",
-                "signal" : "For shutdown messages",
-                "display_signal" : "Outbox used for communicating to the display surface" }
-   
-   def __init__(self, caption=None, position=None, margin=8, bgcolour = (224,224,224), fgcolour = (0,0,0), msg=None,
-                key = None,
-                transparent = False, size=None):
-      """x.__init__(...) initializes x; see x.__class__.__doc__ for signature"""
-      super(Button,self).__init__()
-      
-      self.backgroundColour = bgcolour
-      self.foregroundColour = fgcolour
-      self.margin = margin
-      self.key = key
-###      print "KEY",key
-      
-      if caption is None:
-         caption = "Button "+str(self.id)
-      
-      self.size = size
-      
-      pygame.font.init()      
-      self.buildCaption(caption)
-
-      if msg is None:
-         msg = ("CLICK", self.id)
-      self.eventMsg = msg      
-      if transparent:
-         transparency = bgcolour
-      else:
-         transparency = None
-      self.disprequest = { "DISPLAYREQUEST" : True,
-                           "callback" : (self,"callback"),
-                           "events" : (self, "inbox"),
-                           "size": self.size,
-                           "transparency" : transparency }
-      
-      if not position is None:
-        self.disprequest["position"] = position         
-
-   def buildCaption(self, text):
-      """Pre-render the text to go on the button label."""
-      # Text is rendered to self.image
-      font = pygame.font.Font(None, 14)
-      self.image = font.render(text,True, self.foregroundColour, )
-      
-      (w,h) = self.image.get_size()
-      if not self.size:
-          self.size = (w + 2*self.margin, h + 2*self.margin)
-          self.imagePosition = (self.margin, self.margin)
-      else:
-          self.imagePosition = ( (self.size[0]-w)/2, (self.size[1]-h)/2 )
-      
-       
-   def waitBox(self,boxname):
-      """Generator. yields 1 until data ready on the named inbox."""
-      waiting = True
-      while waiting:
-        if self.dataReady(boxname): return
-        else: yield 1
-
-   
-   def main(self):
-      """Main loop."""
-      displayservice = PygameDisplay.getDisplayService()
-      self.link((self,"display_signal"), displayservice)
-
-      self.send( self.disprequest,
-                  "display_signal")
-             
-      for _ in self.waitBox("callback"): yield 1
-      self.display = self.recv("callback")
-      self.blitToSurface()
-      
-      self.send({ "ADDLISTENEVENT" : pygame.MOUSEBUTTONDOWN,
-                  "surface" : self.display},
-                  "display_signal")
-      if self.key is not None:
-###         print "ADDING LISTEN", self.key, pygame.KEYDOWN, self.display
-         message = { "ADDLISTENEVENT" : pygame.KEYDOWN,
-                     "surface" : self.display,
-                     "TRACE" : "ME"}
-###         print "----------------", message
-         self.send(message, "display_signal")
-#      while self.outboxes["display_signal"]  != []:
-#         print dir(self)
-#         print "Waiting clean out", self.outboxes["display_signal"], self
-#         yield 1
-      done = False
-      while not done:
-         while self.dataReady("control"):
-            cmsg = self.recv("control")
-            if isinstance(cmsg, producerFinished) or isinstance(cmsg, shutdownMicroprocess):
-               self.send(cmsg, "signal")
-               done = True
-         
-         while self.dataReady("inbox"):
-            for event in self.recv("inbox"):
-###                print event
-                if event.type == pygame.MOUSEBUTTONDOWN:
-#                   print "BUTTON", event.button
-                   bounds = self.display.get_rect()
-                   if bounds.collidepoint(*event.pos):
-                      self.send( self.eventMsg, "outbox" )
-                if event.type == pygame.KEYDOWN:
-###                   print "EVENT", event.type, event.key
-                   if event.key == self.key:
-                      self.send( self.eventMsg, "outbox" )
-         self.pause()
-         yield 1
-            
-      
-   def blitToSurface(self):
-       """Clears the background and renders the text label onto the button surface."""
-       try:
-           self.display.fill( self.backgroundColour )
-           self.display.blit( self.image, self.imagePosition )
-       except:
-           pass
-       self.send({"REDRAW":True, "surface":self.display}, "display_signal")
-
-__kamaelia_components__  = ( Button, )
 
 class ImageButton( Button ):
     def __init__(self, caption=None, position=None, margin=8, bgcolour = (224,224,224), fgcolour = (0,0,0), msg=None,
                 key = None,
                 transparent = False, size=None):
         super(ImageButton,self).__init__(caption, position, margin, bgcolour, fgcolour, msg, key, transparent, size)
-       # Button.__init__(self, caption, position, margin, bgcolour, fgcolour, msg, key, transparent, size)
- #       self.caption = caption
         
     def buildCaption(self, text):
        """Pre-render the image to go on the button label."""
@@ -248,19 +100,23 @@ class ImageButton( Button ):
            self.imagePosition = (self.margin, self.margin)
        else:
            self.imagePosition = ( (self.size[0]-w)/2, (self.size[1]-h)/2 )
+
+__kamaelia_components__  = ( ImageButton, )
+
+
 if __name__ == "__main__":
-   from Kamaelia.Util.Console import ConsoleEchoer
-   from pygame.locals import *
-   import os
-   button0 = ImageButton(caption=(os.path.join('', 'test.png')),key=K_SPACE).activate()
-   button1 = Button(caption="Press SPACE or click",key=K_SPACE).activate()
-   button2 = Button(caption="Reverse colours",fgcolour=(255,255,255),bgcolour=(0,0,0)).activate()
-   button3 = Button(caption="Mary...",msg="Mary had a little lamb", position=(200,100)).activate()
+    from pygame.locals import *
+    from Kamaelia.Chassis.PAR import PAR
+    from Kamaelia.Chassis.Pipeline import Pipeline
+    from Kamaelia.Util.Console import ConsoleEchoer
    
-   ce = ConsoleEchoer().activate()
-   button0.link( (button0,"outbox"), (ce,"inbox") )
-   button1.link( (button1,"outbox"), (ce,"inbox") )
-   button2.link( (button2,"outbox"), (ce,"inbox") )
-   button3.link( (button3,"outbox"), (ce,"inbox") )
-   
-   Axon.Scheduler.scheduler.run.runThreads()  
+    import os
+    Pipeline(
+        PAR(
+            ImageButton(caption='../../../Examples/SupportingMediaFiles/cat-logo.png',key=K_SPACE,msg="cat"),
+            Button(caption="Press SPACE or click",key=K_SPACE, msg="space"),
+            Button(caption="Reverse colours",fgcolour=(255,255,255),bgcolour=(0,0,0), msg="reverse"),
+            Button(caption="Mary...",msg="Mary had a little lamb", position=(200,100)),
+        ),
+        ConsoleEchoer(),
+    ).run()
