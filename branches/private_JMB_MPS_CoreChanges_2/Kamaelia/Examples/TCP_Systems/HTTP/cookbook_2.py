363c382f4cc11e0a042b589abc1e7b4ca5e5f51d
sparks.m <sparks.m@2682807a-6752-0410-967a-793f22d1dfe9>
2009-07-25 15:43:19 +0000

    Make comments easier to read

--- branches/private_JMB_MPS_CoreChanges_2/Kamaelia/Examples/TCP_Systems/HTTP/cookbook_2.py
+++ branches/private_JMB_MPS_CoreChanges_2/Kamaelia/Examples/TCP_Systems/HTTP/cookbook_2.py
@@ -20,8 +20,10 @@ from Kamaelia.Chassis.Pipeline import Pipeline
 homedirectory = "/srv/www/htdocs/"
 indexfilename = "index.html"
 
-#This is the hello handler.  It will respond to every request with a Hello world
-#message (with some HTML formatting)
+#
+# This is the hello handler.  It will respond to every request with a Hello
+# world message (with some HTML formatting)
+#
 class HelloHandler(Axon.Component.component):
     def __init__(self, request):
         super(HelloHandler, self).__init__()
@@ -55,10 +57,12 @@ class Cat(Axon.Component.component):
         self.send(Axon.Ipc.producerFinished(self), "signal")        
         yield 1
 
-#This component will simply wrap any input it receives into a dictionary that the
-#HTTP Server can use to form a response.  Note that this code works this way for
-#example purposes.  In most instances, it's better to buffer your output and send
-#everything at once.
+#
+# This component will simply wrap any input it receives into a dictionary
+# that the HTTP Server can use to form a response.  Note that this code
+# works this way for example purposes.  In most instances, it's better to
+# buffer your output and send everything at once.
+#
 class ExampleWrapper(Axon.Component.component):
  
     def main(self):
@@ -97,10 +101,12 @@ class ExampleWrapper(Axon.Component.component):
         self.send(Axon.Ipc.producerFinished(self), "signal")        
         yield 1
 
-#This handler will join the 'Cat' handler and ExampleWrapper together such that
-#the 'Cat' will forward the request dictionary on to the ExampleWrapper,
-#which will wrap the data in a response dictionary and forward the data on to the
-#HTTP Server.
+#
+# This handler will join the 'Cat' handler and ExampleWrapper together such
+# that the 'Cat' will forward the request dictionary on to the
+# ExampleWrapper, which will wrap the data in a response dictionary and
+# forward the data on to the HTTP Server.
+#
 def EchoHandler(request):
     return Pipeline ( Cat(request), ExampleWrapper() )
 
