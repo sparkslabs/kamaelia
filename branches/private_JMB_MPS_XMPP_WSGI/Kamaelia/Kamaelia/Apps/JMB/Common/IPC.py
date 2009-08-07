8a6a9d2d6a7f5da5acc4275d1186cd1f99eac914
sparks.m <sparks.m@2682807a-6752-0410-967a-793f22d1dfe9>
2009-08-07 11:53:35 +0000

    Pull in rather than redefine the standard ipc/control messages

--- branches/private_JMB_MPS_XMPP_WSGI/Kamaelia/Kamaelia/Apps/JMB/Common/IPC.py
+++ branches/private_JMB_MPS_XMPP_WSGI/Kamaelia/Kamaelia/Apps/JMB/Common/IPC.py
@@ -22,96 +22,12 @@
 
 from Axon.Ipc import producerFinished, notify
 
-class socketShutdown(producerFinished):
-   """Message to indicate that the network connection has been closed."""
-   pass
+# Pull in rather than redefine the standard ipc/control messages
+from Kamaelia.IPC import socketShutdown, serverShutdown
+from Kamaelia.IPC import newCSA, shutdownCSA, newServer
+from Kamaelia.IPC import newWriter, newReader, newExceptional
+from Kamaelia.IPC import removeReader, removeWriter, removeExceptional
 
-class serverShutdown(producerFinished):
-   """Message to indicate that the server should shutdown"""
-   pass
-
-class newCSA(notify):
-   """Helper class to notify of new CSAs as they are created.  newCSA.object
-   will return the CSA."""
-   def __init__(self, caller, CSA, sock=None):
-      super(newCSA, self).__init__(caller, CSA)
-      self.sock = sock
-   def handlesWriting(self):
-      return True
-
-class shutdownCSA(notify):
-   """Helper class to notify of new CSAs as they are created.  newCSA.object
-   will return the CSA."""
-   def __init__(self, caller, CSA):
-      super(shutdownCSA, self).__init__(caller, CSA)
-   def shutdown(self):
-      return True
-
-class newServer(notify):
-   """Helper class to notify of new CSAs as they are created.  newCSA.object
-   will return the CSA."""
-   def __init__(self, caller, CSA):
-      super(newServer, self).__init__(caller, CSA)
-   def handlesWriting(self):
-      return False
-
-#
-# Two new classes to simplify the selector.
-# 
-# Note that hasOOB is only to be set for SOCKETS, and relates to out of band
-# information regarding the socket. This is used to put the specific thing
-# to monitor into the exceptions set (places we get out of band info)
-#
-# For more help as to what's going on with exceptionals, look at
-#   $ man 2 select_tut
-#
-# This may get simplified further at some point to simply add in a newExceptionalReader
-# message (indeed I'm in two minds right now about that!)
-#
-
-
-class newWriter(notify):
-    """Helper class to notify of new CSAs as they are created.  newCSA.object
-    will return the CSA."""
-    def __init__(self, caller, CSA):
-        super(newWriter, self).__init__(caller, CSA)
-        self.hasOOB = False 
-
-class newReader(notify):
-   """Helper class to notify of new CSAs as they are created.  newCSA.object
-   will return the CSA."""
-   def __init__(self, caller, CSA):
-      super(newReader, self).__init__(caller, CSA)
-      self.hasOOB = False
-
-class newExceptional(notify):
-   """Helper class to notify of new CSAs as they are created.  newCSA.object
-   will return the CSA."""
-   def __init__(self, caller, CSA):
-      super(newExceptional, self).__init__(caller, CSA)
-      self.hasOOB = False
-
-class removeReader(notify):
-   """Helper class to notify of new CSAs as they are created.  newCSA.object
-   will return the CSA."""
-   def __init__(self, caller, CSA):
-      super(removeReader, self).__init__(caller, CSA)
-      self.hasOOB = False
-
-class removeWriter(notify):
-   """Helper class to notify of new CSAs as they are created.  newCSA.object
-   will return the CSA."""
-   def __init__(self, caller, CSA):
-      super(removeWriter, self).__init__(caller, CSA)
-      self.hasOOB = False
-
-class removeExceptional(notify):
-   """Helper class to notify of new CSAs as they are created.  newCSA.object
-   will return the CSA."""
-   def __init__(self, caller, CSA):
-      super(removeExceptional, self).__init__(caller, CSA)
-      self.hasOOB = False
-      
 class userLoggedOut(notify):
    def __init__(self, thread):
       self.thread = thread
