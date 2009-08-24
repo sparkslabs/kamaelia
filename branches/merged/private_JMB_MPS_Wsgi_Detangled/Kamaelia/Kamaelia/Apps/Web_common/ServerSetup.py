54995bc689f7dc28755fba316c98af0082d723a7
sparks.m <sparks.m@2682807a-6752-0410-967a-793f22d1dfe9>
2009-08-24 22:39:17 +0000

    Changes now either merged on trunk or in another simpler branch nealry ready for merge

---
+++ branches/merged/private_JMB_MPS_Wsgi_Detangled/Kamaelia/Kamaelia/Apps/Web_common/ServerSetup.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2008 British Broadcasting Corporation and Kamaelia Contributors(1)
+#     All Rights Reserved.
+#
+# You may only modify and redistribute this under the terms of any of the
+# following licenses(2): Mozilla Public License, V1.1, GNU General
+# Public License, V2.0, GNU Lesser General Public License, V2.1
+#
+# (1) Kamaelia Contributors are listed in the AUTHORS file and at
+#     http://kamaelia.sourceforge.net/AUTHORS - please extend this file,
+#     not this notice.
+# (2) Reproduced in the COPYING file, and at:
+#     http://kamaelia.sourceforge.net/COPYING
+# Under section 3.5 of the MPL, we are using this text since we deem the MPL
+# notice inappropriate for this file. As per MPL/GPL/LGPL removal of this
+# notice is prohibited.
+#
+# Please contact us via: kamaelia-list-owner@lists.sourceforge.net
+# to discuss alternative licensing.
+# -------------------------------------------------------------------------
+"""This module includes various useful functions for setting a webserver up."""
+
+import sys, logging, os
+def processPyPath(ServerConfig):
+    """Use ServerConfig to add to the python path."""
+    if ServerConfig.get('pypath_append'):
+        path_append = ServerConfig['pypath_append'].split(':')
+        #expand all ~'s in the list
+        path_append = [os.path.expanduser(path) for path in path_append]
+        sys.path.extend(path_append)
+    
+    if ServerConfig.get('pypath_prepend'):
+        path_prepend = ServerConfig['pypath_prepend'].split(':')
+        path_prepend.reverse()
+        for path in path_prepend:
+            path = os.path.expanduser(path)
+            sys.path.insert(0, path)
+            
+def normalizeUrlList(url_list):
+    """Add necessary default entries that the user did not enter."""
+    for dict in url_list:
+        if not dict.get('kp.app_object'):
+            dict['kp.app_object'] = 'application'
+            
+def normalizeWsgiVars(WsgiConfig):
+    """Put WSGI config data in a state that the server expects."""
+    WsgiConfig['wsgi_ver'] = tuple(WsgiConfig['wsgi_ver'].split('.'))
+    
+def initializeLogger(consolename='kamaelia'):
+    """This sets up the logging system."""
+    formatter = logging.Formatter('%(levelname)s/%(name)s: %(message)s')
+    
+    console = logging.StreamHandler(sys.stdout)
+    console.setFormatter(formatter)
+    consolelogger = logging.getLogger(consolename)
+    consolelogger.setLevel(logging.DEBUG)
+    consolelogger.addHandler(console)
+    from Kamaelia.Apps.Web_common.Console import setConsoleName
+    setConsoleName(consolename)
+    
+    from atexit import register
+    register(killLoggers)
+
+def killLoggers():
+    """Shuts down the logging system and flushes input."""
+    logging.shutdown()
---
+++ branches/merged/private_JMB_MPS_Wsgi_Detangled/Kamaelia/Kamaelia/Visualisation/PhysicsGraph/TopologyViewerServer.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2006 British Broadcasting Corporation and Kamaelia Contributors(1)
+#     All Rights Reserved.
+#
+# You may only modify and redistribute this under the terms of any of the
+# following licenses(2): Mozilla Public License, V1.1, GNU General
+# Public License, V2.0, GNU Lesser General Public License, V2.1
+#
+# (1) Kamaelia Contributors are listed in the AUTHORS file and at
+#     http://kamaelia.sourceforge.net/AUTHORS - please extend this file,
+#     not this notice.
+# (2) Reproduced in the COPYING file, and at:
+#     http://kamaelia.sourceforge.net/COPYING
+# Under section 3.5 of the MPL, we are using this text since we deem the MPL
+# notice inappropriate for this file. As per MPL/GPL/LGPL removal of this
+# notice is prohibited.
+#
+# Please contact us via: kamaelia-list-owner@lists.sourceforge.net
+# to discuss alternative licensing.
+# -------------------------------------------------------------------------
+
+"""\
+==============================
+Generic Topology Viewer Server
+==============================
+
+A generic topology viewer that one client can connect to at a time over a
+TCP socket and send topology change data for visualisation.
+
+
+
+Example Usage
+-------------
+Visualiser that listens on port 1500 for a TCP connection through which
+it receives topology change data to render::
+    
+    TopologyViewerServer( serverPort = 1500 ).run()
+    
+A simple client to drive the visualiser::
+    
+    Pipeline( ConsoleReader(),
+              TCPClient( server=<address>, port=1500 ),
+            ).run()
+    
+Run the server, then run the client::
+    
+    >>> DEL ALL
+    >>> ADD NODE 1 "1st node" randompos -
+    >>> ADD NODE 2 "2nd node" randompos -
+    >>> ADD NODE 3 "3rd node" randompos -
+    >>> ADD LINK 1 2
+    >>> ADD LINK 3 2
+    >>> DEL LINK 1 2
+    >>> DEL NODE 1
+
+See also Kamaelia.Visualisation.Axon.AxonVisualiserServer - which is a
+specialisation of this component.
+
+
+
+How does it work?
+-----------------
+
+TopologyViewerServer is a Pipeline of the following components:
+    
+- Kamaelia.Internet.SingleServer
+- chunks_to_lines
+- lines_to_tokenlists
+- TopologyViewer
+- ConsoleEchoer
+
+This Pipeline serves to listen on the specified port (defaults to 1500) for
+clients. One client is allowed to connect at a time.
+
+That client can then send topology change commands formatted as lines of text.
+The lines are parsed and tokenised for the TopologyViewer.
+
+Any output from the TopologyViewer is sent to the console.
+
+If the noServer option is used at initialisation, then the Pipeline is built
+without the SingleServer component. It then becomes a TopologyViewer
+capable of processing non-tokenised input and with diagnostic console output.
+
+See TopologyViewer for more detail on topology change data and
+its behaviour.
+"""
+
+from Kamaelia.Chassis.Pipeline import Pipeline
+
+from Kamaelia.Visualisation.PhysicsGraph.chunks_to_lines import chunks_to_lines
+from Kamaelia.Visualisation.PhysicsGraph.lines_to_tokenlists import lines_to_tokenlists
+from Kamaelia.Visualisation.PhysicsGraph.TopologyViewer import TopologyViewer
+from Kamaelia.Util.Console import ConsoleEchoer
+from Kamaelia.Chassis.ConnectedServer import SimpleServer
+from Kamaelia.Util.Backplane import *
+
+Backplane("NODEEVENTS").activate()
+
+
+def Users():
+    return PublishTo("NODEEVENTS")
+
+def TopologyViewerServer(serverPort = 1500, **dictArgs):
+    """\
+    TopologyViewerServer([noServer][,serverPort],**args) -> new TopologyViewerServer component.
+
+    Multiple-clients-at-a-time TCP socket Topology viewer server. Connect on the
+    specified port and send topology change data for display by a
+    TopologyViewer.
+
+    Keyword arguments:
+    
+    - serverPort  -- None, or port number to listen on (default=1500)
+    - args        -- all remaining keyword arguments passed onto TopologyViewer
+    """
+    SimpleServer(protocol=Users, port=serverPort).activate()
+    return Pipeline( SubscribeTo("NODEEVENTS"),
+                     chunks_to_lines(),
+                     lines_to_tokenlists(),
+                     TopologyViewer(**dictArgs),
+                     ConsoleEchoer()
+               )
+
+def TextControlledTopologyViewer(**dictArgs):
+    return Pipeline( chunks_to_lines(),
+                     lines_to_tokenlists(),
+                     TopologyViewer(**dictArgs),
+                     ConsoleEchoer()
+            )
+
+__kamaelia_prefabs__ = ( TopologyViewerServer, TextControlledTopologyViewer)
+
