59cec9b27c028f0ab54e7bd0c5c28c378ae5458e
sparks.m <sparks.m@2682807a-6752-0410-967a-793f22d1dfe9>
2009-10-04 18:20:00 +0000

    Better seperated out example

--- trunk/Sketches/MPS/AxonDecorators/example.py
+++ trunk/Sketches/MPS/AxonDecorators/example.py
@@ -5,62 +5,7 @@ import time
 import re
 import Axon
 from Kamaelia.Chassis.Pipeline import Pipeline
-
-class BlockingProducer(Axon.ThreadedComponent.threadedcomponent):
-    g = None
-    def main(self):
-        def source():
-            for i in range(10):
-                yield str(i)+"\n"
-            yield
-            print "Source done"
-        if self.g:
-            source = self.g
-
-        while not self.dataReady("control"):
-            for data in source():
-                self.send(data, "outbox")
-            break
-
-        if self.dataReady("control"):
-            self.send(self.recv("control"), "signal")
-        else:
-            self.send(Axon.Ipc.producerFinished(), "signal")
-
-def blockingProducer(GF):
-    def replacement(*argv, **argd):
-        return BlockingProducer(g= lambda : GF(*argv,**argd))
-    return replacement
-
-def TransformerGenComponent(GF):
-    class SourceIt(Axon.Component.component):
-        def __init__(self, *argv, **argd):
-            self.argv = argv
-            self.argd = argd
-            self.F = GF
-            super(SourceIt, self).__init__()
-        def main(self):
-            F = self.F
-            argv = self.argv
-            if argv[0] == None:
-                argv = (self.Inbox,) + argv[1:]
-
-            argd = self.argd
-
-            gen = F( *argv, **argd )
-            while not self.dataReady("control"):
-                for line in gen:
-                    if line:
-                        self.send(line, "outbox")
-                    else:
-                        break
-                yield
-
-            if self.dataReady("control"):
-                self.send(self.recv("control"), "signal")
-            else:
-                self.send(Axon.Ipc.producerFinished(), "signal")
-    return SourceIt
+from decorators import blockingProducer, TransformerGenComponent
 
 @blockingProducer
 def follow(fname):
