2feb9b89da630c8e9b0a7e465e5fc46fb83da1fc
sparks.m <sparks.m@2682807a-6752-0410-967a-793f22d1dfe9>
2009-12-21 01:02:53 +0000

    Moved test code to after the fold.

--- branches/private_MPS_AlsaAudio/Kamaelia/Kamaelia/Device/Alsa/Play.py
+++ branches/private_MPS_AlsaAudio/Kamaelia/Kamaelia/Device/Alsa/Play.py
@@ -2,22 +2,6 @@
 
 import Axon
 import alsaaudio
-import time
-
-class SimpleReader(Axon.Component.component):
-    def __init__(self, filename, chunksize=320):
-        super(SimpleReader, self).__init__()
-        self.filename = filename
-        self.chunksize = chunksize
-    def main(self):
-        f = open(self.filename, "r")
-        data = f.read(self.chunksize)
-        while len(data) > 0:
-            self.send(data, "outbox")
-            yield 1
-            data = f.read(self.chunksize)
-        self.send(Axon.Ipc.producerFinished(), "signal")
-        print "finished reading"
 
 class AlsaPlayer(Axon.Component.component):
     channels = 2
@@ -26,7 +10,6 @@ class AlsaPlayer(Axon.Component.component):
     periodsize = 160
     maxloops = 1000000
     delay = 0.001
-
     def main(self):
         out = alsaaudio.PCM(alsaaudio.PCM_PLAYBACK)
 
@@ -51,49 +34,66 @@ class AlsaPlayer(Axon.Component.component):
         print "Shutdown :-)"
 
 
-def parseargs(argv, longopts, longflags):
-    args = {}
-    for k, key in longopts:
-        try:
-            i = argv.index("--"+key)
-            F = longopts[k,key].__class__(argv[i+1])
-            args[key] = F
-            del argv[i+1]
-            del argv[i]
-        except ValueError:
+
+
+if __name__ == "__main__":
+    import sys
+    from Kamaelia.Chassis.Pipeline import Pipeline
+
+    def parseargs(argv, longopts, longflags):
+        args = {}
+        for k, key in longopts:
             try:
-                i = argv.index("-"+k)
+                i = argv.index("--"+key)
                 F = longopts[k,key].__class__(argv[i+1])
                 args[key] = F
                 del argv[i+1]
                 del argv[i]
             except ValueError:
-                if longopts[k,key] == None:
-                    print "missing argument: --"+key, "-"+k
-                    sys.exit(0)
-                args[key] = longopts[k,key]
+                try:
+                    i = argv.index("-"+k)
+                    F = longopts[k,key].__class__(argv[i+1])
+                    args[key] = F
+                    del argv[i+1]
+                    del argv[i]
+                except ValueError:
+                    if longopts[k,key] == None:
+                        print "missing argument: --"+key, "-"+k
+                        sys.exit(0)
+                    args[key] = longopts[k,key]
 
-    for f,flag in longflags:
-        try:
-            i = argv.index("--"+flag)
-            args[flag] = True
-            del argv[i]
-        except ValueError:
+        for f,flag in longflags:
             try:
-                i = argv.index("-"+f)
+                i = argv.index("--"+flag)
                 args[flag] = True
                 del argv[i]
             except ValueError:
-                args[flag] = False
+                try:
+                    i = argv.index("-"+f)
+                    args[flag] = True
+                    del argv[i]
+                except ValueError:
+                    args[flag] = False
 
-    rest = [a for a in argv if len(argv)>0 and a[0] != "-"]
-    args["__anon__"] = rest
-    return args
+        rest = [a for a in argv if len(argv)>0 and a[0] != "-"]
+        args["__anon__"] = rest
+        return args
 
+    class SimpleReader(Axon.Component.component):
+        def __init__(self, filename, chunksize=320):
+            super(SimpleReader, self).__init__()
+            self.filename = filename
+            self.chunksize = chunksize
+        def main(self):
+            f = open(self.filename, "r")
+            data = f.read(self.chunksize)
+            while len(data) > 0:
+                self.send(data, "outbox")
+                yield 1
+                data = f.read(self.chunksize)
+            self.send(Axon.Ipc.producerFinished(), "signal")
+            print "finished reading"
 
-if __name__ == "__main__":
-    from Kamaelia.Chassis.Pipeline import Pipeline
-    import sys
     args = parseargs( sys.argv[1:],
                       { ("f", "file" ): "audio.raw",
                         ("c", "channels"): 2,
