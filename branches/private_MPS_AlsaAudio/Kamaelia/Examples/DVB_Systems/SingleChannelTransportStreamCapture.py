a13f26ac497564e3850965cea919086e4316d4ed
sparks.m <sparks.m@2682807a-6752-0410-967a-793f22d1dfe9>
2009-12-21 00:57:12 +0000

    For merging alsa audio components

---
+++ branches/private_MPS_AlsaAudio/Kamaelia/Examples/DVB_Systems/SingleChannelTransportStreamCapture.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+#
+# This code is designed soley for the purposes of demonstrating the tools
+# for timeshifting.
+#
+
+from Kamaelia.Device.DVB.Core import DVB_Multiplex
+from Kamaelia.Chassis.Pipeline import Pipeline
+from Kamaelia.File.Writing import SimpleFileWriter
+
+import dvb3
+
+feparams = {
+    "inversion" : dvb3.frontend.INVERSION_AUTO,
+    "constellation" : dvb3.frontend.QAM_16,
+    "code_rate_HP" : dvb3.frontend.FEC_3_4,
+    "code_rate_LP" : dvb3.frontend.FEC_3_4,
+}
+
+Pipeline(
+   DVB_Multiplex(505.8, [640, 641], feparams), # BBC NEWS CHANNEL
+   SimpleFileWriter("BBC_NEWS_CHANNEL.ts")
+).run()
+
+# RELEASE: MH, MPS
