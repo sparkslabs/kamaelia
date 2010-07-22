ac25d28fe08727e4ce2d8841848d31c2727a7d55
sparks.m <sparks.m@2682807a-6752-0410-967a-793f22d1dfe9>
2010-07-22 00:28:04 +0000

    Switch off rotation

--- Sketches/MPS/VideoConf/Displayer3D.py
+++ Sketches/MPS/VideoConf/Displayer3D.py
@@ -21,16 +21,12 @@ ogl_display = OpenGLDisplay.getDisplayService()
 PygameDisplay.setDisplayService(ogl_display[0])
 from Kamaelia.UI.OpenGL.MatchedTranslationInteractor import MatchedTranslationInteractor
 
-
-
 def player(*argv, **argd):
     screen = VideoSurface()
     screen_in_scene = PygameWrapper(wrap=screen, position=(0, 0,-8), rotation=(-30,15,3)).activate()
 
     i1 = MatchedTranslationInteractor(target=screen_in_scene).activate()
 
-#    rotator = SimpleRotator(amount=(0.0,0.0,0.5)).activate()
-#    rotator.link((rotator,"outbox"), (screen__in_scene,"rel_rotation"))
     return Pipeline(
                DiracDecoder(),
                ToRGB_interleaved(),
@@ -42,5 +38,5 @@ from Play import AlsaPlayer
 def audioplayer(*argv, **argd):
     return AlsaPlayer()
 
-ServerCore(protocol=audioplayer, port=1501).activate()
+#ServerCore(protocol=audioplayer, port=1501).activate()
 ServerCore(protocol=player, port=1500).run()
