c15022e6fee7d1e2414670a0156700f230d6f6ff
sparks.m <sparks.m@2682807a-6752-0410-967a-793f22d1dfe9>
2010-07-24 18:31:46 +0000

    Change license to Apache 2

--- trunk/Code/Python/Apps/GSOC_JMB/App/PersonalWsgiServer/make-unix.sh
+++ trunk/Code/Python/Apps/GSOC_JMB/App/PersonalWsgiServer/make-unix.sh
@@ -12,6 +12,23 @@
 #If clean was specified at the command line, the script will remove the assembly directory
 #once it is finished.  It is recommended that you remove the assembly directory prior to
 #running this script again.
+# Copyright 2010 British Broadcasting Corporation and Kamaelia Contributors(1)
+#
+# (1) Kamaelia Contributors are listed in the AUTHORS file and at
+#     http://www.kamaelia.org/AUTHORS - please extend this file,
+#     not this notice.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 ./prepare.sh #assemble everything we need in the assembly directory
 #python byte-compile.py urls.py main.py ServerConfig.py
