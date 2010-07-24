6ee541abef9adfec6e0243d2d4b6c6eb5c59e040
sparks.m <sparks.m@2682807a-6752-0410-967a-793f22d1dfe9>
2010-07-24 22:28:08 +0000

    Change license to Apache 2

--- Sketches/MPS/MixerProject/mix_client.py
+++ Sketches/MPS/MixerProject/mix_client.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 #
 # Mix Client. Passes received data onto stdout. Allows piping into a streaming server.
 #
@@ -13,6 +14,24 @@
 #  *    8 Bytes -> bits
 #  ie 1411200b/s
 #  ie 1.3Mbit/s
+#
+# Copyright 2010 British Broadcasting Corporation and Kamaelia Contributors(1)
+#
+# (1) Kamaelia Contributors are listed in the AUTHORS file and at
+#     http://www.kamaelia.org/AUTHORS - please extend this file,
+#     not this notice.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 import traceback
