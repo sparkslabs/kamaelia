87222053d65dabeaef82decb32c4f5674cf20625
sparks.m <sparks.m@2682807a-6752-0410-967a-793f22d1dfe9>
2010-07-26 11:49:37 +0000

    To allow development to be ongoing here

---
+++ trunk/Sketches/AB/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+
+from distutils.core import setup
+
+setup(name = "Kamaelia-Whiteboard-Dev",
+      version = "1.0.1",
+      description = "Kamaelia Whiteboard",
+      author = "Matt Hammond, Michael Sparks, Andrew Bonney",
+      author_email = "sparks.m@gmail.com,andrew.bonney@bbc.co.uk",
+      url = "",
+      license = "Apache Software License",
+      packages = [\
+                  "Kamaelia.Apps.Whiteboard", #START #LAST
+                  ""],
+      long_description = """\
+
+""",
+      )
