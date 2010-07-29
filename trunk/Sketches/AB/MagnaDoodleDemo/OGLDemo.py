5cd09e5165efb908e184aae526f21adb90808a6e
andrew.bonney89 <andrew.bonney89@2682807a-6752-0410-967a-793f22d1dfe9>
2010-07-29 10:28:54 +0000

    OpenGL whiteboard with local moveable webcam - Very slow

--- trunk/Sketches/AB/MagnaDoodleDemo/OGLDemo.py
+++ trunk/Sketches/AB/MagnaDoodleDemo/OGLDemo.py
@@ -32,13 +32,24 @@ from Kamaelia.UI.OpenGL.MatchedTranslationInteractor import MatchedTranslationIn
 from Kamaelia.Util.Console import ConsoleReader
 
 from Kamaelia.UI.Pygame.MagnaDoodle import MagnaDoodle
-from Kamaelia.UI.PygameDisplay import PygameDisplay
+from Kamaelia.UI.Pygame.Display import PygameDisplay
 from Kamaelia.UI.Pygame.Ticker import Ticker
 
+from Kamaelia.Apps.Whiteboard.TagFiltering import TagAndFilterWrapper, FilterAndTagWrapper
+from Kamaelia.Apps.Whiteboard.TagFiltering import TagAndFilterWrapperKeepingTag, FilterAndTagWrapperKeepingTag
+from Kamaelia.Apps.Whiteboard.Tokenisation import tokenlists_to_lines, lines_to_tokenlists
 from Kamaelia.Apps.Whiteboard.Canvas import Canvas
 from Kamaelia.Apps.Whiteboard.Painter import Painter
+from Kamaelia.Apps.Whiteboard.SingleShot import OneShot
+from Kamaelia.Apps.Whiteboard.CheckpointSequencer import CheckpointSequencer
+from Kamaelia.Apps.Whiteboard.Entuple import Entuple
+from Kamaelia.Apps.Whiteboard.Routers import Router, TwoWaySplitter, ConditionalSplitter
 from Kamaelia.Apps.Whiteboard.Palette import buildPalette, colours
-from Kamaelia.Apps.Whiteboard.UI import Eraser, ClearPage
+from Kamaelia.Apps.Whiteboard.Options import parseOptions
+from Kamaelia.Apps.Whiteboard.UI import PagingControls, LocalPagingControls, Eraser, ClearPage, SaveDeck, LoadDeck, ClearScribbles
+from Kamaelia.Apps.Whiteboard.CommandConsole import CommandConsole
+from Kamaelia.Apps.Whiteboard.SmartBoard import SmartBoard
+from Kamaelia.Apps.Whiteboard.Webcam import Webcam
 
 #from Webcam import Webcam
 from Webcam import VideoCaptureSource
@@ -59,21 +70,39 @@ if __name__=="__main__":
     ogl_display = OpenGLDisplay.getDisplayService()
     PygameDisplay.setDisplayService(ogl_display[0])
     
-    #PygameDisplay.setDisplayService(ogl_display)
+    if (0):
+        #PygameDisplay.setDisplayService(ogl_display)
+        CANVAS = Canvas( position=(left,top+32),size=(1200,(900-(32+15))),notepad="Test" ).activate() #(replace width with 'width' and height with 'height-(32+15)'
+        PAINTER = Painter().activate()
+        CANVAS_WRAPPER = PygameWrapper(wrap=CANVAS, position=(0,0,-10), rotation=(0,0,0)).activate() 
+        ERASER  = Eraser(left,top).activate()
+        PALETTE = buildPalette( cols=colours, order=colours_order, topleft=(left+64,top), size=32 ).activate()
+        CLEAR = ClearPage(left+(64*5)+32*len(colours),top).activate()
+        #PALETTE_WRAPPER = PygameWrapper(wrap=PALETTE, position=(4,1,-10), rotation=(-20,15,3)).activate()
+    
+        #PAINTER_WRAPPER = PygameWrapper(wrap=PAINTER, position=(4,1,-10), rotation=(-20,15,3)).activate()
+        CANVAS.link( (PAINTER,"outbox"), (CANVAS, "inbox") )
+        PAINTER.link( (CANVAS,"eventsOut"), (PAINTER, "inbox") )
+        PAINTER.link( (PALETTE,"outbox"), (PAINTER, "colour") )
+        PAINTER.link( (ERASER, "outbox"), (PAINTER, "erase") )
+        PAINTER.link( (CLEAR, "outbox"), (CANVAS, "inbox") )
+        
     CANVAS = Canvas( position=(left,top+32),size=(1200,(900-(32+15))),notepad="Test" ).activate() #(replace width with 'width' and height with 'height-(32+15)'
     PAINTER = Painter().activate()
-    CANVAS_WRAPPER = PygameWrapper(wrap=CANVAS, position=(0,0,-10), rotation=(0,0,0)).activate() 
     ERASER  = Eraser(left,top).activate()
     PALETTE = buildPalette( cols=colours, order=colours_order, topleft=(left+64,top), size=32 ).activate()
-    CLEAR = ClearPage(left+(64*5)+32*len(colours),top).activate()
-    #PALETTE_WRAPPER = PygameWrapper(wrap=PALETTE, position=(4,1,-10), rotation=(-20,15,3)).activate()
-    
-    #PAINTER_WRAPPER = PygameWrapper(wrap=PAINTER, position=(4,1,-10), rotation=(-20,15,3)).activate()
-    CANVAS.link( (PAINTER,"outbox"), (CANVAS, "inbox") )
-    PAINTER.link( (CANVAS,"eventsOut"), (PAINTER, "inbox") )
-    PAINTER.link( (PALETTE,"outbox"), (PAINTER, "colour") )
-    PAINTER.link( (ERASER, "outbox"), (PAINTER, "erase") )
-    PAINTER.link( (CLEAR, "outbox"), (CANVAS, "inbox") )
+    CLEAR = ClearPage(left+(64*5)+32*len(colours),top).activate()    
+        
+    MAIN = Graphline(CANVAS = CANVAS,PAINTER = PAINTER,ERASER = ERASER,PALETTE = PALETTE,CLEAR = CLEAR,
+                    CANVAS_WRAPPER = PygameWrapper(wrap=CANVAS, position=(0,0,-10), rotation=(0,0,0)),
+                    linkages = {
+                        ("PAINTER","outbox") : ("CANVAS", "inbox"),
+                        ("CANVAS","eventsOut") : ("PAINTER", "inbox"),
+                        ("PALETTE","outbox") : ("PAINTER", "colour"),
+                        ("ERASER", "outbox") : ("PAINTER", "erase"),
+                        ("CLEAR", "outbox") : ("CANVAS", "inbox"),
+                        },
+                     ).activate()
               
     WEBCAM = VideoCaptureSource().activate()
     BLANKCANVAS = Canvas( position=(left,top+32),size=((63*3+2),140),notepad="Test",bgcolour=(200,200,200) ).activate()
