124ec97ec16ce5c0680bb2750d194e8dbc87f1a1
andrew.bonney89 <andrew.bonney89@2682807a-6752-0410-967a-793f22d1dfe9>
2010-09-15 13:40:02 +0000

    Added iPlayer bookmark jump links

--- trunk/Sketches/AB/Bookmarks/web/bookmarks/output/views.py
+++ trunk/Sketches/AB/Bookmarks/web/bookmarks/output/views.py
@@ -1,7 +1,7 @@
 # Create your views here.
 from django.http import HttpResponse
 from bookmarks.output.models import programmes,analyseddata
-from datetime import date
+from datetime import date,timedelta,datetime
 from dateutil.parser import parse
 
 tvchannels = ["bbcone","bbctwo","bbcthree","bbcfour","cbbc","cbeebies","bbcnews","bbcparliament"]
@@ -59,13 +59,14 @@ def channel(request,channel,year=0,month=0,day=0):
                         }\n \
                     </script>\n"
         
-        output += "<img src=\"/media/channels/" + channel + ".gif\" style=\"border: none\"><br />"
+        output += "<a href=\"http://www.bbc.co.uk/" + channel + "\" target=\"_blank\"><img src=\"/media/channels/" + channel + ".gif\" style=\"border: none\"></a><br />"
         if len(data) < 1:
             output += "<br />Please note: No data has yet been captured for this channel."
         else:
             output += '<br /><br /><div id="inlineDatepicker"></div>'
             if len(str(day)) == 2 and len(str(month)) == 2 and len(str(year)) == 4:
                 datecomp = year + "-" + month + "-" + day
+                output += "<br />Currently viewing shows for " + day + "/" + month + "/" + year + "<br />"
                 data = programmes.objects.filter(channel__exact=channel,expectedstart__contains=datecomp).order_by('expectedstart').all()
                 for programme in data:
                     progdate = parse(programme.expectedstart)
@@ -75,7 +76,7 @@ def channel(request,channel,year=0,month=0,day=0):
                     output += "<br />No data for this date - please select another from the picker above.<br />"
             else:
                 output += "<br />Please select a date from the picker above.<br />"
-        output += "<br /><br /><a href=\"/\">Back to index</a>"
+        output += "<br /><br /><a href=\"/\">Back to index</a> - <a href=\"http://www.bbc.co.uk/" + channel + "\" target=\"_blank\">View BBC channel page</a>"
 
     output += footer
     return HttpResponse(output)
@@ -88,13 +89,37 @@ def programme(request,pid):
         output += "Invalid pid supplied or no data has yet been captured for this programme."
         output += "<br /><br /><a href=\"/\">Back to index</a>"
     elif len(data) == 1:
+        channel = data[0].channel
+        output += "<a href=\"http://www.bbc.co.uk/" + channel + "\" target=\"_blank\"><img src=\"/media/channels/" + channel + ".gif\" style=\"border: none\"></a><br /><br />"
         progdate = parse(data[0].expectedstart)
+        tz = progdate.tzinfo
         progdate = progdate.replace(tzinfo=None)
-        minutedata = analyseddata.objects.filter(pid=pid)
-        output += data[0].title + "<br />"
-        output += str(progdate.strftime("%d/%m/%Y %H:%M")) + "<br />"
-        output += str(data[0].duration)
-        output += "<br /><br /><a href=\"/channels/" + data[0].channel + "/" + str(progdate.strftime("%Y/%m/%d")) + "/\">Back to channel page</a>" #TODO - get data from database
+        actualstart = progdate + timedelta(seconds=data[0].timediff)
+        minutedata = analyseddata.objects.filter(pid=pid).order_by('datetime').all()
+        output += str(progdate.strftime("%d/%m/%Y")) + "<br />"
+        output += "<strong>" + data[0].title + "</strong><br />"
+        output += "Expected show times: " + str(progdate.strftime("%H:%M:%S")) + " to " + str((progdate + timedelta(seconds=data[0].duration)).strftime("%H:%M:%S")) + "<br />"
+        output += "Actual show times (estimated): " + str(actualstart.strftime("%H:%M:%S")) + " to " + str((actualstart + timedelta(seconds=data[0].duration)).strftime("%H:%M:%S")) + "<br />"
+        for minute in minutedata:
+            # This isn't the most elegant BST solution, but it appears to work
+            offset = datetime.strptime(str(tz.utcoffset(parse(minute.datetime))),"%H:%M:%S")
+            offset = timedelta(hours=offset.hour)
+            tweettime = parse(minute.datetime) + offset
+            proghour = tweettime.hour - actualstart.hour
+            progmin = tweettime.minute - actualstart.minute
+            progsec = tweettime.second - actualstart.second
+            playertime = (((proghour * 60) + progmin) * 60) + progsec
+            if playertime > (data[0].duration - 60):
+                playertimemin = (data[0].duration/60) - 1
+                playertimesec = playertime%60
+            elif playertime > 0:
+                playertimemin = playertime/60
+                playertimesec = playertime%60
+            else:
+                playertimemin = 0
+                playertimesec = 0
+            output += "<br />" + str(tweettime.strftime("%H:%M")) + ": <a href=\"http://bbc.co.uk/i/" + pid + "/?t=" + str(playertimemin) + "m" + str(playertimesec) + "s\" target=\"_blank\">" + str(minute.totaltweets) + "</a>"
+        output += "<br /><br /><a href=\"/channels/" + data[0].channel + "/" + str(progdate.strftime("%Y/%m/%d")) + "/\">Back to channel page</a> - <a href=\"http://www.bbc.co.uk/programmes/" + data[0].pid + "\" target=\"_blank\">View BBC /programmes page</a>"
     else:
         output += "Database consistency error - somehow a primary key appears twice..."
         output += "<br /><br /><a href=\"/\">Back to index</a>"
