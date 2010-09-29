67ada8ed791a37fd28959641e8994870174b2574
andrew.bonney89 <andrew.bonney89@2682807a-6752-0410-967a-793f22d1dfe9>
2010-09-29 14:33:16 +0000

    All stats now fixed for LiveAnalysis

--- Sketches/AB/Bookmarks/LiveAnalysis.py
+++ Sketches/AB/Bookmarks/LiveAnalysis.py
@@ -6,7 +6,6 @@
 # Need to ensure one rogue user can't cause a trend - things must be mentioned by several
 
 import MySQLdb
-import _mysql_exceptions
 import cjson
 import os
 import time
@@ -14,19 +13,6 @@ from dateutil.parser import parse
 from datetime import timedelta, datetime
 import math
 import sys
-
-exclusions = ["a","able","about","across","after","all","almost","also","am",\
-            "among","an","and","any","are","as","at","be","because","been","but",\
-            "by","can","cannot","could","dear","did","do","does","either","else",\
-            "ever","every","for","from","get","got","had","has","have","he","her",\
-            "hers","him","his","how","however","i","if","in","into","is","it",\
-            "its","just","least","let","like","likely","may","me","might","most",\
-            "must","my","neither","no","nor","not","of","off","often","on","only",\
-            "or","other","our","own","rather","said","say","says","she","should",\
-            "since","so","some","than","that","the","their","them","then","there",\
-            "these","they","this","tis","to","too","twas","us","wants","was","we",\
-            "were","what","when","where","which","while","who","whom","why","will",\
-            "with","would","yet","you","your"]
 
 
 def dbConnect(dbuser,dbpass):
@@ -126,7 +112,6 @@ if __name__ == "__main__":
             except ZeroDivisionError, e:
                 meantweets = 0
                 
-            #TODO: Median, mode and stdev really aren't right yet
             cursor.execute("""SELECT totaltweets FROM analyseddata WHERE pid = %s""",(pid))
             analyseddata = cursor.fetchall()
 
@@ -192,7 +177,6 @@ if __name__ == "__main__":
 
                 meantweets = float(totaltweets) / (duration / 60) # Mean tweets per minute
 
-                #TODO: Median, mode and stdev really aren't right yet
                 cursor.execute("""SELECT totaltweets FROM analyseddata WHERE pid = %s""",(pid))
                 analyseddata = cursor.fetchall()
 
