05e1bf9c345aa9e9e3cfbbb284eb611ca0aed5bf
andrew.bonney89 <andrew.bonney89@2682807a-6752-0410-967a-793f22d1dfe9>
2010-09-29 11:40:54 +0000

    Live analysis attempt - some suspect results at the moment

--- Sketches/AB/Bookmarks/LiveAnalysis.py
+++ Sketches/AB/Bookmarks/LiveAnalysis.py
@@ -11,8 +11,9 @@ import cjson
 import os
 import time
 from dateutil.parser import parse
-from datetime import timedelta
+from datetime import timedelta, datetime
 import math
+import sys
 
 exclusions = ["a","able","about","across","after","all","almost","also","am",\
             "among","an","and","any","are","as","at","be","because","been","but",\
@@ -58,12 +59,12 @@ if __name__ == "__main__":
 
     while 1:
         # The below does LIVE and FINAL analysis - do NOT run DataAnalyser at the same time
-        # Check if analysed = 0
+        # Bookmarks could be made more accurate by applying the timediff offset to the tweettime here
 
-        print "Checking for unanalysed data..."
+        print "Checking for new data..."
 
         # Stage 1: Live analysis
-        cursor.execute("""SELECT tid,pid,datetime,text,user FROM rawdata WHERE analysed = 0""")
+        cursor.execute("""SELECT tid,pid,datetime,text,user FROM rawdata WHERE analysed = 0 ORDER BY tid""")
         data = cursor.fetchall()
 
         for result in data:
@@ -72,11 +73,157 @@ if __name__ == "__main__":
             tweettime = result[2]
             tweettext = result[3]
             tweetuser = result[4]
+            dbtime = parse(tweettime)
+            dbtime = dbtime.replace(tzinfo=None)
+            dbtime = dbtime.replace(second=0)
+            print "Analysing new tweet for pid", pid, "(" + str(dbtime) + "):"
+            print "'" + tweettext + "'"
+            cursor.execute("""SELECT duration,totaltweets,meantweets,mediantweets,modetweets,stdevtweets,timediff,expectedstart FROM programmes WHERE pid = %s""",(pid))
+            progdata = cursor.fetchone()
+            duration = progdata[0]
+            totaltweets = progdata[1]
+            totaltweets += 1
+            meantweets = progdata[2]
+            mediantweets = progdata[3]
+            modetweets = progdata[4]
+            stdevtweets = progdata[5]
+            timediff = progdata[6]
+            expectedstart = progdata[7]
+            cursor.execute("""SELECT did,totaltweets FROM analyseddata WHERE pid = %s AND datetime = %s""",(pid,dbtime))
+            analyseddata = cursor.fetchone()
+            if analyseddata == None: # No tweets yet recorded for this minute
+                minutetweets = 1
+                cursor.execute("""INSERT INTO analyseddata (pid,datetime,wordfreqexpected,wordfrequnexpected,totaltweets) VALUES (%s,%s,%s,%s,%s)""", (pid,dbtime,0,0,minutetweets))
+            else:
+                did = analyseddata[0]
+                minutetweets = analyseddata[1] # Get current number of tweets for this minute
+                minutetweets += 1 # Add one to it for this tweet
+                cursor.execute("""UPDATE analyseddata SET totaltweets = %s WHERE did = %s""",(minutetweets,did))
 
+            # Averages / stdev are calculated roughly based on the programme's running time at this point
+            progdate = parse(expectedstart)
+            tz = progdate.tzinfo
+            progdate = progdate.replace(tzinfo=None)
+            actualstart = progdate - timedelta(seconds=timediff)
+
+            offset = datetime.strptime(str(tz.utcoffset(parse(tweettime))),"%H:%M:%S")
+            offset = timedelta(hours=offset.hour)
+            actualtweettime = parse(tweettime) + offset
+            actualtweettime = actualtweettime.replace(tzinfo=None)
+
+            # Calculate how far through the programme this tweet occurred
+            runningtime = actualtweettime - actualstart
+            runningtime = runningtime.seconds
+
+            if runningtime < 0:
+                runningtime = 0
+            else:
+                runningtime = float(runningtime) / 60
+                
+            meantweets = totaltweets / runningtime
+
+            #TODO: Median, mode and stdev really aren't right yet
+            cursor.execute("""SELECT totaltweets FROM analyseddata WHERE pid = %s""",(pid))
+            analyseddata = cursor.fetchall()
+
+            runningtime = int(runningtime)
+
+            tweetlist = list()
+            for result in analyseddata:
+                totaltweetsmin = result[0]
+                tweetlist.append(int(totaltweetsmin))
+
+            # Ensure tweetlist has enough entries
+            if len(tweetlist) < runningtime:
+                additions = runningtime - len(tweetlist)
+                while additions > 0:
+                    tweetlist.append(0)
+                    additions -= 1
+
+            tweetlist.sort()
+
+            mediantweets = tweetlist[int(len(tweetlist)/2)]
+
+            modes = dict()
+            stdevlist = list()
+            for tweet in tweetlist:
+                modes[tweet] = tweetlist.count(tweet)
+                stdevlist.append((tweet - meantweets)*(tweet - meantweets))
+
+            modeitems = [[v, k] for k, v in modes.items()]
+            modeitems.sort(reverse=True)
+            modetweets = int(modeitems[0][1])
+
+            stdevtweets = 0
+            for val in stdevlist:
+                stdevtweets += val
+            stdevtweets = math.sqrt(stdevtweets / meantweets)
+
+            # Finished analysis
+            cursor.execute("""UPDATE programmes SET totaltweets = %s, meantweets = %s, mediantweets = %s, modetweets = %s, stdevtweets = %s WHERE pid = %s""",(totaltweets,meantweets,mediantweets,modetweets,stdevtweets,pid))
+            cursor.execute("""UPDATE rawdata SET analysed = 1 WHERE tid = %s""",(tid))
+            print "Done!"
         
-        # Stage 2: If all raw tweets analysed and imported = 1, finalise the analysis
-        #cursor.execute("""SELECT pid,title,duration FROM programmes WHERE programmes.imported = 1 AND programmes.analysed = 0 AND rawdata.pid = programmes.pid AND rawdata.analysed = 0""") # Gah, mindfail hence queryfail
+        # Stage 2: If all raw tweets analysed and imported = 1, finalise the analysis - could do bookmark identification here too?
+        cursor.execute("""SELECT pid,duration,totaltweets,meantweets,mediantweets,modetweets,stdevtweets,title FROM programmes WHERE programmes.imported = 1 AND programmes.analysed = 0""")
+        data = cursor.fetchall()
+        for result in data:
+            pid = data[0]
+            duration = data[1]
+            totaltweets = data[2]
+            meantweets = data[3]
+            mediantweets = data[4]
+            modetweets = data[5]
+            stdevtweets = data[6]
+            title = data[7]
+            # Cycle through checking if all tweets for this programme have been analysed - if so finalise the stats
+            cursor.execute("""SELECT tid FROM rawdata WHERE analysed = 0 AND pid = %s""", (pid))
+            if cursor.fetchone() == None:
+                # OK to finalise stats here
+                print "Finalising stats for pid:", pid, "(" + title + ")"
 
+                meantweets = float(totaltweets) / (duration / 60) # Mean tweets per minute
+
+                #TODO: Median, mode and stdev really aren't right yet
+                cursor.execute("""SELECT totaltweets FROM analyseddata WHERE pid = %s""",(pid))
+                analyseddata = cursor.fetchall()
+
+                runningtime = duration / 60
+
+                tweetlist = list()
+                for result in analyseddata:
+                    totaltweetsmin = result[0]
+                    tweetlist.append(int(totaltweetsmin))
+
+                # Ensure tweetlist has enough entries
+                if len(tweetlist) < runningtime:
+                    additions = runningtime - len(tweetlist)
+                    while additions > 0:
+                        tweetlist.append(0)
+                        additions -= 1
+
+                tweetlist.sort()
+
+                mediantweets = tweetlist[int(len(tweetlist)/2)]
+
+                modes = dict()
+                stdevlist = list()
+                for tweet in tweetlist:
+                    modes[tweet] = tweetlist.count(tweet)
+                    stdevlist.append((tweet - meantweets)*(tweet - meantweets))
+
+                modeitems = [[v, k] for k, v in modes.items()]
+                modeitems.sort(reverse=True)
+                modetweets = int(modeitems[0][1])
+
+                stdevtweets = 0
+                for val in stdevlist:
+                    stdevtweets += val
+                stdevtweets = math.sqrt(stdevtweets / meantweets)
+                
+                cursor.execute("""UPDATE programmes SET meantweets = %s, mediantweets = %s, modetweets = %s, stdevtweets = %s, analysed = 1 WHERE pid = %s""",(meantweets,mediantweets,modetweets,stdevtweets,pid))
+                print "Done!"
+                
         # Sleep here until more data is available to analyse
         print "Sleeping for 1 second..."
         time.sleep(1)
