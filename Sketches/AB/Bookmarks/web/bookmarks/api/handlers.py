c16c7ffa6b6d3a8297891f800f29caa20077fdf0
andrew.bonney89 <andrew.bonney89@2682807a-6752-0410-967a-793f22d1dfe9>
2010-10-11 13:30:46 +0000

    Bookmarks added to API (not yet in DB)

--- Sketches/AB/Bookmarks/web/bookmarks/api/handlers.py
+++ Sketches/AB/Bookmarks/web/bookmarks/api/handlers.py
@@ -1,5 +1,7 @@
 from piston.handler import BaseHandler
-from bookmarks.output.models import programmes, keywords
+from bookmarks.output.models import programmes, keywords, analyseddata
+from datetime import timedelta,datetime
+from dateutil.parser import parse
 
 class ProgrammesHandler(BaseHandler):
     allowed_methods = ('GET',)
@@ -28,6 +30,57 @@ class ProgrammesHandler(BaseHandler):
             retdata['keywords'] = list()
             for row in kwdata:
                 retdata['keywords'].append({'keyword' : row.keyword, 'type' : row.type})
+            retdata['bookmarks'] = list()
+
+            progdate = parse(data[0].expectedstart)
+            tz = progdate.tzinfo
+            progdate = progdate.replace(tzinfo=None)
+            actualstart = progdate - timedelta(seconds=data[0].timediff)
+            minutedata = analyseddata.objects.filter(pid=pid).order_by('datetime').all()
+            tweetmins = dict()
+            lastwasbookmark = False
+            bookmarks = list()
+            bookmarkcont = list()
+            for minute in minutedata:
+                # This isn't the most elegant BST solution, but it appears to work
+                offset = datetime.strptime(str(tz.utcoffset(parse(minute.datetime))),"%H:%M:%S")
+                offset = timedelta(hours=offset.hour)
+                tweettime = parse(minute.datetime) + offset
+                proghour = tweettime.hour - actualstart.hour
+                progmin = tweettime.minute - actualstart.minute
+                progsec = tweettime.second - actualstart.second
+                playertime = (((proghour * 60) + progmin) * 60) + progsec - 90 # needs between 60 and 120 secs removing to allow for tweeting time - using 90 for now
+                if playertime > (data[0].duration - 60):
+                    playertimemin = (data[0].duration/60) - 1
+                    playertimesec = playertime%60
+                elif playertime > 0:
+                    playertimemin = playertime/60
+                    playertimesec = playertime%60
+                else:
+                    playertimemin = 0
+                    playertimesec = 0
+                if minute.totaltweets > (1.5*data[0].stdevtweets+data[0].meantweets):
+                    if lastwasbookmark == True:
+                        bookmarkcont.append(playertimemin)
+                    else:
+                        if minute.totaltweets > (2.2*data[0].stdevtweets+data[0].meantweets):
+                            lastwasbookmark = True
+                            bookmarks.append(playertimemin)
+                        else:
+                            lastwasbookmark = False
+                else:
+                    lastwasbookmark = False
+                if not tweetmins.has_key(str(playertimemin)):
+                    tweetmins[str(playertimemin)] = int(minute.totaltweets)
+            if len(tweetmins) > 0:
+                xlist = range(0,data[0].duration/60)
+                for min in xlist:
+                    if min in bookmarks and max(tweetmins.values()) > 9: # Arbitrary value chosen for now - needs experimentation - was 9
+                        retdata['bookmarks'].append({'iplayer' : "http://bbc.co.uk/i/" + pid + "/?t=" + str(min) + "m" + str(playertimesec) + "s", 'startseconds' : min*60+playertimesec, 'endseconds' : min*60+playertimesec+60})
+                        lastindex = len(retdata['bookmarks']) - 1
+                    elif min in bookmarkcont and max(tweetmins.values()) > 9: # Arbitrary value chosen for now - needs experimentation - was 9
+                        retdata['bookmarks'][lastindex]['endseconds'] = min*60+playertimesec+60
+
         else:
             retdata['status'] = "ERROR"
         return retdata
