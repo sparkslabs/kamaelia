d88334a6bd18943a71b054d6c07c1af67756fca8
sparks.m <sparks.m@2682807a-6752-0410-967a-793f22d1dfe9>
2010-11-03 14:31:23 +0000

    Restructure for maintainability

--- branches/private_MPS_Macro/Apps/Kamaelia-Macro/App/Macro.py
+++ branches/private_MPS_Macro/Apps/Kamaelia-Macro/App/Macro.py
@@ -23,19 +23,20 @@
 # limitations under the License.
 #
 
+import time, os
+import struct
+import dvb3
+
+import Axon
+from Axon.Ipc import shutdownMicroprocess, producerFinished
 from Kamaelia.Device.DVB.Core import DVB_Demuxer,DVB_Multiplex
 from Kamaelia.Chassis.Graphline import Graphline
 from Kamaelia.File.Writing import SimpleFileWriter
 from Kamaelia.File.UnixProcess import UnixProcess
-import Axon
-import struct
-import dvb3
 
-from Axon.Ipc import shutdownMicroprocess, producerFinished
 from Kamaelia.Device.DVB.EIT import PSIPacketReconstructor, EITPacketParser, NowNextServiceFilter, NowNextChanges, TimeAndDatePacketParser
 from Kamaelia.Chassis.Carousel import Carousel
 from Kamaelia.Chassis.Pipeline import Pipeline
-import time, os
 
 class EITDemux(Axon.Component.component):
     Outboxes = {"outbox":"",
@@ -168,66 +169,69 @@ def ChannelTranscoder(service_id, mencoder_options, dir_prefix): # BBC ONE
         }
     )
 
-location = "manchester"
 
-if location == "london": # Crystal Palace
-    freq = 505.833330 # 529.833330   # 505.833330
-    feparams = {
-        "inversion" : dvb3.frontend.INVERSION_AUTO,
-        "constellation" : dvb3.frontend.QAM_16,
-        "code_rate_HP" : dvb3.frontend.FEC_3_4,
-        "code_rate_LP" : dvb3.frontend.FEC_3_4,
-    }
-elif location == "manchester": # WinterHill
-    freq = 754.166670
-    feparams = {
-        "inversion" : dvb3.frontend.INVERSION_AUTO,
-        "constellation" : dvb3.frontend.QAM_16,
-        "code_rate_HP" : dvb3.frontend.FEC_3_4,
-        "code_rate_LP" : dvb3.frontend.FEC_3_4,
-    }
+if __name__ == "__main__":
+    location = "manchester"
 
-from Kamaelia.File.ReadFileAdaptor import ReadFileAdaptor
-from Kamaelia.File.Reading import RateControlledFileReader
-from Kamaelia.Util.Console import ConsoleEchoer
+    from Kamaelia.File.ReadFileAdaptor import ReadFileAdaptor
+    from Kamaelia.File.Reading import RateControlledFileReader
+    from Kamaelia.Util.Console import ConsoleEchoer
 
-params={}
-params["LO"] = {
-    "mencoder_options" : " -ovc lavc -oac mp3lame -ffourcc DX50 -lavcopts acodec=mp3:vbitrate=200:abitrate=128 -vf scale=320:-2 -",
-    "dir_prefix" : "200",
-    }
-params["HI"] = {
-    "mencoder_options" : " -ovc lavc -oac mp3lame -ffourcc DX50 -lavcopts acodec=mp3:vbitrate=512:abitrate=128 -vf scale=640:-2 -",
-    "dir_prefix" : "512"
-    }
+    if location == "london": # Crystal Palace
+        freq = 505.833330 # 529.833330   # 505.833330
+        feparams = {
+            "inversion" : dvb3.frontend.INVERSION_AUTO,
+            "constellation" : dvb3.frontend.QAM_16,
+            "code_rate_HP" : dvb3.frontend.FEC_3_4,
+            "code_rate_LP" : dvb3.frontend.FEC_3_4,
+        }
+    elif location == "manchester": # WinterHill
+        freq = 754.166670
+        feparams = {
+            "inversion" : dvb3.frontend.INVERSION_AUTO,
+            "constellation" : dvb3.frontend.QAM_16,
+            "code_rate_HP" : dvb3.frontend.FEC_3_4,
+            "code_rate_LP" : dvb3.frontend.FEC_3_4,
+        }
 
-pids = { "BBC ONE" : [600,601],
-         "BBC TWO" : [610,611],
-         "CBEEBIES": [201,401],
-         "CBBC"    : [620,621],
-         "EIT"     : [18],
-       }
 
-service_ids = { "BBC ONE": 4164,
-                "BBC TWO": 4228,
-                "CBEEBIES":16960,
-                "CBBC":4671,
-              }
+    params={}
+    params["LO"] = {
+        "mencoder_options" : " -ovc lavc -oac mp3lame -ffourcc DX50 -lavcopts acodec=mp3:vbitrate=200:abitrate=128 -vf scale=320:-2 -",
+        "dir_prefix" : "200",
+        }
+    params["HI"] = {
+        "mencoder_options" : " -ovc lavc -oac mp3lame -ffourcc DX50 -lavcopts acodec=mp3:vbitrate=512:abitrate=128 -vf scale=640:-2 -",
+        "dir_prefix" : "512"
+        }
 
-print "-----STARTING MACRO----- time =",time.time()
+    pids = { "BBC ONE" : [600,601],
+             "BBC TWO" : [610,611],
+             "CBEEBIES": [201,401],
+             "CBBC"    : [620,621],
+             "EIT"     : [18],
+           }
 
-Graphline(
-    SOURCE=DVB_Multiplex(freq, pids["BBC TWO"]+pids["EIT"], feparams), # BBC Channels + EIT data
-    DEMUX=DVB_Demuxer({
-        610: ["BBCTWO"],
-        611: ["BBCTWO"],
-        18: ["BBCTWO"],   # BBCONE","BBCONE_2","BBCTWO","BBCTWO_2", "CBEEBIES"
-    }),
-    BBCTWO_HI = ChannelTranscoder(service_ids["BBC TWO"], **params["HI"]),
-    linkages={
-       ("SOURCE", "outbox"):("DEMUX","inbox"),
-       ("DEMUX", "BBCTWO"): ("BBCTWO_HI", "inbox"),
-    }
-).run()
+    service_ids = { "BBC ONE": 4164,
+                    "BBC TWO": 4228,
+                    "CBEEBIES":16960,
+                    "CBBC":4671,
+                  }
 
-# RELEASE: MH, MPS
+    print "-----STARTING MACRO----- time =",time.time()
+
+    Graphline(
+        SOURCE=DVB_Multiplex(freq, pids["BBC TWO"]+pids["EIT"], feparams), # BBC Channels + EIT data
+        DEMUX=DVB_Demuxer({
+            610: ["BBCTWO"],
+            611: ["BBCTWO"],
+            18: ["BBCTWO"],   # BBCONE","BBCONE_2","BBCTWO","BBCTWO_2", "CBEEBIES"
+        }),
+        BBCTWO_HI = ChannelTranscoder(service_ids["BBC TWO"], **params["HI"]),
+        linkages={
+           ("SOURCE", "outbox"):("DEMUX","inbox"),
+           ("DEMUX", "BBCTWO"): ("BBCTWO_HI", "inbox"),
+        }
+    ).run()
+
+    # RELEASE: MH, MPS
