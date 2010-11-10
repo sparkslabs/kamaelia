ebeb4d4a9c8e813c1d6809ac70fbefda2fee6483
andrew.bonney89 <andrew.bonney89@2682807a-6752-0410-967a-793f22d1dfe9>
2010-11-10 12:05:12 +0000

    Updated Django models to latest DB revision

--- trunk/Sketches/AB/Bookmarks/web/bookmarks/output/models.py
+++ trunk/Sketches/AB/Bookmarks/web/bookmarks/output/models.py
@@ -8,7 +8,6 @@ class programmes(models.Model):
     pid = models.CharField(max_length=10,primary_key=True,db_column="pid")
     channel = models.CharField(max_length=20,db_column="channel")
     title = models.CharField(max_length=200,db_column="title")
-    #expectedstart = models.CharField(max_length=100,db_column="expectedstart")
     timestamp = models.FloatField(db_column="timestamp",default=0)
     utcoffset = models.IntegerField(db_column="utcoffset",default=0)
     timediff = models.IntegerField(db_column="timediff",default=0)
@@ -27,10 +26,9 @@ class programmes(models.Model):
 class analyseddata(models.Model):
     did = models.IntegerField(primary_key=True,db_column="did")
     pid = models.ForeignKey(programmes,db_column="pid")
-    #datetime = models.CharField(max_length=100,db_column="datetime")
     timestamp = models.FloatField(db_column="timestamp",default=0)
-    wordfreqexpected = models.CharField(max_length=500,db_column="wordfreqexpected")
-    wordfrequnexpected = models.CharField(max_length=500,db_column="wordfrequnexpected")
+    wordfreqexpected = models.CharField(max_length=2000,db_column="wordfreqexpected")
+    wordfrequnexpected = models.CharField(max_length=2000,db_column="wordfrequnexpected")
     totaltweets = models.IntegerField(db_column="totaltweets",default=0)
 
     class Meta:
@@ -49,7 +47,6 @@ class rawdata(models.Model):
     tweet_id = models.FloatField(db_column="tweet_id")
     tid = models.IntegerField(primary_key=True,db_column="tid")
     pid = models.ForeignKey(programmes,db_column="pid")
-    #datetime = models.CharField(max_length=100,db_column="datetime")
     timestamp = models.FloatField(db_column="timestamp",default=0)
     text = models.CharField(max_length=200,db_column="text")
     user = models.CharField(max_length=200,db_column="user")
@@ -58,3 +55,22 @@ class rawdata(models.Model):
 
     class Meta:
         db_table = 'rawdata'
+
+class rawtweets(models.Model):
+    tweet_id = models.FloatField(primary_key=True,db_column="tweet_id")
+    tweet_json = models.CharField(max_length=16000,db_column="tweet_json")
+    tweet_stored_seconds = models.IntegerField(db_column="tweet_stored_seconds")
+    tweet_stored_fraction = models.FloatField(db_column="tweet_stored_fraction")
+
+    class Meta:
+        db_table = 'rawtweets'
+
+class wordanalysis(models.Model):
+    wid = models.IntegerField(primary_key=True,db_column="wid")
+    pid = models.ForeignKey(programmes,db_column="pid")
+    timestamp = models.FloatField(db_column="timestamp")
+    word = models.CharField(max_length=30,db_column="word")
+    word_count = models.IntegerField(db_column="word_count")
+
+    class Meta:
+        db_table = 'wordanalysis'
