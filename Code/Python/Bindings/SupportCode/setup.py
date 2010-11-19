116d4d20a54a69b3f00b36b53f9c6ef3117e0051
sparks.m <sparks.m@2682807a-6752-0410-967a-793f22d1dfe9>
2010-11-19 17:57:56 +0000

    Changed to use Cython instead - and necessary bugfixes to enable that

--- Code/Python/Bindings/SupportCode/setup.py
+++ Code/Python/Bindings/SupportCode/setup.py
@@ -20,7 +20,8 @@
 # -------------------------------------------------------------------------
 from distutils.core import setup
 from distutils.extension import Extension
-from Pyrex.Distutils import build_ext
+# from Pyrex.Distutils import build_ext
+from Cython.Distutils import build_ext
 
 setup(
   name = 'KamaeliaSupport',
