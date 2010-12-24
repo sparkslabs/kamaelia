77bc7409ba3ff0090183bb5f66778ecf4d0285c5
sparks.m <sparks.m@2682807a-6752-0410-967a-793f22d1dfe9>
2010-12-24 13:39:45 +0000

    Now merged onto /trunk :-)

--- branches/private_MPS_CL_Topology3D/Kamaelia/Examples/Introspection/IntrospectionTutorial/.backup/EchoServer.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-import Axon
-from Kamaelia.Protocol.EchoProtocol import EchoProtocol
-from Kamaelia.Chassis.ConnectedServer import FastRestartServer
-
-FastRestartServer(protocol=EchoProtocol, port=1500).run()
-
