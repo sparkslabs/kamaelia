13695b05ef925ecc9160ba5a9ce0d30ace29a51a
sparks.m <sparks.m@2682807a-6752-0410-967a-793f22d1dfe9>
2010-12-24 09:59:01 +0000

    Changes on this branch merged onto a new branch - private_MPS_CL_Topology3D - in more appropriate locations, making this branch now moot, hence rejecting in favour of newer branch (of same code)

---
+++ private_CL_Topology3D/tagAxonRelease.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -x
+
+
+
+(
+   cd Axon-RELEASE/
+
+   # Extract the version number from setup.py, replace periods with
+   # underscores and create a branch point release tag. (Means that
+   # if necessary we can instantiate a branch easily, but don't have to)
+   RELEASETAG=bp_`egrep "^ *version *=" setup.py |cut -d\" -f2|sed -e "s/\./_/g"`
+
+   cvs tag $RELEASETAG
+
+)
