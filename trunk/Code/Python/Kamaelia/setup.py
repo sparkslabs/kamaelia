5a02ed9efe4e348f7111987d01d47979fa207e26
sparks.m <sparks.m@2682807a-6752-0410-967a-793f22d1dfe9>
2011-01-13 18:27:37 +0000

    In preparation for next month's release

--- trunk/Code/Python/Kamaelia/setup.py
+++ trunk/Code/Python/Kamaelia/setup.py
@@ -22,7 +22,7 @@
 from distutils.core import setup
 
 setup(name = "Kamaelia",
-      version = "1.0.12.0", # Year.Year.Month.ReleaseCountThisMonth
+      version = "1.1.2.0", # Year.Year.Month.ReleaseCountThisMonth
       description = "Kamaelia - Multimedia & Server Development Kit",
 
       author = "Kamaelia Contributors.",
