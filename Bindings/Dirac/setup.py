e0375e8da85c9dd7a69238478f927ccea2387ea9
sparks.m <sparks.m@2682807a-6752-0410-967a-793f22d1dfe9>
2011-02-27 13:57:02 +0000

    moved files into more appropriate locations

--- Bindings/Dirac/setup.py
+++ Bindings/Dirac/setup.py
@@ -18,35 +18,43 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # -------------------------------------------------------------------------
+
+#
+# Please note that whilst these bindings are available under the Apache 2
+# license, any usage or build of these bindings must inherit from the dirac
+# bindings, and you'll be bound by your choice of the MPL/GPL/LGPL.  Since
+# the LGPL boils down to something similar to the apache 2 license, this is
+# unlikely to be an issue for you.
+#
+# In particular these bindings are able to be Apache 2 licensed because they
+# dynamically link to the LGPL'd dirac code, and as a result don't modify
+# the dirac code or include the dirac code directly in any way.
+#
+
+
 from distutils.core import setup
 from distutils.extension import Extension
-# from Pyrex.Distutils import build_ext
 from Cython.Distutils import build_ext
 
 setup(
-  name = 'Dirac',
-  version = "0.0.3",
-  description = "Dirac 1.0.2 bindings for python",
-  author = "Michael",
-  author_email = "ms_@users.sourceforge.net",
-  url = "http://kamaelia.sourceforge.net/",
-  ext_modules=[ 
-    Extension("dirac_parser",
-              ["dirac_parser.pyx"],
-              libraries = ["dirac_decoder"],
-              include_dirs = ["/usr/include/dirac"],
-             ),
-    Extension("dirac_encoder",
-              ["dirac_encoder.pyx"],
-              libraries = ["dirac_encoder"],
-              include_dirs = ["/usr/include/dirac"],
-             ),
+    name = 'Dirac',
+    version = "1.0.2-0", # Bump revision to match dirac version that work for. 4th digit is local version number
+    description = "Dirac 1.0.2 bindings for python",
+    author = "Michael Sparks, Matt Hammond",
+    author_email = "ms_@users.sourceforge.net",
+    url = "http://www.kamaelia.org/Home.html",
+    py_modules=[
+                "dirac.__init__"
     ],
-  cmdclass = {'build_ext': build_ext},
-  long_description = """Initial set of python bindings for Dirac 1.0.2 release. 
+    ext_modules=[ 
+                  Extension("dirac.dirac_parser",  ["dirac/dirac_parser.pyx"],  libraries = ["dirac_decoder"], include_dirs = ["/usr/include/dirac"] ),
+                  Extension("dirac.dirac_encoder", ["dirac/dirac_encoder.pyx"], libraries = ["dirac_encoder"], include_dirs = ["/usr/include/dirac"] ),
+      ],
+    cmdclass = {'build_ext': build_ext},
+    long_description = """Initial set of python bindings for Dirac 1.0.2 release. 
 This API is subject to change. Requires Cython, Dirac, and Dirac
 headers are expected to live in /usr/include/dirac
-For information on dirac, see http://dirac.sf.net/
+For information on dirac, see http://www.diracvideo.org/
 
 This particular version is created with regard to Dirac and Cython as included
 in Ubuntu 10.10
