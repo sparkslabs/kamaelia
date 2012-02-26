0488b45a4bc1449595f96b580009e93d4cd5b8dd
sparks.m <sparks.m@2682807a-6752-0410-967a-793f22d1dfe9>
2012-02-26 22:13:54 +0000

    sync

--- Code/interim-build/build-interim-release.sh
+++ Code/interim-build/build-interim-release.sh
@@ -10,6 +10,3 @@ echo "Adding interim/monthly release readme"
 sed "s/VERSION/$VERSION/" interim-tarball-readme > Kamaelia-$VERSION/README
 tar zcvf Kamaelia-$VERSION.tar.gz Kamaelia-$VERSION/
 
-echo "Uploading $VERSION"
-
-# scp Kamaelia-$VERSION.tar.gz michaels@132.185.142.2:/srv/www/sites/www.kamaelia.org/docs/release/MonthlyReleases
