a111d5aa312e56a96504216c2b7924233915ff86
Michael Sparks <sparks.m@gmail.com>
2024-03-24 19:43:27 +0000

    Update checked for ManualAxonVisualiser example

--- Code/Python/Kamaelia/Examples/Introspection/ManualAxonVisualiser.py
+++ Code/Python/Kamaelia/Examples/Introspection/ManualAxonVisualiser.py
@@ -19,6 +19,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Checked 2024/03/24
+
 import Axon
 from Kamaelia.Visualisation.Axon.AxonVisualiserServer import AxonVisualiser
 from Kamaelia.Util.Console import ConsoleReader, ConsoleEchoer
