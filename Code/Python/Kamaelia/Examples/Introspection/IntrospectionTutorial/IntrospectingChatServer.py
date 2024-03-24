a8a774b260564cd2ce252fe01d173139639bf8c6
Michael Sparks <sparks.m@gmail.com>
2024-03-24 16:43:26 +0000

    Re-enable IntrospectingChatServer example

--- Code/Python/Kamaelia/Examples/Introspection/IntrospectionTutorial/IntrospectingChatServer.py
+++ Code/Python/Kamaelia/Examples/Introspection/IntrospectionTutorial/IntrospectingChatServer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# Checked: 2024/03/24
+
 import Axon
 from Kamaelia.Chassis.ConnectedServer import FastRestartServer
 from Kamaelia.Chassis.Pipeline import Pipeline
@@ -13,6 +15,7 @@ from Kamaelia.Internet.TCPClient import TCPClient
 
 Pipeline(
     Introspector(),
+    PureTransformer(lambda x: x.encode("utf8")),
     TCPClient("127.0.0.1", 1600),
 ).activate()
 # ------- END OF CODE FRAGMENT NEEDED TO CONNECT TO INTROSPECTOR ----
@@ -23,10 +26,12 @@ def EchoEveryone(**kwargs):
         peer = str(kwargs.get("peer", "<>"))
         peerport = str(kwargs.get("peerport", "<>"))
         return Pipeline(
+                PureTransformer(lambda x: x.decode("utf8")),
                 PureTransformer(lambda x: "%s:%s says %s" % (peer,peerport,x)),
                 PublishTo("CHAT_ONE"),
                 # ------------
                 SubscribeTo("CHAT_ONE"),
+                PureTransformer(lambda x: x.encode("utf8")),
             )
 
 FastRestartServer(protocol=EchoEveryone, port=1500).run()
