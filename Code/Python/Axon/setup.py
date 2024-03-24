98a1b3fcb887ef78a3f64d09a9861ae9cab24596
Michael Sparks <sparks.m@gmail.com>
2024-03-24 23:08:11 +0000

    Bump versions for 1.14.32 releases of Kamaelia and Axon

--- Code/Python/Axon/setup.py
+++ Code/Python/Axon/setup.py
@@ -48,7 +48,7 @@ if len(sys.argv) >= 2:
             pass
 
 setup(name = "kamaelia-axon",
-      version = "1.13.31",
+      version = "1.14.32",
       description = "Axon - Asynchronous Isolated Generator Component System",
       author = "Michael Sparks (sparkslabs)",
       author_email = "sparks.m@gmail.com",
