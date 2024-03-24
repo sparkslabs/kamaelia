e75b7538670ca70fc3254882befcaf636f779e07
Michael Sparks <sparks.m@gmail.com>
2024-03-24 17:05:00 +0000

    Updated ./IntrospectingEchoServer.py example

--- Code/Python/Kamaelia/Examples/Introspection/IntrospectionTutorial/IntrospectingEchoServer.py
+++ Code/Python/Kamaelia/Examples/Introspection/IntrospectionTutorial/IntrospectingEchoServer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+# Checked: 2024/03/24
+
 from Kamaelia.Protocol.EchoProtocol import EchoProtocol
 from Kamaelia.Chassis.ConnectedServer import FastRestartServer
 
@@ -7,13 +10,14 @@ from Kamaelia.Chassis.ConnectedServer import FastRestartServer
 from Kamaelia.Chassis.Pipeline import Pipeline
 from Kamaelia.Util.Introspector import Introspector
 from Kamaelia.Internet.TCPClient import TCPClient
+from Kamaelia.Util.PureTransformer import PureTransformer
 
 Pipeline(
     Introspector(),
+    PureTransformer(lambda x: x.encode("utf8")),
     TCPClient("127.0.0.1", 1600),
 ).activate()
 # ------- END OF CODE FRAGMENT NEEDED TO CONNECT TO INTROSPECTOR ----
 
 
 FastRestartServer(protocol=EchoProtocol, port=1500).run()
-
