7de159d30c9d0d8697ae7457f0ad6e6488a7c76d
Michael Sparks <sparks.m@gmail.com>
2024-03-24 22:16:19 +0000

    Minor tweak to the https example - needs more fixes

--- Code/Python/Kamaelia/Examples/TCP_Systems/SSL/https.py
+++ Code/Python/Kamaelia/Examples/TCP_Systems/SSL/https.py
@@ -24,7 +24,7 @@ from Kamaelia.Internet.TCPClient import TCPClient
 from Kamaelia.Util.Console import ConsoleEchoer, ConsoleReader
 from Kamaelia.Util.OneShot import OneShot
 
-print """
+print( """
 This is a simple demonstration program that shows that it is possible to
 build simple clients for manually connecting to SSL based sources - such
 as HTTPS sources.
@@ -34,17 +34,17 @@ This program connects the the subversion server for Kamaelia on port
 connected you are connected through an encrypted connection, which means
 you could type the following and get code back from the server:
 
-GET /svnroot/kamaelia/trunk/Code/Python/Kamaelia/Examples/SimpleGraphicalApps/Ticker/Ulysses HTTP/1.0
-Host: kamaelia.svn.sourceforge.net
+GET /Home.html HTTP/1.0
+Host: kamaelia.org
 
 That's pretty much the purpose of this example program.
-"""
+""")
 
 Graphline(
     MAKESSL = OneShot(" make ssl "), # The actual message here is not necessary
     CONSOLE = ConsoleReader(),
     ECHO = ConsoleEchoer(),
-    CONNECTION = TCPClient("kamaelia.svn.sourceforge.net", 443),
+    CONNECTION = TCPClient("kamaelia.org", 443),
     linkages = {
         ("MAKESSL", "outbox"): ("CONNECTION", "makessl"),
         ("CONSOLE", "outbox"): ("CONNECTION", "inbox"),
